346c317de75afe63ada702e6a281a64aa0c3e3e8
Luc Esape <luc.esape@gmail.com>
2017-05-08 09:08:37 +0200

    Bug commit from the following repository hypercube1024/firefly This bug commit is a reflect of source code from: http://github.com/hypercube1024/firefly/commit/7924a2520f5920d372f7d133c31f3735731898d5.

---
+++ firefly/src/test/java/test/http/DirectHTTP2ClientDemo.java
@@ -0,0 +1,19 @@
+package test.http;
+
+import com.firefly.$;
+
+/**
+ * @author Pengtao Qiu
+ */
+public class DirectHTTP2ClientDemo {
+    public static void main(String[] args) {
+        $.cleartextHTTP2Client()
+         .post("http://localhost:2242/cleartextHttp2")
+         .body("post data")
+         .submit().thenAccept(res -> {
+            System.out.println(res);
+            System.out.println(res.getFields());
+            System.out.println(res.getStringBody());
+        });
+    }
+}
---
+++ firefly/src/test/java/test/http/DirectHTTP2ServerDemo.java
@@ -0,0 +1,16 @@
+package test.http;
+
+import com.firefly.$;
+
+/**
+ * @author Pengtao Qiu
+ */
+public class DirectHTTP2ServerDemo {
+    public static void main(String[] args) {
+        $.cleartextHTTP2Server().router().post("/cleartextHttp2").handler(ctx -> {
+            System.out.println(ctx.getFields());
+            System.out.println(ctx.getStringBody());
+            ctx.end("test cleartext http2");
+        }).listen("localhost", 2242);
+    }
+}
