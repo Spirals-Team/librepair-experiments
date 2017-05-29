4f3de99da2033bdb93317feeb6d20b7e7770fa43
Luc Esape <luc.esape@gmail.com>
2017-05-29 20:43:21 +0200

    Bug commit from the following repository CorfuDB/CorfuDB This bug commit is a reflect of source code from: http://github.com/CorfuDB/CorfuDB/commit/2feb751a4156801b43a70a017155018425daeb5f.

---
+++ runtime/src/main/java/org/corfudb/runtime/exceptions/AbortCause.java
@@ -0,0 +1,16 @@
+package org.corfudb.runtime.exceptions;
+
+import lombok.RequiredArgsConstructor;
+
+/**
+ * Created by dalia on 4/8/17.
+ */
+@RequiredArgsConstructor
+public enum AbortCause {
+    CONFLICT,
+    NEW_SEQUENCER,
+    USER,
+    NETWORK,
+    TRIM, /** Aborted because an access to this snapshot resulted in a trim exception. */
+    UNDEFINED;
+}
