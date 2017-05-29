c96422f890ebcd1ac3a0e950712d43efc69ccc0a
Luc Esape <luc.esape@gmail.com>
2017-05-30 00:31:53 +0200

    Bug commit from the following repository blynkkk/blynk-server This bug commit is a reflect of source code from: http://github.com/blynkkk/blynk-server/commit/67f1f249819644f326151b754949fe85b702b93c.

---
+++ server/tcp-app-server/src/main/java/cc/blynk/server/application/handlers/main/logic/UpdateFaceLogic.java
@@ -0,0 +1,90 @@
+package cc.blynk.server.application.handlers.main.logic;
+
+import cc.blynk.server.Holder;
+import cc.blynk.server.core.BlockingIOProcessor;
+import cc.blynk.server.core.dao.UserDao;
+import cc.blynk.server.core.model.DashBoard;
+import cc.blynk.server.core.model.auth.App;
+import cc.blynk.server.core.model.auth.User;
+import cc.blynk.server.core.protocol.model.messages.StringMessage;
+import cc.blynk.server.db.DBManager;
+import cc.blynk.server.notifications.mail.MailWrapper;
+import cc.blynk.utils.ArrayUtil;
+import cc.blynk.utils.ParseUtil;
+import io.netty.channel.ChannelHandlerContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.HashSet;
+
+import static cc.blynk.utils.BlynkByteBufUtil.notAllowed;
+import static cc.blynk.utils.BlynkByteBufUtil.ok;
+
+/**
+ * Update faces of related project.
+ *
+ * The Blynk Project.
+ * Created by Dmitriy Dumanskiy.
+ * Created on 2/1/2015.
+ *
+ */
+public class UpdateFaceLogic {
+
+    private static final Logger log = LogManager.getLogger(UpdateFaceLogic.class);
+
+    private final BlockingIOProcessor blockingIOProcessor;
+    private final MailWrapper mailWrapper;
+    private final DBManager dbManager;
+    private final UserDao userDao;
+
+    public UpdateFaceLogic(Holder holder) {
+        this.userDao = holder.userDao;
+        this.blockingIOProcessor = holder.blockingIOProcessor;
+        this.mailWrapper =  holder.mailWrapper;
+        this.dbManager = holder.dbManager;
+    }
+
+    public void messageReceived(ChannelHandlerContext ctx, User user, StringMessage message) {
+        int dashId = ParseUtil.parseInt(message.body);
+
+        DashBoard dash = user.profile.getDashByIdOrThrow(dashId);
+
+        HashSet<String> appIds = new HashSet<>();
+        for (App app : user.profile.apps) {
+            if (ArrayUtil.contains(app.projectIds, dashId)) {
+                appIds.add(app.id);
+            }
+        }
+
+        if (appIds.size() == 0) {
+            log.debug("Passed dash is not assigned to any app.");
+            ctx.writeAndFlush(notAllowed(message.id));
+            return;
+        }
+
+        boolean hasFaces = false;
+        for (User existingUser : userDao.users.values()) {
+            for (DashBoard existingDash : existingUser.profile.dashBoards) {
+                if (existingDash.parentId == dashId && (existingUser == user || appIds.contains(existingUser.appName))) {
+                    hasFaces = true;
+                    //we found child project-face
+                    try {
+                        existingDash.updateFaceFields(dash);
+                    } catch (Exception e) {
+                        log.error("Error updating face for user {}, dashId {}.", existingUser.email, existingDash.id, e);
+                        ctx.writeAndFlush(notAllowed(message.id));
+                        return;
+                    }
+                }
+            }
+        }
+
+        if (hasFaces) {
+            ctx.writeAndFlush(ok(message.id));
+        } else {
+            log.info("No child faces found for update.");
+            ctx.writeAndFlush(notAllowed(message.id));
+        }
+    }
+
+}
