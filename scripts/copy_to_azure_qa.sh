cb84b7495e36914d958427fe0a382ec2dc096d80
Luc Esape <luc.esape@gmail.com>
2017-06-22 00:37:33 +0200

    Bug commit from the following repository blynkkk/blynk-server This bug commit is a reflect of source code from: http://github.com/blynkkk/blynk-server/commit/68ef7a5df5eb65d1d91432e241033d06e0b646f9.

---
+++ scripts/copy_to_azure_qa.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+scp ../server/launcher/target/server-0.25.1-SNAPSHOT.jar azureuser@blynk-qa.cloudapp.net:/home/azureuser
---
+++ server/core/src/main/java/cc/blynk/server/core/model/widgets/Target.java
@@ -0,0 +1,18 @@
+package cc.blynk.server.core.model.widgets;
+
+/**
+ * The Blynk Project.
+ * Created by Dmitriy Dumanskiy.
+ * Created on 05.01.17.
+ */
+public interface Target {
+
+    int[] getDeviceIds();
+
+    int getDeviceId();
+
+    default boolean isTag() {
+        return false;
+    }
+
+}
