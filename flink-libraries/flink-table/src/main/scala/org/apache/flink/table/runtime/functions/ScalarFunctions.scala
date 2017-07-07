60ced3dfe591fca60f5af250bf2d0438ce39661f
Luc Esape <luc.esape@gmail.com>
2017-07-07 13:01:12 +0200

    Bug commit from the following repository apache/flink This bug commit is a reflect of source code from: http://github.com/apache/flink/commit/8bbba6813e7ade6cac68f8598334b6a1b2dafc8b.

---
+++ flink-libraries/flink-table/src/main/scala/org/apache/flink/table/runtime/functions/ScalarFunctions.scala
@@ -0,0 +1,183 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.flink.table.runtime.functions
+
+import scala.annotation.varargs
+import java.math.{BigDecimal => JBigDecimal}
+import java.lang.StringBuilder
+
+/**
+  * Built-in scalar runtime functions.
+  */
+class ScalarFunctions {}
+
+object ScalarFunctions {
+
+  def power(a: Double, b: JBigDecimal): Double = {
+    Math.pow(a, b.doubleValue())
+  }
+
+  /**
+    * Returns the string that results from concatenating the arguments.
+    * Returns NULL if any argument is NULL.
+    */
+  @varargs
+  def concat(args: String*): String = {
+    val sb = new StringBuilder
+    var i = 0
+    while (i < args.length) {
+      if (args(i) == null) {
+        return null
+      }
+      sb.append(args(i))
+      i += 1
+    }
+    sb.toString
+  }
+
+  /**
+    * Returns the string that results from concatenating the arguments and separator.
+    * Returns NULL If the separator is NULL.
+    *
+    * Note: CONCAT_WS() does not skip empty strings. However, it does skip any NULL values after
+    * the separator argument.
+    *
+    **/
+  @varargs
+  def concat_ws(separator: String, args: String*): String = {
+    if (null == separator) {
+      return null
+    }
+
+    val sb = new StringBuilder
+
+    var i = 0
+
+    var hasValueAppended = false
+
+    while (i < args.length) {
+      if (null != args(i)) {
+        if (hasValueAppended) {
+          sb.append(separator)
+        }
+        sb.append(args(i))
+        hasValueAppended = true
+      }
+      i = i + 1
+    }
+    sb.toString
+  }
+
+  /**
+    * Returns the string str, left-padded with the string pad to a length of len characters.
+    * If str is longer than len, the return value is shortened to len characters.
+    */
+  def lpad(base: String, len: Integer, pad: String): String = {
+    if (base == null || len == null || pad == null) {
+      return null
+    }
+    var data = "".getBytes
+    if (data.length < len) {
+      data = new Array[Byte](len)
+    }
+    val baseBytes = base.getBytes
+    val padBytes = pad.getBytes
+
+    // The length of the padding needed
+    val pos = Math.max(len - base.length, 0)
+
+    // Copy the padding
+    var i = 0
+
+    while (i < pos) {
+      {
+        var j = 0
+        while (j < pad.length && j < pos - i) {
+          {
+            data(i + j) = padBytes(j)
+          }
+          {
+            j += 1;
+            j - 1
+          }
+        }
+      }
+      i += pad.length
+    }
+
+    // Copy the base
+    i = 0
+    while (pos + i < len && i < base.length) {
+      {
+        data(pos + i) = baseBytes(i)
+      }
+      {
+        i += 1;
+        i - 1
+      }
+    }
+
+    new String(data)
+  }
+
+  /**
+    * Returns the string str, right-padded with the string pad to a length of len characters.
+    * If str is longer than len, the return value is shortened to len characters.
+    */
+  def rpad(base: String, len: Integer, pad: String): String = {
+    if (base == null || len == null || pad == null) {
+      return null
+    }
+    var data = "".getBytes
+    if (data.length < len) {
+      data = new Array[Byte](len)
+    }
+    val baseBytes = base.getBytes
+    val padBytes = pad.getBytes
+
+    var pos = 0
+
+    // Copy the base
+    while (pos < base.length && pos < len) {
+      {
+        data(pos) = baseBytes(pos)
+      }
+      {
+        pos += 1;
+        pos - 1
+      }
+    }
+
+    // Copy the padding
+    while (pos < len) {
+      var i = 0
+      while (i < pad.length && i < len - pos) {
+        {
+          data(pos + i) = padBytes(i)
+        }
+        {
+          i += 1;
+          i - 1
+        }
+      }
+      pos += pad.length
+    }
+
+    new String(data)
+  }
+}
