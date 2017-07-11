f1d11533bf4ab076cf293875d2206979e01f78d5
Luc Esape <luc.esape@gmail.com>
2017-07-11 13:04:42 +0200

    Bug commit from the following repository apache/flink This bug commit is a reflect of source code from: http://github.com/apache/flink/commit/165ad6a2e50e98a8f17bc9bd628545aa54d72fef.

---
+++ flink-core/src/main/java/org/apache/flink/api/common/PlanExecutor.java
@@ -0,0 +1,59 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.flink.api.common;
+
+import org.apache.flink.annotation.Internal;
+
+/**
+ * A PlanExecutor executes a Flink program's dataflow plan. All Flink programs are translated to
+ * dataflow plans prior to execution.
+ * 
+ * <p>The specific implementation (such as the org.apache.flink.client.LocalExecutor
+ * and org.apache.flink.client.RemoteExecutor) is created by {@link ExecutorFactory}</p>
+ * 
+ */
+@Internal
+public interface PlanExecutor extends Executor {
+
+	/**
+	 * Execute the given program.
+	 * 
+	 * <p>If the executor has not been started before, then this method will start the
+	 * executor and stop it after the execution has completed. This implies that one needs
+	 * to explicitly start the executor for all programs where multiple dataflow parts
+	 * depend on each other. Otherwise, the previous parts will no longer
+	 * be available, because the executor immediately shut down after the execution.</p>
+	 * 
+	 * @param plan The plan of the program to execute.
+	 * @return The execution result, containing for example the net runtime of the program, and the accumulators.
+	 * 
+	 * @throws Exception Thrown, if job submission caused an exception.
+	 */
+	JobExecutionResult executePlan(Plan plan) throws Exception;
+	
+	/**
+	 * Gets the programs execution plan in a JSON format.
+	 * 
+	 * @param plan The program to get the execution plan for.
+	 * @return The execution plan, as a JSON string.
+	 * 
+	 * @throws Exception Thrown, if the executor could not connect to the compiler.
+	 */
+	String getOptimizerPlanAsJSON(Plan plan) throws Exception;
+}
