80504f2043240799fb7c14a8d5bf810b17f4fcf6
Luc Esape <luc.esape@gmail.com>
2017-07-17 23:59:46 +0200

    Bug commit from the following repository dropwizard/metrics This bug commit is a reflect of source code from: http://github.com/dropwizard/metrics/commit/7e64ce7f963e2c5f10a7346014c2e3b8f5162d87.

---
+++ metrics-httpclient/src/test/java/com/codahale/metrics/httpclient/InstrumentedHttpClientsTest.java
@@ -0,0 +1,84 @@
+package com.codahale.metrics.httpclient;
+
+import com.codahale.metrics.MetricRegistry;
+import com.codahale.metrics.MetricRegistryListener;
+import com.codahale.metrics.Timer;
+import org.apache.http.HttpRequest;
+import org.apache.http.NoHttpResponseException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.fail;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.*;
+
+public class InstrumentedHttpClientsTest {
+    private final HttpClientMetricNameStrategy metricNameStrategy =
+            mock(HttpClientMetricNameStrategy.class);
+    private final MetricRegistryListener registryListener =
+            mock(MetricRegistryListener.class);
+    private final MetricRegistry metricRegistry = new MetricRegistry();
+    private final HttpClient client =
+            InstrumentedHttpClients.custom(metricRegistry, metricNameStrategy).disableAutomaticRetries().build();
+
+    @Before
+    public void setUp() throws Exception {
+        metricRegistry.addListener(registryListener);
+    }
+
+    @Test
+    public void registersExpectedMetricsGivenNameStrategy() throws Exception {
+        final HttpGet get = new HttpGet("http://example.com?q=anything");
+        final String metricName = "some.made.up.metric.name";
+
+        when(metricNameStrategy.getNameFor(any(), any(HttpRequest.class)))
+                .thenReturn(metricName);
+
+        client.execute(get);
+
+        verify(registryListener).onTimerAdded(eq(metricName), any(Timer.class));
+    }
+
+    @Test
+    public void registersExpectedExceptionMetrics() throws Exception {
+        ServerSocket server = new ServerSocket();
+        server.bind(new InetSocketAddress("localhost", 0));
+        final HttpGet get = new HttpGet("http://localhost:" + server.getLocalPort() + "/");
+        final String requestMetricName = "request";
+        final String exceptionMetricName = "exception";
+
+        Thread serverThread = new Thread(() -> {
+            try {
+                final Socket socket = server.accept();
+                socket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+        serverThread.start();
+
+        when(metricNameStrategy.getNameFor(any(), any(HttpRequest.class)))
+                .thenReturn(requestMetricName);
+        when(metricNameStrategy.getNameFor(any(), any(Exception.class)))
+                .thenReturn(exceptionMetricName);
+
+        try {
+            client.execute(get);
+            fail();
+        } catch (NoHttpResponseException expected) {
+            assertThat(metricRegistry.getMeters()).containsKey("exception");
+        } finally {
+            serverThread.interrupt();
+            serverThread.join();
+        }
+    }
+}
