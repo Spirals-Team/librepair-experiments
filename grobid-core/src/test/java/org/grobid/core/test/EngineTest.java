d1f13a62fa334f498101e5ca53325c3c50541f28
Luc Esape <luc.esape@gmail.com>
2017-07-31 13:19:30 +0200

    Bug commit from the following repository kermitt2/grobid This bug commit is a reflect of source code from: http://github.com/kermitt2/grobid/commit/dbc03321fa5e38a236c3359801db678f9886aa4f.

---
+++ grobid-core/src/test/java/org/grobid/core/test/EngineTest.java
@@ -0,0 +1,22 @@
+package org.grobid.core.test;
+
+import org.grobid.core.engines.Engine;
+import org.grobid.core.factory.GrobidFactory;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+
+/**
+ * User: zholudev Date: 11/21/11 Time: 7:17 PM
+ */
+public abstract class EngineTest {
+    protected static Engine engine;
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+        engine = GrobidFactory.getInstance().createEngine();
+    }
+
+    @AfterClass
+    public static void destroyInitialContext() throws Exception {
+    }
+}
