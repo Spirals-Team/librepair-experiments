95f589eaaeb6bc2bc4e4fc43d505e07c0b9efbe7
Luc Esape <luc.esape@gmail.com>
2017-09-08 16:37:27 +0200

    Bug commit from the following repository chocoteam/choco-solver This bug commit is a reflect of source code from: http://github.com/chocoteam/choco-solver/commit/34db419b71185ed916b64acad1861648c9557c1d.

---
+++ .travis/install-ibex.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -ex
+wget http://www.ibex-lib.org/sites/default/files/ibex-2.3.4.tgz
+wget http://www.ibex-lib.org/sites/default/files/ibex-java-ibex2.3.tar.gz
+tar -xzvf ibex-2.3.4.tgz ibex-2.3.4/
+mv ibex-java-ibex2.3.tar.gz ibex-2.3.4/plugins/ibex-java-ibex2.3.tar.gz
+cd ibex-2.3.4/plugins
+tar -xzvf ibex-java-ibex2.3.tar.gz
+cd ..
+./waf configure --enable-shared --with-jni --with-java-package=org.chocosolver.solver.constraints.real
+sudo ./waf install
+# to avoid error with LICENSE header update
+mv __build__ ../ibex
+cd ..
+sudo rm -r ibex-2.3.4/
+sudo find ./ibex -name "*.java" -exec rm {} \;
