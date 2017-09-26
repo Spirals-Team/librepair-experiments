88886984ab5cb93151ff7093536796a09bf4baf7
Luc Esape <luc.esape@gmail.com>
2017-09-26 23:12:30 +0200

    Bug commit from the following repository INRIA/spoon This bug commit is a reflect of source code from: http://github.com/INRIA/spoon/commit/7d12a0c1148a4b3bee41bb67ed2a2d767f4687be.

---
+++ src/main/java/spoon/reflect/visitor/PrinterHelper.java
@@ -0,0 +1,252 @@
+/**
+ * Copyright (C) 2006-2017 INRIA and contributors
+ * Spoon - http://spoon.gforge.inria.fr/
+ *
+ * This software is governed by the CeCILL-C License under French law and
+ * abiding by the rules of distribution of free software. You can use, modify
+ * and/or redistribute the software under the terms of the CeCILL-C license as
+ * circulated by CEA, CNRS and INRIA at http://www.cecill.info.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the CeCILL-C License for more details.
+ *
+ * The fact that you are presently reading this means that you have had
+ * knowledge of the CeCILL-C license and that you accept its terms.
+ */
+package spoon.reflect.visitor;
+
+import spoon.compiler.Environment;
+import spoon.reflect.cu.CompilationUnit;
+import spoon.reflect.cu.position.NoSourcePosition;
+import spoon.reflect.declaration.CtElement;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Supports configurable printing of text with indentations and line and column counting
+ */
+public class PrinterHelper {
+	/**
+	 * Line separator which is used by the printer helper.
+	 * By default the system line separator is used
+	 */
+	private String lineSeparator = System.getProperty("line.separator");
+
+	/**
+	 * Environment which Spoon is executed.
+	 */
+	private Environment env;
+
+	/**
+	 * The string buffer in which the code is generated.
+	 */
+	private final StringBuffer sbf = new StringBuffer();
+
+	/**
+	 * Number of tabs when we print the source code.
+	 */
+	private int nbTabs = 0;
+
+	/**
+	 * Current line number.
+	 */
+	private int line = 1;
+
+	/**
+	 * Current column number
+	 * Not used yet, but this shows the advantage of encapsulating all sbf.append in calls to {@link #write(char)} or {@link #write(String)}.
+	 * This will be used for sniper mode later
+	 */
+	private int column = 1;
+
+	/**
+	 * Mapping for line numbers.
+	 */
+	private Map<Integer, Integer> lineNumberMapping = new HashMap<>();
+
+	public PrinterHelper(Environment env) {
+		this.env = env;
+	}
+
+	/**
+	 * resets to the initial state
+	 */
+	public void reset() {
+		sbf.setLength(0);
+		nbTabs = 0;
+		line = 1;
+		column = 1;
+		//create new map, because clients keeps reference to it
+		lineNumberMapping = new HashMap<>();
+	}
+
+	/**
+	 * Outputs a string.
+	 */
+	public PrinterHelper write(String s) {
+		if (s != null) {
+			sbf.append(s);
+			column += s.length();
+		}
+		return this;
+	}
+
+	/**
+	 * Outputs a char.
+	 */
+	public PrinterHelper write(char c) {
+		sbf.append(c);
+		column += 1;
+		return this;
+	}
+
+	/**
+	 * Generates a new line.
+	 */
+	public PrinterHelper writeln() {
+		write(lineSeparator);
+		line++;
+		// reset the column index
+		column = 1;
+		return this;
+	}
+
+	public PrinterHelper writeTabs() {
+		for (int i = 0; i < nbTabs; i++) {
+			if (env.isUsingTabulations()) {
+				write('\t');
+			} else {
+				for (int j = 0; j < env.getTabulationSize(); j++) {
+					write(' ');
+				}
+			}
+		}
+		return this;
+	}
+
+	/**
+	 * Increments the current number of tabs.
+	 */
+	public PrinterHelper incTab() {
+		nbTabs++;
+		return this;
+	}
+
+	/**
+	 * Decrements the current number of tabs.
+	 */
+	public PrinterHelper decTab() {
+		nbTabs--;
+		return this;
+	}
+
+	/**
+	 * @return the current number of tabs.
+	 */
+	public int getTabCount() {
+		return nbTabs;
+	}
+
+	/**
+	 * Sets the current number of tabs.
+	 */
+	public PrinterHelper setTabCount(int tabCount) {
+		nbTabs = tabCount;
+		return this;
+	}
+
+	boolean removeLine() {
+		String ls = lineSeparator;
+		int i = sbf.length() - ls.length();
+		boolean hasWhite = false;
+		while (i > 0 && !ls.equals(sbf.substring(i, i + ls.length()))) {
+			if (!isWhite(sbf.charAt(i))) {
+				return false;
+			}
+			hasWhite = true;
+			i--;
+		}
+		if (i <= 0) {
+			return false;
+		}
+		hasWhite = hasWhite || isWhite(sbf.charAt(i - 1));
+		sbf.replace(i, i + ls.length(), hasWhite ? "" : " ");
+		line--;
+		return true;
+	}
+
+	private boolean isWhite(char c) {
+		return (c == ' ') || (c == '\t') || (c == '\n') || (c == '\r');
+	}
+
+	void adjustStartPosition(CtElement e) {
+		if (e.getPosition() != null && !e.isImplicit() && !(e.getPosition() instanceof NoSourcePosition)) {
+			// we should add some lines
+			while (line < e.getPosition().getLine()) {
+				writeln();
+			}
+			// trying to remove some lines
+			while (line > e.getPosition().getLine()) {
+				if (!removeLine()) {
+					break;
+				}
+			}
+		}
+	}
+
+	void adjustEndPosition(CtElement e) {
+		if (env.isPreserveLineNumbers() && e.getPosition() != null) {
+			// let's add lines if required
+			while (line < e.getPosition().getEndLine()) {
+				writeln();
+			}
+		}
+	}
+
+	void undefineLine() {
+		if (lineNumberMapping.get(line) == null) {
+			putLineNumberMapping(0);
+		}
+	}
+
+	void mapLine(CtElement e, CompilationUnit unitExpected) {
+		if ((e.getPosition() != null) && (e.getPosition().getCompilationUnit() == unitExpected)) {
+			// only map elements coming from the source CU
+			putLineNumberMapping(e.getPosition().getLine());
+		} else {
+			undefineLine();
+		}
+	}
+
+	void putLineNumberMapping(int valueLine) {
+		lineNumberMapping.put(this.line, valueLine);
+	}
+
+	Map<Integer, Integer> getLineNumberMapping() {
+		return Collections.unmodifiableMap(lineNumberMapping);
+	}
+
+	@Override
+	public String toString() {
+		return sbf.toString();
+	}
+
+	/**
+	 * @return current line separator. By default there is CR LF, LF or CR depending on the Operation system
+	 * defined by System.getProperty("line.separator")
+	 */
+	public String getLineSeparator() {
+		return lineSeparator;
+	}
+
+	/**
+	 * @param lineSeparator characters which will be printed as End of line.
+	 * By default there is System.getProperty("line.separator")
+	 */
+	public void setLineSeparator(String lineSeparator) {
+		this.lineSeparator = lineSeparator;
+	}
+}
---
+++ src/main/java/spoon/reflect/visitor/PrinterTokenWriterDelegate.java
@@ -0,0 +1,129 @@
+/**
+ * Copyright (C) 2006-2017 INRIA and contributors
+ * Spoon - http://spoon.gforge.inria.fr/
+ *
+ * This software is governed by the CeCILL-C License under French law and
+ * abiding by the rules of distribution of free software. You can use, modify
+ * and/or redistribute the software under the terms of the CeCILL-C license as
+ * circulated by CEA, CNRS and INRIA at http://www.cecill.info.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the CeCILL-C License for more details.
+ *
+ * The fact that you are presently reading this means that you have had
+ * knowledge of the CeCILL-C license and that you accept its terms.
+ */
+package spoon.reflect.visitor;
+
+import spoon.SpoonException;
+import spoon.reflect.code.CtComment;
+
+/**
+ * {@link PrinterTokenWriter} implementation, which simply sends all tokens to next {@link PrinterTokenWriter}
+ * Inherit your own implementation from this class to manipulates print tokens - e.g. to change formatting of printed output
+ */
+class PrinterTokenWriterDelegate implements PrinterTokenWriter {
+
+	private PrinterTokenWriter next;
+
+	PrinterTokenWriterDelegate() {
+	}
+
+	PrinterTokenWriterDelegate(PrinterTokenWriter next) {
+		setNext(next);
+	}
+
+	@Override
+	public PrinterHelper getPrinterHelper() {
+		return next.getPrinterHelper();
+	}
+
+	public PrinterTokenWriter getNext() {
+		return next;
+	}
+
+	public void setNext(PrinterTokenWriter next) {
+		if (next == null) {
+			throw new SpoonException("Next PrinterTokenWriter must not be null");
+		}
+		this.next = next;
+	}
+
+	@Override
+	public void reset() {
+		next.reset();
+	}
+
+	@Override
+	public PrinterTokenWriterDelegate writeWhitespace(String token) {
+		next.writeWhitespace(token);
+		return this;
+	}
+
+	@Override
+	public PrinterTokenWriterDelegate writeSeparator(String token) {
+		next.writeSeparator(token);
+		return this;
+	}
+
+	@Override
+	public PrinterTokenWriterDelegate writeOperator(String token) {
+		next.writeOperator(token);
+		return this;
+	}
+
+	@Override
+	public PrinterTokenWriterDelegate writeLiteral(String token) {
+		next.writeLiteral(token);
+		return this;
+	}
+
+	@Override
+	public PrinterTokenWriterDelegate writeKeyword(String token) {
+		next.writeKeyword(token);
+		return this;
+	}
+
+	@Override
+	public PrinterTokenWriterDelegate writeIdentifier(String token) {
+		next.writeIdentifier(token);
+		return this;
+	}
+
+	@Override
+	public PrinterTokenWriterDelegate writeCodeSnippet(String token) {
+		next.writeCodeSnippet(token);
+		return this;
+	}
+
+	@Override
+	public PrinterTokenWriterDelegate writeComment(CtComment comment) {
+		next.writeComment(comment);
+		return this;
+	}
+
+	@Override
+	public PrinterTokenWriterDelegate writeln() {
+		next.writeln();
+		return this;
+	}
+
+	@Override
+	public PrinterTokenWriterDelegate writeTabs() {
+		next.writeTabs();
+		return this;
+	}
+
+	@Override
+	public PrinterTokenWriterDelegate incTab() {
+		next.incTab();
+		return this;
+	}
+
+	@Override
+	public PrinterTokenWriterDelegate decTab() {
+		next.decTab();
+		return this;
+	}
+}
