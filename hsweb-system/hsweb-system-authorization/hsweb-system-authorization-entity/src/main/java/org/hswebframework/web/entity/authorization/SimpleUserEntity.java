d1a1bee1e6995f57743c09ab011095adf6f74836
Luc Esape <luc.esape@gmail.com>
2017-10-21 04:22:06 +0200

    Bug commit from the following repository hs-web/hsweb-framework This bug commit is a reflect of source code from: http://github.com/hs-web/hsweb-framework/commit/9d914098fffa4fe2b292135841f4c4a16065b135.

---
+++ hsweb-system/hsweb-system-authorization/hsweb-system-authorization-entity/src/main/java/org/hswebframework/web/entity/authorization/SimpleUserEntity.java
@@ -0,0 +1,99 @@
+package org.hswebframework.web.entity.authorization;
+
+import org.hswebframework.web.commons.entity.SimpleGenericEntity;
+
+/**
+ * TODO 完成注释
+ *
+ * @author zhouhao
+ */
+public class SimpleUserEntity extends SimpleGenericEntity<String> implements UserEntity {
+    private String name;
+
+    private String username;
+
+    private String password;
+
+    private String salt;
+
+    private Long createTime;
+
+    private String creatorId;
+
+    private Byte status;
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String getSalt() {
+        return salt;
+    }
+
+    @Override
+    public void setSalt(String salt) {
+        this.salt = salt;
+    }
+
+    @Override
+    public Long getCreateTime() {
+        return createTime;
+    }
+
+    @Override
+    public void setCreateTime(Long createTime) {
+        this.createTime = createTime;
+    }
+
+    @Override
+    public String getCreatorId() {
+        return creatorId;
+    }
+
+    @Override
+    public void setCreatorId(String creatorId) {
+        this.creatorId = creatorId;
+    }
+
+    @Override
+    public Byte getStatus() {
+        return status;
+    }
+
+    @Override
+    public void setStatus(Byte status) {
+        this.status = status;
+    }
+
+    @Override
+    public SimpleUserEntity clone() {
+        return ((SimpleUserEntity) super.clone());
+    }
+}
