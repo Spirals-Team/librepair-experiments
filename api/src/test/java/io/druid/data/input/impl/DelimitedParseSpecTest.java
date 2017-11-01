8331ced3337be8eab75719c7e679e37ff872d1e0
Luc Esape <luc.esape@gmail.com>
2017-11-01 07:49:41 +0100

    Bug commit from the following repository druid-io/druid This bug commit is a reflect of source code from: http://github.com/druid-io/druid/commit/5b84c3883ec41886a64e8fb438c7b32cd62e912f.

---
+++ api/src/test/java/io/druid/data/input/impl/DelimitedParseSpecTest.java
@@ -0,0 +1,217 @@
+/*
+ * Licensed to Metamarkets Group Inc. (Metamarkets) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. Metamarkets licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package io.druid.data.input.impl;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Lists;
+import io.druid.TestObjectMapper;
+import io.druid.java.util.common.parsers.DelimitedParser;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+public class DelimitedParseSpecTest
+{
+  private final ObjectMapper jsonMapper = new TestObjectMapper();
+
+  @Test
+  public void testSerde() throws IOException
+  {
+    DelimitedParseSpec spec = new DelimitedParseSpec(
+        new TimestampSpec("abc", "iso", null),
+        new DimensionsSpec(DimensionsSpec.getDefaultSchemas(Collections.singletonList("abc")), null, null),
+        "\u0001",
+        "\u0002",
+        null,
+        Collections.singletonList("abc"),
+        false,
+        0
+    );
+    final DelimitedParseSpec serde = jsonMapper.readValue(
+        jsonMapper.writeValueAsString(spec),
+        DelimitedParseSpec.class
+    );
+    Assert.assertEquals("abc", serde.getTimestampSpec().getTimestampColumn());
+    Assert.assertEquals("iso", serde.getTimestampSpec().getTimestampFormat());
+
+    Assert.assertEquals(Collections.singletonList("abc"), serde.getColumns());
+    Assert.assertEquals("\u0001", serde.getDelimiter());
+    Assert.assertEquals("\u0002", serde.getListDelimiter());
+    Assert.assertEquals(Collections.singletonList("abc"), serde.getDimensionsSpec().getDimensionNames());
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testColumnMissing() throws Exception
+  {
+    final ParseSpec spec = new DelimitedParseSpec(
+        new TimestampSpec(
+            "timestamp",
+            "auto",
+            null
+        ),
+        new DimensionsSpec(
+            DimensionsSpec.getDefaultSchemas(Arrays.asList("a", "b")),
+            Lists.<String>newArrayList(),
+            Lists.<SpatialDimensionSchema>newArrayList()
+        ),
+        ",",
+        " ",
+        null,
+        Collections.singletonList("a"),
+        false,
+        0
+    );
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testComma() throws Exception
+  {
+    final ParseSpec spec = new DelimitedParseSpec(
+        new TimestampSpec(
+            "timestamp",
+            "auto",
+            null
+        ),
+        new DimensionsSpec(
+            DimensionsSpec.getDefaultSchemas(Arrays.asList("a,", "b")),
+            Lists.<String>newArrayList(),
+            Lists.<SpatialDimensionSchema>newArrayList()
+        ),
+        ",",
+        null,
+        null,
+        Collections.singletonList("a"),
+        false,
+        0
+    );
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testDefaultColumnList()
+  {
+    final DelimitedParseSpec spec = new DelimitedParseSpec(
+        new TimestampSpec(
+            "timestamp",
+            "auto",
+            null
+        ),
+        new DimensionsSpec(
+            DimensionsSpec.getDefaultSchemas(Arrays.asList("a", "b")),
+            Lists.<String>newArrayList(),
+            Lists.<SpatialDimensionSchema>newArrayList()
+        ),
+        ",",
+        null,
+        null,
+        null,
+        false,
+        0
+    );
+  }
+
+  @Test
+  public void testDefaultListDelimiter()
+  {
+    final DelimitedParseSpec spec = new DelimitedParseSpec(
+            new TimestampSpec(
+                    "timestamp",
+                    "auto",
+                    null
+            ),
+            new DimensionsSpec(
+                    DimensionsSpec.getDefaultSchemas(Arrays.asList("col2")),
+                    Lists.<String>newArrayList(),
+                    Lists.<SpatialDimensionSchema>newArrayList()
+            ),
+            ",",
+            null,
+            null,
+            Arrays.asList("col1", "col2", "col3")
+    );
+
+    DelimitedParser parser = (DelimitedParser) spec.makeParser();
+    Map<String, Object> map = parser.parse("1,2,x\u0001y\u0001z");
+    Assert.assertEquals(ImmutableMap.of("col1", "1", "col2", "2", "col3", Arrays.asList("x", "y", "z")), map);
+  }
+
+  @Test
+  public void testDisableListDelimiter()
+  {
+    final DelimitedParseSpec spec = new DelimitedParseSpec(
+            new TimestampSpec(
+                    "timestamp",
+                    "auto",
+                    null
+            ),
+            new DimensionsSpec(
+                    DimensionsSpec.getDefaultSchemas(Arrays.asList("col2")),
+                    Lists.<String>newArrayList(),
+                    Lists.<SpatialDimensionSchema>newArrayList()
+            ),
+            ",",
+            null,
+            new HashMap<>(),
+            Arrays.asList("col1", "col2", "col3")
+    );
+
+    DelimitedParser parser = (DelimitedParser) spec.makeParser();
+    Map<String, Object> map = parser.parse("1,2,x\u0001y\u0001z");
+    Assert.assertEquals(ImmutableMap.of("col1", "1", "col2", "2", "col3", "x\u0001y\u0001z"), map);
+  }
+
+  @Test
+  public void testMultiValueDelimiter()
+  {
+    Map<String, String> multiValueDelimiter = new HashMap<>();
+    multiValueDelimiter.put("col3", "\u0002");
+    multiValueDelimiter.put("col4", "\u0003");
+
+    final DelimitedParseSpec spec = new DelimitedParseSpec(
+            new TimestampSpec(
+                    "timestamp",
+                    "auto",
+                    null
+            ),
+            new DimensionsSpec(
+                    DimensionsSpec.getDefaultSchemas(Arrays.asList("col2")),
+                    Lists.<String>newArrayList(),
+                    Lists.<SpatialDimensionSchema>newArrayList()
+            ),
+            ",",
+            null,
+            multiValueDelimiter,
+            Arrays.asList("col1", "col2", "col3", "col4", "col5")
+    );
+
+    DelimitedParser parser = (DelimitedParser) spec.makeParser();
+    Map<String, Object> map = parser.parse("1,2,x\u0002y\u0002z,a\u0003b\u0003c,e\u0002f\u0002g");
+    Assert.assertEquals(ImmutableMap.of("col1", "1", "col2", "2",
+            "col3", Arrays.asList("x", "y", "z"),
+            "col4", Arrays.asList("a", "b", "c"),
+            "col5", "e\u0002f\u0002g"),
+            map);
+  }
+}
---
+++ java-util/src/main/java/io/druid/java/util/common/parsers/AbstractFlatTextFormatParser.java
@@ -0,0 +1,189 @@
+/*
+ * Licensed to Metamarkets Group Inc. (Metamarkets) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. Metamarkets licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package io.druid.java.util.common.parsers;
+
+import com.google.common.base.Splitter;
+import com.google.common.base.Strings;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+import io.druid.java.util.common.collect.Utils;
+
+import javax.annotation.Nullable;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
+public abstract class AbstractFlatTextFormatParser implements Parser<String, Object>
+{
+  public enum FlatTextFormat
+  {
+    CSV(","),
+    DELIMITED("\t");
+
+    private final String defaultDelimiter;
+
+    FlatTextFormat(String defaultDelimiter)
+    {
+      this.defaultDelimiter = defaultDelimiter;
+    }
+
+    public String getDefaultDelimiter()
+    {
+      return defaultDelimiter;
+    }
+  }
+
+  private final String listDelimiter;
+  private final Map<String, String> multiValueDelimiter;
+  private final Maps.EntryTransformer<String, String, Object> multiValueTransformer;
+  private final boolean hasHeaderRow;
+  private final int maxSkipHeaderRows;
+
+  private List<String> fieldNames = null;
+  private boolean hasParsedHeader = false;
+  private int skippedHeaderRows;
+  private boolean supportSkipHeaderRows;
+
+  public AbstractFlatTextFormatParser(
+      @Nullable final String listDelimiter,
+      @Nullable final Map<String, String> multiValueDelimiter,
+      final boolean hasHeaderRow,
+      final int maxSkipHeaderRows
+  )
+  {
+    this.listDelimiter = listDelimiter != null ? listDelimiter : Parsers.DEFAULT_LIST_DELIMITER;
+    this.multiValueDelimiter = multiValueDelimiter;
+    this.multiValueTransformer = new Maps.EntryTransformer<String, String, Object>()
+    {
+      @Override
+      public Object transformEntry(String key, String value)
+      {
+        String deli = null;
+        if (AbstractFlatTextFormatParser.this.multiValueDelimiter == null) {
+          deli = AbstractFlatTextFormatParser.this.listDelimiter;
+        } else if (AbstractFlatTextFormatParser.this.multiValueDelimiter.get(key) != null) {
+          deli = AbstractFlatTextFormatParser.this.multiValueDelimiter.get(key);
+        }
+
+        if (deli != null && value.contains(deli)) {
+          Splitter multiSplitter = Splitter.on(deli);
+          return StreamSupport.stream(multiSplitter.split(value).spliterator(), false)
+                  .map(Strings::emptyToNull)
+                  .collect(Collectors.toList());
+        } else {
+          return Strings.emptyToNull(value);
+        }
+      }
+    };
+
+    this.hasHeaderRow = hasHeaderRow;
+    this.maxSkipHeaderRows = maxSkipHeaderRows;
+  }
+
+  @Override
+  public void startFileFromBeginning()
+  {
+    if (hasHeaderRow) {
+      fieldNames = null;
+    }
+    hasParsedHeader = false;
+    skippedHeaderRows = 0;
+    supportSkipHeaderRows = true;
+  }
+
+  public String getListDelimiter()
+  {
+    return listDelimiter;
+  }
+
+  @Override
+  public List<String> getFieldNames()
+  {
+    return fieldNames;
+  }
+
+  @Override
+  public void setFieldNames(final Iterable<String> fieldNames)
+  {
+    if (fieldNames != null) {
+      final List<String> fieldsList = Lists.newArrayList(fieldNames);
+      this.fieldNames = new ArrayList<>(fieldsList.size());
+      for (int i = 0; i < fieldsList.size(); i++) {
+        if (Strings.isNullOrEmpty(fieldsList.get(i))) {
+          this.fieldNames.add(ParserUtils.getDefaultColumnName(i));
+        } else {
+          this.fieldNames.add(fieldsList.get(i));
+        }
+      }
+      ParserUtils.validateFields(this.fieldNames);
+    }
+  }
+
+  public void setFieldNames(final String header)
+  {
+    try {
+      setFieldNames(parseLine(header));
+    }
+    catch (Exception e) {
+      throw new ParseException(e, "Unable to parse header [%s]", header);
+    }
+  }
+
+  @Override
+  public Map<String, Object> parse(final String input)
+  {
+    if (!supportSkipHeaderRows && (hasHeaderRow || maxSkipHeaderRows > 0)) {
+      throw new UnsupportedOperationException("hasHeaderRow or maxSkipHeaderRows is not supported. "
+                                              + "Please check the indexTask supports these options.");
+    }
+
+    try {
+      List<String> values = parseLine(input);
+
+      if (skippedHeaderRows < maxSkipHeaderRows) {
+        skippedHeaderRows++;
+        return null;
+      }
+
+      if (hasHeaderRow && !hasParsedHeader) {
+        if (fieldNames == null) {
+          setFieldNames(values);
+        }
+        hasParsedHeader = true;
+        return null;
+      }
+
+      if (fieldNames == null) {
+        setFieldNames(ParserUtils.generateFieldNames(values.size()));
+      }
+
+      Map<String, String> fieldValueMap = Utils.zipMapPartial(fieldNames, values);
+      return Maps.transformEntries(fieldValueMap, multiValueTransformer);
+    }
+    catch (Exception e) {
+      throw new ParseException(e, "Unable to parse row [%s]", input);
+    }
+  }
+
+  protected abstract List<String> parseLine(String input) throws IOException;
+}
