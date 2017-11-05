6404c2fda750df2e625b373a1a4ad12b4d2d6eb8
Luc Esape <luc.esape@gmail.com>
2017-11-05 08:41:08 +0100

    Bug commit from the following repository prestodb/presto This bug commit is a reflect of source code from: http://github.com/prestodb/presto/commit/75295cb79f15baac44d0c020b592137292ceee7b.

---
+++ presto-main/src/main/java/com/facebook/presto/server/security/ServerSecurityModule.java
@@ -0,0 +1,62 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.facebook.presto.server.security;
+
+import com.google.inject.Binder;
+import com.google.inject.Module;
+import com.google.inject.Scopes;
+import io.airlift.configuration.AbstractConfigurationAwareModule;
+import io.airlift.http.server.TheServlet;
+
+import javax.servlet.Filter;
+
+import java.util.function.Predicate;
+
+import static com.facebook.presto.server.security.SecurityConfig.AuthenticationType.KERBEROS;
+import static com.facebook.presto.server.security.SecurityConfig.AuthenticationType.LDAP;
+import static com.google.inject.multibindings.Multibinder.newSetBinder;
+import static io.airlift.configuration.ConditionalModule.installModuleIf;
+import static io.airlift.configuration.ConfigBinder.configBinder;
+
+public class ServerSecurityModule
+        extends AbstractConfigurationAwareModule
+{
+    @Override
+    protected void setup(Binder securityBinder)
+    {
+        newSetBinder(securityBinder, Filter.class, TheServlet.class).addBinding()
+                .to(AuthenticationFilter.class).in(Scopes.SINGLETON);
+
+        bindSecurityConfig(
+                config -> config.getAuthenticationTypes().contains(KERBEROS),
+                binder -> {
+                    configBinder(binder).bindConfig(KerberosConfig.class);
+                    newSetBinder(binder, Authenticator.class).addBinding()
+                            .to(KerberosAuthenticator.class).in(Scopes.SINGLETON);
+                });
+
+        bindSecurityConfig(
+                config -> config.getAuthenticationTypes().contains(LDAP),
+                binder -> {
+                    configBinder(binder).bindConfig(LdapConfig.class);
+                    newSetBinder(binder, Authenticator.class).addBinding()
+                            .to(LdapAuthenticator.class).in(Scopes.SINGLETON);
+                });
+    }
+
+    private void bindSecurityConfig(Predicate<SecurityConfig> predicate, Module module)
+    {
+        install(installModuleIf(SecurityConfig.class, predicate, module));
+    }
+}
