d0100f49b972677e48525137a56f18cbf031bf16
Luc Esape <luc.esape@gmail.com>
2017-11-06 16:15:17 +0100

    Bug commit from the following repository spring-cloud/spring-cloud-dataflow This bug commit is a reflect of source code from: http://github.com/spring-cloud/spring-cloud-dataflow/commit/22420465c89d9680858547b3f27a83a55567b261.

---
+++ spring-cloud-dataflow-rest-client/src/main/java/org/springframework/cloud/dataflow/rest/client/StreamOperations.java
@@ -0,0 +1,98 @@
+/*
+ * Copyright 2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.cloud.dataflow.rest.client;
+
+import java.util.Map;
+
+import org.springframework.cloud.dataflow.rest.resource.StreamDefinitionResource;
+import org.springframework.cloud.skipper.domain.PackageIdentifier;
+import org.springframework.hateoas.PagedResources;
+
+/**
+ * Interface defining operations available against streams.
+ *
+ * @author Eric Bottard
+ * @author Ilayaperumal Gopinathan
+ * @author Mark Fisher
+ */
+public interface StreamOperations {
+
+	/**
+	 * @return the list streams known to the system.
+	 */
+	PagedResources<StreamDefinitionResource> list();
+
+	/**
+	 * Create a new stream, optionally deploying it.
+	 *
+	 * @param name the name of the stream
+	 * @param definition the stream definition DSL
+	 * @param deploy whether to deploy the stream after creating its definition
+	 * @return the new stream definition
+	 */
+	StreamDefinitionResource createStream(String name, String definition, boolean deploy);
+
+	/**
+	 * Deploy an already created stream.
+	 *
+	 * @param name the name of the stream
+	 * @param properties the deployment properties
+	 */
+	void deploy(String name, Map<String, String> properties);
+
+	/**
+	 * Undeploy a deployed stream, retaining its definition.
+	 *
+	 * @param name the name of the stream
+	 */
+	void undeploy(String name);
+
+	/**
+	 * Undeploy all currently deployed streams.
+	 */
+	void undeployAll();
+
+	/**
+	 * Destroy an existing stream.
+	 *
+	 * @param name the name of the stream
+	 */
+	void destroy(String name);
+
+	/**
+	 * Destroy all streams known to the system.
+	 */
+	void destroyAll();
+
+	/**
+	 * Update the stream given its corresponding releaseName in Skipper using the specified
+	 * package and updated yaml config.
+	 * @param streamName the name of the stream to update
+	 * @param releaseName the corresponding release name of the stream in skipper
+	 * @param packageIdentifier the package that corresponds to this stream
+	 * @param updateProperties a map of properties to use for updating the stream
+	 */
+	void updateStream(String streamName, String releaseName, PackageIdentifier packageIdentifier,
+			Map<String, String> updateProperties);
+
+	/**
+	 * Queries the server for the stream definition
+	 * @param streamName the name of the stream to get status
+	 * @return The current stream definition with updated status
+	 */
+	StreamDefinitionResource getStreamDefinition(String streamName);
+}
---
+++ spring-cloud-dataflow-server-core/src/main/java/org/springframework/cloud/dataflow/server/controller/StreamDeploymentController.java
@@ -0,0 +1,124 @@
+/*
+ * Copyright 2015-2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.cloud.dataflow.server.controller;
+
+import java.util.Map;
+
+import org.springframework.cloud.dataflow.core.StreamDefinition;
+import org.springframework.cloud.dataflow.rest.UpdateStreamRequest;
+import org.springframework.cloud.dataflow.rest.resource.StreamDeploymentResource;
+import org.springframework.cloud.dataflow.server.repository.NoSuchStreamDefinitionException;
+import org.springframework.cloud.dataflow.server.repository.StreamDefinitionRepository;
+import org.springframework.cloud.dataflow.server.service.StreamService;
+import org.springframework.cloud.deployer.spi.app.AppDeployer;
+import org.springframework.hateoas.ExposesResourceFor;
+import org.springframework.http.HttpStatus;
+import org.springframework.util.Assert;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Controller for deployment operations on {@link StreamDefinition}.
+ *
+ * @author Eric Bottard
+ * @author Mark Fisher
+ * @author Patrick Peralta
+ * @author Ilayaperumal Gopinathan
+ * @author Marius Bogoevici
+ * @author Janne Valkealahti
+ */
+@RestController
+@RequestMapping("/streams/deployments")
+@ExposesResourceFor(StreamDeploymentResource.class)
+public class StreamDeploymentController {
+
+	private final StreamService streamService;
+
+	/**
+	 * The repository this controller will use for stream CRUD operations.
+	 */
+	private final StreamDefinitionRepository repository;
+
+	/**
+	 * Create a {@code StreamDeploymentController} that delegates
+	 * <ul>
+	 * <li>CRUD operations to the provided {@link StreamDefinitionRepository}</li>
+	 * <li>deployment operations to the provided {@link AppDeployer} via
+	 * {@link StreamService}</li>
+	 * </ul>
+	 *
+	 * @param repository the repository this controller will use for stream CRUD operations
+	 * @param streamService the underlying StreamService to deploy the stream
+	 */
+	public StreamDeploymentController(StreamDefinitionRepository repository, StreamService streamService) {
+		Assert.notNull(repository, "StreamDefinitionRepository must not be null");
+		Assert.notNull(streamService, "StreamService must not be null");
+		this.repository = repository;
+		this.streamService = streamService;
+	}
+
+	/**
+	 * Request un-deployment of an existing stream.
+	 *
+	 * @param name the name of an existing stream (required)
+	 */
+	@RequestMapping(value = "/{name}", method = RequestMethod.DELETE)
+	@ResponseStatus(HttpStatus.OK)
+	public void undeploy(@PathVariable("name") String name) {
+		StreamDefinition stream = this.repository.findOne(name);
+		if (stream == null) {
+			throw new NoSuchStreamDefinitionException(name);
+		}
+		this.streamService.undeployStream(name);
+	}
+
+	/**
+	 * Request un-deployment of all streams.
+	 */
+	@RequestMapping(value = "", method = RequestMethod.DELETE)
+	@ResponseStatus(HttpStatus.OK)
+	public void undeployAll() {
+		for (StreamDefinition stream : this.repository.findAll()) {
+			this.streamService.undeployStream(stream.getName());
+		}
+	}
+
+	/**
+	 * Request deployment of an existing stream definition.
+	 * @param name the name of an existing stream definition (required)
+	 * @param properties the deployment properties for the stream as a comma-delimited list of
+	 * key=value pairsef
+	 */
+	@RequestMapping(value = "/{name}", method = RequestMethod.POST)
+	@ResponseStatus(HttpStatus.CREATED)
+	public void deploy(@PathVariable("name") String name,
+			@RequestBody(required = false) Map<String, String> properties) {
+		this.streamService.deployStream(name, properties);
+	}
+
+	@RequestMapping(value = "/update/{name}", method = RequestMethod.POST)
+	@ResponseStatus(HttpStatus.CREATED)
+	public void update(@PathVariable("name") String name,
+			@RequestBody UpdateStreamRequest updateStreamRequest) {
+		this.streamService.updateStream(name, updateStreamRequest);
+	}
+
+}
