8e897c68e3857415de9784c4b4f7b671fa845e9a
Luc Esape <luc.esape@gmail.com>
2017-11-07 15:12:39 +0100

    Bug commit from the following repository bonigarcia/webdrivermanager This bug commit is a reflect of source code from: http://github.com/bonigarcia/webdrivermanager/commit/dda3c82ccf498b6d2773052ca7b60963fa7d4728.

---
+++ src/main/java/io/github/bonigarcia/wdm/FirefoxDriverManager.java
@@ -0,0 +1,88 @@
+/*
+ * (C) Copyright 2016 Boni Garcia (http://bonigarcia.github.io/)
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser General Public License
+ * (LGPL) version 2.1 which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/lgpl-2.1.html
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ */
+package io.github.bonigarcia.wdm;
+
+import static io.github.bonigarcia.wdm.OperativeSystem.MAC;
+import static java.util.Arrays.asList;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.List;
+
+/**
+ * Manager for Firefox (same as Marionette).
+ *
+ * @author Boni Garcia (boni.gg@gmail.com)
+ * @since 1.5.0
+ */
+public class FirefoxDriverManager extends BrowserManager {
+
+    public static synchronized BrowserManager getInstance() {
+        if (instance == null
+                || !instance.getClass().equals(FirefoxDriverManager.class)) {
+            instance = new FirefoxDriverManager();
+        }
+        return instance;
+    }
+
+    public FirefoxDriverManager() {
+        exportParameter = "wdm.geckoDriverExport";
+        driverVersionKey = "wdm.geckoDriverVersion";
+        driverUrlKey = "wdm.geckoDriverVersion";
+        driverName = asList("wires", "geckodriver");
+    }
+
+    @Override
+    protected List<URL> getDrivers() throws IOException {
+        return getDriversFromGitHub();
+    }
+
+    @Override
+    protected String getCurrentVersion(URL url, String driverName)
+            throws MalformedURLException {
+        String currentVersion = url.getFile().substring(
+                url.getFile().indexOf("-") + 1, url.getFile().lastIndexOf("-"));
+        if (currentVersion.startsWith("v")) {
+            currentVersion = currentVersion.substring(1);
+        }
+        return currentVersion;
+    }
+
+    @Override
+    protected String preDownload(String target, String version)
+            throws IOException {
+        int iSeparator = target.indexOf(version) - 1;
+        int iDash = target.lastIndexOf(version) + version.length();
+        int iPoint = target.lastIndexOf("tar.gz") != -1
+                ? target.lastIndexOf(".tar.gz")
+                : target.lastIndexOf(".gz") != -1 ? target.lastIndexOf(".gz")
+                        : target.lastIndexOf(".zip");
+        target = target.substring(0, iSeparator + 1)
+                + target.substring(iDash + 1, iPoint).toLowerCase()
+                + target.substring(iSeparator);
+        return target;
+    }
+
+    @Override
+    public BrowserManager useTaobaoMirror() {
+        return useTaobaoMirror("wdm.geckoDriverTaobaoUrl");
+    }
+
+    @Override
+    protected boolean shouldCheckArchitecture() {
+        return !MY_OS_NAME.contains(MAC.name());
+    }
+}
