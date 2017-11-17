ff4c950ca187e3efa779d881b3b70c7504760399
Luc Esape <luc.esape@gmail.com>
2017-11-17 02:08:29 +0100

    Bug commit from the following repository fabric8io/docker-maven-plugin This bug commit is a reflect of source code from: http://github.com/fabric8io/docker-maven-plugin/commit/047693e06ac6f3767bd8be8736a623f4b67ec462.

---
+++ src/main/java/io/fabric8/maven/docker/StartMojo.java
@@ -0,0 +1,384 @@
+package io.fabric8.maven.docker;
+
+/*
+ * Copyright 2009-2014 Roland Huss Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0 Unless required by
+ * applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+import java.io.IOException;
+import java.util.*;
+import java.util.concurrent.*;
+
+import com.google.common.util.concurrent.MoreExecutors;
+import io.fabric8.maven.docker.access.DockerAccessException;
+import io.fabric8.maven.docker.access.PortMapping;
+import io.fabric8.maven.docker.config.*;
+import io.fabric8.maven.docker.log.LogDispatcher;
+import io.fabric8.maven.docker.model.Container;
+import io.fabric8.maven.docker.service.*;
+import io.fabric8.maven.docker.util.StartOrderResolver;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.codehaus.plexus.util.StringUtils;
+
+
+/**
+ * Goal for creating and starting a docker container. This goal evaluates the image configuration
+ *
+ * @author roland
+ */
+@Mojo(name = "start", defaultPhase = LifecyclePhase.PRE_INTEGRATION_TEST)
+public class StartMojo extends AbstractDockerMojo {
+
+    @Parameter(property = "docker.showLogs")
+    private String showLogs;
+
+    @Parameter(property = "docker.pull.registry")
+    private String pullRegistry;
+
+    @Parameter(property = "docker.skip.run", defaultValue = "false")
+    private boolean skipRun;
+
+    @Parameter(property = "docker.startParallel", defaultValue = "false")
+    private boolean startParallel;
+
+    // whether to block during to start. Set it via System property docker.follow
+    private boolean follow;
+
+    /**
+     * Expose container information like the internal IP as Maven properties which
+     * can be reused in the build information. The value of this property is the prefix
+     * used for the properties. The default prefix is "docker.container". Only information
+     * of images having an alias are exposed and have the format <code>&lt;prefix&gt;.&lt;alias&gt;.&lt;property&gt;</code>.
+     * (e.g. <code>docker.container.mycontainer.ip</code>
+     * The following properties are currently supported:
+     * <ul>
+     *   <li><strong>ip</strong> : the container's internal IP address as chosen by Docker</li>
+     * </ul>
+     *
+     * If set to an empty string, no properties are exposed.
+     */
+    @Parameter(property = "docker.exposeContainerInfo")
+    private String exposeContainerProps = "docker.container";
+
+    /**
+     * Whether to create the customs networks (user-defined bridge networks) before starting automatically
+     */
+    @Parameter(property = "docker.autoCreateCustomNetworks", defaultValue = "false")
+    protected boolean autoCreateCustomNetworks;
+
+    // property file to write out with port mappings
+    @Parameter
+    protected String portPropertyFile;
+
+    private static final class StartedContainer {
+        public final ImageConfiguration imageConfig;
+        public final String containerId;
+
+        private StartedContainer(ImageConfiguration imageConfig, String containerId) {
+            this.imageConfig = imageConfig;
+            this.containerId = containerId;
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public synchronized void executeInternal(final ServiceHub hub) throws DockerAccessException,
+                                                                          MojoExecutionException {
+        if (skipRun) {
+            return;
+        }
+        getPluginContext().put(CONTEXT_KEY_START_CALLED, true);
+
+        this.follow = followLogs();
+
+        QueryService queryService = hub.getQueryService();
+        final RunService runService = hub.getRunService();
+
+        PortMapping.PropertyWriteHelper portMappingPropertyWriteHelper = new PortMapping.PropertyWriteHelper(portPropertyFile);
+
+        boolean success = false;
+
+        final ExecutorService executorService = getExecutorService();
+        final ExecutorCompletionService<StartedContainer> containerStartupService = new ExecutorCompletionService<>(executorService);
+
+        try {
+            // All aliases which are provided in the image configuration:
+            final Set<String> imageAliases = new HashSet<>();
+            // Remember all aliases which has been started
+            final Set<String> startedContainerAliases = new HashSet<>();
+
+            // All images to to start
+            Queue<ImageConfiguration> imagesWaitingToStart = prepareStart(hub, queryService, runService, imageAliases);
+
+            // Queue of images to start as containers
+            final Queue<ImageConfiguration> imagesStarting = new ArrayDeque<>();
+
+            // Loop until every image has been started and the start of all images has been completed
+            while (!hasBeenAllImagesStarted(imagesWaitingToStart, imagesStarting)) {
+
+                final List<ImageConfiguration> imagesReadyToStart =
+                    getImagesWhoseDependenciesHasStarted(imagesWaitingToStart, startedContainerAliases, imageAliases);
+
+                for (final ImageConfiguration image : imagesReadyToStart) {
+
+                    startImage(image, hub, containerStartupService, portMappingPropertyWriteHelper);
+
+                    // Move from waiting to starting status
+                    imagesStarting.add(image);
+                    imagesWaitingToStart.remove(image);
+                }
+
+                // Wait for the next container to finish startup
+                final Future<StartedContainer> startedContainerFuture = containerStartupService.take();
+                try {
+                    final StartedContainer startedContainer = startedContainerFuture.get();
+                    final ImageConfiguration imageConfig = startedContainer.imageConfig;
+
+                    updateAliasesSet(startedContainerAliases, imageConfig.getAlias());
+                    exposeContainerProps(hub.getQueryService(), startedContainer);
+
+                    // All done with this image
+                    imagesStarting.remove(imageConfig);
+                } catch (ExecutionException e) {
+                    rethrowCause(e);
+                }
+            }
+
+            portMappingPropertyWriteHelper.write();
+
+            if (follow) {
+                runService.addShutdownHookForStoppingContainers(keepContainer, removeVolumes, autoCreateCustomNetworks);
+                wait();
+            }
+
+            success = true;
+        } catch (InterruptedException e) {
+            log.warn("Interrupted");
+            Thread.currentThread().interrupt();
+            throw new MojoExecutionException("interrupted", e);
+        } catch (IOException e) {
+            throw new MojoExecutionException("I/O Error", e);
+        } finally {
+            shutdownExecutorService(executorService);
+
+            // Rollback if not all could be started
+            if (!success) {
+                log.error("Error occurred during container startup, shutting down...");
+                runService.stopStartedContainers(keepContainer, removeVolumes, autoCreateCustomNetworks, getPomLabel());
+            }
+        }
+    }
+
+    protected Boolean followLogs() {
+        return Boolean.valueOf(System.getProperty("docker.follow", "false"));
+    }
+
+    // Check if we are done
+    private boolean hasBeenAllImagesStarted(Queue<ImageConfiguration> imagesWaitingToStart, Queue<ImageConfiguration> imagesStarting) {
+        return imagesWaitingToStart.isEmpty() && imagesStarting.isEmpty();
+    }
+
+    private void shutdownExecutorService(ExecutorService executorService) {
+        if (!executorService.isShutdown()) {
+            executorService.shutdown();
+            try {
+                executorService.awaitTermination(10, TimeUnit.SECONDS);
+            } catch (InterruptedException e) {
+                log.warn("ExecutorService did not shutdown normally.");
+                executorService.shutdownNow();
+            }
+        }
+    }
+
+    private void rethrowCause(ExecutionException e) throws IOException, InterruptedException {
+        Throwable cause = e.getCause();
+        if (cause instanceof RuntimeException) {
+            throw (RuntimeException) cause;
+        } else if (cause instanceof IOException) {
+            throw (IOException) cause;
+        } else if (cause instanceof InterruptedException) {
+                throw (InterruptedException) cause;
+        } else {
+            throw new RuntimeException("Start-Job failed with unexpected exception: " + e.getCause().getMessage(),
+                                       e.getCause());
+        }
+    }
+
+    private void updateAliasesSet(Set<String> aliasesSet, String alias) {
+        // Add the alias to the set only when it is set. When it's
+        // not set it cant be used in the dependency resolution anyway, so we are ignoring
+        // it hence.
+        if (alias != null) {
+            aliasesSet.add(alias);
+        }
+    }
+
+    private void startImage(final ImageConfiguration image,
+                            final ServiceHub hub,
+                            final ExecutorCompletionService<StartedContainer> startingContainers,
+                            final PortMapping.PropertyWriteHelper portMappingPropertyWriteHelper) {
+
+        final RunService runService = hub.getRunService();
+        final Properties projProperties = project.getProperties();
+        final RunImageConfiguration runConfig = image.getRunConfiguration();
+        final PortMapping portMapping = runService.createPortMapping(runConfig, projProperties);
+        final LogDispatcher dispatcher = getLogDispatcher(hub);
+
+        startingContainers.submit(new Callable<StartedContainer>() {
+            @Override
+            public StartedContainer call() throws Exception {
+                final String containerId = runService.createAndStartContainer(image, portMapping, getPomLabel(), projProperties, project.getBasedir());
+
+                // Update port-mapping writer
+                portMappingPropertyWriteHelper.add(portMapping, runConfig.getPortPropertyFile());
+
+                if (showLogs(image)) {
+                    dispatcher.trackContainerLog(containerId,
+                            serviceHubFactory.getLogOutputSpecFactory().createSpec(containerId, image));
+                }
+
+                // Wait if requested
+                hub.getWaitService().wait(image, projProperties, containerId);
+                WaitConfiguration waitConfig = runConfig.getWaitConfiguration();
+                if (waitConfig != null && waitConfig.getExec() != null && waitConfig.getExec().getPostStart() != null) {
+                    runService.execInContainer(containerId, waitConfig.getExec().getPostStart(), image);
+                }
+
+                return new StartedContainer(image, containerId);
+            }
+        });
+    }
+
+    // Pick out all images who can be started right now because all their dependencies has been started
+    private List<ImageConfiguration> getImagesWhoseDependenciesHasStarted(Queue<ImageConfiguration> imagesRemaining,
+                                                                          Set<String> containersStarted,
+                                                                          Set<String> aliases) {
+        final List<ImageConfiguration> ret = new ArrayList<>();
+
+        // Check for all images which can be already started
+        for (ImageConfiguration imageWaitingToStart : imagesRemaining) {
+            List<String> allDependencies = imageWaitingToStart.getDependencies();
+            List<String> aliasDependencies = filterOutNonAliases(aliases, allDependencies);
+            if (containersStarted.containsAll(aliasDependencies)) {
+                ret.add(imageWaitingToStart);
+            }
+        }
+        return ret;
+    }
+
+    // Prepare start like creating custom networks, auto pull images, map aliases and return the list of images
+    // to start in the correct order
+    private Queue<ImageConfiguration> prepareStart(ServiceHub hub, QueryService queryService, RunService runService, Set<String> imageAliases)
+        throws DockerAccessException, MojoExecutionException {
+        final Queue<ImageConfiguration> imagesWaitingToStart = new ArrayDeque<>();
+        for (StartOrderResolver.Resolvable resolvable : runService.getImagesConfigsInOrder(queryService, getResolvedImages())) {
+            final ImageConfiguration imageConfig = (ImageConfiguration) resolvable;
+
+            // Still to check: How to work with linking, volumes, etc ....
+            //String imageName = new ImageName(imageConfig.getName()).getFullNameWithTag(registry);
+
+            String imageName = imageConfig.getName();
+
+            RegistryService.RegistryConfig registryConfig = getRegistryConfig();
+            hub.getRegistryService().checkImageWithAutoPull(imageName,
+                                   getConfiguredRegistry(imageConfig, pullRegistry),
+                                   imageConfig.getBuildConfiguration() == null, registryConfig);
+
+            RunImageConfiguration runConfig = imageConfig.getRunConfiguration();
+            NetworkConfig config = runConfig.getNetworkingConfig();
+            if (autoCreateCustomNetworks && config.isCustomNetwork()) {
+                runService.createCustomNetworkIfNotExistant(config.getCustomNetwork());
+            }
+            imagesWaitingToStart.add(imageConfig);
+            updateAliasesSet(imageAliases, imageConfig.getAlias());
+        }
+        return imagesWaitingToStart;
+    }
+
+    private List<String> filterOutNonAliases(Set<String> imageAliases, List<String> dependencies) {
+        List<String> ret = new ArrayList<>();
+        for (String alias : dependencies) {
+            if (imageAliases.contains(alias)) {
+                ret.add(alias);
+            }
+        }
+        return ret;
+    }
+
+    private ExecutorService getExecutorService() {
+        final ExecutorService executorService;
+        if (startParallel) {
+            executorService = Executors.newCachedThreadPool();
+        } else {
+            executorService = MoreExecutors.newDirectExecutorService();
+        }
+        return executorService;
+    }
+
+    protected boolean showLogs(ImageConfiguration imageConfig) {
+        if (showLogs != null) {
+            if (showLogs.equalsIgnoreCase("true")) {
+                return true;
+            } else if (showLogs.equalsIgnoreCase("false")) {
+                return false;
+            } else {
+                return ConfigHelper.matchesConfiguredImages(showLogs, imageConfig);
+            }
+        }
+
+        RunImageConfiguration runConfig = imageConfig.getRunConfiguration();
+        if (runConfig != null) {
+            LogConfiguration logConfig = runConfig.getLogConfiguration();
+            if (logConfig != null) {
+                return logConfig.isEnabled();
+            } else {
+                // Default is to show logs if "follow" is true
+                return follow;
+            }
+        }
+        return false;
+    }
+
+    // Expose ports as project properties
+    private void exposeContainerProps(QueryService queryService, StartedContainer startedContainer)
+        throws DockerAccessException {
+        String propKey = getExposedPropertyKeyPart(startedContainer.imageConfig);
+        if (StringUtils.isNotEmpty(exposeContainerProps) && StringUtils.isNotEmpty(propKey)) {
+            Container container = queryService.getMandatoryContainer(startedContainer.containerId);
+            Properties props = project.getProperties();
+            String prefix = addDot(exposeContainerProps) + addDot(propKey);
+            props.put(prefix + "id", startedContainer.containerId);
+            String ip = container.getIPAddress();
+            if (StringUtils.isNotEmpty(ip)) {
+                props.put(prefix + "ip", ip);
+            }
+
+            Map<String, String> nets = container.getCustomNetworkIpAddresses();
+            if (nets != null) {
+                for (Map.Entry<String, String> entry : nets.entrySet()) {
+                    props.put(prefix + addDot("net") + addDot(entry.getKey()) + "ip", entry.getValue());
+                }
+            }
+        }
+    }
+
+    private String getExposedPropertyKeyPart(ImageConfiguration image) {
+        String propKey = image.getRunConfiguration() != null ? image.getRunConfiguration().getExposedPropertyKey() : null;
+        if (StringUtils.isEmpty(propKey)) {
+            propKey = image.getAlias();
+        }
+        return propKey;
+    }
+
+    private String addDot(String part) {
+        return part.endsWith(".") ? part : part + ".";
+    }
+}
---
+++ src/main/java/io/fabric8/maven/docker/util/DockerFileUtil.java
@@ -0,0 +1,136 @@
+package io.fabric8.maven.docker.util;/*
+ *
+ * Copyright 2015 Roland Huss
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.apache.maven.plugin.assembly.interpolation.AssemblyInterpolator;
+import org.apache.maven.plugin.assembly.io.DefaultAssemblyReader;
+import org.codehaus.plexus.interpolation.fixed.FixedStringSearchInterpolator;
+
+import io.fabric8.maven.docker.assembly.DockerAssemblyConfigurationSource;
+
+
+/**
+ * Utility class for dealing with dockerfiles
+ * @author roland
+ * @since 21/01/16
+ */
+public class DockerFileUtil {
+
+    private DockerFileUtil() {}
+
+    /**
+     * Extract the base image from a dockerfile. The first line containing a <code>FROM</code> is
+     * taken.
+     *
+     * @param dockerFile file from where to extract the base image
+     * @param interpolator interpolator for replacing properties
+     */
+    public static String extractBaseImage(File dockerFile, FixedStringSearchInterpolator interpolator) throws IOException {
+        List<String[]> fromLines = extractLines(dockerFile, "FROM", interpolator);
+        if (!fromLines.isEmpty()) {
+            String[] parts = fromLines.get(0);
+            if (parts.length > 1) {
+                return parts[1];
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Extract all lines containing the given keyword
+     *
+     * @param dockerFile dockerfile to examine
+     * @param keyword keyword to extract the lines for
+     * @param interpolator interpolator for replacing properties
+     * @return list of matched lines or an empty list
+     */
+    public static List<String[]> extractLines(File dockerFile, String keyword, FixedStringSearchInterpolator interpolator) throws IOException {
+        List<String[]> ret = new ArrayList<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader(dockerFile))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String lineInterpolated = interpolator.interpolate(line);
+                String[] lineParts = lineInterpolated.split("\\s+");
+                if (lineParts.length > 0 && lineParts[0].equalsIgnoreCase(keyword)) {
+                    ret.add(lineParts);
+                }
+            }
+        }
+        return ret;
+    }
+
+    /**
+     * Interpolate a docker file with the given properties and filter
+     *
+     * @param dockerFile docker file to interpolate
+     * @param interpolator interpolator for replacing properties
+     * @return The interpolated contents of the file.
+     * @throws IOException
+     */
+    public static String interpolate(File dockerFile, FixedStringSearchInterpolator interpolator) throws IOException {
+        StringBuilder ret = new StringBuilder();
+        try (BufferedReader reader = new BufferedReader(new FileReader(dockerFile))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                ret.append(interpolator.interpolate(line)).append(System.lineSeparator());
+            }
+        }
+        return ret.toString();
+    }
+
+    /**
+     * Create an interpolator for the given maven parameters and filter configuration.
+     *
+     * @param params The maven parameters.
+     * @param filter The filter configuration.
+     * @return An interpolator for replacing maven properties.
+     */
+    public static FixedStringSearchInterpolator createInterpolator(MojoParameters params, String filter) {
+        String[] delimiters = extractDelimiters(filter);
+        if (delimiters == null) {
+            // Don't interpolate anything
+            return FixedStringSearchInterpolator.create();
+        }
+
+        DockerAssemblyConfigurationSource configSource = new DockerAssemblyConfigurationSource(params, null, null);
+        // Patterned after org.apache.maven.plugin.assembly.interpolation.AssemblyExpressionEvaluator
+        return AssemblyInterpolator
+                .fullInterpolator(params.getProject(),
+                        DefaultAssemblyReader.createProjectInterpolator(params.getProject()), configSource)
+                .withExpressionMarkers(delimiters[0], delimiters[1]);
+    }
+
+    private static String[] extractDelimiters(String filter) {
+        if (filter == null ||
+            filter.equalsIgnoreCase("false") ||
+            filter.equalsIgnoreCase("none")) {
+            return null;
+        }
+        if (filter.contains("*")) {
+            Matcher matcher = Pattern.compile("^(?<start>[^*]+)\\*(?<end>.*)$").matcher(filter);
+            if (matcher.matches()) {
+                return new String[] { matcher.group("start"), matcher.group("end") };
+            }
+        }
+        return new String[] { filter, filter };
+    }
+}
