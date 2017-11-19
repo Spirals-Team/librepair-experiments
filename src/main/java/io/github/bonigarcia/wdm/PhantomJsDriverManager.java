7ac25937f5197ee64a52f8fc69d802fbd82aaedd
Luc Esape <luc.esape@gmail.com>
2017-11-19 19:09:56 +0100

    Bug commit from the following repository bonigarcia/webdrivermanager This bug commit is a reflect of source code from: http://github.com/bonigarcia/webdrivermanager/commit/c6e53ff991da0aff2fe4e7b7debbcb4ba424ec75.

---
+++ src/main/java/io/github/bonigarcia/wdm/PhantomJsDriverManager.java
@@ -0,0 +1,120 @@
+/*
+ * (C) Copyright 2016 Boni Garcia (http://bonigarcia.github.io/)
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser General Public License
+ * (LGPL) version 2.1 which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/lgpl-2.1.html
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ */
+package io.github.bonigarcia.wdm;
+
+import static io.github.bonigarcia.wdm.WdmConfig.getString;
+import static java.io.File.separator;
+import static java.util.Arrays.asList;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.util.List;
+
+/**
+ * Manager for PhantomJs.
+ *
+ * @author Boni Garcia (boni.gg@gmail.com)
+ * @since 1.4.0
+ */
+public class PhantomJsDriverManager extends BrowserManager {
+
+    public static synchronized BrowserManager getInstance() {
+        if (instance == null
+                || !instance.getClass().equals(PhantomJsDriverManager.class)) {
+            instance = new PhantomJsDriverManager();
+        }
+        return instance;
+    }
+
+    public PhantomJsDriverManager() {
+        exportParameter = getString("wdm.phantomjsDriverExport");
+        driverVersionKey = "wdm.phantomjsDriverVersion";
+        driverUrlKey = "wdm.phantomjsDriverUrl";
+        driverName = asList("phantomjs");
+    }
+
+    @Override
+    protected List<URL> getDrivers() throws IOException {
+        return getDriversFromMirror(getDriverUrl());
+    }
+
+    @Override
+    protected String getCurrentVersion(URL url, String driverName) {
+        String file = url.getFile();
+        file = url.getFile().substring(file.lastIndexOf(SEPARATOR),
+                file.length());
+        int matchIndex = file.indexOf(driverName);
+        String currentVersion = file
+                .substring(matchIndex + driverName.length() + 1, file.length());
+        int dashIndex = currentVersion.indexOf('-');
+        currentVersion = currentVersion.substring(0, dashIndex);
+        return currentVersion;
+    }
+
+    @Override
+    protected String preDownload(String target, String version) {
+        int iSeparator = target.indexOf(version) - 1;
+        int iDash = target.lastIndexOf(version) + version.length();
+        int iPoint = target.lastIndexOf(".tar") != -1
+                ? target.lastIndexOf(".tar")
+                : target.lastIndexOf(".zip");
+        target = target.substring(0, iSeparator + 1)
+                + target.substring(iDash + 1, iPoint)
+                + target.substring(iSeparator);
+        target = target.replace("beta-", "");
+        return target;
+    }
+
+    @Override
+    protected File postDownload(File archive) {
+        log.trace("PhatomJS package name: {}", archive);
+
+        File extractFolder = archive.getParentFile().listFiles()[0];
+        log.trace("PhatomJS extract folder (to be deleted): {}", extractFolder);
+
+        File binFolder = new File(
+                extractFolder.getAbsoluteFile() + separator + "bin");
+        // Exception for older version of PhantomJS
+        int binaryIndex = 0;
+        if (!binFolder.exists()) {
+            binFolder = extractFolder;
+            binaryIndex = 3;
+        }
+
+        log.trace("PhatomJS bin folder: {} (index {})", binFolder, binaryIndex);
+
+        File phantomjs = binFolder.listFiles()[binaryIndex];
+        log.trace("PhatomJS binary: {}", phantomjs);
+
+        File target = new File(archive.getParentFile().getAbsolutePath()
+                + separator + phantomjs.getName());
+        log.trace("PhatomJS target: {}", target);
+
+        downloader.renameFile(phantomjs, target);
+        downloader.deleteFolder(extractFolder);
+        return target;
+    }
+
+    @Override
+    public BrowserManager useTaobaoMirror() {
+        return useTaobaoMirror("wdm.phantomjsDriverTaobaoUrl");
+    }
+
+    @Override
+    protected boolean shouldCheckArchitecture() {
+        return false;
+    }
+}
---
+++ src/test/java/io/github/bonigarcia/wdm/test/TaobaoTest.java
@@ -0,0 +1,73 @@
+/*
+ * (C) Copyright 2016 Boni Garcia (http://bonigarcia.github.io/)
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser General Public License
+ * (LGPL) version 2.1 which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/lgpl-2.1.html
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ */
+package io.github.bonigarcia.wdm.test;
+
+import static java.lang.System.setProperty;
+import static java.util.Arrays.asList;
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.junit.Assert.assertThat;
+
+import java.lang.reflect.Method;
+import java.net.URL;
+import java.util.Collection;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameter;
+import org.junit.runners.Parameterized.Parameters;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.BrowserManager;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+/**
+ * Test for taobao.org mirror.
+ *
+ * @author Boni Garcia (boni.gg@gmail.com)
+ * @since 1.6.1
+ */
+@RunWith(Parameterized.class)
+public class TaobaoTest {
+
+    @Parameter(0)
+    public Class<? extends WebDriver> driverClass;
+
+    @Parameter(1)
+    public String taobaoUrl;
+
+    @Parameters(name = "{index}: {0}")
+    public static Collection<Object[]> data() {
+        return asList(new Object[][] { { ChromeDriver.class,
+                "http://npm.taobao.org/mirrors/chromedriver/2.33/" } });
+    }
+
+    @Test
+    public void testTaobao() throws Exception {
+        setProperty("wdm.chromeDriverTaobaoUrl", taobaoUrl);
+
+        BrowserManager browserManager = WebDriverManager
+                .getInstance(driverClass);
+        browserManager.useTaobaoMirror().setup();
+
+        Method method = BrowserManager.class.getDeclaredMethod("getDriverUrl");
+        method.setAccessible(true);
+        URL driverUrl = (URL) method.invoke(browserManager);
+
+        assertThat(driverUrl.toString(), containsString("taobao.org"));
+    }
+
+}
