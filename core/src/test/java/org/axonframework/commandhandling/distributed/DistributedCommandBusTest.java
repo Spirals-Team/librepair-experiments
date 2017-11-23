c5b1170f082e016d7f5b675cddd543ecb89abbad
Luc Esape <luc.esape@gmail.com>
2017-11-23 12:10:17 +0100

    Bug commit from the following repository AxonFramework/AxonFramework This bug commit is a reflect of source code from: http://github.com/AxonFramework/AxonFramework/commit/82e39551b68e1e64d2847f581dc0ca381d894290.

---
+++ core/src/test/java/org/axonframework/commandhandling/distributed/DistributedCommandBusTest.java
@@ -0,0 +1,174 @@
+package org.axonframework.commandhandling.distributed;
+
+import org.axonframework.commandhandling.CommandCallback;
+import org.axonframework.commandhandling.CommandMessage;
+import org.axonframework.commandhandling.GenericCommandMessage;
+import org.axonframework.common.Registration;
+import org.axonframework.messaging.MessageHandler;
+import org.axonframework.monitoring.MessageMonitor;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Spy;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import java.util.Optional;
+
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Matchers.isA;
+import static org.mockito.Mockito.*;
+
+@SuppressWarnings("unchecked")
+@RunWith(MockitoJUnitRunner.class)
+public class DistributedCommandBusTest {
+
+    private DistributedCommandBus testSubject;
+
+    @Mock
+    private CommandRouter mockCommandRouter;
+    @Spy
+    private CommandBusConnector mockConnector = new StubCommandBusConnector();
+    @Mock
+    private MessageMonitor<? super CommandMessage<?>> mockMessageMonitor;
+    @Mock
+    private MessageMonitor.MonitorCallback mockMonitorCallback;
+    @Mock
+    private Member mockMember;
+
+    @Before
+    public void setUp() throws Exception {
+        testSubject = new DistributedCommandBus(mockCommandRouter, mockConnector, mockMessageMonitor);
+        when(mockCommandRouter.findDestination(any())).thenReturn(Optional.of(mockMember));
+        when(mockMessageMonitor.onMessageIngested(any())).thenReturn(mockMonitorCallback);
+    }
+
+    @Test
+    public void testDispatchWithoutCallbackWithMessageMonitor() throws Exception {
+        CommandMessage<Object> testCommandMessage = GenericCommandMessage.asCommandMessage("test");
+
+        testSubject.dispatch(testCommandMessage);
+
+        verify(mockCommandRouter).findDestination(testCommandMessage);
+        verify(mockConnector).send(eq(mockMember), eq(testCommandMessage), any(CommandCallback.class));
+        verify(mockMessageMonitor).onMessageIngested(any());
+        verify(mockMonitorCallback).reportSuccess();
+    }
+
+    @Test
+    public void testDispatchFailingCommandWithoutCallbackWithMessageMonitor() throws Exception {
+        CommandMessage<Object> testCommandMessage = GenericCommandMessage.asCommandMessage("fail");
+
+        testSubject.dispatch(testCommandMessage);
+
+        verify(mockCommandRouter).findDestination(testCommandMessage);
+        verify(mockConnector).send(eq(mockMember), eq(testCommandMessage), any(CommandCallback.class));
+        verify(mockMessageMonitor).onMessageIngested(any());
+        verify(mockMonitorCallback).reportFailure(isA(Exception.class));
+    }
+
+    @Test
+    public void testDispatchWithoutCallbackAndWithoutMessageMonitor() throws Exception {
+        testSubject = new DistributedCommandBus(mockCommandRouter, mockConnector);
+        CommandMessage<Object> testCommandMessage = GenericCommandMessage.asCommandMessage("test");
+
+        testSubject.dispatch(testCommandMessage);
+
+        verify(mockCommandRouter).findDestination(testCommandMessage);
+        verify(mockConnector, never()).send(eq(mockMember), eq(testCommandMessage), any(CommandCallback.class));
+        verify(mockConnector).send(eq(mockMember), eq(testCommandMessage));
+        verify(mockMessageMonitor, never()).onMessageIngested(any());
+        verify(mockMonitorCallback, never()).reportSuccess();
+    }
+
+    @Test
+    public void testUnknownCommandWithoutCallbackAndWithoutMessageMonitor() throws Exception {
+        testSubject = new DistributedCommandBus(mockCommandRouter, mockConnector);
+        CommandMessage<Object> testCommandMessage = GenericCommandMessage.asCommandMessage("unknown");
+        when(mockCommandRouter.findDestination(testCommandMessage)).thenReturn(Optional.empty());
+
+        try {
+            testSubject.dispatch(testCommandMessage);
+        } catch (CommandDispatchException e) {
+            assertTrue(e.getMessage().startsWith("No node known to accept"));
+        }
+
+        verify(mockCommandRouter).findDestination(testCommandMessage);
+        verify(mockConnector, never()).send(eq(mockMember), eq(testCommandMessage), any(CommandCallback.class));
+        verify(mockConnector, never()).send(eq(mockMember), eq(testCommandMessage));
+        verify(mockMessageMonitor, never()).onMessageIngested(any());
+        verify(mockMonitorCallback, never()).reportSuccess();
+    }
+
+    @Test
+    public void testDispatchWithCallbackAndMessageMonitor() throws Exception {
+        CommandMessage<Object> testCommandMessage = GenericCommandMessage.asCommandMessage("test");
+
+        CommandCallback mockCallback = mock(CommandCallback.class);
+        testSubject.dispatch(testCommandMessage, mockCallback);
+
+        verify(mockCommandRouter).findDestination(testCommandMessage);
+        verify(mockConnector).send(eq(mockMember), eq(testCommandMessage), any(CommandCallback.class));
+        verify(mockMessageMonitor).onMessageIngested(any());
+        verify(mockMonitorCallback).reportSuccess();
+        verify(mockCallback).onSuccess(testCommandMessage, null);
+    }
+
+    @Test
+    public void testUnknownCommandWithCallbackAndMessageMonitor() throws Exception {
+        CommandMessage<Object> testCommandMessage = GenericCommandMessage.asCommandMessage("test");
+        when(mockCommandRouter.findDestination(testCommandMessage)).thenReturn(Optional.empty());
+
+        CommandCallback mockCallback = mock(CommandCallback.class);
+        try {
+            testSubject.dispatch(testCommandMessage);
+        } catch (CommandDispatchException e) {
+            assertTrue(e.getMessage().startsWith("No node known to accept"));
+        }
+
+        verify(mockCommandRouter).findDestination(testCommandMessage);
+        verify(mockConnector, never()).send(eq(mockMember), eq(testCommandMessage), any(CommandCallback.class));
+        verify(mockConnector, never()).send(eq(mockMember), eq(testCommandMessage));
+        verify(mockMessageMonitor).onMessageIngested(any());
+        verify(mockMonitorCallback).reportFailure(any());
+        verify(mockCallback, never()).onFailure(any(), any());
+        verify(mockCallback, never()).onSuccess(any(), any());
+    }
+
+    @Test
+    public void testDispatchFailingCommandWithCallbackAndMessageMonitor() throws Exception {
+        CommandMessage<Object> testCommandMessage = GenericCommandMessage.asCommandMessage("fail");
+
+        CommandCallback mockCallback = mock(CommandCallback.class);
+        testSubject.dispatch(testCommandMessage, mockCallback);
+
+        verify(mockCommandRouter).findDestination(testCommandMessage);
+        verify(mockConnector).send(eq(mockMember), eq(testCommandMessage), any(CommandCallback.class));
+        verify(mockMessageMonitor).onMessageIngested(any());
+        verify(mockMonitorCallback).reportFailure(isA(Exception.class));
+        verify(mockCallback).onFailure(eq(testCommandMessage), isA(Exception.class));
+    }
+
+    private static class StubCommandBusConnector implements CommandBusConnector {
+        @Override
+        public <C> void send(Member destination, CommandMessage<? extends C> command) throws Exception {
+            //Do nothing
+        }
+
+        @Override
+        public <C, R> void send(Member destination, CommandMessage<C> command, CommandCallback<? super C, R> callback) throws Exception {
+            if ("fail".equals(command.getPayload())) {
+                callback.onFailure(command, new Exception("Failing"));
+            } else {
+                callback.onSuccess(command, null);
+            }
+        }
+
+        @Override
+        public Registration subscribe(String commandName, MessageHandler<? super CommandMessage<?>> handler) {
+            return null;
+        }
+    }
+}
