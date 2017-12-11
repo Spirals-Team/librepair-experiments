6cb7e35b8284b04fe66d8530e7e1663722a9928e
Luc Esape <luc.esape@gmail.com>
2017-12-11 01:11:27 +0100

    Bug commit from the following repository graphhopper/graphhopper This bug commit is a reflect of source code from: http://github.com/graphhopper/graphhopper/commit/e0a4cca5b5fe74307f3aa3a63783c84f4ca9efa3.

---
+++ web/src/main/java/com/graphhopper/http/RealtimeFeedConfiguration.java
@@ -0,0 +1,60 @@
+/*
+ *  Licensed to GraphHopper GmbH under one or more contributor
+ *  license agreements. See the NOTICE file distributed with this work for
+ *  additional information regarding copyright ownership.
+ *
+ *  GraphHopper GmbH licenses this file to you under the Apache License,
+ *  Version 2.0 (the "License"); you may not use this file except in
+ *  compliance with the License. You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package com.graphhopper.http;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.google.transit.realtime.GtfsRealtime;
+
+import java.io.IOException;
+import java.net.URL;
+
+public class RealtimeFeedConfiguration {
+
+    private URL url;
+    private String agencyId;
+
+    @JsonProperty
+    public URL getUrl() {
+        return url;
+    }
+
+    @JsonProperty
+    public void setUrl(URL url) {
+        this.url = url;
+    }
+
+    @JsonProperty
+    public String getAgencyId() {
+        return agencyId;
+    }
+
+    @JsonProperty
+    public void setAgencyId(String agencyId) {
+        this.agencyId = agencyId;
+    }
+
+    public GtfsRealtime.FeedMessage getFeedMessage() {
+        try {
+            return GtfsRealtime.FeedMessage.parseFrom(url.openStream());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
