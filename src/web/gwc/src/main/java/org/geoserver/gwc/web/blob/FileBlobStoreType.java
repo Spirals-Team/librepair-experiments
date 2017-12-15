db4fd57c90c10f508e13944c2856100933b1ebb2
Luc Esape <luc.esape@gmail.com>
2017-12-15 04:25:36 +0100

    Bug commit from the following repository geoserver/geoserver This bug commit is a reflect of source code from: http://github.com/geoserver/geoserver/commit/083ad0793133526bf7e481114375d7a029751d8e.

---
+++ src/web/gwc/src/main/java/org/geoserver/gwc/web/blob/FileBlobStoreType.java
@@ -0,0 +1,37 @@
+/* (c) 2017 Open Source Geospatial Foundation - all rights reserved
+ * This code is licensed under the GPL 2.0 license, available at the root
+ * application directory.
+ */
+package org.geoserver.gwc.web.blob;
+
+import org.apache.wicket.markup.html.panel.Panel;
+import org.apache.wicket.model.IModel;
+import org.geowebcache.config.FileBlobStoreConfig;
+
+public class FileBlobStoreType implements BlobStoreType<FileBlobStoreConfig> {
+    private static final long serialVersionUID = 6825505034831901062L;
+
+    @Override
+    public String toString(){
+        return "File BlobStore";
+    }
+    
+    @Override
+    public FileBlobStoreConfig newConfigObject() {
+        FileBlobStoreConfig config = new FileBlobStoreConfig();
+        config.setEnabled(true);
+        config.setFileSystemBlockSize(4096);
+        return config;
+    }
+
+    @Override
+    public Class<FileBlobStoreConfig> getConfigClass() {
+        return FileBlobStoreConfig.class;
+    }
+
+    @Override
+    public Panel createPanel(String id, IModel<FileBlobStoreConfig> model) {
+        return new FileBlobStorePanel(id, model);
+    }
+
+}
