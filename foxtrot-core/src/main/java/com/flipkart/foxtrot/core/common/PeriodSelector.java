a1b7e4caa48bb53fa640a5680942c481c9df5dc3
Luc Esape <luc.esape@gmail.com>
2018-01-03 18:20:30 +0100

    Bug commit from the following repository Flipkart/foxtrot This bug commit is a reflect of source code from: http://github.com/Flipkart/foxtrot/commit/d198ced6180a00bfdd082499e0558959ed72d333.

---
+++ foxtrot-core/src/main/java/com/flipkart/foxtrot/core/common/PeriodSelector.java
@@ -0,0 +1,94 @@
+/**
+ * Copyright 2014 Flipkart Internet Pvt. Ltd.
+ * <p/>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p/>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p/>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.flipkart.foxtrot.core.common;
+
+import com.flipkart.foxtrot.common.query.Filter;
+import com.flipkart.foxtrot.common.query.FilterVisitorAdapter;
+import com.flipkart.foxtrot.common.query.datetime.LastFilter;
+import com.flipkart.foxtrot.common.query.datetime.TimeWindow;
+import com.flipkart.foxtrot.common.query.numeric.*;
+import org.joda.time.Interval;
+
+import java.util.List;
+
+public class PeriodSelector extends FilterVisitorAdapter<Void> {
+
+    private final TimeWindow timeWindow = new TimeWindow();
+    private final List<Filter> filters;
+
+    public PeriodSelector(final List<Filter> filters) {
+        super(null);
+        this.filters = filters;
+        timeWindow.setStartTime(Long.MAX_VALUE);
+        timeWindow.setEndTime(Long.MIN_VALUE);
+    }
+
+    public Interval analyze() throws Exception {
+        return analyze(System.currentTimeMillis());
+    }
+
+    public Interval analyze(long currentTime) throws Exception {
+        for (Filter filter : filters) {
+            if (filter.isFilterTemporal()) {
+                filter.accept(this);
+            }
+        }
+        timeWindow.setStartTime(timeWindow.getStartTime() == Long.MAX_VALUE ? 0 : timeWindow.getStartTime());
+        timeWindow.setEndTime(timeWindow.getEndTime() == Long.MIN_VALUE ? currentTime : timeWindow.getEndTime());
+        return new Interval(timeWindow.getStartTime(), timeWindow.getEndTime());
+    }
+
+    @Override
+    public Void visit(BetweenFilter betweenFilter) throws Exception {
+        timeWindow.setStartTime(Math.min((Long) betweenFilter.getFrom(), timeWindow.getStartTime()));
+        timeWindow.setEndTime(Math.max((Long) betweenFilter.getTo(), timeWindow.getEndTime()));
+        return null;
+    }
+
+    @Override
+    public Void visit(GreaterThanFilter greaterThanFilter) throws Exception {
+        timeWindow.setStartTime(Math.min((Long) greaterThanFilter.getValue() + 1, timeWindow.getStartTime()));
+        return null;
+    }
+
+    @Override
+    public Void visit(GreaterEqualFilter greaterEqualFilter) throws Exception {
+        timeWindow.setStartTime(Math.min((Long) greaterEqualFilter.getValue(), timeWindow.getStartTime()));
+        return null;
+    }
+
+    @Override
+    public Void visit(LessThanFilter lessThanFilter) throws Exception {
+        timeWindow.setEndTime(Math.max((Long) lessThanFilter.getValue() - 1, timeWindow.getEndTime()));
+        return null;
+    }
+
+    @Override
+    public Void visit(LessEqualFilter lessEqualFilter) throws Exception {
+        timeWindow.setEndTime(Math.max((Long) lessEqualFilter.getValue(), timeWindow.getEndTime()));
+        return null;
+    }
+
+    @Override
+    public Void visit(LastFilter lastFilter) throws Exception {
+        TimeWindow window = lastFilter.getWindow();
+        timeWindow.setStartTime(window.getStartTime());
+        timeWindow.setEndTime(window.getEndTime());
+        return null;
+    }
+
+}
---
+++ foxtrot-server/src/test/java/com/flipkart/foxtrot/server/resources/FoxtrotResourceTest.java
@@ -0,0 +1,177 @@
+package com.flipkart.foxtrot.server.resources;
+
+import com.codahale.metrics.health.HealthCheckRegistry;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.jsontype.SubtypeResolver;
+import com.fasterxml.jackson.databind.jsontype.impl.StdSubtypeResolver;
+import com.flipkart.foxtrot.common.Table;
+import com.flipkart.foxtrot.core.MockElasticsearchServer;
+import com.flipkart.foxtrot.core.TestUtils;
+import com.flipkart.foxtrot.core.cache.CacheManager;
+import com.flipkart.foxtrot.core.cache.impl.DistributedCacheFactory;
+import com.flipkart.foxtrot.core.datastore.DataStore;
+import com.flipkart.foxtrot.core.exception.FoxtrotException;
+import com.flipkart.foxtrot.core.querystore.QueryExecutor;
+import com.flipkart.foxtrot.core.querystore.QueryStore;
+import com.flipkart.foxtrot.core.querystore.actions.spi.AnalyticsLoader;
+import com.flipkart.foxtrot.core.querystore.impl.ElasticsearchConnection;
+import com.flipkart.foxtrot.core.querystore.impl.ElasticsearchQueryStore;
+import com.flipkart.foxtrot.core.querystore.impl.HazelcastConnection;
+import com.flipkart.foxtrot.core.table.TableMetadataManager;
+import com.flipkart.foxtrot.core.table.impl.DistributedTableMetadataManager;
+import com.flipkart.foxtrot.server.config.FoxtrotServerConfiguration;
+import com.flipkart.foxtrot.server.providers.exception.FoxtrotExceptionMapper;
+import com.hazelcast.config.Config;
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.test.TestHazelcastInstanceFactory;
+import io.dropwizard.jersey.DropwizardResourceConfig;
+import io.dropwizard.jersey.setup.JerseyEnvironment;
+import io.dropwizard.jersey.validation.Validators;
+import io.dropwizard.jetty.MutableServletContextHandler;
+import io.dropwizard.lifecycle.setup.LifecycleEnvironment;
+import io.dropwizard.setup.Bootstrap;
+import io.dropwizard.setup.Environment;
+import org.junit.After;
+import org.mockito.Mockito;
+
+import java.util.UUID;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import static org.mockito.Mockito.*;
+
+/**
+ * Created by rishabh.goyal on 27/12/15.
+ */
+public abstract class FoxtrotResourceTest {
+
+    private TableMetadataManager tableMetadataManager;
+    private MockElasticsearchServer elasticsearchServer;
+    private HazelcastInstance hazelcastInstance;
+    private QueryExecutor queryExecutor;
+    private QueryStore queryStore;
+    private DataStore dataStore;
+    private static ObjectMapper mapper;
+    private CacheManager cacheManager;
+    private AnalyticsLoader analyticsLoader;
+
+
+    protected final static HealthCheckRegistry healthChecks = mock(HealthCheckRegistry.class);
+    protected final static JerseyEnvironment jerseyEnvironment = mock(JerseyEnvironment.class);
+    protected final static LifecycleEnvironment lifecycleEnvironment = new LifecycleEnvironment();
+    protected static final Environment environment = mock(Environment.class);
+    protected final static Bootstrap<FoxtrotServerConfiguration> bootstrap = mock(Bootstrap.class);
+
+    protected static final ObjectMapper objectMapper = new ObjectMapper();
+
+    static {
+        when(jerseyEnvironment.getResourceConfig()).thenReturn(new DropwizardResourceConfig());
+        when(environment.jersey()).thenReturn(jerseyEnvironment);
+        when(environment.lifecycle()).thenReturn(lifecycleEnvironment);
+        when(environment.healthChecks()).thenReturn(healthChecks);
+        when(environment.getObjectMapper()).thenReturn(objectMapper);
+        when(bootstrap.getObjectMapper()).thenReturn(objectMapper);
+        when(environment.getApplicationContext()).thenReturn(new MutableServletContextHandler());
+        when(environment.getAdminContext()).thenReturn(new MutableServletContextHandler());
+        when(environment.getValidator()).thenReturn(Validators.newValidator());
+
+        environment.getObjectMapper().setSerializationInclusion(JsonInclude.Include.NON_NULL);
+        environment.getObjectMapper().setSerializationInclusion(JsonInclude.Include.NON_EMPTY);
+
+        environment.getObjectMapper().disable(SerializationFeature.FAIL_ON_EMPTY_BEANS);
+        environment.getObjectMapper().disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+        SubtypeResolver subtypeResolver = new StdSubtypeResolver();
+        environment.getObjectMapper().setSubtypeResolver(subtypeResolver);
+        environment.jersey().register(new FoxtrotExceptionMapper(mapper));
+        mapper = environment.getObjectMapper();
+
+    }
+
+    public FoxtrotResourceTest() throws Exception {
+        try {
+            dataStore = TestUtils.getDataStore();
+        } catch (FoxtrotException e) {
+            e.printStackTrace();
+        }
+
+        Config config = new Config();
+        //Initializing Cache Factory
+        hazelcastInstance = new TestHazelcastInstanceFactory(1).newHazelcastInstance(config);
+        HazelcastConnection hazelcastConnection = Mockito.mock(HazelcastConnection.class);
+        when(hazelcastConnection.getHazelcast()).thenReturn(hazelcastInstance);
+        when(hazelcastConnection.getHazelcastConfig()).thenReturn(config);
+
+        cacheManager = new CacheManager(new DistributedCacheFactory(hazelcastConnection, mapper));
+
+        elasticsearchServer = new MockElasticsearchServer(UUID.randomUUID().toString());
+        ElasticsearchConnection elasticsearchConnection = TestUtils.initESConnection(elasticsearchServer);
+
+        tableMetadataManager = new DistributedTableMetadataManager(hazelcastConnection, elasticsearchConnection, mapper);
+        tableMetadataManager.start();
+        tableMetadataManager.save(
+                Table.builder()
+                        .name(TestUtils.TEST_TABLE_NAME)
+                        .ttl(7)
+                        .build());
+        queryStore = new ElasticsearchQueryStore(tableMetadataManager, elasticsearchConnection, dataStore, mapper);
+        queryStore = spy(queryStore);
+
+        analyticsLoader = new AnalyticsLoader(tableMetadataManager, dataStore, queryStore, elasticsearchConnection, cacheManager, mapper);
+        try {
+            analyticsLoader.start();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        try {
+            TestUtils.registerActions(analyticsLoader, mapper);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        ExecutorService executorService = Executors.newFixedThreadPool(1);
+        queryExecutor = new QueryExecutor(analyticsLoader, executorService);
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        elasticsearchServer.shutdown();
+        hazelcastInstance.shutdown();
+        tableMetadataManager.stop();
+        analyticsLoader.stop();
+    }
+
+    public TableMetadataManager getTableMetadataManager() {
+        return tableMetadataManager;
+    }
+
+    public MockElasticsearchServer getElasticsearchServer() {
+        return elasticsearchServer;
+    }
+
+    public HazelcastInstance getHazelcastInstance() {
+        return hazelcastInstance;
+    }
+
+    public QueryExecutor getQueryExecutor() {
+        return queryExecutor;
+    }
+
+    public ObjectMapper getMapper() {
+        return mapper;
+    }
+
+    public CacheManager getCacheManager() {
+        return cacheManager;
+    }
+
+    public QueryStore getQueryStore() {
+        return queryStore;
+    }
+
+    public DataStore getDataStore() {
+        return dataStore;
+    }
+}
