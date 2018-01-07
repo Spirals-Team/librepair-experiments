184e8c1764dc3b1d8d18c6abdc85ac965eaba416
Luc Esape <luc.esape@gmail.com>
2018-01-07 14:28:04 +0100

    Bug commit from the following repository prestodb/presto This bug commit is a reflect of source code from: http://github.com/prestodb/presto/commit/7086f786af9955eacbddac2b365e7fe9c6aa020c.

---
+++ presto-main/src/main/java/com/facebook/presto/operator/SimplePageComparator.java
@@ -0,0 +1,59 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.facebook.presto.operator;
+
+import com.facebook.presto.spi.Page;
+import com.facebook.presto.spi.block.SortOrder;
+import com.facebook.presto.spi.type.Type;
+import com.google.common.primitives.Ints;
+
+import java.util.List;
+
+import static com.google.common.base.Preconditions.checkArgument;
+import static java.util.Objects.requireNonNull;
+
+public class SimplePageComparator
+        implements PageComparator
+{
+    private final int[] sortChannels;
+    private final SortOrder[] sortOrders;
+    private final Type[] sortTypes;
+
+    public SimplePageComparator(List<Integer> sortChannels, List<SortOrder> sortOrders, List<Type> sortTypes)
+    {
+        this.sortChannels = Ints.toArray(requireNonNull(sortChannels, "sortChannels is null"));
+        this.sortOrders = requireNonNull(sortOrders, "sortOrders is null").toArray(new SortOrder[sortOrders.size()]);
+        this.sortTypes = requireNonNull(sortTypes, "sortTypes is null").toArray(new Type[sortTypes.size()]);
+        checkArgument(sortTypes.size() == sortChannels.size(), "sortTypes size (%s) doesn't match sortChannels size (%s)", sortTypes.size(), sortChannels.size());
+        checkArgument(sortOrders.size() == sortChannels.size(), "sortOrders size (%s) doesn't match sortChannels size (%s)", sortChannels.size(), sortChannels.size());
+    }
+
+    @Override
+    public int compareTo(Page leftPage, int leftPosition, Page rightPage, int rightPosition)
+    {
+        for (int i = 0; i < sortChannels.length; i++) {
+            int sortChannel = sortChannels[i];
+            int compare = sortOrders[i].compareBlockValue(
+                    sortTypes[i],
+                    leftPage.getBlock(sortChannel),
+                    leftPosition,
+                    rightPage.getBlock(sortChannel),
+                    rightPosition);
+            if (compare != 0) {
+                return compare;
+            }
+        }
+        return 0;
+    }
+}
