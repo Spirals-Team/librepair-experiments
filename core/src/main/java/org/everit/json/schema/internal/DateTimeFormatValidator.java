83c323369c3fbfbe175da33c292199fda1ae88c5
Luc Esape <luc.esape@gmail.com>
2018-01-10 23:25:43 +0100

    Bug commit from the following repository everit-org/json-schema This bug commit is a reflect of source code from: http://github.com/everit-org/json-schema/commit/8b762235ec39cc3cd9647e95a47267bc7defcb95.

---
+++ core/src/main/java/org/everit/json/schema/internal/DateTimeFormatValidator.java
@@ -0,0 +1,40 @@
+package org.everit.json.schema.internal;
+
+import com.google.common.collect.ImmutableList;
+
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
+import java.util.List;
+
+/**
+ * Implementation of the "date-time" format value.
+ */
+public class DateTimeFormatValidator extends TemporalFormatValidator {
+
+    private static final List<String> FORMATS_ACCEPTED = ImmutableList.of(
+            "yyyy-MM-dd'T'HH:mm:ssZ", "yyyy-MM-dd'T'HH:mm:ss.[0-9]{1,9}Z, yyyy-MM-dd'T'HH:mm:ss[+-]HH:mm",
+            "yyyy-MM-dd'T'HH:mm:ss.[0-9]{1,9}[+-]HH:mm"
+    );
+
+    private static final String PARTIAL_DATETIME_PATTERN = "yyyy-MM-dd'T'HH:mm:ss";
+
+    private static final DateTimeFormatter FORMATTER;
+
+    static {
+        final DateTimeFormatterBuilder builder = new DateTimeFormatterBuilder()
+                .appendPattern(PARTIAL_DATETIME_PATTERN)
+                .appendOptional(SECONDS_FRACTION_FORMATTER)
+                .appendPattern(ZONE_OFFSET_PATTERN);
+
+        FORMATTER = builder.toFormatter();
+    }
+
+    public DateTimeFormatValidator() {
+        super(FORMATTER, FORMATS_ACCEPTED);
+    }
+
+    @Override
+    public String formatName() {
+        return "date-time";
+    }
+}
