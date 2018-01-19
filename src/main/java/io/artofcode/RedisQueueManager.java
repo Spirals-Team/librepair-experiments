37149822c9c3048ec0aca0c46708bd614cc92b6c
Luc Esape <luc.esape@gmail.com>
2018-01-19 13:25:41 +0100

    Bug commit from the following repository n4j/jeeves This bug commit is a reflect of source code from: http://github.com/n4j/jeeves/commit/bba0d4246a3598a9052bfeee5a466d4390f3429a.

---
+++ src/main/java/io/artofcode/RedisQueueManager.java
@@ -0,0 +1,52 @@
+/*
+Copyright 2018 Neeraj Shah
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package io.artofcode;
+import static java.lang.String.*;
+import redis.clients.jedis.*;
+
+public class RedisQueueManager 
+	implements AutoCloseable {
+
+	private final String queue;
+
+	private final Jedis jedis;
+
+	public RedisQueueManager(String queue) {
+		this.queue = queue;
+		this.jedis = new Jedis("localhost");
+	}
+
+	public String get() {
+		return jedis.lpop(queue);
+	}
+
+	/**
+	 * All the workers processing a given queue are given unique ids which is used
+	 * to identify worker specific in-process queue and metadata. The id is created
+	 * by simple incrementing counter of queue:worker-ids key in Redis.
+	 *
+	 * @return Worker id created by incrementing key queue:worker-ids
+	 */
+	public long createWorkerId() {
+		return jedis.incr(format("%s:worker-ids", queue));
+	}
+
+	@Override
+	public void close() {
+		jedis.close();
+	}
+}
---
+++ src/test/java/io/artofcode/state/StateManagerTest.java
@@ -0,0 +1,79 @@
+/*
+Copyright 2018 Neeraj Shah
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package io.artofcode.state;
+
+import static java.lang.String.*;
+import java.io.File;
+import java.util.logging.Logger;
+import java.util.Map;
+import java.util.HashMap;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+public class StateManagerTest extends TestCase {
+
+    private final String queueName = "test-queue";
+
+	public StateManagerTest(String testName) { 
+		super(testName); 
+	}
+
+	public static Test suite() {
+        return new TestSuite( StateManagerTest.class );
+    }
+
+    public void testStateDirCreation() {
+        StatePersistenceManager manager = StatePersistenceManager.getInstance();
+        String stateDirPath = manager.getStateDirPath();
+        File stateDir = new File(stateDirPath);
+        
+        assertTrue( new File(stateDirPath).exists() );
+
+        logger.info(format("Path of state directory %s", stateDirPath));
+    }
+
+    public void testStatePersistence() {
+        Map<String, String> state = new HashMap<>();
+        state.put("queue-id", "495012");
+        state.put("lastjob-id", "2341324");
+        state.put("worker-name", "jeeves:url:processor");
+
+        StatePersistenceManager manager = StatePersistenceManager.getInstance();
+        manager.saveState(state, queueName);
+        
+        assertTrue( new File(manager.getQueueStatePath(queueName)).exists() );
+    }
+
+    public void testStateRead() {
+        StatePersistenceManager manager = StatePersistenceManager.getInstance();
+        Map<String, String> state = manager.getState(queueName);
+
+        for(Map.Entry<String, String> entry : state.entrySet()) {
+            logger.info(format("[%s]=%s", entry.getKey(), entry.getValue()));
+        }
+
+        assertTrue( state!= null );
+        assertTrue(state.get("queue-id").equals("495012"));
+        assertTrue(state.get("lastjob-id").equals("2341324"));
+        assertTrue(state.get("worker-name").equals("jeeves:url:processor"));
+    }
+
+
+    private final Logger logger = Logger.getLogger(StateManagerTest.class.toString());
+}
