b6001bffc56b15f9b3622f5b53ad62c9929a2152
Luc Esape <luc.esape@gmail.com>
2018-01-22 12:14:43 +0100

    Bug commit from the following repository algolia/algoliasearch-client-java-2 This bug commit is a reflect of source code from: http://github.com/algolia/algoliasearch-client-java-2/commit/3ef2e4b4d110573bdfc781c777a2f151737121c1.

---
+++ algoliasearch/src/main/java/com/algolia/search/ApacheAPIClientBuilder.java
@@ -0,0 +1,72 @@
+package com.algolia.search;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.List;
+import javax.annotation.Nonnull;
+
+public final class ApacheAPIClientBuilder extends APIClientBuilder {
+
+  public ApacheAPIClientBuilder(@Nonnull String applicationId, @Nonnull String apiKey) {
+    super(applicationId, apiKey);
+  }
+
+  @Override
+  public ApacheAPIClientBuilder setUserAgent(
+      @Nonnull String customAgent, @Nonnull String customAgentVersion) {
+    super.setUserAgent(customAgent, customAgentVersion);
+    return this;
+  }
+
+  @Override
+  public ApacheAPIClientBuilder setExtraHeader(@Nonnull String key, String value) {
+    super.setExtraHeader(key, value);
+    return this;
+  }
+
+  @Override
+  public ApacheAPIClientBuilder setConnectTimeout(int connectTimeout) {
+    super.setConnectTimeout(connectTimeout);
+    return this;
+  }
+
+  @Override
+  public ApacheAPIClientBuilder setReadTimeout(int readTimeout) {
+    super.setReadTimeout(readTimeout);
+    return this;
+  }
+
+  @Override
+  public ApacheAPIClientBuilder setHostDownTimeout(int hostDownTimeout) {
+    super.setHostDownTimeout(hostDownTimeout);
+    return this;
+  }
+
+  @Override
+  public ApacheAPIClientBuilder setObjectMapper(@Nonnull ObjectMapper objectMapper) {
+    super.setObjectMapper(objectMapper);
+    return this;
+  }
+
+  @Override
+  public ApacheAPIClientBuilder setQueryHosts(List<String> queryHosts) {
+    super.setQueryHosts(queryHosts);
+    return this;
+  }
+
+  @Override
+  public ApacheAPIClientBuilder setBuildHosts(List<String> buildHosts) {
+    super.setBuildHosts(buildHosts);
+    return this;
+  }
+
+  @Override
+  public ApacheAPIClientBuilder setMaxConnTotal(int maxConnTotal) {
+    super.setMaxConnTotal(maxConnTotal);
+    return this;
+  }
+
+  @Override
+  protected APIClient build(@Nonnull APIClientConfiguration configuration) {
+    return new APIClient(new ApacheHttpClient(configuration), configuration);
+  }
+}
