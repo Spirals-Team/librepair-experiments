ec4834a8949ad402a805eb18fcd7fdee08f7afdf
Luc Esape <luc.esape@gmail.com>
2018-01-23 12:57:11 +0100

    Bug commit from the following repository proteus-h2020/proteus-web This bug commit is a reflect of source code from: http://github.com/proteus-h2020/proteus-web/commit/c176a7a22887550a5fb748b86f7df81a7e418010.

---
+++ proteus-spring-dashboard/src/app/pages/pages.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { Routes } from '@angular/router';
+
+import { BaMenuService } from '../theme';
+import { PAGES_MENU } from './pages.menu';
+
+@Component({
+  selector: 'pages',
+  templateUrl: './pages.html'
+})
+export class Pages implements OnInit {
+
+  constructor(private _menuService: BaMenuService) {
+  }
+
+  ngOnInit() {
+    this._menuService.updateMenuByRoutes(<Routes>PAGES_MENU);
+  }
+}
