bac16ac927e4b623f2533d7a4e73f54c613a5bb8
Luc Esape <luc.esape@gmail.com>
2018-01-23 11:09:37 +0100

    Bug commit from the following repository algolia/algoliasearch-client-java-2 This bug commit is a reflect of source code from: http://github.com/algolia/algoliasearch-client-java-2/commit/619d76712ce48478b939c1505c9e5d8ba0ab9868.

---
+++ algoliasearch-tests/src/test/java/com/algolia/search/integration/common/async/AsyncApiKeysTest.java
@@ -0,0 +1,116 @@
+package com.algolia.search.integration.common.async;
+
+import static java.util.concurrent.TimeUnit.SECONDS;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.algolia.search.AlgoliaObject;
+import com.algolia.search.AsyncAlgoliaIntegrationTest;
+import com.algolia.search.AsyncIndex;
+import com.algolia.search.objects.ApiKey;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import javax.annotation.Nonnull;
+import org.junit.Test;
+
+public abstract class AsyncApiKeysTest extends AsyncAlgoliaIntegrationTest {
+
+  private void waitForKeyPresent(AsyncIndex<AlgoliaObject> index, @Nonnull String description)
+      throws Exception {
+    for (int i = 0; i < 100; i++) {
+      Thread.sleep(1000);
+      CompletableFuture<List<ApiKey>> apiKeys =
+          index == null ? client.listApiKeys() : index.listApiKeys();
+      boolean found =
+          apiKeys
+              .get(WAIT_TIME_IN_SECONDS, SECONDS)
+              .stream()
+              .map(ApiKey::getDescription)
+              .anyMatch(description::equals);
+      if (found) {
+        return;
+      }
+    }
+
+    // will fail
+    futureAssertThat(client.listApiKeys()).extracting("description").contains(description);
+  }
+
+  private void waitForKeyNotPresent(AsyncIndex<AlgoliaObject> index, String description)
+      throws Exception {
+    for (int i = 0; i < 100; i++) {
+      Thread.sleep(1000);
+      CompletableFuture<List<ApiKey>> apiKeys =
+          index == null ? client.listApiKeys() : index.listApiKeys();
+      boolean found =
+          apiKeys
+              .get(WAIT_TIME_IN_SECONDS, SECONDS)
+              .stream()
+              .map(ApiKey::getDescription)
+              .anyMatch(k -> k.equals(description));
+      if (!found) {
+        return;
+      }
+    }
+
+    // will fail
+    futureAssertThat(client.listApiKeys()).extracting("description").doesNotContain(description);
+  }
+
+  @SuppressWarnings({"OptionalGetWithoutIsPresent", "ConstantConditions"})
+  @Test
+  public void manageKeys() throws Exception {
+    AsyncIndex<AlgoliaObject> index = createIndex(AlgoliaObject.class);
+    // Fill index
+    waitForCompletion(index.addObject(new AlgoliaObject("1", 1)));
+
+    ApiKey apiKey =
+        new ApiKey()
+            .setDescription("toto" + System.currentTimeMillis())
+            .setIndexes(Collections.singletonList(index.getName()));
+
+    String keyName = client.addApiKey(apiKey).get().getKey();
+    assertThat(keyName).isNotNull();
+
+    waitForKeyPresent(null, apiKey.getDescription());
+
+    apiKey = apiKey.setDescription("toto2" + System.currentTimeMillis());
+    client.updateApiKey(keyName, apiKey).get();
+
+    waitForKeyPresent(null, apiKey.getDescription());
+
+    assertThat(client.getApiKey(keyName).get().get().getDescription())
+        .isEqualTo(apiKey.getDescription());
+
+    client.deleteApiKey(keyName).get();
+
+    waitForKeyNotPresent(null, apiKey.getDescription());
+  }
+
+  @SuppressWarnings({"OptionalGetWithoutIsPresent", "ConstantConditions"})
+  @Test
+  public void manageKeysForIndex() throws Exception {
+    // Fill index
+    AsyncIndex<AlgoliaObject> index = createIndex(AlgoliaObject.class);
+    waitForCompletion(index.addObject(new AlgoliaObject("1", 1)));
+
+    ApiKey apiKey = new ApiKey().setDescription("toto" + System.currentTimeMillis());
+
+    String keyName = index.addApiKey(apiKey).get().getKey();
+    assertThat(keyName).isNotNull();
+
+    waitForKeyPresent(index, apiKey.getDescription());
+
+    apiKey = apiKey.setDescription("toto2" + System.currentTimeMillis());
+    index.updateApiKey(keyName, apiKey).get();
+
+    waitForKeyPresent(index, apiKey.getDescription());
+
+    assertThat(index.getApiKey(keyName).get().get().getDescription())
+        .isEqualTo(apiKey.getDescription());
+
+    index.deleteApiKey(keyName).get();
+
+    waitForKeyNotPresent(index, apiKey.getDescription());
+  }
+}
---
+++ algoliasearch/src/main/java/com/algolia/search/ApacheHttpClient.java
@@ -0,0 +1,137 @@
+package com.algolia.search;
+
+import com.algolia.search.exceptions.AlgoliaException;
+import com.algolia.search.http.AlgoliaHttpClient;
+import com.algolia.search.http.AlgoliaHttpRequest;
+import com.algolia.search.http.AlgoliaHttpResponse;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.nio.charset.Charset;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import javax.annotation.Nonnull;
+import org.apache.http.Header;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.RequestBuilder;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicHeader;
+
+public class ApacheHttpClient extends AlgoliaHttpClient {
+
+  private static final Charset UTF8 = Charset.forName("UTF-8");
+  private final CloseableHttpClient internal;
+  private final ObjectMapper objectMapper;
+  private final List<String> queryHosts;
+  private final List<String> buildHosts;
+  private final int hostDownTimeout;
+
+  public ApacheHttpClient(APIClientConfiguration configuration) {
+    logger.debug("Create ApacheHttpClient with configuration {}", configuration);
+
+    List<Header> httpHeaders =
+        configuration
+            .getHeaders()
+            .entrySet()
+            .stream()
+            .map(e -> new BasicHeader(e.getKey(), e.getValue()))
+            .collect(Collectors.toList());
+    RequestConfig requestConfig =
+        RequestConfig.custom()
+            .setConnectTimeout(configuration.getConnectTimeout())
+            .setSocketTimeout(configuration.getReadTimeout())
+            .setConnectionRequestTimeout(configuration.getConnectTimeout())
+            .build();
+
+    this.internal =
+        HttpClients.custom()
+            .disableAutomaticRetries()
+            .setDefaultHeaders(httpHeaders)
+            .setDnsResolver(new TimeoutableHostNameResolver(configuration.getConnectTimeout()))
+            .setDefaultRequestConfig(requestConfig)
+            .setMaxConnTotal(configuration.getMaxConnTotal())
+            .build();
+
+    this.objectMapper = configuration.getObjectMapper();
+    this.queryHosts = configuration.getQueryHosts();
+    this.buildHosts = configuration.getBuildHosts();
+    this.hostDownTimeout = configuration.getHostDownTimeout();
+  }
+
+  @Override
+  protected AlgoliaHttpResponse request(@Nonnull AlgoliaHttpRequest request) throws IOException {
+    RequestBuilder builder =
+        RequestBuilder.create(request.getMethod().name)
+            .setUri("https://" + request.getHost() + "/" + String.join("/", request.getPath()));
+
+    for (Map.Entry<String, String> entry : request.getParameters().entrySet()) {
+      builder = builder.addParameter(entry.getKey(), entry.getValue());
+    }
+
+    for (Map.Entry<String, String> entry : request.getHeaders().entrySet()) {
+      builder = builder.addHeader(entry.getKey(), entry.getValue());
+    }
+
+    if (request.getContent() != null) {
+      builder =
+          builder.setEntity(new StringEntity(request.getContent(), ContentType.APPLICATION_JSON));
+    } else {
+      builder = builder.setEntity(new StringEntity("", ContentType.APPLICATION_JSON));
+    }
+    final CloseableHttpResponse response = internal.execute(builder.build());
+
+    return new AlgoliaHttpResponse() {
+      @Override
+      public void close() throws IOException {
+        response.close();
+      }
+
+      @Override
+      public int getStatusCode() {
+        return response.getStatusLine().getStatusCode();
+      }
+
+      @Override
+      public Reader getBody() throws IOException {
+        return new InputStreamReader(response.getEntity().getContent(), UTF8);
+      }
+    };
+  }
+
+  @Override
+  protected ObjectMapper getObjectMapper() {
+    return objectMapper;
+  }
+
+  @Override
+  public List<String> getQueryHosts() {
+    return queryHosts;
+  }
+
+  @Override
+  public List<String> getBuildHosts() {
+    return buildHosts;
+  }
+
+  @Override
+  public int getHostDownTimeout() {
+    return hostDownTimeout;
+  }
+
+  @Override
+  public void close() throws AlgoliaException {
+    logger.debug("Closing ApacheHttpClient");
+    try {
+      internal.close();
+    } catch (IOException e) {
+      logger.debug("Couldn't close ApacheHttpClient", e);
+      throw new AlgoliaException("Couldn't close ApacheHttpClient", e);
+    }
+  }
+}
