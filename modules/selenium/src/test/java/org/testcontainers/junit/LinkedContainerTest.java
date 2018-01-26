c61c2d0a85969e0c28ddb0b87ac351fa069d1151
Luc Esape <luc.esape@gmail.com>
2018-01-27 00:31:51 +0100

    Bug commit from the following repository testcontainers/testcontainers-java This bug commit is a reflect of source code from: http://github.com/testcontainers/testcontainers-java/commit/590bd764c46dd4d195cbd61761c2696ac4ff1c6e.

---
+++ modules/selenium/src/test/java/org/testcontainers/junit/LinkedContainerTest.java
@@ -0,0 +1,59 @@
+package org.testcontainers.junit;
+
+import lombok.Cleanup;
+import org.junit.BeforeClass;
+import org.junit.Rule;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testcontainers.containers.BrowserWebDriverContainer;
+import org.testcontainers.containers.NginxContainer;
+
+import java.io.*;
+
+import static org.rnorth.visibleassertions.VisibleAssertions.*;
+
+
+/**
+ * @author richardnorth
+ */
+public class LinkedContainerTest {
+
+    private static File contentFolder = new File(System.getProperty("user.home") + "/.tmp-test-container");
+
+    @Rule
+    public NginxContainer nginx = new NginxContainer()
+            .withCustomContent(contentFolder.toString());
+
+    @Rule
+    public BrowserWebDriverContainer chrome = new BrowserWebDriverContainer()
+            .withDesiredCapabilities(DesiredCapabilities.chrome())
+            .withLinkToContainer(nginx, "nginx");
+
+    @BeforeClass
+    public static void setupContent() throws FileNotFoundException {
+        contentFolder.mkdir();
+        contentFolder.setReadable(true, false);
+        contentFolder.setWritable(true, false);
+        contentFolder.setExecutable(true, false);
+
+
+        File indexFile = new File(contentFolder, "index.html");
+        indexFile.setReadable(true, false);
+        indexFile.setWritable(true, false);
+        indexFile.setExecutable(true, false);
+
+        @Cleanup PrintStream printStream = new PrintStream(new FileOutputStream(indexFile));
+        printStream.println("<html><body>This worked</body></html>");
+    }
+
+    @Test
+    public void testWebDriverToNginxContainerAccessViaContainerLink() throws Exception {
+        RemoteWebDriver driver = chrome.getWebDriver();
+
+        driver.get("http://nginx/");
+
+        assertEquals("Using selenium, an HTTP GET from the nginx server returns the index.html from the custom content directory", "This worked", driver.findElement(By.tagName("body")).getText());
+    }
+}
