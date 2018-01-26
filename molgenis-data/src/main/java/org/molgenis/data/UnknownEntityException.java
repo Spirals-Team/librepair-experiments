32be8cba89ec34f4c62e06f714dcf4e325d70600
Luc Esape <luc.esape@gmail.com>
2018-01-26 13:28:40 +0100

    Bug commit from the following repository fdlk/molgenis This bug commit is a reflect of source code from: http://github.com/fdlk/molgenis/commit/9a5ad0491acacf2de7a46e0d7ea970288926c446.

---
+++ molgenis-data/src/main/java/org/molgenis/data/UnknownEntityException.java
@@ -0,0 +1,39 @@
+package org.molgenis.data;
+
+import org.molgenis.data.meta.model.EntityType;
+
+import static java.util.Objects.requireNonNull;
+
+@SuppressWarnings({ "squid:MaximumInheritanceDepth" })
+public class UnknownEntityException extends UnknownDataException
+{
+	private static final String ERROR_CODE = "D02";
+
+	private final transient EntityType entityType;
+
+	private final transient Object entityId;
+
+	public UnknownEntityException(EntityType entityType, Object entityId)
+	{
+		super(ERROR_CODE);
+		this.entityType = requireNonNull(entityType);
+		this.entityId = requireNonNull(entityId);
+	}
+
+	public Object getEntityId()
+	{
+		return entityId;
+	}
+
+	@Override
+	public String getMessage()
+	{
+		return String.format("type:%s id:%s", entityType.getId(), entityId.toString());
+	}
+
+	@Override
+	protected Object[] getLocalizedMessageArguments()
+	{
+		return new Object[] { entityType, entityId };
+	}
+}
