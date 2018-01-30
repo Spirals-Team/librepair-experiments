1445695fb1924f0cdaca683506ecfb5f10f82b1d
Luc Esape <luc.esape@gmail.com>
2018-01-30 23:39:37 +0100

    Bug commit from the following repository prestodb/presto This bug commit is a reflect of source code from: http://github.com/prestodb/presto/commit/37107ba7d0c4f3c711b5af7ee3dfcb2f239d8051.

---
+++ presto-tests/src/main/java/com/facebook/presto/tests/statistics/Metric.java
@@ -0,0 +1,37 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.facebook.presto.tests.statistics;
+
+import com.facebook.presto.cost.PlanNodeStatsEstimate;
+import com.facebook.presto.spi.statistics.Estimate;
+
+import java.util.function.Function;
+
+public enum Metric
+{
+    OUTPUT_ROW_COUNT(PlanNodeStatsEstimate::getOutputRowCount),
+    OUTPUT_SIZE_BYTES(PlanNodeStatsEstimate::getOutputSizeInBytes);
+
+    private final Function<PlanNodeStatsEstimate, Estimate> extractor;
+
+    Metric(Function<PlanNodeStatsEstimate, Estimate> extractor)
+    {
+        this.extractor = extractor;
+    }
+
+    Estimate getValue(PlanNodeStatsEstimate stats)
+    {
+        return extractor.apply(stats);
+    }
+}
