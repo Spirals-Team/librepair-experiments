066a5998736fc4ef395beea79a94a98283a89434
Luc Esape <luc.esape@gmail.com>
2018-01-31 14:59:52 +0100

    Bug commit from the following repository prestodb/presto This bug commit is a reflect of source code from: http://github.com/prestodb/presto/commit/b4d1d26d7c664a83290da73d0aca6e923bc50b7c.

---
+++ presto-main/src/main/java/com/facebook/presto/cost/CoefficientBasedStatsCalculator.java
@@ -0,0 +1,237 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.facebook.presto.cost;
+
+import com.facebook.presto.Session;
+import com.facebook.presto.metadata.Metadata;
+import com.facebook.presto.spi.ColumnHandle;
+import com.facebook.presto.spi.Constraint;
+import com.facebook.presto.spi.statistics.TableStatistics;
+import com.facebook.presto.spi.type.Type;
+import com.facebook.presto.sql.planner.Symbol;
+import com.facebook.presto.sql.planner.plan.EnforceSingleRowNode;
+import com.facebook.presto.sql.planner.plan.ExchangeNode;
+import com.facebook.presto.sql.planner.plan.FilterNode;
+import com.facebook.presto.sql.planner.plan.JoinNode;
+import com.facebook.presto.sql.planner.plan.LimitNode;
+import com.facebook.presto.sql.planner.plan.OutputNode;
+import com.facebook.presto.sql.planner.plan.PlanNode;
+import com.facebook.presto.sql.planner.plan.PlanNodeId;
+import com.facebook.presto.sql.planner.plan.PlanVisitor;
+import com.facebook.presto.sql.planner.plan.ProjectNode;
+import com.facebook.presto.sql.planner.plan.SemiJoinNode;
+import com.facebook.presto.sql.planner.plan.TableScanNode;
+import com.facebook.presto.sql.planner.plan.ValuesNode;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Iterables;
+
+import javax.annotation.concurrent.ThreadSafe;
+import javax.inject.Inject;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import static com.facebook.presto.cost.PlanNodeStatsEstimate.UNKNOWN_STATS;
+import static java.util.Objects.requireNonNull;
+
+/**
+ * Simple implementation of StatsCalculator. It make many arbitrary decisions (e.g filtering selectivity, join matching).
+ * It serves POC purpose. To be replaced with more advanced implementation.
+ */
+@ThreadSafe
+public class CoefficientBasedStatsCalculator
+        implements StatsCalculator
+{
+    private static final Double FILTER_COEFFICIENT = 0.5;
+    private static final Double JOIN_MATCHING_COEFFICIENT = 2.0;
+
+    // todo some computation for outputSizeInBytes
+
+    private final Metadata metadata;
+
+    @Inject
+    public CoefficientBasedStatsCalculator(Metadata metadata)
+    {
+        this.metadata = metadata;
+    }
+
+    @Override
+    public Map<PlanNodeId, PlanNodeStatsEstimate> calculateStatsForPlan(Session session, Map<Symbol, Type> types, PlanNode node)
+    {
+        Visitor visitor = new Visitor(session, types);
+        node.accept(visitor, null);
+        return ImmutableMap.copyOf(visitor.getStats());
+    }
+
+    private class Visitor
+            extends PlanVisitor<PlanNodeStatsEstimate, Void>
+    {
+        private final Session session;
+        private final Map<Symbol, Type> types;
+        private final Map<PlanNodeId, PlanNodeStatsEstimate> stats;
+
+        public Visitor(Session session, Map<Symbol, Type> types)
+        {
+            this.session = requireNonNull(session, "session is null");
+            this.types = ImmutableMap.copyOf(types);
+            this.stats = new HashMap<>();
+        }
+
+        public Map<PlanNodeId, PlanNodeStatsEstimate> getStats()
+        {
+            return ImmutableMap.copyOf(stats);
+        }
+
+        @Override
+        protected PlanNodeStatsEstimate visitPlan(PlanNode node, Void context)
+        {
+            visitSources(node);
+            stats.put(node.getId(), UNKNOWN_STATS);
+            return UNKNOWN_STATS;
+        }
+
+        @Override
+        public PlanNodeStatsEstimate visitOutput(OutputNode node, Void context)
+        {
+            return copySourceStats(node);
+        }
+
+        @Override
+        public PlanNodeStatsEstimate visitFilter(FilterNode node, Void context)
+        {
+            PlanNodeStatsEstimate sourceStats = visitSource(node);
+            PlanNodeStatsEstimate filterStats = sourceStats.mapOutputRowCount(value -> value * (double) FILTER_COEFFICIENT);
+            stats.put(node.getId(), filterStats);
+            return filterStats;
+        }
+
+        @Override
+        public PlanNodeStatsEstimate visitProject(ProjectNode node, Void context)
+        {
+            return copySourceStats(node);
+        }
+
+        @Override
+        public PlanNodeStatsEstimate visitJoin(JoinNode node, Void context)
+        {
+            List<PlanNodeStatsEstimate> sourceStats = visitSources(node);
+            PlanNodeStatsEstimate leftStats = sourceStats.get(0);
+            PlanNodeStatsEstimate rightStats = sourceStats.get(1);
+
+            PlanNodeStatsEstimate.Builder joinStats = PlanNodeStatsEstimate.builder();
+            double rowCount = Math.max(leftStats.getOutputRowCount(), rightStats.getOutputRowCount()) * JOIN_MATCHING_COEFFICIENT;
+            joinStats.setOutputRowCount(rowCount);
+            stats.put(node.getId(), joinStats.build());
+            return joinStats.build();
+        }
+
+        @Override
+        public PlanNodeStatsEstimate visitExchange(ExchangeNode node, Void context)
+        {
+            List<PlanNodeStatsEstimate> sourceStats = visitSources(node);
+            double rowCount = 0;
+            for (PlanNodeStatsEstimate sourceStat : sourceStats) {
+                rowCount = rowCount + sourceStat.getOutputRowCount();
+            }
+
+            PlanNodeStatsEstimate exchangeStats = PlanNodeStatsEstimate.builder()
+                    .setOutputRowCount(rowCount)
+                    .build();
+            stats.put(node.getId(), exchangeStats);
+            return exchangeStats;
+        }
+
+        @Override
+        public PlanNodeStatsEstimate visitTableScan(TableScanNode node, Void context)
+        {
+            Constraint<ColumnHandle> constraint = new Constraint<>(node.getCurrentConstraint(), bindings -> true);
+            TableStatistics tableStatistics = metadata.getTableStatistics(session, node.getTable(), constraint);
+            PlanNodeStatsEstimate tableScanStats = PlanNodeStatsEstimate.builder()
+                    .setOutputRowCount(tableStatistics.getRowCount().getValue())
+                    .build();
+
+            stats.put(node.getId(), tableScanStats);
+            return tableScanStats;
+        }
+
+        @Override
+        public PlanNodeStatsEstimate visitValues(ValuesNode node, Void context)
+        {
+            int valuesCount = node.getRows().size();
+            PlanNodeStatsEstimate valuesStats = PlanNodeStatsEstimate.builder()
+                    .setOutputRowCount(valuesCount)
+                    .build();
+            stats.put(node.getId(), valuesStats);
+            return valuesStats;
+        }
+
+        @Override
+        public PlanNodeStatsEstimate visitEnforceSingleRow(EnforceSingleRowNode node, Void context)
+        {
+            visitSources(node);
+            PlanNodeStatsEstimate nodeStats = PlanNodeStatsEstimate.builder()
+                    .setOutputRowCount(1.0)
+                    .build();
+            stats.put(node.getId(), nodeStats);
+            return nodeStats;
+        }
+
+        @Override
+        public PlanNodeStatsEstimate visitSemiJoin(SemiJoinNode node, Void context)
+        {
+            visitSources(node);
+            PlanNodeStatsEstimate sourceStatitics = stats.get(node.getSource().getId());
+            PlanNodeStatsEstimate semiJoinStats = sourceStatitics.mapOutputRowCount(rowCount -> rowCount * JOIN_MATCHING_COEFFICIENT);
+            stats.put(node.getId(), semiJoinStats);
+            return semiJoinStats;
+        }
+
+        @Override
+        public PlanNodeStatsEstimate visitLimit(LimitNode node, Void context)
+        {
+            PlanNodeStatsEstimate sourceStats = visitSource(node);
+            PlanNodeStatsEstimate.Builder limitStats = PlanNodeStatsEstimate.builder();
+            if (sourceStats.getOutputRowCount() < node.getCount()) {
+                limitStats.setOutputRowCount(sourceStats.getOutputRowCount());
+            }
+            else {
+                limitStats.setOutputRowCount(node.getCount());
+            }
+            stats.put(node.getId(), limitStats.build());
+            return limitStats.build();
+        }
+
+        private PlanNodeStatsEstimate copySourceStats(PlanNode node)
+        {
+            PlanNodeStatsEstimate sourceStats = visitSource(node);
+            stats.put(node.getId(), sourceStats);
+            return sourceStats;
+        }
+
+        private List<PlanNodeStatsEstimate> visitSources(PlanNode node)
+        {
+            return node.getSources().stream()
+                    .map(source -> source.accept(this, null))
+                    .collect(Collectors.toList());
+        }
+
+        private PlanNodeStatsEstimate visitSource(PlanNode node)
+        {
+            return Iterables.getOnlyElement(visitSources(node));
+        }
+    }
+}
---
+++ presto-tests/src/main/java/com/facebook/presto/tests/statistics/MetricComparison.java
@@ -0,0 +1,84 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.facebook.presto.tests.statistics;
+
+import com.facebook.presto.sql.planner.plan.PlanNode;
+
+import java.util.Optional;
+
+import static com.facebook.presto.tests.statistics.MetricComparison.Result.DIFFER;
+import static com.facebook.presto.tests.statistics.MetricComparison.Result.MATCH;
+import static com.facebook.presto.tests.statistics.MetricComparison.Result.NO_BASELINE;
+import static com.facebook.presto.tests.statistics.MetricComparison.Result.NO_ESTIMATE;
+import static java.lang.String.format;
+import static java.util.Objects.requireNonNull;
+
+public class MetricComparison
+{
+    private final PlanNode planNode;
+    private final Metric metric;
+    private final Optional<Double> estimatedCost;
+    private final Optional<Double> executionCost;
+
+    public MetricComparison(PlanNode planNode, Metric metric, Optional<Double> estimatedCost, Optional<Double> executionCost)
+    {
+        this.planNode = planNode;
+        this.metric = metric;
+        this.estimatedCost = estimatedCost;
+        this.executionCost = executionCost;
+    }
+
+    public Metric getMetric()
+    {
+        return metric;
+    }
+
+    public PlanNode getPlanNode()
+    {
+        return planNode;
+    }
+
+    @Override
+    public String toString()
+    {
+        return format("Metric [%s] - estimated: [%s], real: [%s] - plan node: [%s]",
+                metric, print(estimatedCost), print(executionCost), planNode);
+    }
+
+    public Result result(MetricComparisonStrategy metricComparisonStrategy)
+    {
+        requireNonNull(metricComparisonStrategy, "metricComparisonStrategy is null");
+
+        if (!estimatedCost.isPresent()) {
+            return NO_ESTIMATE;
+        }
+        if (!executionCost.isPresent()) {
+            return NO_BASELINE;
+        }
+        return metricComparisonStrategy.matches(executionCost.get(), estimatedCost.get()) ? MATCH : DIFFER;
+    }
+
+    private String print(Optional<Double> cost)
+    {
+        return cost.map(Object::toString).orElse("UNKNOWN");
+    }
+
+    public enum Result
+    {
+        NO_ESTIMATE,
+        NO_BASELINE,
+        DIFFER,
+        MATCH
+    }
+}
