704e274278e2f238f95c1b63f3ca8268747b9614
Luc Esape <luc.esape@gmail.com>
2018-02-24 17:14:28 +0100

    Bug commit from the following repository zentol/flink This bug commit is a reflect of source code from: http://github.com/zentol/flink/commit/e676b233abf71d8155d2389676be0bef1a3c5992.

---
+++ tools/travis_controller.sh
@@ -0,0 +1,83 @@
+#!/usr/bin/env bash
+################################################################################
+#  Licensed to the Apache Software Foundation (ASF) under one
+#  or more contributor license agreements.  See the NOTICE file
+#  distributed with this work for additional information
+#  regarding copyright ownership.  The ASF licenses this file
+#  to you under the Apache License, Version 2.0 (the
+#  "License"); you may not use this file except in compliance
+#  with the License.  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+# limitations under the License.
+################################################################################
+
+set -x
+
+BASE_DIR="$HOME/test_directory/$TRAVIS_BUILD_ID"
+FLINK_DIR="$BASE_DIR/flink"
+MARKER_DIR="$BASE_DIR/markers"
+
+function handleError() {
+	echo "Error on line $2"
+	cleanUpCache
+	exit "$1"
+}
+
+function cleanUpCache() {
+	echo "cleaning up $BASE_DIR"
+	rm -rf "$BASE_DIR"
+}
+
+trap 'handleError $? $LINENO' ERR
+
+if ! [ -d "$BASE_DIR" ]; then
+    echo "Creating test build directories"
+    mkdir -p "$FLINK_DIR"
+    mkdir -p "$MARKER_DIR"
+
+	cp -r . "$FLINK_DIR"
+fi
+
+function getLatestMarker() {
+	for stage in "compile" "core" "libraries" "connectors" "tests" "mist"
+	do
+		if ! [ -e "$MARKER_DIR/marker_$stage" ]; then
+			echo "$stage"
+			return 0
+		fi
+	done
+
+	echo "cleanup"
+	return 0
+}
+
+STAGE=$(getLatestMarker)
+echo "In stage $STAGE"
+
+EXIT_CODE=0
+
+case $STAGE in
+	(cleanup)
+		cleanUpCache
+		;;
+	(*)
+		"$FLINK_DIR/tools/travis_mvn_watchdog.sh" $STAGE 300
+		# if this returns an error the trap will fire and cleanup all files
+		EXIT_CODE=$?
+
+		if [ $STAGE != 255 ]; then
+			touch "$MARKER_DIR/marker_$STAGE"
+			ls "$BASE_DIR"
+		fi
+
+		;;
+esac
+
+# Exit code for Travis build success/failure
+exit $EXIT_CODE
