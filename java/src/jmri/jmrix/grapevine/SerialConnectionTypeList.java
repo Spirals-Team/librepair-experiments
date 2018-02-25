6c67099daefef1a028f303ef9dca73342e347d7f
Luc Esape <luc.esape@gmail.com>
2018-02-25 07:27:17 +0100

    Bug commit from the following repository JMRI/JMRI This bug commit is a reflect of source code from: http://github.com/JMRI/JMRI/commit/5942255e3fc400897c18f235190659297f0bc921.

---
+++ java/src/jmri/jmrix/grapevine/SerialConnectionTypeList.java
@@ -0,0 +1,30 @@
+package jmri.jmrix.grapevine;
+
+import jmri.jmrix.ConnectionTypeList;
+import org.openide.util.lookup.ServiceProvider;
+
+/**
+ * Return a list of valid Protrak (Grapevine) Connection Types.
+ *
+ * @author Bob Jacobsen Copyright (C) 2010
+ * @author Kevin Dickerson Copyright (C) 2010
+  *
+ */
+@ServiceProvider(service = ConnectionTypeList.class)
+public class SerialConnectionTypeList implements jmri.jmrix.ConnectionTypeList {
+
+    public static final String PROTRAK = "Protrak";
+
+    @Override
+    public String[] getAvailableProtocolClasses() {
+        return new String[]{
+            "jmri.jmrix.grapevine.serialdriver.ConnectionConfig"
+        };
+    }
+
+    @Override
+    public String[] getManufacturers() {
+        return new String[]{PROTRAK};
+    }
+
+}
