ebecad64fc2a2b74dd734378940ec56322dae861
Luc Esape <luc.esape@gmail.com>
2018-02-25 04:18:07 +0100

    Bug commit from the following repository SpoonLabs/astor This bug commit is a reflect of source code from: http://github.com/SpoonLabs/astor/commit/e29021e24ecf5bd37ba3fe530ef8d897bca0be21.

---
+++ src/main/java/fr/inria/astor/core/entities/Ingredient.java
@@ -0,0 +1,65 @@
+package fr.inria.astor.core.entities;
+
+import fr.inria.astor.core.loop.spaces.ingredients.scopes.IngredientSpaceScope;
+import spoon.reflect.declaration.CtElement;
+
+/**
+ * 
+ * @author Matias Martinez matias.martinez@inria.fr
+ *
+ */
+public class Ingredient {
+	
+	protected CtElement code;
+	protected IngredientSpaceScope scope;
+	protected CtElement derivedFrom;
+	
+	public Ingredient(CtElement code, IngredientSpaceScope scope, CtElement derivedFrom) {
+		super();
+		this.code = code;
+		this.derivedFrom = derivedFrom;
+		this.scope = scope;
+	}
+
+	public Ingredient(CtElement element) {
+		super();
+		this.code = element;
+	}
+	
+	public Ingredient(CtElement element, IngredientSpaceScope scope) {
+		super();
+		this.code = element;
+		this.scope = scope;
+	}
+
+	public CtElement getCode() {
+		return code;
+	}
+
+	public void setCode(CtElement element) {
+		this.code = element;
+	}
+
+	public IngredientSpaceScope getScope() {
+		return scope;
+	}
+
+	public void setScope(IngredientSpaceScope scope) {
+		this.scope = scope;
+	}
+	
+	public CtElement getDerivedFrom() {
+		return derivedFrom;
+	}
+
+	public void setDerivedFrom(CtElement derivedFrom) {
+		this.derivedFrom = derivedFrom;
+	}
+
+	@Override
+	public String toString() {
+		return "Ingredient [code=" + getCode() + "]";
+	}
+
+	
+}
