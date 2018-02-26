75fb9db09f729de35dc76f52e588da67f4a11d67
Luc Esape <luc.esape@gmail.com>
2018-02-26 14:19:45 +0100

    Bug commit from the following repository Zyavaranah/TAU-LeagueOfTests This bug commit is a reflect of source code from: http://github.com/Zyavaranah/TAU-LeagueOfTests/commit/0466fb4b0044539aee912a16f380b72f67afef08.

---
+++ league-of-tests/src/main/java/org/leagueoftests/app/Champions.java
@@ -0,0 +1,14 @@
+package org.leagueoftests.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class Champions
+{
+    public int id;
+    public String championName;
+    public int championPriceBE;
+    public int championPriceRP;   
+    
+}
