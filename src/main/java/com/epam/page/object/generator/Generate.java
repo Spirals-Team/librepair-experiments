ce9cc1c726caa90ac79125cb03c32eb1c045d794
Luc Esape <luc.esape@gmail.com>
2018-02-28 18:36:29 +0100

    Bug commit from the following repository TAI-EPAM/page-objects-generator This bug commit is a reflect of source code from: http://github.com/TAI-EPAM/page-objects-generator/commit/0b078ed4b8b2177c52d4323cfd75d47a090b48ec.

---
+++ src/main/java/com/epam/page/object/generator/Generate.java
@@ -0,0 +1,28 @@
+package com.epam.page.object.generator;
+
+import static java.util.Arrays.asList;
+
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * Created by Roman_Iovlev on 10/16/2017.
+ */
+public class Generate {
+
+    // EXAMPLE
+    public static void main(String[] args) throws IOException {
+        List<String> urls = asList(
+            "https://www.w3schools.com/html/html_forms.asp",
+            "https://www.w3schools.com/css/default.asp",
+            "https://www.w3schools.com/html/html_form_input_types.asp");
+
+
+
+        PageObjectsGenerator pog = PageObjectGeneratorFactory.getPageObjectGenerator("w3c.tests",
+            "/groups.json", true);
+        pog.setForceGenerateFile(true);
+        pog.setAddElementSuffix(true);
+        pog.generatePageObjects("/webElement.json", "src/main/java/", urls);
+    }
+}
