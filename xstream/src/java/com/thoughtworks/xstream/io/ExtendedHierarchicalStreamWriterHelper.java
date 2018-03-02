465b823cefdc46f3086658bcd3e1540c3f999e94
Luc Esape <luc.esape@gmail.com>
2018-03-03 00:54:19 +0100

    Bug commit from the following repository x-stream/xstream This bug commit is a reflect of source code from: http://github.com/x-stream/xstream/commit/ea6d0ad666f1a92c0fe2b7b435b3d0590952449a.

---
+++ xstream/src/java/com/thoughtworks/xstream/io/ExtendedHierarchicalStreamWriterHelper.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright (C) 2006 Joe Walnes.
+ * Copyright (C) 2006, 2007, 2014, 2018 XStream Committers.
+ * All rights reserved.
+ *
+ * The software in this package is published under the terms of the BSD
+ * style license a copy of which has been included with this distribution in
+ * the LICENSE.txt file.
+ * 
+ * Created on 22. June 2006 by Mauro Talevi
+ */
+package com.thoughtworks.xstream.io;
+
+/**
+ * @deprecated As of upcoming, this helper is no longer required since version 1.5.0.
+ */
+@Deprecated
+public class ExtendedHierarchicalStreamWriterHelper {
+
+    /**
+     * @deprecated As of upcoming, use {@link HierarchicalStreamWriter#startNode(String, Class)} directly. This helper
+     *             is no longer required since version 1.5.0.
+     */
+    public static void startNode(final HierarchicalStreamWriter writer, final String name, final Class<?> clazz) {
+        writer.startNode(name, clazz);
+    }
+}
