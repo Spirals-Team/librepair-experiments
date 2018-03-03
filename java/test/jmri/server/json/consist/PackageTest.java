64309afa94df0fce70421e8cd3794d449192b6aa
Luc Esape <luc.esape@gmail.com>
2018-03-03 23:03:32 +0100

    Bug commit from the following repository rhwood/JMRI This bug commit is a reflect of source code from: http://github.com/rhwood/JMRI/commit/eafe76bb04bb34d2f670357b6319396fc419c888.

---
+++ java/test/jmri/server/json/consist/PackageTest.java
@@ -0,0 +1,17 @@
+package jmri.server.json.consist;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+/**
+ *
+ * @author Randall Wood (C) 2016
+ */
+@RunWith(Suite.class)
+@SuiteClasses({
+    BundleTest.class,
+    JsonConsistManagerTest.class
+})
+public class PackageTest {
+}
