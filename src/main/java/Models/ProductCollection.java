bdd8df917fcda65e4e128ce87df83c31924717e4
Luc Esape <luc.esape@gmail.com>
2018-03-05 06:14:02 +0100

    Bug commit from the following repository yegorsch/NUKupi This bug commit is a reflect of source code from: http://github.com/yegorsch/NUKupi/commit/2c79a99663abab33018fd5c5fc82fb4054274c01.

---
+++ src/main/java/Models/ProductCollection.java
@@ -0,0 +1,17 @@
+package Models;
+
+import com.google.gson.*;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class ProductCollection extends ArrayList<Product> {
+    public String toJson(){
+        System.out.println(new Gson().toJson(this));
+        return new Gson().toJson(this);
+    }
+
+
+}
+
---
+++ src/main/java/REST/ProductService.java
@@ -0,0 +1,118 @@
+package REST;
+
+import DB.DatabaseClient;
+import DB.ProductDatabaseClient;
+import Models.Product;
+import Models.ProductCollection;
+import Utils.Filterer;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.Response;
+import java.util.ArrayList;
+
+//import Utils.Filterer;
+
+// URL: http://localhost:8080/rest/products
+@Path("products")
+public class ProductService {
+
+    private ProductDatabaseClient dbc;
+
+    public ProductService() {
+        dbc = new ProductDatabaseClient();
+    }
+
+    @GET
+    public String getItems(@QueryParam("first") int numberOfItems) {
+    //TODO: ADD PAGINATION,
+        return "NOT YEt";
+    }
+
+    /**
+     * Retrieve all products
+     **/
+
+    @GET
+    @Path("/all")
+    public Response getAllProducts() {
+        ProductCollection prod = dbc.runQueryProductsAll();
+        Gson gson = new Gson();
+        Response.ResponseBuilder b = Response.ok(gson.toJson(prod));
+        return b.build();
+    }
+
+    /**
+     * Filters
+     */
+
+    @GET
+    @Path("/filters")
+    public Response getProductByFilters(@QueryParam("title") String title,
+                                        @QueryParam("price") int price,
+                                        @QueryParam("category") String category) {
+        System.out.println(title);
+        System.out.println(price);
+        System.out.println(category);
+        //TODO: USE SQL INSTEAD OF FILTERER, SEE https://stackoverflow.com/questions/10185638/optional-arguments-in-where-clause
+        ProductCollection result = dbc.runQueryProductsAll();
+        result = new Filterer(result).filter(title, price, category);
+        return Response.ok(result.toJson()).build();
+    }
+
+    /**
+     * Retrieve products owned by particular user (change email for user id)
+     **/
+
+    @GET
+    @Path("/myproducts")
+    public Response getProductByUser(@QueryParam("id") String id) {
+        ProductCollection reqProds = null;
+        if (id != null) {
+            reqProds = dbc.runQueryProductsByUser(id);
+        }
+        return Response.ok(new Gson().toJson(reqProds)).build();
+    }
+
+//    @GET
+//    @Path("{ id : [A-Za-z0-9_]+}")
+//    public String getProduct(@PathParam("id") String id) {
+//        System.out.println(id);
+//        for (Product p : products) {
+//            if (p.getID().equals(id)) {
+//                return p.toJSON();
+//            }
+//        }
+//        return "";
+//    }
+
+    @POST
+    @Consumes("application/json")
+    public Response addProduct(String jsonString) {
+        Product p;
+        try {
+            p = new Product(jsonString);
+        } catch (Exception e){
+            return Response.status(400).build();
+        }
+        //TODO: SEND PRODUCT MODEL INSTEAD OF EACH PARAMETER
+        if (dbc.runQueryInsertProduct(p.getID(), p.getTitle(), p.getDescription(),p.getPrice(), p.getCategory(), p.getAuthorID()))
+            return Response.status(Response.Status.OK).build();
+        else
+            return Response.status(Response.Status.NOT_ACCEPTABLE).build();
+    }
+
+    @DELETE
+    @Consumes("application/json")
+    public Response deleteProduct(String jsonString) {
+        ArrayList<String> list = new Gson().fromJson(jsonString, new TypeToken<ArrayList<String>>() {
+        }.getType());
+        for (String id : list) {
+            if (!dbc.runQueryDeleteProduct(id))
+                return Response.status(Response.Status.NOT_ACCEPTABLE).build();
+        }
+        return Response.status(Response.Status.OK).build();
+    }
+
+}
