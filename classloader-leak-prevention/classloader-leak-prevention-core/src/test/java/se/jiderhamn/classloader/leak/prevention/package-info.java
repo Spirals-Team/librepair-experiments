0e8b485250d12b99b8bd620aaaa8685d48c9c04a
Luc Esape <luc.esape@gmail.com>
2018-03-07 20:06:08 +0100

    Bug commit from the following repository mjiderhamn/classloader-leak-prevention This bug commit is a reflect of source code from: http://github.com/mjiderhamn/classloader-leak-prevention/commit/277b99fde43e8559731e031faf290fd0c7babf17.

---
+++ classloader-leak-prevention/classloader-leak-prevention-core/src/test/java/se/jiderhamn/classloader/leak/prevention/package-info.java
@@ -0,0 +1,5 @@
+package se.jiderhamn.classloader.leak.prevention;
+
+/**
+ * Test cases in this package are used to confirm a) that a leak exists and b) that the 
+ * {@link se.jiderhamn.classloader.leak.prevention.ClassLoaderLeakPreventorListener} is able to circumvent the leak. */
