49c72e5d2973b4315167b241c665a3a95c08573a
Luc Esape <luc.esape@gmail.com>
2018-03-14 18:26:13 +0100

    Bug commit from the following repository INRIA/spoon This bug commit is a reflect of source code from: http://github.com/INRIA/spoon/commit/3d14bb1411f0547a1a23c0728e7ec3306fd0a011.

---
+++ src/test/java/spoon/test/template/testclasses/match/MatchMap.java
@@ -0,0 +1,73 @@
+package spoon.test.template.testclasses.match;
+
+import spoon.pattern.ConflictResolutionMode;
+import spoon.pattern.Pattern;
+import spoon.pattern.PatternBuilder;
+import spoon.reflect.code.CtLiteral;
+import spoon.reflect.declaration.CtAnnotation;
+import spoon.reflect.declaration.CtMethod;
+import spoon.reflect.factory.Factory;
+import spoon.reflect.meta.ContainerKind;
+import spoon.reflect.path.CtRole;
+import spoon.reflect.visitor.filter.TypeFilter;
+
+public class MatchMap {
+
+	public static Pattern createPattern(Factory factory, boolean acceptOtherAnnotations) {
+		return PatternBuilder.create(factory, MatchMap.class, tmb -> tmb.setTypeMember("matcher1"))
+			.configureParameters(pb -> {
+				//match any value of @Check annotation to parameter `testAnnotations`
+				pb.parameter("CheckAnnotationValues").attributeOfElementByFilter(CtRole.VALUE, new TypeFilter(CtAnnotation.class)).setContainerKind(ContainerKind.MAP);
+				//match any method name
+				pb.parameter("methodName").byString("matcher1");
+				if (acceptOtherAnnotations) {
+					//match on all annotations of method
+					pb.parameter("allAnnotations")
+						.setConflictResolutionMode(ConflictResolutionMode.APPEND)
+						.attributeOfElementByFilter(CtRole.ANNOTATION, new TypeFilter<>(CtMethod.class));
+				}
+			})
+			.build();
+	}
+	public static Pattern createMatchKeyPattern(Factory factory) {
+		return PatternBuilder.create(factory, MatchMap.class, tmb -> tmb.setTypeMember("m1"))
+			.configureParameters(pb -> {
+				//match any value of @Check annotation to parameter `testAnnotations`
+				pb.parameter("CheckKey").bySubstring("value");
+				pb.parameter("CheckValue").byFilter((CtLiteral lit) -> true);
+				//match any method name
+				pb.parameter("methodName").byString("m1");
+				//match on all annotations of method
+				pb.parameter("allAnnotations")
+					.setConflictResolutionMode(ConflictResolutionMode.APPEND)
+					.attributeOfElementByFilter(CtRole.ANNOTATION, new TypeFilter<>(CtMethod.class));
+			})
+			.build();
+	}
+
+	@Check()
+	void matcher1() {
+	}
+	
+	@Check(value = "xyz")
+	void m1() {
+	}
+	
+	@Check(timeout = 123, value = "abc")
+	void m2() {
+	}
+
+	@Deprecated
+	void notATestAnnotation() {
+	}
+
+	@Deprecated
+	@Check(timeout = 456)
+	void deprecatedTestAnnotation1() {
+	}
+
+	@Check(timeout = 4567)
+	@Deprecated
+	void deprecatedTestAnnotation2() {
+	}
+}
