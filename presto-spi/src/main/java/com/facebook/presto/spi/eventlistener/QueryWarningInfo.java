e8b6197c9df12007f5f708d4dc741eca7a123305
Luc Esape <luc.esape@gmail.com>
2018-03-15 02:15:31 +0100

    Bug commit from the following repository prestodb/presto This bug commit is a reflect of source code from: http://github.com/prestodb/presto/commit/5e098186a885d26b0f6ae6b29d9af0268184a1a2.

---
+++ presto-spi/src/main/java/com/facebook/presto/spi/eventlistener/QueryWarningInfo.java
@@ -0,0 +1,50 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.facebook.presto.spi.eventlistener;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import javax.annotation.Nonnull;
+
+import static java.util.Objects.requireNonNull;
+
+public class QueryWarningInfo
+{
+    private final String code;
+    private final String message;
+
+    @JsonCreator
+    public QueryWarningInfo(
+            @JsonProperty("code") String code,
+            @JsonProperty("message") String message)
+    {
+        this.code = requireNonNull(code, "code is null");
+        this.message = requireNonNull(message, "message is null");
+    }
+
+    @Nonnull
+    @JsonProperty
+    public String getCode()
+    {
+        return code;
+    }
+
+    @Nonnull
+    @JsonProperty
+    public String getMessage()
+    {
+        return message;
+    }
+}
