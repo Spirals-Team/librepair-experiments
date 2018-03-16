8547a51ad87d0a900e60f0bbd22779b9f909f804
Luc Esape <luc.esape@gmail.com>
2018-03-16 19:27:44 +0100

    Bug commit from the following repository prestodb/presto This bug commit is a reflect of source code from: http://github.com/prestodb/presto/commit/96c7db8038dce078f332accea5ea7c2e14df26d5.

---
+++ presto-main/src/test/java/com/facebook/presto/memory/TestNodeMemoryConfig.java
@@ -0,0 +1,77 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.facebook.presto.memory;
+
+import com.google.common.collect.ImmutableMap;
+import io.airlift.units.DataSize;
+import org.testng.annotations.Test;
+
+import java.util.Map;
+
+import static com.facebook.presto.memory.LocalMemoryManager.validateHeapHeadroom;
+import static io.airlift.configuration.testing.ConfigAssertions.assertFullMapping;
+import static io.airlift.units.DataSize.Unit.BYTE;
+import static io.airlift.units.DataSize.Unit.GIGABYTE;
+import static org.testng.Assert.fail;
+
+public class TestNodeMemoryConfig
+{
+    @Test
+    public void testDefaults()
+    {
+        // This can't use assertRecordedDefaults because the default value is dependent on the current max heap size, which varies based on the current size of the survivor space.
+        NodeMemoryConfig config = new NodeMemoryConfig();
+        for (int i = 0; i < 1_000; i++) {
+            long maxMemory = Runtime.getRuntime().maxMemory();
+            DataSize expectedQueryMaxMemory = new DataSize(maxMemory * 0.1, BYTE);
+            DataSize expectedQueryMaxTotalMemory = new DataSize(maxMemory, BYTE);
+            DataSize expectedQueryDefaultHeadroom = new DataSize(maxMemory * 0.3, BYTE);
+            if (expectedQueryMaxMemory.equals(config.getMaxQueryMemoryPerNode())
+                    && expectedQueryMaxTotalMemory.equals(config.getMaxQueryTotalMemoryPerNode())
+                    && expectedQueryDefaultHeadroom.equals(config.getHeapHeadroom())) {
+                return;
+            }
+        }
+        // We can't make this 100% deterministic, since we don't know when the survivor space will change sizes, but assume that something is broken if we got the wrong answer 1000 times
+        fail();
+    }
+
+    @Test
+    public void testExplicitPropertyMappings()
+    {
+        Map<String, String> properties = new ImmutableMap.Builder<String, String>()
+                .put("query.max-memory-per-node", "1GB")
+                .put("query.max-total-memory-per-node", "3GB")
+                .put("memory.heap-headroom-per-node", "1GB")
+                .build();
+
+        NodeMemoryConfig expected = new NodeMemoryConfig()
+                .setMaxQueryMemoryPerNode(new DataSize(1, GIGABYTE))
+                .setMaxQueryTotalMemoryPerNode(new DataSize(3, GIGABYTE))
+                .setHeapHeadroom(new DataSize(1, GIGABYTE));
+
+        assertFullMapping(properties, expected);
+    }
+
+    @Test(expectedExceptions = IllegalArgumentException.class)
+    public void testInvalidValues()
+    {
+        NodeMemoryConfig config = new NodeMemoryConfig();
+        config.setMaxQueryMemoryPerNode(new DataSize(1, GIGABYTE));
+        config.setHeapHeadroom(new DataSize(3.1, GIGABYTE));
+        // In this case we have 4GB - 1GB = 3GB available memory for the general pool
+        // and the heap headroom and the config is more than that.
+        validateHeapHeadroom(config, new DataSize(4, GIGABYTE).toBytes());
+    }
+}
