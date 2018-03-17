4c668b8df3356dd766ed01628268c5e0c4e22fe9
Luc Esape <luc.esape@gmail.com>
2018-03-18 00:20:44 +0100

    Bug commit from the following repository palmithor/ci-tdd-java-template This bug commit is a reflect of source code from: http://github.com/palmithor/ci-tdd-java-template/commit/68d5cbfba05687bc1697bcdec71eaa5f1fe3bf1c.

---
+++ src/test/java/se/cygni/palmithor/tdd/CalculatorTest.java
@@ -0,0 +1,20 @@
+package se.cygni.palmithor.tdd;
+
+
+import org.junit.Test;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CalculatorTest {
+
+    private Calculator calculator = new Calculator();
+
+    @Test
+    public void test() {
+        assertThat(calculator.sumAll().get()).isEqualTo(1); // TODO failing on purpose please fix
+    }
+
+
+}
