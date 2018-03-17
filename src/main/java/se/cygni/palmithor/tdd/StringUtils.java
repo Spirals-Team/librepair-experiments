3307fee4c2271149f82aaf768cd2e61f7a05ef4c
Luc Esape <luc.esape@gmail.com>
2018-03-18 00:34:30 +0100

    Bug commit from the following repository palmithor/ci-tdd-java-template This bug commit is a reflect of source code from: http://github.com/palmithor/ci-tdd-java-template/commit/58728d043225b2d2593d1fb42adb980a05b629cd.

---
+++ src/main/java/se/cygni/palmithor/tdd/StringUtils.java
@@ -0,0 +1,20 @@
+package se.cygni.palmithor.tdd;
+
+public class StringUtils {
+
+
+    public boolean isPalindrome(final String str) {
+        throw new RuntimeException("Not yet implemented");
+    }
+
+
+    /**
+     * Checks if a String is empty (""), null or whitespace only.
+     * @param str the string to check
+     *
+     * @return true if str is null, empty or whitespace only, otherwise false
+     */
+    public boolean isBlank(final String str) {
+        throw new RuntimeException("Not yet implemented");
+    }
+}
