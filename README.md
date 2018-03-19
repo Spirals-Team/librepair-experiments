12fcdda4c81dfbd485f121cf1eae2443e497f912
Luc Esape <luc.esape@gmail.com>
2018-03-19 16:45:57 +0100

    Bug commit from the following repository tipsy/javalin This bug commit is a reflect of source code from: http://github.com/tipsy/javalin/commit/99e918210945156e67ef9e9be5f9bf60b6c8e8a1.

---
+++ README.md
@@ -0,0 +1,56 @@
+[![Chat at https://gitter.im/javalin-io/general](https://badges.gitter.im/javalin-io/general.svg)](https://gitter.im/javalin-io/general)
+[![Travis](https://img.shields.io/travis/tipsy/javalin.svg)](https://travis-ci.org/tipsy/javalin/builds)
+[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
+[![Maven](https://img.shields.io/maven-central/v/io.javalin/javalin.svg)](https://search.maven.org/#search%7Cgav%7C1%7Cg%3A%22io.javalin%22%20AND%20a%3A%22javalin%22)
+
+# Javalin - A Simple REST API Library for Java/Kotlin
+
+The project webpage is [javalin.io](https://javalin.io).
+
+## Java quickstart
+
+### Add dependency (maven)
+
+```xml
+<dependency>
+    <groupId>io.javalin</groupId>
+    <artifactId>javalin</artifactId>
+    <version>1.4.1</version>
+</dependency>
+```
+
+### Start programming:
+
+```java
+import io.javalin.Javalin;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        Javalin app = Javalin.start(7000);
+        app.get("/", ctx -> ctx.result("Hello World"));
+    }
+}
+```
+
+## Kotlin quickstart
+
+### Add dependency (gradle)
+```kotlin
+compile 'io.javalin:javalin:1.4.1'
+```
+
+### Start programming
+```kotlin
+import io.javalin.Javalin
+
+fun main(args: Array<String>) {
+    val app = Javalin.start(7000)
+    app.get("/") { ctx -> ctx.result("Hello World") }
+}
+```
+
+## Special thanks
+* Blake Mizerany, for creating [Sinatra](http://www.sinatrarb.com/)
+* Per Wendel, for creating [Spark](http://sparkjava.com/)
+* [Christian Rasmussen](https://github.com/chrrasmussen), for being a great guy
+* [Per Kristian Kummermo](https://github.com/pkkummermo), also for being a great guy
---
+++ src/main/java/io/javalin/embeddedserver/jetty/websocket/WebSocketHandler.java
@@ -0,0 +1,110 @@
+/*
+ * Javalin - https://javalin.io
+ * Copyright 2017 David Åse
+ * Licensed under Apache 2.0: https://github.com/tipsy/javalin/blob/master/LICENSE
+ */
+
+package io.javalin.embeddedserver.jetty.websocket;
+
+import io.javalin.core.PathParser;
+import io.javalin.embeddedserver.jetty.websocket.interfaces.CloseHandler;
+import io.javalin.embeddedserver.jetty.websocket.interfaces.ConnectHandler;
+import io.javalin.embeddedserver.jetty.websocket.interfaces.ErrorHandler;
+import io.javalin.embeddedserver.jetty.websocket.interfaces.MessageHandler;
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+import org.eclipse.jetty.websocket.api.Session;
+import org.eclipse.jetty.websocket.api.annotations.WebSocket;
+import org.jetbrains.annotations.NotNull;
+
+@WebSocket
+public class WebSocketHandler {
+
+    public WebSocketHandler(@NotNull String contextPath, @NotNull String path) {
+        pathParser = new PathParser(contextPath + path);
+    }
+
+    private final PathParser pathParser;
+
+    private final ConcurrentMap<Session, String> sessions = new ConcurrentHashMap<>();
+
+    private ConnectHandler connectHandler = null;
+    private MessageHandler messageHandler = null;
+    private CloseHandler closeHandler = null;
+    private ErrorHandler errorHandler = null;
+
+    /**
+     * Add a ConnectHandler to the WebSocketHandler.
+     * The handler is called when a WebSocket client connects.
+     */
+    public void onConnect(@NotNull ConnectHandler connectHandler) {
+        this.connectHandler = connectHandler;
+    }
+
+    /**
+     * Add a MessageHandler to the WebSocketHandler.
+     * The handler is called when a WebSocket client sends
+     * a String message.
+     */
+    public void onMessage(@NotNull MessageHandler messageHandler) {
+        this.messageHandler = messageHandler;
+    }
+
+    /**
+     * Add a CloseHandler to the WebSocketHandler.
+     * The handler is called when a WebSocket client closes
+     * the connection. The handler is not called in case of
+     * network issues, only when the client actively closes the
+     * connection (or times out).
+     */
+    public void onClose(@NotNull CloseHandler closeHandler) {
+        this.closeHandler = closeHandler;
+    }
+
+    /**
+     * Add a errorHandler to the WebSocketHandler.
+     * The handler is called when an error is detected.
+     */
+    public void onError(@NotNull ErrorHandler errorHandler) {
+        this.errorHandler = errorHandler;
+    }
+
+    void onConnect(Session session) throws Exception {
+        WsSession wsSession = registerAndWrapSession(session);
+        if (connectHandler != null) {
+            connectHandler.handle(wsSession);
+        }
+    }
+
+    void onMessage(Session session, String message) throws Exception {
+        WsSession wsSession = registerAndWrapSession(session);
+        if (messageHandler != null) {
+            messageHandler.handle(wsSession, message);
+        }
+    }
+
+    void onClose(Session session, int statusCode, String reason) throws Exception {
+        WsSession wsSession = registerAndWrapSession(session);
+        if (closeHandler != null) {
+            closeHandler.handle(wsSession, statusCode, reason);
+        }
+        sessions.remove(session);
+    }
+
+    void onError(Session session, Throwable throwable) throws Exception {
+        WsSession wsSession = registerAndWrapSession(session);
+        if (errorHandler != null) {
+            errorHandler.handle(wsSession, throwable);
+        }
+    }
+
+    public boolean matches(String requestUri) {
+        return pathParser.matches(requestUri);
+    }
+
+    private WsSession registerAndWrapSession(Session session) {
+        sessions.putIfAbsent(session, UUID.randomUUID().toString());
+        return new WsSession(sessions.get(session), session, pathParser.extractParams(session.getUpgradeRequest().getRequestURI().getPath()));
+    }
+}
