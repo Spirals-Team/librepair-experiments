fff022b38e3dfc572df596978c6f97422c02385c
Luc Esape <luc.esape@gmail.com>
2018-03-21 19:54:38 +0100

    Bug commit from the following repository twitter/GraphJet This bug commit is a reflect of source code from: http://github.com/twitter/GraphJet/commit/57a06a9628278117005422345a01dd18862fead8.

---
+++ graphjet-core/src/main/java/com/twitter/graphjet/algorithms/RecommendationType.java
@@ -0,0 +1,44 @@
+/**
+ * Copyright 2016 Twitter. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package com.twitter.graphjet.algorithms;
+
+public enum RecommendationType {
+  HASHTAG(0),       // hashtag metadata type
+  URL(1),           // url metadata type
+  TWEETFEATURE(2),  // tweet feature metadata type
+  METADATASIZE(3),  // the size of supported metadata types
+  TWEET(4),         // tweet, not a metadata type
+  USER(5),          // users, in follows, mentions & mediatags
+  MOMENT(6);        // moment
+
+  private final int value;
+
+  private RecommendationType(int value) {
+    this.value = value;
+  }
+
+  public int getValue() {
+    return value;
+  }
+
+  private static final RecommendationType[] VALUES = {HASHTAG, URL, TWEETFEATURE, METADATASIZE, TWEET, USER, MOMENT};
+
+  public static RecommendationType at(int index) {
+    return VALUES[index];
+  }
+}
