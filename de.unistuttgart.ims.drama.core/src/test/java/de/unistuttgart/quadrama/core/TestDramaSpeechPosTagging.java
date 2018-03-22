6682152e888a9c369538f86e14e7eb2a1c9cf7f5
Luc Esape <luc.esape@gmail.com>
2018-03-22 07:43:59 +0100

    Bug commit from the following repository quadrama/DramaNLP This bug commit is a reflect of source code from: http://github.com/quadrama/DramaNLP/commit/ec25106eb702e4762ea7678f8b422111d25dedbb.

---
+++ de.unistuttgart.ims.drama.core/src/test/java/de/unistuttgart/quadrama/core/TestDramaSpeechPosTagging.java
@@ -0,0 +1,52 @@
+package de.unistuttgart.quadrama.core;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.io.IOException;
+
+import org.apache.uima.UIMAException;
+import org.apache.uima.analysis_engine.AnalysisEngineDescription;
+import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
+import org.apache.uima.cas.impl.XmiCasDeserializer;
+import org.apache.uima.fit.factory.AnalysisEngineFactory;
+import org.apache.uima.fit.factory.JCasFactory;
+import org.apache.uima.fit.factory.TypeSystemDescriptionFactory;
+import org.apache.uima.fit.pipeline.SimplePipeline;
+import org.apache.uima.fit.util.JCasUtil;
+import org.apache.uima.jcas.JCas;
+import org.apache.uima.resource.ResourceInitializationException;
+import org.apache.uima.resource.metadata.TypeSystemDescription;
+import org.junit.Before;
+import org.junit.Test;
+import org.xml.sax.SAXException;
+
+import de.tudarmstadt.ukp.dkpro.core.api.lexmorph.type.pos.NN;
+import de.tudarmstadt.ukp.dkpro.core.api.lexmorph.type.pos.POS;
+import de.tudarmstadt.ukp.dkpro.core.languagetool.LanguageToolSegmenter;
+import de.tudarmstadt.ukp.dkpro.core.stanfordnlp.StanfordPosTagger;
+
+public class TestDramaSpeechPosTagging {
+	JCas jcas;
+
+	AnalysisEngineDescription desc;
+
+	@Before
+	public void setUp() throws UIMAException, SAXException, IOException {
+
+		TypeSystemDescription tsd = TypeSystemDescriptionFactory.createTypeSystemDescription();
+		jcas = JCasFactory.createJCas(tsd);
+		XmiCasDeserializer.deserialize(getClass().getResourceAsStream("/rfxf.0.xmi"), jcas.getCas(), true);
+		jcas.setDocumentLanguage("de");
+		desc = D.getWrappedSegmenterDescription(LanguageToolSegmenter.class);
+	}
+
+	@Test
+	public void testSegmentation() throws ResourceInitializationException, AnalysisEngineProcessException {
+
+		SimplePipeline.runPipeline(jcas, desc, AnalysisEngineFactory.createEngineDescription(StanfordPosTagger.class));
+
+		assertTrue(JCasUtil.exists(jcas, POS.class));
+		assertEquals("Liebe", JCasUtil.selectByIndex(jcas, NN.class, 0).getCoveredText());
+	}
+}
---
+++ de.unistuttgart.ims.drama.core/src/test/java/de/unistuttgart/quadrama/core/TestFigureReferenceAnnotator.java
@@ -0,0 +1,81 @@
+package de.unistuttgart.quadrama.core;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.uima.UIMAException;
+import org.apache.uima.analysis_engine.AnalysisEngineDescription;
+import org.apache.uima.collection.CollectionReaderDescription;
+import org.apache.uima.fit.factory.AnalysisEngineFactory;
+import org.apache.uima.fit.factory.CollectionReaderFactory;
+import org.apache.uima.fit.pipeline.JCasIterator;
+import org.apache.uima.fit.pipeline.SimplePipeline;
+import org.apache.uima.fit.util.JCasUtil;
+import org.apache.uima.jcas.JCas;
+import org.apache.uima.resource.ResourceInitializationException;
+import org.junit.Test;
+
+import de.tudarmstadt.ukp.dkpro.core.io.xmi.XmiReader;
+import de.tudarmstadt.ukp.dkpro.core.io.xmi.XmiWriter;
+import de.unistuttgart.ims.drama.api.Figure;
+import de.unistuttgart.quadrama.io.tei.TextgridTEIUrlReader;
+
+public class TestFigureReferenceAnnotator {
+	@Test
+	public void testFigureReferenceAnnotator1() throws ResourceInitializationException {
+		CollectionReaderDescription reader = CollectionReaderFactory.createReaderDescription(XmiReader.class,
+				XmiReader.PARAM_SOURCE_LOCATION, "src/test/resources/FigureReferenceAnnotator/rfxf.0.xmi",
+				XmiReader.PARAM_LENIENT, true);
+		AnalysisEngineDescription engine = AnalysisEngineFactory
+				.createEngineDescription(FigureReferenceAnnotator.class);
+		JCasIterator iterator = SimplePipeline.iteratePipeline(reader, engine).iterator();
+		while (iterator.hasNext()) {
+			JCas jcas = iterator.next();
+			assertTrue(JCasUtil.exists(jcas, Figure.class));
+			for (Figure figure : JCasUtil.select(jcas, Figure.class)) {
+				assertNotNull(figure.getReference());
+				assertFalse(figure.getReference().contains(","));
+			}
+		}
+	}
+
+	@Test
+	public void testFigureReferenceAnnotator2() throws ResourceInitializationException {
+		CollectionReaderDescription reader = CollectionReaderFactory.createReaderDescription(XmiReader.class,
+				XmiReader.PARAM_SOURCE_LOCATION, "src/test/resources/FigureReferenceAnnotator/w3zd.0.xmi",
+				XmiReader.PARAM_LENIENT, true);
+		AnalysisEngineDescription engine = AnalysisEngineFactory
+				.createEngineDescription(FigureReferenceAnnotator.class);
+		JCasIterator iterator = SimplePipeline.iteratePipeline(reader, engine).iterator();
+		while (iterator.hasNext()) {
+			JCas jcas = iterator.next();
+			assertTrue(JCasUtil.exists(jcas, Figure.class));
+			for (Figure figure : JCasUtil.select(jcas, Figure.class)) {
+				assertNotNull(figure.getReference());
+				// assertFalse(figure.getReference().contains(","));
+			}
+		}
+	}
+
+	public static void main(String[] args) throws ResourceInitializationException, UIMAException, IOException {
+		SimplePipeline.runPipeline(
+				CollectionReaderFactory.createReaderDescription(TextgridTEIUrlReader.class,
+						TextgridTEIUrlReader.PARAM_INPUT, "http://www.textgridrep.org/textgrid:rfxf.0",
+						TextgridTEIUrlReader.PARAM_REMOVE_XML_ANNOTATIONS, true),
+				AnalysisEngineFactory.createEngineDescription(XmiWriter.class, XmiWriter.PARAM_USE_DOCUMENT_ID, true,
+						XmiWriter.PARAM_TARGET_LOCATION, "src/test/resources/FigureReferenceAnnotator/"));
+
+		SimplePipeline.runPipeline(
+				CollectionReaderFactory.createReaderDescription(TextgridTEIUrlReader.class,
+						TextgridTEIUrlReader.PARAM_INPUT, "http://www.textgridrep.org/textgrid:w3zd.0",
+						TextgridTEIUrlReader.PARAM_REMOVE_XML_ANNOTATIONS, true),
+				AnalysisEngineFactory.createEngineDescription(XmiWriter.class, XmiWriter.PARAM_USE_DOCUMENT_ID, true,
+						XmiWriter.PARAM_TARGET_LOCATION, "src/test/resources/FigureReferenceAnnotator/"));
+
+		new File("src/test/resources/FigureReferenceAnnotator/typesystem.xml").delete();
+	}
+}
