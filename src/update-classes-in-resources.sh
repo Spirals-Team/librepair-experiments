2799e1b07e70ee052ad321b2e57077a4c0c40a36
Luc Esape <luc.esape@gmail.com>
2018-03-23 12:27:09 +0100

    Bug commit from the following repository STAMP-project/testrunner This bug commit is a reflect of source code from: http://github.com/STAMP-project/testrunner/commit/583405b86629a7c9b69d269df65e44125db97edc.

---
+++ src/update-classes-in-resources.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+rm -rf src/main/resources/runner-classes/
+mkdir --parent src/main/resources/runner-classes/eu/stamp/project/testrunner/runner/test/
+mkdir --parent src/main/resources/runner-classes/eu/stamp/project/testrunner/runner/coverage/
+cp -r target/classes/eu/stamp/project/testrunner/runner/test/*.class src/main/resources/runner-classes/eu/stamp/project/testrunner/runner/test/
+cp -r target/classes/eu/stamp/project/testrunner/runner/coverage/*.class src/main/resources/runner-classes/eu/stamp/project/testrunner/runner/coverage/
