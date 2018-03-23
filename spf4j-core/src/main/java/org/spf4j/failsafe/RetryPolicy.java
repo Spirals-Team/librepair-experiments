46d30420b4570845d6b86a0f544b113206b7bd29
Luc Esape <luc.esape@gmail.com>
2018-03-23 16:56:52 +0100

    Bug commit from the following repository zolyfarkas/spf4j This bug commit is a reflect of source code from: http://github.com/zolyfarkas/spf4j/commit/699299b7ba3f93b7080b2078cafdab76291f98fc.

---
+++ spf4j-core/src/main/java/org/spf4j/failsafe/RetryPolicy.java
@@ -0,0 +1,338 @@
+/*
+ * Copyright (c) 2001-2017, Zoltan Farkas All Rights Reserved.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * Additionally licensed with:
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.spf4j.failsafe;
+
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.concurrent.Callable;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+import java.util.function.Supplier;
+import javax.annotation.CheckReturnValue;
+import javax.annotation.ParametersAreNonnullByDefault;
+import org.spf4j.base.Throwables;
+import org.spf4j.failsafe.concurrent.DefaultContextAwareRetryExecutor;
+import org.spf4j.failsafe.concurrent.RetryExecutor;
+
+/**
+ * @author Zoltan Farkas
+ */
+@ParametersAreNonnullByDefault
+@SuppressFBWarnings("FCCD_FIND_CLASS_CIRCULAR_DEPENDENCY")
+public final class RetryPolicy<T, C extends Callable<? extends T>> implements PolicyExecutor<T, C> {
+
+
+  public static final RetryPolicy<Object, Callable<? extends Object>> DEFAULT;
+  static {
+    RetryPolicy p;
+    String policySupplierClass = System.getProperty("spf4j.failsafe.defaultRetryPolicySupplier");
+    if (policySupplierClass == null) {
+      p = RetryPolicy.newBuilder()
+            .withDefaultThrowableRetryPredicate()
+            .withRetryOnException(Exception.class, 2) // will retry any other exception twice.
+            .build();
+    } else {
+      try {
+        p = ((Supplier<RetryPolicy>) Class.forName(policySupplierClass).newInstance()).get();
+      } catch (ClassNotFoundException | InstantiationException | IllegalAccessException ex) {
+        throw new ExceptionInInitializerError(ex);
+      }
+    }
+    DEFAULT = p;
+  }
+
+  public static final RetryPolicy<Object, Callable<? extends Object>> NO_RETRY
+          = RetryPolicy.newBuilder().build();
+
+  private final Supplier<RetryPredicate<T, C>> retryPredicate;
+
+  private final Supplier<RetryExecutor> execSupplier;
+
+  private final int maxExceptionChain;
+
+  private RetryPolicy(final Supplier<RetryPredicate<T, C>> retryPredicate,
+          final Supplier<RetryExecutor> execSupplier,
+          final int maxExceptionChain) {
+    this.retryPredicate = retryPredicate;
+    this.maxExceptionChain = maxExceptionChain;
+    this.execSupplier = execSupplier;
+  }
+
+  public static <T, C extends Callable<? extends T>> RetryPolicy<T, C> noRetryPolicy() {
+    return (RetryPolicy<T, C>) NO_RETRY;
+  }
+
+  public static <T, C extends Callable<? extends T>> RetryPolicy<T, C> defaultPolicy() {
+    return (RetryPolicy<T, C>) DEFAULT;
+  }
+
+  @Override
+  public <R extends T, W extends C, EX extends Exception> R call(
+          final W pwhat, final Class<EX> exceptionClass)
+          throws InterruptedException, TimeoutException, EX {
+    return (R) SyncRetry.call(pwhat, getRetryPredicate(), exceptionClass, maxExceptionChain);
+  }
+
+  @Override
+  public <R extends T, W extends C, EX extends Exception> R call(
+          final W pwhat, final Class<EX> exceptionClass, final long deadlineNanos)
+          throws InterruptedException, TimeoutException, EX {
+    return (R) SyncRetry.call(pwhat, getRetryPredicate(deadlineNanos), exceptionClass, maxExceptionChain);
+  }
+
+
+  @Override
+  public <R extends T, W extends C> Future<R> submit(final W pwhat) {
+    return (Future<R>) execSupplier.get().submit(pwhat, getRetryPredicate());
+  }
+
+  @Override
+  public <R extends T, W extends C> Future<R> submit(final W pwhat, final long deadlineNanos) {
+    return (Future<R>) execSupplier.get().submit(pwhat, getRetryPredicate(deadlineNanos));
+  }
+
+
+  @Override
+  public <W extends C> void execute(final W pwhat) {
+    execSupplier.get().execute(pwhat, getRetryPredicate());
+  }
+
+  public <W extends C> void execute(final W pwhat, final long deadlineNanos) {
+    execSupplier.get().execute(pwhat, getRetryPredicate(deadlineNanos));
+  }
+
+
+  public RetryPredicate<T, C> getRetryPredicate() {
+    return retryPredicate.get();
+  }
+
+  public RetryPredicate<T, C> getRetryPredicate(final long deadlineNanos) {
+    return new TimeoutRetryPredicate(retryPredicate.get(), deadlineNanos);
+  }
+
+  @Override
+  public String toString() {
+    return "RetryPolicy{" + "retryPredicate=" + retryPredicate + ", execSupplier=" + execSupplier
+            + ", maxExceptionChain=" + maxExceptionChain + '}';
+  }
+
+  public static final class Builder<T, C extends Callable<? extends T>> {
+
+    private static final int MAX_EX_CHAIN_DEFAULT = Integer.getInteger("spf4j.failsafe.defaultMaxExceptionChain", 10);
+
+    private static final long DEFAULT_MAX_DELAY_NANOS
+            = TimeUnit.MILLISECONDS.toNanos(Long.getLong("spf4j.failsafe.defaultMaxRetryDelayMillis", 5000));
+
+    private static final long DEFAULT_INITIAL_DELAY_NANOS
+            = Long.getLong("spf4j.failsafe.defaultInitialRetryDelayNanos", 10000);
+
+    private static final int DEFAULT_INITIAL_NODELAY_RETRIES
+            = Integer.getInteger("spf4j.failsafe.defaultInitialNoDelayRetries", 3);
+
+    private static final int DEFAULT_MAX_NR_RETRIES
+            = Integer.getInteger("spf4j.failsafe.defaultMaxNrRetries", MAX_EX_CHAIN_DEFAULT);
+
+
+    private int maxExceptionChain = MAX_EX_CHAIN_DEFAULT;
+
+    private Supplier<RetryExecutor> execSupplier = () -> DefaultContextAwareRetryExecutor.instance();
+
+    private final List<Supplier<? extends PartialResultRetryPredicate<T, C>>> resultPredicates;
+
+    private final List<Supplier<? extends PartialExceptionRetryPredicate<T, C>>> exceptionPredicates;
+
+    private int nrInitialRetries;
+
+    private long startDelayNanos;
+
+    private long maxDelayNanos;
+
+    private double jitterFactor;
+
+    private Builder() {
+      this.nrInitialRetries = DEFAULT_INITIAL_NODELAY_RETRIES;
+      this.startDelayNanos = DEFAULT_INITIAL_DELAY_NANOS;
+      this.maxDelayNanos = DEFAULT_MAX_DELAY_NANOS;
+      this.jitterFactor = 0.2;
+      this.resultPredicates = new ArrayList<>(2);
+      this.exceptionPredicates = new ArrayList<>(2);
+    }
+
+    @CheckReturnValue
+    public Builder<T, C> withDefaultThrowableRetryPredicate() {
+      return withDefaultThrowableRetryPredicate(DEFAULT_MAX_NR_RETRIES);
+    }
+
+
+    @CheckReturnValue
+    public Builder<T, C> withDefaultThrowableRetryPredicate(final int maxNrRetries) {
+      return withExceptionPartialPredicate((e, c) -> Throwables.isRetryable(e) ? RetryDecision.retryDefault(c) : null,
+              maxNrRetries);
+    }
+
+    @CheckReturnValue
+    public Builder<T, C> withRetryOnException(final Class<? extends Exception> clasz) {
+      return withRetryOnException(clasz, DEFAULT_MAX_NR_RETRIES);
+    }
+
+    @CheckReturnValue
+    public Builder<T, C> withRetryOnException(final Class<? extends Exception> clasz, final int maxRetries) {
+      return withExceptionPartialPredicate((e, c)
+              -> clasz.isAssignableFrom(e.getClass())
+              ? RetryDecision.retryDefault(c) : null, maxRetries);
+    }
+
+    @CheckReturnValue
+    public Builder<T, C> withRetryOnResult(final T result, final int maxRetries) {
+      return withResultPartialPredicate((r, c) ->
+              Objects.equals(result, r)
+              ? RetryDecision.retryDefault(c) : null, maxRetries);
+    }
+
+    @CheckReturnValue
+    public Builder<T, C> withExceptionPartialPredicate(
+            final PartialExceptionRetryPredicate<T, C> predicate) {
+      exceptionPredicates.add(() -> predicate);
+      return this;
+    }
+
+    @CheckReturnValue
+    public Builder<T, C> withExceptionPartialPredicate(
+            final PartialExceptionRetryPredicate<T, C> predicate,
+            final int maxRetries) {
+      exceptionPredicates.add(() -> {
+        CountLimitedPartialRetryPredicate<T, Exception, C> p
+                = new CountLimitedPartialRetryPredicate<T, Exception, C>(maxRetries, predicate);
+        return (Exception value, C what) -> p.apply(value, what);
+      });
+      return this;
+    }
+
+    @CheckReturnValue
+    public Builder<T, C> withExceptionStatefulPartialPredicate(
+            final Supplier<PartialExceptionRetryPredicate<T, C>> predicateSupplier) {
+      exceptionPredicates.add(predicateSupplier);
+      return this;
+    }
+
+    @CheckReturnValue
+    public Builder<T, C> withResultPartialPredicate(
+            final PartialResultRetryPredicate<T, C> predicate) {
+      resultPredicates.add(() -> predicate);
+      return this;
+    }
+
+    @CheckReturnValue
+    public Builder<T, C> withResultPartialPredicate(
+            final PartialResultRetryPredicate<T, C> predicate,
+            final int maxRetries) {
+      resultPredicates.add(() -> {
+        CountLimitedPartialRetryPredicate<T, T, C> p = new CountLimitedPartialRetryPredicate<>(maxRetries, predicate);
+        return (T value, C what) -> p.apply(value, what);
+      });
+      return this;
+    }
+
+    @CheckReturnValue
+    public Builder<T, C> withResultStatefulPartialPredicate(
+            final Supplier<PartialResultRetryPredicate<T, C>> predicateSupplier) {
+      resultPredicates.add(predicateSupplier);
+      return this;
+    }
+
+    @CheckReturnValue
+    public Builder<T, C> withJitterFactor(final double jitterfactor) {
+      if (jitterFactor > 1 || jitterFactor < 0) {
+        throw new IllegalArgumentException("Invalid jitter factor " + jitterfactor);
+      }
+      this.jitterFactor = jitterfactor;
+      return this;
+    }
+
+    @CheckReturnValue
+    public Builder<T, C> withInitialRetries(final int retries) {
+      this.nrInitialRetries = retries;
+      return this;
+    }
+
+    @CheckReturnValue
+    public Builder<T, C> withInitialDelay(final long delay, final TimeUnit unit) {
+      this.startDelayNanos = unit.toNanos(delay);
+      return this;
+    }
+
+    @CheckReturnValue
+    public Builder<T, C> withMaxDelay(final long delay, final TimeUnit unit) {
+      this.maxDelayNanos = unit.toNanos(delay);
+      return this;
+    }
+
+    public Builder<T, C> withMaxExceptionChain(final int maxExChain) {
+      maxExceptionChain = maxExChain;
+      return this;
+    }
+
+    public Builder<T, C> withExecutorService(final RetryExecutor es) {
+      execSupplier = () -> es;
+      return this;
+    }
+
+    @CheckReturnValue
+    @SuppressWarnings("unchecked")
+    public RetryPolicy<T, C> build() {
+      Supplier[] rps = resultPredicates.toArray(new Supplier[resultPredicates.size()]);
+      Supplier[] eps = exceptionPredicates.toArray(new Supplier[exceptionPredicates.size()]);
+      Supplier<RetryPredicate<T, C>> retryPredicate =
+              () -> new DefaultRetryPredicate(() -> new TypeBasedRetryDelaySupplier<>(
+              (x) -> new JitteredDelaySupplier(new FibonacciRetryDelaySupplier(nrInitialRetries,
+                      startDelayNanos, maxDelayNanos), jitterFactor)), rps, eps);
+      return new RetryPolicy<>(retryPredicate, execSupplier, maxExceptionChain
+      );
+    }
+
+  }
+
+  /**
+   * Create a retry policy builder.
+   * @param <T> the Type returned by the retried callables.
+   * @param <C> the type of the Callable's returned.
+   * @return
+   */
+  @CheckReturnValue
+  public static <T, C extends Callable<? extends T>> Builder<T, C> newBuilder() {
+    return new Builder<>();
+  }
+
+}
---
+++ spf4j-slf4j-test/src/main/java/org/spf4j/test/log/junit4/Spf4jTestLogRunListenerSingleton.java
@@ -0,0 +1,164 @@
+/*
+ * Copyright 2018 SPF4J.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.spf4j.test.log.junit4;
+
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+import java.util.ArrayDeque;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import javax.annotation.WillNotClose;
+import org.junit.runner.Description;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
+import org.junit.runner.notification.RunListener;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.spf4j.test.log.CollectTrobleshootingLogs;
+import org.spf4j.test.log.ExceptionHandoverRegistry;
+import org.spf4j.test.log.Level;
+import org.spf4j.test.log.LogCollection;
+import org.spf4j.test.log.LogPrinter;
+import org.spf4j.test.log.LogRecord;
+import org.spf4j.test.log.TestLoggers;
+import org.spf4j.test.log.UncaughtExceptionDetail;
+
+/**
+ *
+ * @author Zoltan Farkas
+ */
+@SuppressFBWarnings("FCCD_FIND_CLASS_CIRCULAR_DEPENDENCY")
+public final class Spf4jTestLogRunListenerSingleton extends RunListener {
+
+  private static final Logger LOG = LoggerFactory.getLogger(Spf4jTestLogRunListenerSingleton.class);
+
+  private static final Spf4jTestLogRunListenerSingleton INSTANCE = new Spf4jTestLogRunListenerSingleton();
+
+  private final Level minLogLevel;
+
+  private final int maxDebugLogsCollected;
+
+  private final Map<Description, LogCollection<ArrayDeque<LogRecord>>> collections;
+
+  private final boolean collectPrinted;
+
+  private final ExceptionAsserterUncaughtExceptionHandler uncaughtExceptionHandler;
+
+  private Spf4jTestLogRunListenerSingleton() {
+    minLogLevel = Level.valueOf(System.getProperty("spf4j.test.log.collectMinLevel", "DEBUG"));
+    maxDebugLogsCollected = Integer.getInteger("spf4j.test.log.collectmaxLogs", 100);
+    collectPrinted = Boolean.getBoolean("spf4j.test.log.collectPrintedLogs");
+    collections = new ConcurrentHashMap<>();
+    synchronized (Thread.class) {
+      final Thread.UncaughtExceptionHandler defaultHandler = Thread.getDefaultUncaughtExceptionHandler();
+      uncaughtExceptionHandler = new ExceptionAsserterUncaughtExceptionHandler(defaultHandler);
+      Thread.setDefaultUncaughtExceptionHandler(uncaughtExceptionHandler);
+    }
+  }
+
+  public static Spf4jTestLogRunListenerSingleton getInstance() {
+    return INSTANCE;
+  }
+
+  @Override
+  public void testRunFinished(final Result result) {
+    List<UncaughtExceptionDetail> exceptions = uncaughtExceptionHandler.getUncaughtExceptions();
+    if (!exceptions.isEmpty()) {
+      AssertionError assertionError = new AssertionError("Uncaught exceptions encountered " + exceptions);
+      for (UncaughtExceptionDetail ex : exceptions) {
+        Throwable throwable = ex.getThrowable();
+        assertionError.addSuppressed(throwable);
+        LOG.info("Uncaught exceptions, failures = {} in thread {}", result.getFailures(),
+                ex.getThread(), throwable);
+      }
+      throw assertionError;
+    }
+  }
+
+  @Override
+  public void testFailure(final Failure failure) {
+    Description description = failure.getDescription();
+    LogCollection<ArrayDeque<LogRecord>> handler = collections.get(description);
+    if (handler != null) { // will Happen when a Uncaught Exception causes a test to fail.
+      try (LogCollection<ArrayDeque<LogRecord>> h = handler) {
+        dumpDebugInfo(h.get(), description);
+      }
+    }
+  }
+
+  private void dumpDebugInfo(final Collection<LogRecord> logs,
+          final Description description) {
+    synchronized (System.out) { // do not interleave other stuff.
+      boolean first = true;
+      if (!logs.isEmpty()) {
+        for (LogRecord record : logs) {
+          if (first) {
+            LOG.info("Dumping last {} unprinted logs for {}", maxDebugLogsCollected, description);
+            first = false;
+          }
+          LogPrinter.printTo(System.out, record, "");
+        }
+        LOG.info("End dump for {}", description);
+      }
+    }
+
+  }
+
+  @Override
+  public synchronized void testFinished(final Description description) {
+    LogCollection<ArrayDeque<LogRecord>> handler = collections.remove(description);
+    try (LogCollection<ArrayDeque<LogRecord>> h = handler) {
+      handleUncaughtExceptions(description, h.get());
+    }
+  }
+
+  private void handleUncaughtExceptions(final Description description,
+          @WillNotClose final Collection<LogRecord> logs) {
+    List<UncaughtExceptionDetail> exceptions = uncaughtExceptionHandler.getUncaughtExceptions();
+    if (!exceptions.isEmpty()) {
+      AssertionError assertionError = new AssertionError("Uncaught exceptions encountered " + exceptions);
+      for (UncaughtExceptionDetail ex : exceptions) {
+        Throwable throwable = ex.getThrowable();
+        LOG.info("Uncaught exceptions during {} in thread {}", description, ex.getThread(), ex.getThrowable());
+        assertionError.addSuppressed(throwable);
+      }
+      dumpDebugInfo(logs, description);
+      throw assertionError;
+    }
+  }
+
+  @Override
+  public void testStarted(final Description description) throws Exception {
+    CollectTrobleshootingLogs annotation = description.getAnnotation(CollectTrobleshootingLogs.class);
+    Level mll = annotation == null ? minLogLevel : annotation.minLevel();
+    boolean clp = annotation == null ? collectPrinted : annotation.collectPrinted();
+    collections.put(description, TestLoggers.sys().collect(mll, maxDebugLogsCollected, clp));
+    super.testStarted(description);
+  }
+
+  public ExceptionHandoverRegistry getUncaughtExceptionHandler() {
+    return uncaughtExceptionHandler;
+  }
+
+  @Override
+  public String toString() {
+    return "DetailOnFailureRunListener{" + "minLogLevel=" + minLogLevel
+            + ", maxDebugLogsCollected=" + maxDebugLogsCollected + ", collections="
+            + collections + ", collectPrinted=" + collectPrinted + '}';
+  }
+
+}
