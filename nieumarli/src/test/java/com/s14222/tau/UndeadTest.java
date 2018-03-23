c170099cffd77c177a9824114f75a8f585ab4ef2
Luc Esape <luc.esape@gmail.com>
2018-03-23 18:58:50 +0100

    Bug commit from the following repository s14222/Nieumarli This bug commit is a reflect of source code from: http://github.com/s14222/Nieumarli/commit/0a2e3ad8528a2bb6ea8c96711350f8def9ece4af.

---
+++ nieumarli/src/test/java/com/s14222/tau/UndeadTest.java
@@ -0,0 +1,62 @@
+package com.s14222.tau;
+
+import com.s14222.tau.dbdemo.service.UndeadManagerImpl;
+import com.s14222.tau.domain.Undead;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class UndeadTest {
+
+    @Test
+    public void TestDodania(){
+        Undead undead = new Undead(0, "Trups",1,100,100);
+        try {
+            UndeadManagerImpl undeadManager = new UndeadManagerImpl();
+            undeadManager.addUndead(undead);
+            List<Undead> undeads = undeadManager.getAllUndeads();
+            for(Undead u : undeads) {
+                if(u.getNazwa().contains("Trups")){
+                    //undeadManager.deleteUndead(u);
+                    return;
+                }
+            }
+        } catch (SQLException e) {
+            fail();
+        }
+        fail();
+
+    }
+
+    @Test
+    public void TestUsuniecia() {
+        Undead undead = new Undead(0, "Trups", 1, 100, 100);
+        Undead toDelete = null;
+        try {
+            UndeadManagerImpl undeadManager = new UndeadManagerImpl();
+            undeadManager.addUndead(undead);
+            List<Undead> undeads = undeadManager.getAllUndeads();
+
+            for (Undead u : undeads) {
+                if (u.getNazwa().contains("Trups")) {
+                    toDelete = u;
+                }
+            }
+            if(toDelete == null) {
+                fail();
+            }
+            undeadManager.deleteUndead(toDelete);
+            for (Undead u : undeads) {
+                if (u.getNazwa().contains("Trups")) {
+                   fail();
+                }
+            }
+        } catch (SQLException e) {
+            fail();
+        }
+        fail();
+    }
+}
+
