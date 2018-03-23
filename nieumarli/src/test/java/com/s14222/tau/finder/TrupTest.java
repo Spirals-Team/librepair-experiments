15e897452ffbd1c0803120dc21390fb3642252bf
Luc Esape <luc.esape@gmail.com>
2018-03-23 18:50:47 +0100

    Bug commit from the following repository s14222/Nieumarli This bug commit is a reflect of source code from: http://github.com/s14222/Nieumarli/commit/f5615ec0604d2db9566fdf91fba1291c29b9eff3.

---
+++ nieumarli/src/test/java/com/s14222/tau/finder/TrupTest.java
@@ -0,0 +1,25 @@
+package com.s14222.tau.finder;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import org.junit.*;
+import com.s14222.tau.finder.*;
+public class TrupTest {
+
+  @Test
+    public void testTrupa() {
+      Trup trup = new Trup();
+      trup.sila = 3;
+      trup.aktywujSuperMoc();
+      assertNotNull(trup);
+    }
+
+    @Test
+    public void KiedySuperMocAktywowana_ToZwiekszSileO3() {
+      Trup trup = new Trup();
+      trup.sila = 3;
+      trup.aktywujSuperMoc();
+      assertEquals(trup.sila, 6);
+    }
+}
