847751f30967f921ffcd10a36bd350011807c21d
Luc Esape <luc.esape@gmail.com>
2018-03-25 18:54:15 +0200

    Bug commit from the following repository Arquisoft/Loader_i3a This bug commit is a reflect of source code from: http://github.com/Arquisoft/Loader_i3a/commit/be2b406d973a749510c46e94e1377f805660a06c.

---
+++ src/main/java/es/uniovi/asw/parser/ReadList.java
@@ -0,0 +1,7 @@
+package es.uniovi.asw.parser;
+
+import java.util.Set;
+
+public interface ReadList {
+	Set<Agent> parse(String ruta);
+}
---
+++ src/main/java/es/uniovi/asw/parser/lettergenerators/LetterGenerator.java
@@ -0,0 +1,8 @@
+package es.uniovi.asw.parser.lettergenerators;
+
+import es.uniovi.asw.parser.Agent;
+
+
+public interface LetterGenerator {
+	void generatePersonalLetter(Agent c);
+}
