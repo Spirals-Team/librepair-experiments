c81811cf65773595881415567f563475b19b226f
Luc Esape <luc.esape@gmail.com>
2018-03-25 16:59:48 +0200

    Bug commit from the following repository JMRI/JMRI This bug commit is a reflect of source code from: http://github.com/JMRI/JMRI/commit/5dae61e929d04813804fc79e53a058a12ca71c44.

---
+++ java/test/jmri/jmrix/openlcb/OlcbConfigurationManagerTest.java
@@ -0,0 +1,50 @@
+package jmri.jmrix.openlcb;
+
+import jmri.util.JUnitUtil;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import jmri.jmrix.can.TestTrafficController;
+
+/**
+ *
+ * @author Paul Bender Copyright (C) 2017	
+ */
+public class OlcbConfigurationManagerTest {
+
+    OlcbSystemConnectionMemo scm;
+
+    @Test
+    public void testCTor() {
+        OlcbConfigurationManager t = new OlcbConfigurationManager(scm);
+        Assert.assertNotNull("exists",t);
+    }
+
+    @Test
+    public void testConfigureManagers() {
+        OlcbConfigurationManager t = new OlcbConfigurationManager(scm);
+        // this tet verifies this does not throw an exception
+        t.configureManagers(); 
+    }
+
+    // The minimal setup for log4J
+    @Before
+    public void setUp() {
+        JUnitUtil.setUp();
+        //OlcbTestInterface testIf = new OlcbTestInterface();
+        scm  = new OlcbSystemConnectionMemo();
+        //scm.setTrafficController(testIf.tc);
+        //scm.setInterface(testIf.iface);
+        TestTrafficController tc = new TestTrafficController();
+        scm.setTrafficController(tc);
+    }
+
+    @After
+    public void tearDown() {
+        JUnitUtil.tearDown();
+    }
+
+    // private final static Logger log = LoggerFactory.getLogger(OlcbConfigurationManagerTest.class);
+
+}
