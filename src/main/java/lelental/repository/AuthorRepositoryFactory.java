f77809a2208eebab10da51f9ba68c1005b5de9ce
Luc Esape <luc.esape@gmail.com>
2018-03-25 12:45:13 +0200

    Bug commit from the following repository Veok/lab01 This bug commit is a reflect of source code from: http://github.com/Veok/lab01/commit/7f38d193c8a0ec97910ce04cf018f8f48140a927.

---
+++ src/main/java/lelental/repository/AuthorRepositoryFactory.java
@@ -0,0 +1,16 @@
+package lelental.repository;
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ * @author Paweł Lelental
+ **/
+public class AuthorRepositoryFactory {
+
+
+    public static AuthorRepository getInstance() throws SQLException {
+        String dbConnectionURL = "jdbc:hsqldb:hsql://localhost";
+        return new AuthorRepositoryImpl(DriverManager.getConnection(dbConnectionURL));
+    }
+}
