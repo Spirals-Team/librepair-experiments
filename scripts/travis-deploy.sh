ce5068b66ad846f92343d93457ff80bc95aee7fe
Luc Esape <luc.esape@gmail.com>
2018-04-07 19:19:13 +0200

    Bug commit from the following repository Willena/sqlite-jdbc-crypt This bug commit is a reflect of source code from: http://github.com/Willena/sqlite-jdbc-crypt/commit/1d994635df58286e986a78ad7142534170712932.

---
+++ scripts/travis-deploy.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+_SCRIPT_DIR=$(dirname $(readlink -f $0))
+_BASE_DIR=$_SCRIPT_DIR/../
+_REPO_URL="https://github.com/Willena/sqlite-jdbc-crypt.git"
+
+source $_SCRIPT_DIR/git-config.sh
+set -evx
+
+#VERSION=$(mvn -q -Dexec.executable="echo" -Dexec.args='${project.version}' --non-recursive org.codehaus.mojo:exec-maven-plugin:1.2.1:exec)
+
+# Deploy a snapshot version only for master branch and jdk8
+#if [[ "$TRAVIS_JDK_VERSION" == "oraclejdk8" ]]; then
+  #if [[ "$TRAVIS_PULL_REQUEST" == "false" ]] && [[ "$VERSION" == *SNAPSHOT ]]; then
+  #   make && mvn -s settings.xml deploy -DskipTests;
+  #else
+#    make;
+  #fi;
+#else
+#  make linux64 && mvn test;
+#fi;
+
+cd $_BASE_DIR
+
+
+echo "The Given type is $TYPE"
+echo "Starting build !"
+
+make CODEC_TYPE=$TYPE;
+
+ls /home/travis/build/Willena/sqlite-jdbc-crypt/target/
+. $_BASE_DIR/VERSION
+
+#If we are on a tag
+if git describe --exact-match --tags HEAD
+then
+  echo "Do not create another tag... we are on it .. "
+else
+  git tag -f "$version"
+  git push --tags --quiet "https://${GH_TOKEN}@github.com/Willena/sqlite-jdbc-crypt.git"
+fi
