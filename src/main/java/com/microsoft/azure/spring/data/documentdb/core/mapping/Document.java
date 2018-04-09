b10563676200aef2ecfbae78781d8c80bad5629c
Luc Esape <luc.esape@gmail.com>
2018-04-09 07:55:02 +0200

    Bug commit from the following repository Microsoft/spring-data-documentdb This bug commit is a reflect of source code from: http://github.com/Microsoft/spring-data-documentdb/commit/d2a66fa94c8cfda608e90b29d6d2bca47c4ea681.

---
+++ src/main/java/com/microsoft/azure/spring/data/documentdb/core/mapping/Document.java
@@ -0,0 +1,22 @@
+/**
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See LICENSE in the project root for
+ * license information.
+ */
+package com.microsoft.azure.spring.data.documentdb.core.mapping;
+
+import com.microsoft.azure.spring.data.documentdb.Constants;
+import org.springframework.data.annotation.Persistent;
+
+import java.lang.annotation.*;
+
+@Persistent
+@Inherited
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.TYPE})
+public @interface Document {
+
+    String collection() default Constants.DEFAULT_COLLECTION_NAME;
+
+    String ru() default Constants.DEFAULT_REQUEST_UNIT;
+}
