88bda0de51e9ecd9c23be68ddaf6df69bf874774
Luc Esape <luc.esape@gmail.com>
2018-04-09 21:03:08 +0200

    Bug commit from the following repository zolyfarkas/spf4j This bug commit is a reflect of source code from: http://github.com/zolyfarkas/spf4j/commit/dbd10c39200d7714f527ed7501d8891cdc183061.

---
+++ spf4j-slf4j-test/src/main/java/org/spf4j/test/log/LogPrinter.java
@@ -0,0 +1,288 @@
+/*
+ * Copyright 2018 SPF4J.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.spf4j.test.log;
+
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.PrintStream;
+import java.io.UncheckedIOException;
+import java.io.Writer;
+import java.nio.charset.Charset;
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
+import java.time.temporal.ChronoField;
+import static java.time.temporal.ChronoField.HOUR_OF_DAY;
+import static java.time.temporal.ChronoField.MINUTE_OF_HOUR;
+import static java.time.temporal.ChronoField.SECOND_OF_MINUTE;
+import java.util.Iterator;
+import javax.activation.MimeType;
+import javax.annotation.Nullable;
+import javax.annotation.ParametersAreNonnullByDefault;
+import org.slf4j.Marker;
+import org.spf4j.base.EscapeJsonStringAppendableWrapper;
+import org.spf4j.base.Slf4jMessageFormatter;
+import org.spf4j.base.Throwables;
+import org.spf4j.io.ByteArrayBuilder;
+import org.spf4j.io.ConfigurableAppenderSupplier;
+import org.spf4j.io.ObjectAppender;
+import org.spf4j.recyclable.impl.ArraySuppliers;
+
+/**
+ * @author Zoltan Farkas
+ */
+@ParametersAreNonnullByDefault
+public final class LogPrinter implements LogHandler {
+
+  public static final String PRINTED = "PRINTED";
+
+  private static final DateTimeFormatter FMT =
+          TestUtils.isExecutedFromIDE() ? new DateTimeFormatterBuilder()
+                .appendValue(HOUR_OF_DAY, 2)
+                .appendLiteral(':')
+                .appendValue(MINUTE_OF_HOUR, 2)
+                .optionalStart()
+                .appendLiteral(':')
+                .appendValue(SECOND_OF_MINUTE, 2)
+                .optionalStart()
+                .appendFraction(ChronoField.MILLI_OF_SECOND, 3, 3, true)
+                .toFormatter().withZone(ZoneId.systemDefault())
+          : DateTimeFormatter.ISO_INSTANT;
+
+  private static final ThreadLocal<Buffer> TL_BUFFER = new ThreadLocal<Buffer>() {
+    @Override
+    protected Buffer initialValue() {
+      return new Buffer();
+    }
+  };
+
+  private static final ConfigurableAppenderSupplier TO_STRINGER = new ConfigurableAppenderSupplier();
+
+  private final Level minLogged;
+
+  private static final class Buffer {
+
+    private final ByteArrayBuilder bab;
+
+    private final Writer writer;
+
+    private final EscapeJsonStringAppendableWrapper writerEscaper;
+
+    Buffer() {
+      bab = new ByteArrayBuilder(512, ArraySuppliers.Bytes.JAVA_NEW);
+      writer = new BufferedWriter(new OutputStreamWriter(bab, Charset.defaultCharset()));
+      writerEscaper = new EscapeJsonStringAppendableWrapper(writer);
+    }
+
+    void clear() {
+      bab.reset();
+    }
+
+    ByteArrayBuilder getBab() {
+      return bab;
+    }
+
+    Writer getWriter() {
+      return writer;
+    }
+
+    EscapeJsonStringAppendableWrapper getWriterEscaper() {
+      return writerEscaper;
+    }
+
+    byte[] getBytes() throws IOException {
+      writer.flush();
+      return bab.getBuffer();
+    }
+
+    int size() {
+      return bab.size();
+    }
+
+  }
+
+  public static ConfigurableAppenderSupplier getAppenderSupplier() {
+    return TO_STRINGER;
+  }
+
+  LogPrinter(final Level minLogged) {
+    this.minLogged = minLogged;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public Handling handles(final Level level) {
+    return level.ordinal() >= minLogged.ordinal() ? Handling.HANDLE_PASS : Handling.NONE;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @SuppressFBWarnings({ "CFS_CONFUSING_FUNCTION_SEMANTICS", "EXS_EXCEPTION_SOFTENING_NO_CHECKED" })
+  @Override
+  public LogRecord handle(final LogRecord record) {
+    if (record.hasAttachment(PRINTED)) {
+      return record;
+    }
+    try {
+      Buffer buff = TL_BUFFER.get();
+      buff.clear();
+      print(record, buff.getWriter(), buff.getWriterEscaper(), "");
+      if (record.getLevel() == Level.ERROR) {
+        System.err.write(buff.getBytes(), 0, buff.size());
+        System.err.flush();
+      } else {
+        System.out.write(buff.getBytes(), 0, buff.size());
+        System.out.flush();
+      }
+    } catch (IOException ex) {
+      throw new UncheckedIOException(ex);
+    }
+    record.attach(PRINTED);
+    return record;
+  }
+
+  public static void printTo(final PrintStream stream, final LogRecord record, final String annotate) {
+    Buffer buff = TL_BUFFER.get();
+    buff.clear();
+    try {
+      print(record, buff.getWriter(), buff.getWriterEscaper(), annotate);
+      stream.write(buff.getBytes(), 0, buff.size());
+      stream.flush();
+    } catch (IOException ex) {
+      throw new UncheckedIOException(ex);
+    }
+  }
+
+   static void printMarker(final Marker marker, final Appendable wr,
+          final EscapeJsonStringAppendableWrapper wrapper)
+          throws IOException {
+      if (marker.hasReferences()) {
+        wr.append('{');
+        wr.append('"');
+        wrapper.append(marker.getName());
+        wr.append("\":[");
+        Iterator<Marker> it = marker.iterator();
+        if (it.hasNext()) {
+          printMarker(it.next(), wr, wrapper);
+          while (it.hasNext()) {
+            wr.append(',');
+            printMarker(it.next(), wr, wrapper);
+          }
+        }
+        wr.append("]}");
+      } else {
+        wr.append('"');
+        wrapper.append(marker.getName());
+        wr.append('"');
+      }
+   }
+
+
+  static void print(final LogRecord record, final Appendable wr,
+          final EscapeJsonStringAppendableWrapper wrapper, final String annotate)
+          throws IOException {
+    wr.append(annotate);
+    FMT.formatTo(Instant.ofEpochMilli(record.getTimeStamp()), wr);
+    wr.append(' ');
+    String level = record.getLevel().toString();
+    wr.append(level);
+    wr.append(' ');
+    Marker marker = record.getMarker();
+    if (marker != null) {
+      printMarker(marker, wr, wrapper);
+      wr.append(' ');
+    }
+    Throwables.writeAbreviatedClassName(record.getLogger().getName(), wr);
+    wr.append(" \"");
+    wrapper.append(record.getThread().getName());
+    wr.append("\" \"");
+    Object[] arguments = record.getArguments();
+    int i = Slf4jMessageFormatter.format(LogPrinter::exHandle, 0, wrapper, record.getFormat(),
+            TO_STRINGER, arguments);
+    wr.append("\" ");
+    Throwable t = null;
+    if (i < arguments.length) {
+      boolean first = true;
+      for (; i < arguments.length; i++) {
+        Object arg = arguments[i];
+        if (arg instanceof Throwable) {
+          if (t == null) {
+            t = (Throwable) arg;
+          } else {
+            t.addSuppressed(t); // not ideal
+          }
+        } else {
+          if (!first) {
+            wr.append(", ");
+          } else {
+            wr.append('[');
+            first = false;
+          }
+          printObject(arg, wr, wrapper);
+        }
+      }
+      if (!first) {
+        wr.append(']');
+      }
+    }
+    if (t != null) {
+      wr.append('\n');
+      Throwables.writeTo(t, wr, Throwables.PackageDetail.SHORT);
+    }
+    wr.append('\n');
+  }
+
+  private static void printObject(@Nullable final Object obj,
+          final Appendable wr, final EscapeJsonStringAppendableWrapper wrapper) throws IOException {
+    if (obj == null) {
+      wr.append("null");
+    } else {
+      ObjectAppender ostrApp = TO_STRINGER.apply(obj.getClass());
+      MimeType type = ostrApp.getAppendedType();
+      if ("json".equalsIgnoreCase(type.getSubType())) {
+        ostrApp.append(obj, wr);
+      } else {
+        wr.append('"');
+        ostrApp.append(obj, wrapper);
+        wr.append('"');
+      }
+    }
+  }
+
+  static void exHandle(final Object obj, final Appendable sbuf, final Throwable t) throws IOException {
+    String className = obj.getClass().getName();
+    sbuf.append("[FAILED toString() for ");
+    sbuf.append(className);
+    sbuf.append("]{");
+    Throwables.writeTo(t, sbuf, Throwables.PackageDetail.SHORT);
+    sbuf.append('}');
+  }
+
+  @Override
+  public String toString() {
+    return "LogPrinter{" + "minLogged=" + minLogged + '}';
+  }
+
+
+
+
+}
---
+++ spf4j-slf4j-test/src/test/java/org/spf4j/test/log/TestLoggerFactoryTest.java
@@ -0,0 +1,298 @@
+/*
+ * Copyright 2018 SPF4J.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.spf4j.test.log;
+
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+import java.util.ArrayDeque;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+import org.junit.Assert;
+import org.hamcrest.Matchers;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Marker;
+import org.slf4j.MarkerFactory;
+import org.spf4j.base.ExecutionContexts;
+import org.spf4j.io.MimeTypes;
+
+/**
+ *
+ * @author Zoltan Farkas
+ */
+@SuppressFBWarnings("LO_INCORRECT_NUMBER_OF_ANCHOR_PARAMETERS")
+public class TestLoggerFactoryTest {
+
+  private static final Logger LOG = LoggerFactory.getLogger(TestLoggerFactoryTest.class);
+
+  @Test
+  public void testSomeHandler() {
+    HandlerRegistration reg = TestLoggers.sys().print("org.spf4j.test", Level.TRACE);
+    Assert.assertNotNull(reg);
+  }
+
+
+  @Test
+  public void testLogging() {
+    TestLoggers tLog = TestLoggers.sys();
+    try (HandlerRegistration printer = tLog.print("org.spf4j.test", Level.TRACE)) {
+      logTests();
+      logMarkerTests();
+      LogAssert expect = tLog.expect("org.spf4j.test", Level.ERROR,
+              LogMatchers.hasMatchingFormat(Matchers.equalTo("Booo")));
+      LOG.error("Booo", new RuntimeException());
+      expect.assertObservation();
+    }
+  }
+
+  public static void logTests() {
+    LOG.trace("Hello logger", new RuntimeException());
+    LOG.trace("Hello logger");
+    LOG.trace("Hello logger {}", 1);
+    LOG.trace("Hello logger {} {} {}", 1, 2, 3);
+    LOG.trace("Hello logger {} {} {}", 1, 2, 3, new RuntimeException());
+    LOG.debug("Hello logger", new RuntimeException());
+    LOG.debug("Hello logger");
+    LOG.debug("Hello logger {}", 1);
+    LOG.debug("Hello logger {} {} {}", 1, 2, 3);
+    LOG.debug("Hello logger {} {} {}", 1, 2, 3, new RuntimeException());
+    LOG.info("Hello logger", new RuntimeException());
+    LOG.info("Hello logger");
+    LOG.info("Hello logger {}", 1);
+    LOG.info("Hello logger {} {} {}", 1, 2, 3);
+    LOG.info("Hello logger {} {} {}", 1, 2, 3, new RuntimeException());
+    LOG.warn("Hello logger", new RuntimeException());
+    LOG.warn("Hello logger");
+    LOG.warn("Hello logger {}", 1);
+    LOG.warn("Hello logger {} {} {}", 1, 2, 3);
+    LOG.warn("Hello logger {} {} {}", 1, 2, 3, new RuntimeException());
+    LogAssert expect = TestLoggers.sys().expect("org.spf4j.test", Level.ERROR, 5,
+            LogMatchers.hasMatchingFormat(Matchers.containsString("Hello logger")));
+    LOG.error("Hello logger", new RuntimeException());
+    LOG.error("Hello logger");
+    LOG.error("Hello logger {}", 1);
+    LOG.error("Hello logger {} {} {}", 1, 2, 3);
+    LOG.error("Hello logger {} {} {}", 1, 2, 3, new RuntimeException());
+    expect.assertObservation();
+  }
+
+  public static void logMarkerTests() {
+    Marker marker = MarkerFactory.getMarker("TEST");
+    Marker marker2 = MarkerFactory.getMarker("TEST2");
+    marker2.add(marker);
+    marker2.add(MarkerFactory.getMarker("TEST3"));
+
+    LOG.trace(marker, "Hello logger", new RuntimeException());
+    LOG.trace(marker, "Hello logger");
+    LOG.trace(marker, "Hello logger {}", 1);
+    LOG.trace(marker, "Hello logger {} {} {}", 1, 2, 3);
+    LOG.trace(marker, "Hello logger {} {} {}", 1, 2, 3, new RuntimeException());
+    LOG.debug(marker, "Hello logger", new RuntimeException());
+    LOG.debug(marker, "Hello logger");
+    LOG.debug(marker, "Hello logger {}", 1);
+    LOG.debug(marker, "Hello logger {} {} {}", 1, 2, 3);
+    LOG.debug(marker, "Hello logger {} {} {}", 1, 2, 3, new RuntimeException());
+    LOG.info(marker, "Hello logger", new RuntimeException());
+    LOG.info(marker2, "Hello logger");
+    LOG.info(marker2, "Hello logger {}", 1);
+    LOG.info(marker2, "Hello logger {} {} {}", 1, 2, 3);
+    LOG.info(marker2, "Hello logger {} {} {}", 1, 2, 3, new RuntimeException());
+    LOG.warn(marker, "Hello logger", new RuntimeException());
+    LOG.warn(marker, "Hello logger");
+    LOG.warn(marker, "Hello logger {}", 1);
+    LOG.warn(marker, "Hello logger {} {} {}", 1, 2, 3);
+    LOG.warn(marker, "Hello logger {} {} {}", 1, 2, 3, new RuntimeException());
+    LogAssert expect = TestLoggers.sys().expect("org.spf4j.test", Level.ERROR, 5,
+            Matchers.allOf(
+                    LogMatchers.hasMatchingFormat(Matchers.containsString("Hello logger")),
+                    LogMatchers.hasMarker(marker)));
+    LOG.error(marker, "Hello logger", new RuntimeException());
+    LOG.error(marker, "Hello logger");
+    LOG.error(marker, "Hello logger {}", 1);
+    LOG.error(marker, "Hello logger {} {} {}", 1, 2, 3);
+    LOG.error(marker, "Hello logger {} {} {}", 1, 2, 3, new RuntimeException());
+    expect.assertObservation();
+  }
+
+  @Test
+  @SuppressFBWarnings("LO_TOSTRING_PARAMETER") // this is on purpose configs change over time.
+  public void testIsEnabled() {
+    LOG.debug("Log Config: {}", TestLoggers.sys().toString());
+    Marker marker = MarkerFactory.getMarker("TEST");
+    Assert.assertFalse(LOG.isTraceEnabled());
+    Assert.assertFalse(LOG.isTraceEnabled(marker));
+
+    //DEBUG is enabled since debug logs are collected in the background...
+    Assert.assertTrue(LOG.isDebugEnabled());
+    Assert.assertTrue(LOG.isDebugEnabled(marker));
+
+    Assert.assertTrue(LOG.isInfoEnabled());
+    Assert.assertTrue(LOG.isInfoEnabled(marker));
+    Assert.assertTrue(LOG.isWarnEnabled());
+    Assert.assertTrue(LOG.isWarnEnabled(marker));
+    Assert.assertTrue(LOG.isErrorEnabled());
+    Assert.assertTrue(LOG.isErrorEnabled(marker));
+  }
+
+  @Test(expected = AssertionError.class)
+  public void testLogging2() {
+    LogAssert expect = TestLoggers.sys().dontExpect("org.spf4j.test", Level.ERROR,
+            LogMatchers.hasFormat("Booo"));
+    LOG.error("Booo", new RuntimeException());
+    expect.assertObservation();
+  }
+
+  @Test
+  public void testLoggingJul() {
+    LogAssert expect = TestLoggers.sys().expect("my.test", Level.DEBUG,
+            LogMatchers.hasFormat("Bla Bla"),
+            LogMatchers.hasFormat("Boo Boo param"));
+    java.util.logging.Logger logger = java.util.logging.Logger.getLogger("my.test");
+    logger.info("Bla Bla");
+    logger.log(java.util.logging.Level.FINE, "Boo Boo {0}", "param");
+    expect.assertObservation();
+  }
+
+  @Test
+  @CollectTrobleshootingLogs(minLevel = Level.TRACE)
+  public void testIgnore() {
+    TestLoggers config = TestLoggers.sys();
+    try (HandlerRegistration ir = config.ignore("org.spf4j.test", Level.DEBUG, Level.ERROR)) {
+      LogAssert assrt = config.expect("", Level.TRACE, LogMatchers.hasFormat("trace"));
+      LogAssert assrt2 = config.dontExpect("", Level.DEBUG, LogMatchers.hasFormat("Bla bla"));
+      LOG.debug("Bla bla");
+      LOG.trace("trace");
+      assrt.assertObservation();
+      assrt2.assertObservation();
+    }
+  }
+
+  @Test
+  @CollectTrobleshootingLogs(minLevel = Level.TRACE)
+  public void testCollect() {
+    TestLoggers config = TestLoggers.sys();
+    try (LogCollection<Long> c = config.collect("org.spf4j.test", Level.INFO, true, Collectors.counting())) {
+      LOG.info("m1");
+      LOG.info("m2");
+      Assert.assertEquals(2L, (long) c.get());
+    }
+  }
+
+  @Test
+  public void testLogJson() {
+    LogAssert expect = TestLoggers.sys().expect("", Level.INFO,
+            Matchers.allOf(LogMatchers.hasFormat("Json Payload"),
+                    LogMatchers.hasMatchingExtraArguments(Matchers.arrayContaining(this))));
+    LogPrinter.getAppenderSupplier().register(TestLoggerFactoryTest.class,
+            MimeTypes.APPLICATION_JSON, (o, a) -> {
+              a.append("{\"a\" : \"b\"}");
+            });
+    LOG.info("Json Payload", this);
+    expect.assertObservation();
+  }
+
+  @Test(expected = AssertionError.class)
+  public void testLogging3() {
+    LogAssert expect = TestLoggers.sys().expect("org.spf4j.test", Level.ERROR,
+            LogMatchers.hasFormat("Booo"));
+    expect.assertObservation();
+  }
+
+  @Test
+  public void testLogging33() {
+    LogAssert expect = TestLoggers.sys().dontExpect("org.spf4j.test", Level.ERROR,
+            LogMatchers.hasMatchingFormat(Matchers.equalTo("Booo")));
+    expect.assertObservation();
+  }
+
+  @Ignore
+  @Test
+  @CollectTrobleshootingLogs(minLevel = Level.TRACE, collectPrinted = true)
+  public void testLogging4() {
+    LOG.trace("lala");
+    LOG.debug("log {}", 1);
+    LOG.debug("log {} {}", 1, 2);
+    LOG.debug("log {} {} {}", 1, 2, 3);
+    LOG.debug("log {} {} {}", 1, 2, 3, 4);
+    Assert.fail("booo");
+  }
+
+  @Test
+  public void testUncaught() throws InterruptedException {
+    IllegalStateException ex = new IllegalStateException();
+    Thread thread = new Thread(() -> {
+      throw ex;
+    });
+    TestLoggers config = TestLoggers.sys();
+    AsyncObservationAssert obs = config
+            .expectUncaughtException(
+                    UncaughtExceptionDetail.hasThrowable(Matchers.equalTo(ex)));
+    thread.start();
+    obs.assertObservation(5, TimeUnit.SECONDS);
+  }
+
+  @SuppressFBWarnings("UTAO_JUNIT_ASSERTION_ODDITIES_NO_ASSERT")
+  @Test
+  public void testUncaught2() throws InterruptedException {
+    TestLoggers config = TestLoggers.sys();
+    AsyncObservationAssert obs = config
+            .expectUncaughtException(
+                    UncaughtExceptionDetail.hasThrowable(Matchers.equalTo(new IllegalStateException())));
+    try {
+      obs.assertObservation(3, TimeUnit.SECONDS);
+      Assert.fail();
+    } catch (AssertionError ex) {
+      // expected
+    }
+  }
+
+  @Test
+  public void testLogging5() {
+    LogCollection<ArrayDeque<LogRecord>> collect = TestLoggers.sys().collect(Level.DEBUG, 10, true);
+    LOG.debug("log {}", 1);
+    LOG.debug("log {} {}", 1, 2);
+    LOG.debug("log {} {} {}", 1, 2, 3);
+    LOG.debug("log {} {} {}", 1, 2, 3, 4);
+    Assert.assertEquals(4, collect.get().size());
+  }
+
+  @Test
+  public void testExecutionContext() {
+     Assert.assertNotNull(ExecutionContexts.current());
+  }
+
+
+  @Test
+  public void testAsyncLogging() throws InterruptedException {
+    LogAssert expect = TestLoggers.sys().expect("org.spf4j.test.log", Level.ERROR, LogMatchers.hasFormat("async"));
+    new Thread(() -> {
+      LOG.error("async");
+    }).start();
+    expect.assertObservation(3, TimeUnit.SECONDS);
+  }
+
+  @Test(expected = AssertionError.class)
+  public void testAsyncLogging2() throws InterruptedException {
+    LogAssert expect = TestLoggers.sys().expect("org.spf4j.test.log", Level.ERROR, LogMatchers.hasFormat("async"));
+    new Thread(() -> {
+      LOG.info("coco");
+    }).start();
+    expect.assertObservation(3, TimeUnit.SECONDS);
+  }
+
+
+}
