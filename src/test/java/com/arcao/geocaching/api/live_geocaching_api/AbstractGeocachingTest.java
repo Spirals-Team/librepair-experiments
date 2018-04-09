170983db1475c55c5342032685c3640a6f8bc1d4
Luc Esape <luc.esape@gmail.com>
2018-04-09 23:36:14 +0200

    Bug commit from the following repository arcao/geocaching-api This bug commit is a reflect of source code from: http://github.com/arcao/geocaching-api/commit/8053e391a279601828861b45e471e9de65c69643.

---
+++ src/test/java/com/arcao/geocaching/api/live_geocaching_api/AbstractGeocachingTest.java
@@ -0,0 +1,38 @@
+package com.arcao.geocaching.api.live_geocaching_api;
+
+import com.arcao.geocaching.api.GeocachingApi;
+import com.arcao.geocaching.api.LiveGeocachingApi;
+import com.arcao.geocaching.api.configuration.GeocachingApiConfiguration;
+import com.arcao.geocaching.api.downloader.DefaultJsonDownloader;
+import com.arcao.geocaching.api.downloader.JsonDownloader;
+import com.arcao.geocaching.api.exception.GeocachingApiException;
+
+import org.apache.commons.lang3.StringUtils;
+import org.junit.BeforeClass;
+
+public abstract class AbstractGeocachingTest {
+    protected static GeocachingApi api = null;
+
+    // generated via oAuth example, see: https://github.com/arcao/geocaching-api-examples
+    private static final String STAGING_AUTH_TOKEN = "JvJcmhBJ88iDoWR107RIKpQgLLU=";
+
+    @BeforeClass
+    public static void setUp() throws GeocachingApiException {
+        LiveGeocachingApi.Builder builder = LiveGeocachingApi.builder();
+
+        GeocachingApiConfiguration configuration = GeocachingApiConfiguration.STAGING;
+
+        if (!Boolean.parseBoolean(StringUtils.defaultIfEmpty(System.getenv("GEOCACHING_STAGING"), "true"))) {
+            configuration = GeocachingApiConfiguration.PRODUCTION;
+        }
+
+        JsonDownloader downloader = new DefaultJsonDownloader(configuration).debug(true);
+
+        builder.configuration(configuration);
+        builder.downloader(downloader);
+
+        api = builder.build();
+
+        api.openSession(StringUtils.defaultIfEmpty(System.getenv("GEOCACHING_TOKEN"), STAGING_AUTH_TOKEN));
+    }
+}
