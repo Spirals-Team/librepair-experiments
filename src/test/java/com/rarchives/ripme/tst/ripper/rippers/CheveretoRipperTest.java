e930731252883394cb2d028ce3974924e8f841c7
Luc Esape <luc.esape@gmail.com>
2018-04-15 00:11:53 +0200

    Bug commit from the following repository kevin51jiang/ripme This bug commit is a reflect of source code from: http://github.com/kevin51jiang/ripme/commit/aae72990a473f9817c001b73e6e514b140d70562.

---
+++ src/test/java/com/rarchives/ripme/tst/ripper/rippers/CheveretoRipperTest.java
@@ -0,0 +1,13 @@
+package com.rarchives.ripme.tst.ripper.rippers;
+
+import java.io.IOException;
+import java.net.URL;
+
+import com.rarchives.ripme.ripper.rippers.CheveretoRipper;
+
+public class CheveretoRipperTest extends RippersTest {
+    public void testTagFox() throws IOException {
+        CheveretoRipper ripper = new CheveretoRipper(new URL("http://tag-fox.com/album/Thjb"));
+        testRipper(ripper);
+    }
+}
