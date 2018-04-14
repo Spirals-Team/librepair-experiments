00bb787721506ad4fbb14a1ea3005fb1288f3630
Luc Esape <luc.esape@gmail.com>
2018-04-14 17:19:48 +0200

    Bug commit from the following repository runelite/runelite This bug commit is a reflect of source code from: http://github.com/runelite/runelite/commit/559483bb94df30ef9a3c8749346f4b30a29375b1.

---
+++ runelite-api/src/main/java/net/runelite/api/SpriteID.java
@@ -0,0 +1,84 @@
+/*
+ * Copyright (c) 2018, Lotto <https://github.com/devLotto>, SoyChai <https://github.com/SoyChai>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package net.runelite.api;
+
+// Note: This class is not complete: these sprites were manually gathered
+// through the cache and widget inspector. Please add sprites as you happen to use them.
+public final class SpriteID
+{
+	public static final int SKILL_ATTACK = 197;
+	public static final int SKILL_STRENGTH = 198;
+	public static final int SKILL_DEFENCE = 199;
+	public static final int SKILL_RANGED = 200;
+	public static final int SKILL_PRAYER = 201;
+	public static final int SKILL_MAGIC = 202;
+	public static final int SKILL_HITPOINTS = 203;
+	public static final int SKILL_AGILITY = 204;
+	public static final int SKILL_HERBLORE = 205;
+	public static final int SKILL_THIEVING = 206;
+	public static final int SKILL_CRAFTING = 207;
+	public static final int SKILL_FLETCHING = 208;
+	public static final int SKILL_MINING = 209;
+	public static final int SKILL_SMITHING = 210;
+	public static final int SKILL_FISHING = 211;
+	public static final int SKILL_COOKING = 212;
+	public static final int SKILL_FIREMAKING = 213;
+	public static final int SKILL_WOODCUTTING = 214;
+	public static final int SKILL_RUNECRAFT = 215;
+	public static final int SKILL_SLAYER = 216;
+	public static final int SKILL_FARMING = 217;
+	public static final int UNKNOWN_SHOVEL = 218;
+	public static final int UNKNOWN_RAT = 219;
+	public static final int SKILL_HUNTER = 220;
+	public static final int SKILL_CONSTRUCTION = 221;
+	public static final int SKILL_TOTAL_XP = 222;
+	public static final int EMOTE_YES = 700;
+	public static final int EMOTE_NO = 701;
+	public static final int EMOTE_THINK = 702;
+	public static final int EMOTE_BOW = 703;
+	public static final int EMOTE_ANGRY = 704;
+	public static final int EMOTE_CRY = 705;
+	public static final int EMOTE_LAUGH = 706;
+	public static final int EMOTE_CHEER = 707;
+	public static final int EMOTE_WAVE = 708;
+	public static final int EMOTE_BECKON = 709;
+	public static final int EMOTE_DANCE = 710;
+	public static final int EMOTE_CLAP = 711;
+	public static final int EMOTE_PANIC = 712;
+	public static final int EMOTE_JIG = 713;
+	public static final int EMOTE_SPIN = 714;
+	public static final int EMOTE_HEADBANG = 715;
+	public static final int EMOTE_JUMP_FOR_JOY = 716;
+	public static final int EMOTE_RASPBERRY = 717;
+	public static final int EMOTE_YAWN = 718;
+	public static final int EMOTE_SALUTE = 719;
+	public static final int EMOTE_SHRUG = 720;
+	public static final int EMOTE_BLOW_KISS = 721;
+	public static final int EMOTE_GOBLIN_SALUTE = 727;
+	public static final int EMOTE_SLAP_HEAD = 729;
+	public static final int EMOTE_STOMP = 730;
+	public static final int EMOTE_FLAP = 731;
+	public static final int EMOTE_PUSH_UP = 1204;
+}
