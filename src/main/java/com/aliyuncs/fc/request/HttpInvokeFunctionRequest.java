60a0a534fb082a446022cff53f6cca39a1b51ed0
Luc Esape <luc.esape@gmail.com>
2018-04-16 12:15:17 +0200

    Bug commit from the following repository aliyun/fc-java-sdk This bug commit is a reflect of source code from: http://github.com/aliyun/fc-java-sdk/commit/578dcd4df3e0349784aa4f4d946c6d9112b164ea.

---
+++ src/main/java/com/aliyuncs/fc/request/HttpInvokeFunctionRequest.java
@@ -0,0 +1,56 @@
+package com.aliyuncs.fc.request;
+
+import com.aliyuncs.fc.constants.Const;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static java.lang.String.format;
+
+/**
+ * used for http invocation
+ */
+public class HttpInvokeFunctionRequest extends InvokeFunctionRequest {
+
+    private final String path;
+
+    private final String method;
+
+    private final String authType;
+
+    private final Map<String, String> queryParams = new HashMap<String, String>();
+
+    public HttpInvokeFunctionRequest(String serviceName, String functionName, String authType, String method) {
+        this(serviceName, functionName, authType, method, "");
+    }
+
+    public HttpInvokeFunctionRequest(String serviceName, String functionName, String authType, String method, String path) {
+        super(serviceName, functionName);
+        this.setInvocationType(Const.INVOCATION_TYPE_HTTP);
+        this.path = path.startsWith("/") ? path.substring(1) : path;
+        this.authType = authType;
+        this.method = method;
+    }
+
+    public void addQuery(String name, String value) {
+        queryParams.put(name, value);
+    }
+
+    @Override
+    public Map<String, String> getQueryParams() {
+        return queryParams;
+    }
+
+    @Override
+    public String getPath() {
+        return format(Const.HTTP_INVOKE_FUNCTION_PATH, Const.API_VERSION, getServiceName(), getFunctionName(), path);
+    }
+
+    public String getMethod() {
+        return method;
+    }
+
+    public String getAuthType() {
+        return authType;
+    }
+}
