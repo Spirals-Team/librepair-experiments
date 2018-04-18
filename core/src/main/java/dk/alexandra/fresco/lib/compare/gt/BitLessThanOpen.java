658c447b659731d687fa367377257654f887ec28
Luc Esape <luc.esape@gmail.com>
2018-04-18 17:03:02 +0200

    Bug commit from the following repository aicis/fresco This bug commit is a reflect of source code from: http://github.com/aicis/fresco/commit/e8d3caa802ac4edd4627b7ec7d8c3fac20bc6ac6.

---
+++ core/src/main/java/dk/alexandra/fresco/lib/compare/gt/BitLessThanOpen.java
@@ -0,0 +1,44 @@
+package dk.alexandra.fresco.lib.compare.gt;
+
+import dk.alexandra.fresco.framework.DRes;
+import dk.alexandra.fresco.framework.builder.Computation;
+import dk.alexandra.fresco.framework.builder.numeric.ProtocolBuilderNumeric;
+import dk.alexandra.fresco.framework.util.MathUtils;
+import dk.alexandra.fresco.framework.value.SInt;
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Given known value a and secret value b represented as bits, computes a <? b.
+ */
+public class BitLessThanOpen implements Computation<SInt, ProtocolBuilderNumeric> {
+
+  private final DRes<BigInteger> openValueDef;
+  private final DRes<List<DRes<SInt>>> secretBitsDef;
+
+  public BitLessThanOpen(DRes<BigInteger> openValue, DRes<List<DRes<SInt>>> secretBits) {
+    this.openValueDef = openValue;
+    this.secretBitsDef = secretBits;
+  }
+
+  @Override
+  public DRes<SInt> buildComputation(ProtocolBuilderNumeric builder) {
+    List<DRes<SInt>> secretBits = secretBitsDef.out();
+    BigInteger openValueA = openValueDef.out();
+    int numBits = secretBits.size();
+    List<DRes<BigInteger>> openBits = MathUtils.toBitsAsDRes(openValueA, numBits);
+    DRes<List<DRes<SInt>>> secretBitsNegated = builder.par(par -> {
+      List<DRes<SInt>> negatedBits = new ArrayList<>(numBits);
+      for (DRes<SInt> secretBit : secretBits) {
+        negatedBits.add(par.numeric().sub(BigInteger.ONE, secretBit));
+      }
+      Collections.reverse(negatedBits);
+      return () -> negatedBits;
+    });
+    DRes<SInt> gt = builder.seq(new CarryOut(() -> openBits, secretBitsNegated, BigInteger.ONE));
+    return builder.numeric().sub(BigInteger.ONE, gt);
+  }
+
+}
