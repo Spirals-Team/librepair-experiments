18b2af1ee2495e4836129785b761dbd7aa522c18
Luc Esape <luc.esape@gmail.com>
2018-04-18 14:16:59 +0200

    Bug commit from the following repository apache/activemq-artemis This bug commit is a reflect of source code from: http://github.com/apache/activemq-artemis/commit/54e4f3cb0a5708f335c96b46f66a67ada068a2c2.

---
+++ artemis-cli/src/main/java/org/apache/activemq/artemis/cli/commands/util/SyncCalculation.java
@@ -0,0 +1,194 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.activemq.artemis.cli.commands.util;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.text.DecimalFormat;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.activemq.artemis.core.io.IOCallback;
+import org.apache.activemq.artemis.core.io.SequentialFile;
+import org.apache.activemq.artemis.core.io.SequentialFileFactory;
+import org.apache.activemq.artemis.core.io.aio.AIOSequentialFileFactory;
+import org.apache.activemq.artemis.core.io.nio.NIOSequentialFileFactory;
+import org.apache.activemq.artemis.core.server.ActiveMQMessageBundle;
+import org.apache.activemq.artemis.core.server.JournalType;
+import org.apache.activemq.artemis.jlibaio.LibaioContext;
+import org.apache.activemq.artemis.utils.ReusableLatch;
+
+/**
+ * It will perform a simple test to evaluate how many syncs a disk can make per second
+ * * *
+ */
+public class SyncCalculation {
+
+   /**
+    * It will perform a write test of blockSize * bocks, sinc on each write, for N tries.
+    * It will return the lowest spent time from the tries.
+    */
+   public static long syncTest(File datafolder,
+                               int blockSize,
+                               int blocks,
+                               int tries,
+                               boolean verbose,
+                               boolean fsync,
+                               JournalType journalType) throws Exception {
+      SequentialFileFactory factory = newFactory(datafolder, fsync, journalType, blockSize * blocks);
+
+      if (verbose) {
+         System.out.println("Using " + factory.getClass().getName() + " to calculate sync times");
+      }
+      SequentialFile file = factory.createSequentialFile("test.tmp");
+
+      try {
+         file.delete();
+         file.open();
+
+         file.fill(blockSize * blocks);
+
+         file.close();
+
+         long[] result = new long[tries];
+
+         byte[] block = new byte[blockSize];
+
+         for (int i = 0; i < block.length; i++) {
+            block[i] = (byte) 't';
+         }
+
+         ByteBuffer bufferBlock = factory.newBuffer(blockSize);
+         bufferBlock.put(block);
+         bufferBlock.position(0);
+
+         final ReusableLatch latch = new ReusableLatch(0);
+
+         IOCallback callback = new IOCallback() {
+            @Override
+            public void done() {
+               latch.countDown();
+            }
+
+            @Override
+            public void onError(int errorCode, String errorMessage) {
+
+            }
+         };
+
+         DecimalFormat dcformat = new DecimalFormat("###.##");
+         for (int ntry = 0; ntry < tries; ntry++) {
+
+            if (verbose) {
+               System.out.println("**************************************************");
+               System.out.println(ntry + " of " + tries + " calculation");
+            }
+            file.open();
+            file.position(0);
+            long start = System.currentTimeMillis();
+            for (int i = 0; i < blocks; i++) {
+               bufferBlock.position(0);
+               latch.countUp();
+               file.writeDirect(bufferBlock, true, callback);
+               if (!latch.await(5, TimeUnit.SECONDS)) {
+                  throw new IOException("Callback wasn't called");
+               }
+            }
+            long end = System.currentTimeMillis();
+
+            result[ntry] = (end - start);
+
+            if (verbose) {
+               double writesPerMillisecond = (double) blocks / (double) result[ntry];
+               System.out.println("Time = " + result[ntry] + " milliseconds");
+               System.out.println("Writes / millisecond = " + dcformat.format(writesPerMillisecond));
+               System.out.println("bufferTimeout = " + toNanos(result[ntry], blocks, verbose));
+               System.out.println("**************************************************");
+            }
+            file.close();
+         }
+
+         factory.releaseDirectBuffer(bufferBlock);
+
+         long totalTime = Long.MAX_VALUE;
+         for (int i = 0; i < tries; i++) {
+            if (result[i] < totalTime) {
+               totalTime = result[i];
+            }
+         }
+
+         return totalTime;
+      } finally {
+         try {
+            file.close();
+         } catch (Exception e) {
+         }
+         try {
+            file.delete();
+         } catch (Exception e) {
+         }
+         try {
+            factory.stop();
+         } catch (Exception e) {
+         }
+      }
+   }
+
+   public static long toNanos(long time, long blocks, boolean verbose) {
+
+      double blocksPerMillisecond = (double) blocks / (double) (time);
+
+      if (verbose) {
+         System.out.println("Blocks per millisecond::" + blocksPerMillisecond);
+      }
+
+      long nanoSeconds = TimeUnit.NANOSECONDS.convert(1, TimeUnit.MILLISECONDS);
+
+      long timeWait = (long) (nanoSeconds / blocksPerMillisecond);
+
+      if (verbose) {
+         System.out.println("your system could make a sync every " + timeWait + " nanoseconds, and this will be your timeout");
+      }
+
+      return timeWait;
+   }
+
+   private static SequentialFileFactory newFactory(File datafolder, boolean datasync, JournalType journalType, int fileSize) {
+      SequentialFileFactory factory;
+
+      if (journalType == JournalType.ASYNCIO && !LibaioContext.isLoaded()) {
+         journalType = JournalType.NIO;
+      }
+
+      switch (journalType) {
+
+         case NIO:
+            factory = new NIOSequentialFileFactory(datafolder, 1).setDatasync(datasync);
+            ((NIOSequentialFileFactory) factory).disableBufferReuse();
+            factory.start();
+            return factory;
+         case ASYNCIO:
+            factory = new AIOSequentialFileFactory(datafolder, 1).setDatasync(datasync);
+            factory.start();
+            ((AIOSequentialFileFactory) factory).disableBufferReuse();
+            return factory;
+         default:
+            throw ActiveMQMessageBundle.BUNDLE.invalidJournalType2(journalType);
+      }
+   }
+}
---
+++ artemis-protocols/artemis-mqtt-protocol/src/main/java/org/apache/activemq/artemis/core/protocol/mqtt/MQTTConnectionManager.java
@@ -0,0 +1,180 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.activemq.artemis.core.protocol.mqtt;
+
+import java.nio.charset.Charset;
+import java.util.Set;
+import java.util.UUID;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.ByteBufAllocator;
+import io.netty.handler.codec.mqtt.MqttConnectReturnCode;
+import org.apache.activemq.artemis.api.core.client.ActiveMQClient;
+import org.apache.activemq.artemis.core.server.ActiveMQServer;
+import org.apache.activemq.artemis.core.server.ServerSession;
+import org.apache.activemq.artemis.core.server.impl.ServerSessionImpl;
+import org.apache.activemq.artemis.utils.ConcurrentHashSet;
+import org.apache.activemq.artemis.utils.UUIDGenerator;
+
+/**
+ * MQTTConnectionMananager is responsible for handle Connect and Disconnect packets and any resulting behaviour of these
+ * events.
+ */
+public class MQTTConnectionManager {
+
+   private MQTTSession session;
+
+   //TODO Read in a list of existing client IDs from stored Sessions.
+   public static Set<String> CONNECTED_CLIENTS = new ConcurrentHashSet<>();
+
+   private MQTTLogger log = MQTTLogger.LOGGER;
+
+   private boolean isWill = false;
+
+   private ByteBuf willMessage;
+
+   private String willTopic;
+
+   private int willQoSLevel;
+
+   private boolean willRetain;
+
+   public MQTTConnectionManager(MQTTSession session) {
+      this.session = session;
+      MQTTFailureListener failureListener = new MQTTFailureListener(this);
+      session.getConnection().addFailureListener(failureListener);
+   }
+
+   /**
+    * Handles the connect packet.  See spec for details on each of parameters.
+    */
+   synchronized void connect(String cId,
+                             String username,
+                             String password,
+                             boolean will,
+                             String willMessage,
+                             String willTopic,
+                             boolean willRetain,
+                             int willQosLevel,
+                             boolean cleanSession) throws Exception {
+      String clientId = validateClientId(cId, cleanSession);
+      if (clientId == null) {
+         session.getProtocolHandler().sendConnack(MqttConnectReturnCode.CONNECTION_REFUSED_IDENTIFIER_REJECTED);
+         session.getProtocolHandler().disconnect(true);
+         return;
+      }
+
+      session.setSessionState(getSessionState(clientId));
+      ServerSessionImpl serverSession = createServerSession(username, password);
+      serverSession.start();
+
+      session.setServerSession(serverSession);
+      session.setIsClean(cleanSession);
+
+      if (will) {
+         isWill = true;
+         byte[] payload = willMessage.getBytes(Charset.forName("UTF-8"));
+         this.willMessage = ByteBufAllocator.DEFAULT.buffer(payload.length);
+         this.willMessage.writeBytes(payload);
+         this.willQoSLevel = willQosLevel;
+         this.willRetain = willRetain;
+         this.willTopic = willTopic;
+      }
+
+      session.getConnection().setConnected(true);
+      session.start();
+      session.getProtocolHandler().sendConnack(MqttConnectReturnCode.CONNECTION_ACCEPTED);
+   }
+
+   /**
+    * Creates an internal Server Session.
+    *
+    * @param username
+    * @param password
+    * @return
+    * @throws Exception
+    */
+   ServerSessionImpl createServerSession(String username, String password) throws Exception {
+      String id = UUIDGenerator.getInstance().generateStringUUID();
+      ActiveMQServer server = session.getServer();
+
+      ServerSession serverSession = server.createSession(id, username, password, ActiveMQClient.DEFAULT_MIN_LARGE_MESSAGE_SIZE, session.getConnection(), MQTTUtil.SESSION_AUTO_COMMIT_SENDS, MQTTUtil.SESSION_AUTO_COMMIT_ACKS, MQTTUtil.SESSION_PREACKNOWLEDGE, MQTTUtil.SESSION_XA, null,
+                                                         session.getSessionCallback(), MQTTUtil.SESSION_AUTO_CREATE_QUEUE, server.newOperationContext());
+      return (ServerSessionImpl) serverSession;
+   }
+
+   synchronized void disconnect(boolean failure) {
+      if (session == null || session.getStopped()) {
+         return;
+      }
+
+      try {
+         if (isWill && failure) {
+            session.getMqttPublishManager().sendInternal(0, willTopic, willQoSLevel, willMessage, willRetain, true);
+         }
+         session.stop();
+         session.getConnection().destroy();
+      } catch (Exception e) {
+         log.error("Error disconnecting client: " + e.getMessage());
+      } finally {
+         if (session.getSessionState() != null) {
+            session.getSessionState().setAttached(false);
+            String clientId = session.getSessionState().getClientId();
+            if (clientId != null) {
+               CONNECTED_CLIENTS.remove(clientId);
+            }
+         }
+      }
+   }
+
+   private MQTTSessionState getSessionState(String clientId) throws InterruptedException {
+      /* [MQTT-3.1.2-6] If CleanSession is set to 1, the Client and Server MUST discard any previous Session and
+       * start a new one  This Session lasts as long as the Network Connection. State data associated with this Session
+       * MUST NOT be reused in any subsequent Session */
+
+      /* [MQTT-3.1.2-4] Attach an existing session if one exists (if cleanSession flag is false) otherwise create
+      a new one. */
+      MQTTSessionState state = MQTTSession.SESSIONS.get(clientId);
+      if (state != null) {
+         return state;
+      } else {
+         state = new MQTTSessionState(clientId);
+         MQTTSession.SESSIONS.put(clientId, state);
+         return state;
+      }
+   }
+
+   private String validateClientId(String clientId, boolean cleanSession) {
+      if (clientId == null || clientId.isEmpty()) {
+         // [MQTT-3.1.3-7] [MQTT-3.1.3-6] If client does not specify a client ID and clean session is set to 1 create it.
+         if (cleanSession) {
+            clientId = UUID.randomUUID().toString();
+         } else {
+            // [MQTT-3.1.3-8] Return ID rejected and disconnect if clean session = false and client id is null
+            return null;
+         }
+      } else if (!CONNECTED_CLIENTS.add(clientId)) {
+         // ^^^ If the client ID is not unique (i.e. it has already registered) then do not accept it.
+
+
+         // [MQTT-3.1.3-9] Return ID Rejected if server rejects the client ID
+         return null;
+      }
+      return clientId;
+   }
+}
