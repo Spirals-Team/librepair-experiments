bb30a7ba2ea5c06ca7276b34cf90bf8e5ecaf608
Luc Esape <luc.esape@gmail.com>
2018-04-20 16:56:19 +0200

    Bug commit from the following repository aliyun/fc-java-sdk This bug commit is a reflect of source code from: http://github.com/aliyun/fc-java-sdk/commit/4d5f122c9734f969c1d0560e64d905c48f436207.

---
+++ src/main/java/com/aliyuncs/fc/request/HttpInvokeFunctionRequest.java
@@ -0,0 +1,75 @@
+package com.aliyuncs.fc.request;
+
+import com.aliyuncs.fc.constants.Const;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.utils.URIBuilder;
+
+import java.net.URISyntaxException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
+
+import static java.lang.String.format;
+
+/**
+ * used for http invocation
+ */
+public class HttpInvokeFunctionRequest extends InvokeFunctionRequest {
+
+    private final String path;
+
+    private final String method;
+
+    private final String authType;
+
+    private final Map<String, String> queryParams = new TreeMap<String, String>();
+
+    public HttpInvokeFunctionRequest(String serviceName, String functionName, String authType, String method) {
+        this(serviceName, functionName, authType, method, "");
+    }
+
+    public HttpInvokeFunctionRequest(String serviceName, String functionName, String authType, String method, String path) {
+        super(serviceName, functionName);
+
+        this.setInvocationType(Const.INVOCATION_TYPE_HTTP);
+
+        try {
+            // parse path, path may contain parameters
+            URIBuilder uriBuilder = new URIBuilder(path);
+            path = uriBuilder.getPath();
+            this.path = path == null ? "" : (path.startsWith("/") ? path.substring(1) : path);
+
+            for (NameValuePair pair : uriBuilder.getQueryParams()) {
+                addQuery(pair.getName(), pair.getValue());
+            }
+        } catch (URISyntaxException e) {
+            throw new IllegalArgumentException(e.getMessage(), e);
+        }
+
+        this.authType = authType;
+        this.method = method;
+    }
+
+    public void addQuery(String name, String value) {
+        this.queryParams.put(name, value);
+    }
+
+    @Override
+    public Map<String, String> getQueryParams() {
+        return queryParams;
+    }
+
+    @Override
+    public String getPath() {
+        return format(Const.HTTP_INVOKE_FUNCTION_PATH, Const.API_VERSION, getServiceName(), getFunctionName(), path);
+    }
+
+    public String getMethod() {
+        return method;
+    }
+
+    public String getAuthType() {
+        return authType;
+    }
+}
---
+++ src/test/java/com/aliyuncs/fc/request/HttpInvokeFunctionRequestTest.java
@@ -0,0 +1,66 @@
+package com.aliyuncs.fc.request;
+
+import org.junit.Test;
+
+import static java.lang.String.format;
+import static org.junit.Assert.*;
+
+public class HttpInvokeFunctionRequestTest {
+
+    private static final String SERVICE_NAME = "service";
+
+    private static final String FUNCTION_NAME = "function";
+
+    @Test
+    public void testGetPath() {
+        HttpInvokeFunctionRequest request = createHttpInvoke("a");
+
+        assertEquals(generatePath("/a"), request.getPath());
+
+        request = createHttpInvoke("/a/b/c");
+
+        assertEquals(generatePath("/a/b/c"), request.getPath());
+
+        request = createHttpInvoke("/a/b/c?a=1&b=2");
+
+        assertEquals(generatePath("/a/b/c"), request.getPath());
+
+        request = createHttpInvoke("/a/b/c?a");
+
+        assertEquals(generatePath("/a/b/c"), request.getPath());
+    }
+
+    @Test
+    public void testGetParamters() {
+        HttpInvokeFunctionRequest request = createHttpInvoke("a");
+
+        assertEquals(0, request.getQueryParams().size());
+
+        request = createHttpInvoke("/a/b/c");
+
+        assertEquals(0, request.getQueryParams().size());
+
+        request = createHttpInvoke("/a/b/c?a=1&b=2");
+
+        assertEquals(2, request.getQueryParams().size());
+        assertEquals("1", request.getQueryParams().get("a"));
+        assertEquals("2", request.getQueryParams().get("b"));
+
+        request = createHttpInvoke("/a/b/c?a");
+
+        assertEquals(1, request.getQueryParams().size());
+        assertEquals(null, request.getQueryParams().get("a"));
+
+        request.addQuery("a", "1");
+        assertEquals("1", request.getQueryParams().get("a"));
+    }
+
+    private static HttpInvokeFunctionRequest createHttpInvoke(String path) {
+        return new HttpInvokeFunctionRequest(SERVICE_NAME, FUNCTION_NAME, "anamonus", "GET", path);
+    }
+
+    private static String generatePath(String path) {
+        return format("/2016-08-15/proxy/service/function%s", path);
+    }
+
+}
