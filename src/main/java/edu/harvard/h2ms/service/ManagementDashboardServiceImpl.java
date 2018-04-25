15bdb10e3cb2640994b4f5a0bd7a9ef518835c02
Luc Esape <luc.esape@gmail.com>
2018-04-25 06:15:38 +0200

    Bug commit from the following repository 2018swecapstone/h2ms This bug commit is a reflect of source code from: http://github.com/2018swecapstone/h2ms/commit/eb4f71dc5788bf5a0d89a71bfc74d79a3f8c8b63.

---
+++ src/main/java/edu/harvard/h2ms/service/ManagementDashboardServiceImpl.java
@@ -0,0 +1,73 @@
+package edu.harvard.h2ms.service;
+
+import edu.harvard.h2ms.domain.admin.Email;
+import edu.harvard.h2ms.domain.core.Event;
+import edu.harvard.h2ms.repository.*;
+import java.util.*;
+import javax.mail.*;
+import javax.mail.internet.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+/** The Management Dashboard Service Implementor... */
+@Service("managementDashboardService")
+@Repository
+@Transactional
+public class ManagementDashboardServiceImpl implements ManagementDashboardService {
+
+  private EventRepository eventRepository;
+  private LocationRepository locationRepository;
+  private SensorRepository sensorRepository;
+  private TagRepository tagRepository;
+  private UserRepository userRepository;
+
+  @Autowired
+  public void setEventRepository(EventRepository EventRepository) {
+    this.eventRepository = eventRepository;
+  }
+
+  @Autowired
+  public void setLocationRepository(LocationRepository locationRepository) {
+    this.locationRepository = locationRepository;
+  }
+
+  @Autowired
+  public void setSensorRepository(SensorRepository sensorRepository) {
+    this.sensorRepository = sensorRepository;
+  }
+
+  @Autowired
+  public void setTagRepository(TagRepository tagRepository) {
+    this.tagRepository = tagRepository;
+  }
+
+  // TODO add the ability to search for anything - this may be covered by tableau
+  // Finds all Events
+  @Transactional(readOnly = true)
+  public Iterable<Event> findAllEvents() {
+    return eventRepository.findAll();
+  }
+
+  /** @param email */
+  public void sendEmail(Email email) {
+    // fixme tomcat not have smtp?
+    String host = "localhost";
+    Properties properties = System.getProperties();
+    properties.setProperty("mail.smtp.host", host);
+    Session session = Session.getDefaultInstance(properties);
+
+    try {
+      MimeMessage message = new MimeMessage(session);
+      message.setFrom(new InternetAddress(email.getFrom()));
+      message.addRecipient(Message.RecipientType.TO, new InternetAddress(email.getTo()));
+      message.setSubject(email.getSubject());
+      message.setText(email.getText());
+      // Sends message
+      Transport.send(message);
+    } catch (MessagingException mex) {
+      mex.printStackTrace();
+    }
+  }
+}
