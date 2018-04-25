9199b86a7689cfc3551c6a74423511f7b789aae0
Luc Esape <luc.esape@gmail.com>
2018-04-25 15:44:41 +0200

    Bug commit from the following repository heptastique/onlygo This bug commit is a reflect of source code from: http://github.com/heptastique/onlygo/commit/04dcaa23ce13bfde839124e61f183d2541c72966.

---
+++ src/main/java/smart/Entities/DonneeAthmospherique.java
@@ -0,0 +1,43 @@
+package smart.Entities;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "donneeathmospherique")
+public class DonneeAthmospherique {
+
+    @Id
+    @Column(name = "ID")
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Long id;
+
+    private Double indice;
+
+
+    @OneToOne(fetch = FetchType.LAZY)
+    private Evaluation evaluation;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Double getIndice() {
+        return indice;
+    }
+
+    public void setIndice(Double indice) {
+        this.indice = indice;
+    }
+
+    public Evaluation getEvaluation() {
+        return evaluation;
+    }
+
+    public void setEvaluation(Evaluation evaluation) {
+        this.evaluation = evaluation;
+    }
+}
