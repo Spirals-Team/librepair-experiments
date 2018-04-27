2d5f0fad8c04fca116eb3e157737fb1297f803a1
Luc Esape <luc.esape@gmail.com>
2018-04-27 20:52:36 +0200

    Bug commit from the following repository Arquisoft/InciManager_e2a This bug commit is a reflect of source code from: http://github.com/Arquisoft/InciManager_e2a/commit/6461214ec35cf730240c5d96254715dfadfa0319.

---
+++ src/main/java/manager/repository/CamposRepository.java
@@ -0,0 +1,9 @@
+package manager.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import manager.entities.Campo;
+
+public interface CamposRepository extends CrudRepository<Campo, Long> {
+
+}
