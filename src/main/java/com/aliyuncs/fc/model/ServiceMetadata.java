bc86a9fa80eb5d5d29dd441a38b072c80238af02
Luc Esape <luc.esape@gmail.com>
2018-04-28 08:16:28 +0200

    Bug commit from the following repository aliyun/fc-java-sdk This bug commit is a reflect of source code from: http://github.com/aliyun/fc-java-sdk/commit/375edf4597415f3a76ea54b24b508a106663fdc5.

---
+++ src/main/java/com/aliyuncs/fc/model/ServiceMetadata.java
@@ -0,0 +1,66 @@
+package com.aliyuncs.fc.model;
+
+/**
+ * TODO: add javadoc
+ */
+public class ServiceMetadata {
+
+    private String serviceName;
+    private String description;
+    private String role;
+    private LogConfig logConfig;
+    private VPCConfig vpcConfig;
+    private Boolean internetAccess;
+    private String serviceId;
+    private String createdTime;
+    private String lastModifiedTime;
+
+    public ServiceMetadata(String serviceName, String description, String role,
+        LogConfig logConfig, VPCConfig vpcConfig, Boolean internetAccess, String serviceId, String createdTime, String lastModifiedTime) {
+        this.serviceName = serviceName;
+        this.description = description;
+        this.role = role;
+        this.logConfig = logConfig;
+        this.serviceId = serviceId;
+        this.createdTime = createdTime;
+        this.lastModifiedTime = lastModifiedTime;
+        this.vpcConfig = vpcConfig;
+        this.internetAccess = internetAccess;
+    }
+
+    public String getServiceName() {
+        return serviceName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public LogConfig getLogConfig() {
+        return logConfig;
+    }
+
+    public String getServiceId() {
+        return serviceId;
+    }
+
+    public String getCreatedTime() {
+        return createdTime;
+    }
+
+    public String getLastModifiedTime() {
+        return lastModifiedTime;
+    }
+
+    public Boolean getInternetAccess() {
+        return internetAccess;
+    }
+
+    public VPCConfig getVpcConfig() {
+        return vpcConfig;
+    }
+}
