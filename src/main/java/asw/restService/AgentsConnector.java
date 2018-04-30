d6b1aa2a311c47f52ef749aece40a9e72e0a0e6f
Luc Esape <luc.esape@gmail.com>
2018-04-30 22:16:15 +0200

    Bug commit from the following repository Arquisoft/InciManager_e1b This bug commit is a reflect of source code from: http://github.com/Arquisoft/InciManager_e1b/commit/251ede017e6d27e589855361aa9f886500dd9f94.

---
+++ src/main/java/asw/restService/AgentsConnector.java
@@ -0,0 +1,57 @@
+package asw.restService;
+
+import java.io.IOException;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.springframework.stereotype.Service;
+
+
+
+@Service
+public class AgentsConnector {
+	private static final String URL = "http://localhost:8091/user";
+
+	private String username;
+	private String password;
+	private String location;
+
+	
+	public HttpResponse launchRequest(String query) throws ClientProtocolException, IOException {
+		StringEntity entity = new StringEntity(query, ContentType.APPLICATION_JSON);
+		HttpClient httpClient = HttpClientBuilder.create().build();
+		HttpPost req = new HttpPost(URL);
+		req.setEntity(entity);
+		HttpResponse response = httpClient.execute(req);
+		return response;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+}
