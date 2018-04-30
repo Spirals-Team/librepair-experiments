d8f41be90bc3ef0330eadeae6fd7734761ff43f8
Luc Esape <luc.esape@gmail.com>
2018-04-30 15:15:30 +0200

    Bug commit from the following repository molgenis/molgenis This bug commit is a reflect of source code from: http://github.com/molgenis/molgenis/commit/32bb91f55ce947019240996991c97991445b97ea.

---
+++ molgenis-security-core/src/main/java/org/molgenis/security/core/UserPermissionEvaluator.java
@@ -0,0 +1,29 @@
+package org.molgenis.security.core;
+
+import org.springframework.security.acls.model.ObjectIdentity;
+
+/**
+ * Evaluates permissions for the currently authenticated user.
+ */
+public interface UserPermissionEvaluator
+{
+	/**
+	 * Returns whether the currently authenticated user has a given {@link Permission} on the given domain object.
+	 *
+	 * @param objectIdentity domain object identity
+	 * @param permission the {@link Permission} to check.
+	 * @return <tt>true</tt> if the permission is granted, <tt>false</tt> otherwise
+	 */
+	boolean hasPermission(ObjectIdentity objectIdentity, Permission permission);
+
+	/**
+	 * Returns whether the currently authenticated user has a given {@link PermissionSet} on the given domain object.
+	 *
+	 * @param objectIdentity domain object identity
+	 * @param permissionSet  the {@link PermissionSet} to check
+	 * @return <tt>true</tt> if the permission is granted, <tt>false</tt> otherwise
+	 * @deprecated check the permission you want to check instead
+	 */
+	@Deprecated
+	boolean hasPermission(ObjectIdentity objectIdentity, PermissionSet permissionSet);
+}
