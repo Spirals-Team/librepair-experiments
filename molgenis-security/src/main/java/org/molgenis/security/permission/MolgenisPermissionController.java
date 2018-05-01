701dc6e4b6e58281c45aa9eaf422c533efbba2a9
Luc Esape <luc.esape@gmail.com>
2018-05-01 10:16:18 +0200

    Bug commit from the following repository molgenis/molgenis This bug commit is a reflect of source code from: http://github.com/molgenis/molgenis/commit/5438c8e020734a97ea7222853e4af3185dda4246.

---
+++ molgenis-security/src/main/java/org/molgenis/security/permission/MolgenisPermissionController.java
@@ -0,0 +1,40 @@
+package org.molgenis.security.permission;
+
+import org.molgenis.data.security.EntityTypeIdentity;
+import org.molgenis.security.core.PermissionSet;
+import org.molgenis.security.core.UserPermissionEvaluator;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import static java.util.Objects.requireNonNull;
+
+@Controller
+@RequestMapping("/permission")
+public class MolgenisPermissionController
+{
+	private final UserPermissionEvaluator permissionService;
+
+	public MolgenisPermissionController(UserPermissionEvaluator permissionService)
+	{
+		this.permissionService = requireNonNull(permissionService);
+	}
+
+	@GetMapping("/{entityTypeId}/read")
+	@ResponseBody
+	public boolean hasReadPermission(@PathVariable("entityTypeId") String entityTypeId)
+	{
+		//TODO: Enable fine-grained permission checks
+		return permissionService.hasPermission(new EntityTypeIdentity(entityTypeId), PermissionSet.READ);
+	}
+
+	@GetMapping("/{entityTypeId}/write")
+	@ResponseBody
+	public boolean hasWritePermission(@PathVariable("entityTypeId") String entityTypeId)
+	{
+		//TODO: Enable fine-grained permission checks
+		return permissionService.hasPermission(new EntityTypeIdentity(entityTypeId), PermissionSet.WRITE);
+	}
+}
