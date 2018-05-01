a3ad9fa17488e2f9b844cb2e1fc497a54b72f4c4
Luc Esape <luc.esape@gmail.com>
2018-05-01 16:35:15 +0200

    Bug commit from the following repository Arquisoft/InciDashboard_i3a This bug commit is a reflect of source code from: http://github.com/Arquisoft/InciDashboard_i3a/commit/503403999cac47f205c836098697258e0b1ed46b.

---
+++ src/main/java/com/uniovi/entitites/Agent.java
@@ -0,0 +1,15 @@
+package com.uniovi.entitites;
+
+import lombok.Data;
+
+@Data
+public class Agent {
+
+	private String name;
+	private String email;
+	private String id;
+	private String location;
+	private int kindCode;
+	private String kind;
+	private String agentId;
+}
