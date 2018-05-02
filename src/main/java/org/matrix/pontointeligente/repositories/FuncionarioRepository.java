ee4fb87943cc73060d9abc207683aeb052c28e3c
Luc Esape <luc.esape@gmail.com>
2018-05-02 22:34:19 +0200

    Bug commit from the following repository matsmatos/ponto-inteligente-api This bug commit is a reflect of source code from: http://github.com/matsmatos/ponto-inteligente-api/commit/7298f71988ec49d6a467c0b85f1e734e0fe5ed2f.

---
+++ src/main/java/org/matrix/pontointeligente/repositories/FuncionarioRepository.java
@@ -0,0 +1,17 @@
+package org.matrix.pontointeligente.repositories;
+
+import org.matrix.pontointeligente.entities.Funcionario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional(readOnly = true)
+public interface FuncionarioRepository extends JpaRepository<Funcionario, Long> {
+
+	Funcionario findByCpf(String cpf);
+	
+	Funcionario findByEmail(String email);
+	
+	Funcionario findByCpfOrEmail(String cpf, String email);
+
+	Funcionario findOne(Long id);
+}
