b6aeafe5375042dee4013165628f7c4008e8f148
Luc Esape <luc.esape@gmail.com>
2018-05-04 17:41:28 +0200

    Bug commit from the following repository algolia/algoliasearch-client-java-2 This bug commit is a reflect of source code from: http://github.com/algolia/algoliasearch-client-java-2/commit/525d67821969fb53b7def1530b96f8d9c5c85ff3.

---
+++ algoliasearch-tests/src/test/java/com/algolia/search/integration/common/sync/SyncMultiClusterTest.java
@@ -0,0 +1,32 @@
+package com.algolia.search.integration.common.sync;
+
+import com.algolia.search.SyncAlgoliaIntegrationTest;
+import com.algolia.search.exceptions.AlgoliaException;
+import com.algolia.search.objects.Cluster;
+import com.algolia.search.responses.UserIDs;
+import java.util.List;
+import org.junit.Before;
+import org.junit.Test;
+
+public abstract class SyncMultiClusterTest extends SyncAlgoliaIntegrationTest {
+
+  @Before
+  public void checkEnvVariables() throws Exception {
+    APPLICATION_ID = System.getenv("ALGOLIA_APPLICATION_ID_MCM");
+    API_KEY = System.getenv("ALGOLIA_API_KEY_MCM");
+
+    super.checkEnvVariables();
+  }
+
+  @Test
+  public void listClusters() throws AlgoliaException {
+    List<Cluster> clusters = client.listClusters();
+    System.out.println(clusters.get(0));
+  }
+
+  @Test
+  public void listUserIds() throws AlgoliaException {
+    UserIDs userIds = client.listUserIDs(0, 2);
+    System.out.println(userIds.getUserIDs().get(0));
+  }
+}
