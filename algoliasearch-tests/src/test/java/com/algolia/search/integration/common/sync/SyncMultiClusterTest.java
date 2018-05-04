98f7736a54fb50b190c04804233b85cf6ffd9bdb
Luc Esape <luc.esape@gmail.com>
2018-05-04 11:19:04 +0200

    Bug commit from the following repository algolia/algoliasearch-client-java-2 This bug commit is a reflect of source code from: http://github.com/algolia/algoliasearch-client-java-2/commit/b9313e38a90d4fd8f148064e4b07d0f46fc4d1ac.

---
+++ algoliasearch-tests/src/test/java/com/algolia/search/integration/common/sync/SyncMultiClusterTest.java
@@ -0,0 +1,25 @@
+package com.algolia.search.integration.common.sync;
+
+import com.algolia.search.SyncAlgoliaIntegrationTest;
+import com.algolia.search.exceptions.AlgoliaException;
+import com.algolia.search.objects.Cluster;
+import java.util.List;
+import org.junit.Before;
+import org.junit.Test;
+
+public abstract class SyncMultiClusterTest extends SyncAlgoliaIntegrationTest {
+
+  @Before
+  public void checkEnvVariables() throws Exception {
+    APPLICATION_ID = System.getenv("ALGOLIA_APPLICATION_ID_MCM");
+    API_KEY = System.getenv("ALGOLIA_API_KEY_MCM");
+
+    super.checkEnvVariables();
+  }
+
+  @Test
+  public void listClusters() throws AlgoliaException {
+    List<Cluster> clusters = client.listClusters();
+    System.out.println(clusters.get(0));
+  }
+}
