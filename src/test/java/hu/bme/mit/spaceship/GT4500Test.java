fc0c400659162573c2627d7cbb5c9fe8d71f2582
Luc Esape <luc.esape@gmail.com>
2018-05-10 10:07:12 +0200

    Bug commit from the following repository nasattila/ivt-lab This bug commit is a reflect of source code from: http://github.com/nasattila/ivt-lab/commit/7ea20845a974cf872be263772080d8926a67d8bb.

---
+++ src/test/java/hu/bme/mit/spaceship/GT4500Test.java
@@ -0,0 +1,58 @@
+package hu.bme.mit.spaceship;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class GT4500Test {
+
+  private GT4500 ship;
+  TorpedoStore primary;
+  TorpedoStore secondary;
+
+  @Before
+  public void init(){
+	primary = mock(TorpedoStore.class);
+	secondary = mock(TorpedoStore.class);
+    this.ship = new GT4500(primary, secondary);
+  }
+
+  @Test
+  public void fireTorpedo_Single_Success(){
+    // Arrange
+	when(primary.isEmpty()).thenReturn(false);
+	when(primary.fire(1)).thenReturn(true);
+
+    // Act
+    boolean result = ship.fireTorpedo(FiringMode.SINGLE);
+
+    // Assert
+    assertEquals(true, result);
+  }
+
+  @Test
+  public void fireTorpedo_All_Success(){
+    // Arrange
+
+    // Act
+    boolean result = ship.fireTorpedo(FiringMode.ALL);
+
+    // Assert
+    assertEquals(true, result);
+  }
+  
+  @Test
+  public void fireTorpedo_Single_Twice_Success(){
+    // Arrange
+
+    // Act
+    ship.fireTorpedo(FiringMode.SINGLE);
+    
+    boolean result = ship.fireTorpedo(FiringMode.SINGLE);
+
+    // Assert
+    assertEquals(true, result);
+  }
+}
