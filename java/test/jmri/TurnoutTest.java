6eb4a53b0ed53f381941f616c07440dbd71cf9b2
Luc Esape <luc.esape@gmail.com>
2018-05-11 05:04:38 +0200

    Bug commit from the following repository JMRI/JMRI This bug commit is a reflect of source code from: http://github.com/JMRI/JMRI/commit/50a619d6366ff03bed98173d4da1bbcb17c3794e.

---
+++ java/test/jmri/TurnoutTest.java
@@ -0,0 +1,31 @@
+package jmri;
+
+import jmri.util.JUnitUtil;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Tests for the Turnout class
+ *
+ * @author	Bob Jacobsen Copyright (C) 2010
+ */
+public class TurnoutTest {
+
+    @SuppressWarnings("all")
+    @Test
+    public void testStateConstants() {
+        Assert.assertTrue("Thrown and Closed differ", (Turnout.THROWN & Turnout.CLOSED) == 0);
+        Assert.assertTrue("Thrown and Unknown differ", (Turnout.THROWN & Turnout.UNKNOWN) == 0);
+        Assert.assertTrue("Off and Unknown differ", (Turnout.CLOSED & Turnout.UNKNOWN) == 0);
+        Assert.assertTrue("Thrown and Inconsistent differ", (Turnout.THROWN & Turnout.INCONSISTENT) == 0);
+        Assert.assertTrue("Closed and Inconsistent differ", (Turnout.CLOSED & Turnout.INCONSISTENT) == 0);
+    }
+
+
+    @Test 
+    public void testInverTurnoutState() {
+        Assert.assertEquals("Closed state Inverted",Turnout.THROWN,Turnout.invertTurnoutState(Turnout.CLOSED));
+        Assert.assertEquals("Thrown state Inverted",Turnout.CLOSED,Turnout.invertTurnoutState(Turnout.THROWN));
+    }
+
+}
