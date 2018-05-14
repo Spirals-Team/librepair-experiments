08178fd93ce34914e52ab75bf17cfcfd202509f2
Luc Esape <luc.esape@gmail.com>
2018-05-14 04:44:33 +0200

    Bug commit from the following repository tiagotds/ponto-inteligente-api This bug commit is a reflect of source code from: http://github.com/tiagotds/ponto-inteligente-api/commit/c4dcbb9b4bee6a73a2a4b1259a9caba754683112.

---
+++ src/main/java/com/tiagotds_/pontointeligente/api/dtos/FuncionarioDto.java
@@ -0,0 +1,91 @@
+package com.tiagotds_.pontointeligente.api.dtos;
+
+import java.util.Optional;
+
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.NotEmpty;
+
+public class FuncionarioDto {
+
+	private int id;
+	@NotEmpty(message = "O nome não pode ser vazio")
+	@Length(min = 2, max = 200, message = "Nome deve conter entre 2 e 200 caracteres")
+	private String nome;
+	@NotEmpty(message = "O e-mail não pode ser vazio")
+	@Length(min = 5, max = 200, message = "E-mail deve conter entre 5 e 200 caracteres")
+	@Email(message = "E-mail inválido")
+	private String email;
+	private Optional<String> senha = Optional.empty();
+	private Optional<String> valorHora = Optional.empty();
+	private Optional<String> qtdHorasTrabalhoDia = Optional.empty();
+	private Optional<String> qtdHorasAlmoco = Optional.empty();
+
+	public FuncionarioDto() {
+
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Optional<String> getSenha() {
+		return senha;
+	}
+
+	public void setSenha(Optional<String> senha) {
+		this.senha = senha;
+	}
+
+	public Optional<String> getValorHora() {
+		return valorHora;
+	}
+
+	public void setValorHora(Optional<String> valorHora) {
+		this.valorHora = valorHora;
+	}
+
+	public Optional<String> getQtdHorasTrabalhoDia() {
+		return qtdHorasTrabalhoDia;
+	}
+
+	public void setQtdHorasTrabalhoDia(Optional<String> qtdHorasTrabalhoDia) {
+		this.qtdHorasTrabalhoDia = qtdHorasTrabalhoDia;
+	}
+
+	public Optional<String> getQtdHorasAlmoco() {
+		return qtdHorasAlmoco;
+	}
+
+	public void setQtdHorasAlmoco(Optional<String> qtdHorasAlmoco) {
+		this.qtdHorasAlmoco = qtdHorasAlmoco;
+	}
+	
+	@Override
+	public String toString() {
+		return "Funcionario[id=" + id + ", nome=" + nome + ", email=" + email + ", senha=" + senha 
+				+ ", valorHora=" + valorHora + ", qtdHorasTrabalhoDia=" + qtdHorasTrabalhoDia + ", qtdHorasAlmoco="
+				+ qtdHorasAlmoco + " ]";
+	}
+
+}
