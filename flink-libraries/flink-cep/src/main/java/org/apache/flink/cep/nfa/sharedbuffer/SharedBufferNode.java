9befebb5eb9714e5c41c4b673b97546b49919272
Luc Esape <luc.esape@gmail.com>
2018-05-17 16:19:04 +0200

    Bug commit from the following repository dawidwys/flink This bug commit is a reflect of source code from: http://github.com/dawidwys/flink/commit/3d5fb3bf4e5a89c479f2599dbba20a3e617e9164.

---
+++ flink-libraries/flink-cep/src/main/java/org/apache/flink/cep/nfa/sharedbuffer/SharedBufferNode.java
@@ -0,0 +1,124 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.flink.cep.nfa.sharedbuffer;
+
+import org.apache.flink.api.common.typeutils.base.IntSerializer;
+import org.apache.flink.api.common.typeutils.base.ListSerializer;
+import org.apache.flink.api.common.typeutils.base.TypeSerializerSingleton;
+import org.apache.flink.core.memory.DataInputView;
+import org.apache.flink.core.memory.DataOutputView;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SharedBufferNode {
+	private final List<SharedBufferEdge> edges;
+	private final Lock lock;
+
+	public SharedBufferNode() {
+		this.edges = new ArrayList<>();
+		this.lock = new Lock();
+	}
+
+	private SharedBufferNode(List<SharedBufferEdge> edges, Lock lock) {
+		this.edges = edges;
+		this.lock = lock;
+	}
+
+	public List<SharedBufferEdge> getEdges() {
+		return edges;
+	}
+
+	public Lock getLock() {
+		return lock;
+	}
+
+	public void addEdge(SharedBufferEdge edge) {
+		this.edges.add(edge);
+	}
+
+	@Override
+	public String toString() {
+		return "SharedBufferNode{" +
+			"edges=" + edges +
+			", lock=" + lock +
+			'}';
+	}
+
+	public static class SharedBufferNodeSerializer extends TypeSerializerSingleton<SharedBufferNode> {
+
+		private static final long serialVersionUID = -6687780732295439832L;
+
+		private final ListSerializer<SharedBufferEdge> edgesSerializer = new ListSerializer<>(SharedBufferEdge.SharedBufferEdgeSerializer.INSTANCE);
+
+		@Override
+		public boolean isImmutableType() {
+			return false;
+		}
+
+		@Override
+		public SharedBufferNode createInstance() {
+			return new SharedBufferNode();
+		}
+
+		@Override
+		public SharedBufferNode copy(SharedBufferNode from) {
+			return new SharedBufferNode(edgesSerializer.copy(from.edges), new Lock(from.lock.getRefCounter()));
+		}
+
+		@Override
+		public SharedBufferNode copy(SharedBufferNode from, SharedBufferNode reuse) {
+			return copy(from);
+		}
+
+		@Override
+		public int getLength() {
+			return -1;
+		}
+
+		@Override
+		public void serialize(SharedBufferNode record, DataOutputView target) throws IOException {
+			edgesSerializer.serialize(record.edges, target);
+			IntSerializer.INSTANCE.serialize(record.lock.getRefCounter(), target);
+		}
+
+		@Override
+		public SharedBufferNode deserialize(DataInputView source) throws IOException {
+			List<SharedBufferEdge> edges = edgesSerializer.deserialize(source);
+			Integer refCount = IntSerializer.INSTANCE.deserialize(source);
+			return new SharedBufferNode(edges, new Lock(refCount));
+		}
+
+		@Override
+		public SharedBufferNode deserialize(SharedBufferNode reuse, DataInputView source) throws IOException {
+			return deserialize(source);
+		}
+
+		@Override
+		public void copy(DataInputView source, DataOutputView target) throws IOException {
+			serialize(deserialize(source), target);
+		}
+
+		@Override
+		public boolean canEqual(Object obj) {
+			return obj instanceof SharedBufferNodeSerializer;
+		}
+	}
+}
