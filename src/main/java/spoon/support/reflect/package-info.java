5f4d106689eee3891313c0f723d00a8464afb819
Luc Esape <luc.esape@gmail.com>
2018-05-23 19:48:13 +0200

    Bug commit from the following repository INRIA/spoon This bug commit is a reflect of source code from: http://github.com/INRIA/spoon/commit/971e2f2ef6e435c8e794db22e11fb07ef02baf0a.

---
+++ src/main/java/spoon/support/reflect/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * <p>This package provides an implementation of the spoon.reflect package.</p>
+ * <p>The classes of this package should not be instantiated directly, but by using {@link spoon.reflect.factory.Factory}.
+ * <h2>Related Documentation</h2>
+ * <ul>
+ * <li><a href="../../../../Spoon_Architecture.pdf">Spoon Architecture (PDF)</a>
+ * </ul>
+ */
+package spoon.support.reflect;
