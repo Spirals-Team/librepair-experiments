ff08b3b72280e523e77ef9d0801cbd580858820f
Luc Esape <luc.esape@gmail.com>
2018-05-24 14:40:59 +0200

    Bug commit from the following repository Innovimax-SARL/mix-them This bug commit is a reflect of source code from: http://github.com/Innovimax-SARL/mix-them/commit/922d4ec039da639f89d69b8ea58115448543bc09.

---
+++ src/main/java/innovimax/mixthem/operation/CopyMode.java
@@ -0,0 +1,20 @@
+package innovimax.mixthem.operation;
+
+/**
+* <p>This is a enumeration of copy mode.</p>
+* <p>Here are the modes:</p>
+* <ul>
+* <li>FIRST: copy first file</li>
+* <li>SECOND: copy second file</li>* 
+* <li>UMPTEENTH: copy the umpteenth file</li>*
+* <li>SELECTION: copy a selection of files (maybe all by default)</li>
+* </ul>
+* @author Innovimax
+* @version 1.0
+*/
+public enum CopyMode {	
+	FIRST,
+	SECOND,
+	UMPTEENTH,
+	SELECTION;
+}
