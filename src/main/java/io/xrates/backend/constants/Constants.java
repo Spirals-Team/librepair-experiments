6e7c6536ef72bede0819a2fa3373355e6aef4ef6
Luc Esape <luc.esape@gmail.com>
2018-05-26 18:09:45 +0200

    Bug commit from the following repository siddharthgoel88/xrates This bug commit is a reflect of source code from: http://github.com/siddharthgoel88/xrates/commit/50293dc4c86c32509649d5255dd56599cfac35d1.

---
+++ src/main/java/io/xrates/backend/constants/Constants.java
@@ -0,0 +1,10 @@
+package io.xrates.backend.constants;
+
+public class Constants {
+	
+	public static final String UNPROCESSED = "UNPROCESSED";
+	public static final String PROCESSED = "PROCESSED";
+	public static final String NOTIFY_ALERT = "NOTIFY ALERT";
+	public static final String NOTIFIED = "NOTIFIED";
+	
+}
