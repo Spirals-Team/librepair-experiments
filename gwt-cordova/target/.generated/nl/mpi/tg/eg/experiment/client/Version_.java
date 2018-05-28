74b616fdd590ebf45b4f03c02c04406ffd79ed77
Luc Esape <luc.esape@gmail.com>
2018-05-28 17:08:01 +0200

    Bug commit from the following repository MPI-ExperimentGroup/ExperimentTemplate This bug commit is a reflect of source code from: http://github.com/MPI-ExperimentGroup/ExperimentTemplate/commit/eb4a3f7ba277bc796f79cacfefaa6238b7bdad35.

---
+++ gwt-cordova/target/.generated/nl/mpi/tg/eg/experiment/client/Version_.java
@@ -0,0 +1,28 @@
+package nl.mpi.tg.eg.experiment.client;
+
+public class Version_ implements nl.mpi.tg.eg.experiment.client.Version {
+  
+  public java.lang.String buildVersion() {
+    return "-1";
+  }
+  
+  public java.lang.String compileDate() {
+    return "2018-05-28 17:04:24 +0200";
+  }
+  
+  public java.lang.String lastCommitDate() {
+    return "\"2018-05-24 17:07:03 +0200\"";
+  }
+  
+  public java.lang.String majorVersion() {
+    return "0";
+  }
+  
+  public java.lang.String minorVersion() {
+    return "1";
+  }
+  
+  public java.lang.String projectVersion() {
+    return "0.1-testing-SNAPSHOT";
+  }
+}
