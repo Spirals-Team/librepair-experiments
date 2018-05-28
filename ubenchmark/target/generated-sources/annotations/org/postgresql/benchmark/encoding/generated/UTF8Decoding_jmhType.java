f49d77f939e42fc00737b1240edad5e5b5569bd2
Luc Esape <luc.esape@gmail.com>
2018-05-28 13:09:57 +0200

    Bug commit from the following repository pgjdbc/pgjdbc This bug commit is a reflect of source code from: http://github.com/pgjdbc/pgjdbc/commit/0980e9cd325eaedd55b2d75138aec8dbf2c786c4.

---
+++ ubenchmark/target/generated-sources/annotations/org/postgresql/benchmark/encoding/generated/UTF8Decoding_jmhType.java
@@ -0,0 +1,4 @@
+package org.postgresql.benchmark.encoding.generated;
+public class UTF8Decoding_jmhType extends UTF8Decoding_jmhType_B3 {
+}
+
---
+++ ubenchmark/target/generated-sources/annotations/org/postgresql/benchmark/encoding/generated/UTF8Encoding_jmhType.java
@@ -0,0 +1,4 @@
+package org.postgresql.benchmark.encoding.generated;
+public class UTF8Encoding_jmhType extends UTF8Encoding_jmhType_B3 {
+}
+
