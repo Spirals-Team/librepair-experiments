1640b29c00407d3087a74994bbad441b53837c67
Luc Esape <luc.esape@gmail.com>
2018-05-29 17:53:43 +0200

    Bug commit from the following repository algolia/algoliasearch-client-java-2 This bug commit is a reflect of source code from: http://github.com/algolia/algoliasearch-client-java-2/commit/2eccfea7cffa8a4e22a4d354ae124940f4e0025e.

---
+++ algoliasearch-common/src/main/java/com/algolia/search/AsyncAPIClientConfiguration.java
@@ -0,0 +1,43 @@
+package com.algolia.search;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ExecutorService;
+
+public class AsyncAPIClientConfiguration extends APIClientConfiguration {
+
+  private final ExecutorService executor;
+
+  public AsyncAPIClientConfiguration(
+      String applicationId,
+      String apiKey,
+      ObjectMapper objectMapper,
+      List<String> buildHosts,
+      List<String> queryHosts,
+      Map<String, String> headers,
+      int connectTimeout,
+      int readTimeout,
+      int hostDownTimeout,
+      int maxConnTotal,
+      int maxConnPerRoute,
+      ExecutorService executor) {
+    super(
+        applicationId,
+        apiKey,
+        objectMapper,
+        buildHosts,
+        queryHosts,
+        headers,
+        connectTimeout,
+        readTimeout,
+        hostDownTimeout,
+        maxConnTotal,
+        maxConnPerRoute);
+    this.executor = executor;
+  }
+
+  public ExecutorService getExecutorService() {
+    return executor;
+  }
+}
