48a193e4dca5b2989ea0393e7f8dc7fa3ca31ccb
Luc Esape <luc.esape@gmail.com>
2018-05-29 13:44:50 +0200

    Bug commit from the following repository dta-sherlock/sirh-gestion-paie This bug commit is a reflect of source code from: http://github.com/dta-sherlock/sirh-gestion-paie/commit/f97979e30a5f017c8cf50a1384da99b46356c73a.

---
+++ src/main/java/dev/paie/entite/Avantage.java
@@ -0,0 +1,66 @@
+package dev.paie.entite;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+
+@Entity
+public class Avantage {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Integer id;
+	private String code;
+	private String nom;
+	private BigDecimal montant;
+
+
+	
+	public Avantage() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public Avantage(Integer id, String code, String nom, BigDecimal montant) {
+		super();
+		this.id = id;
+		this.code = code;
+		this.nom = nom;
+		this.montant = montant;
+	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public BigDecimal getMontant() {
+		return montant;
+	}
+
+	public void setMontant(BigDecimal montant) {
+		this.montant = montant;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+}
