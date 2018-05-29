23c68adbde98eb96e52d693a98bdc5fd023af46d
Luc Esape <luc.esape@gmail.com>
2018-05-29 13:41:19 +0200

    Bug commit from the following repository nagpaulo/ponto-inteligente-api This bug commit is a reflect of source code from: http://github.com/nagpaulo/ponto-inteligente-api/commit/309edd2ff51a2e99263c800b85509e1cdb0c2c01.

---
+++ src/main/java/br/com/modelo/pontointeligente/api/entities/Lancamento.java
@@ -0,0 +1,118 @@
+package br.com.modelo.pontointeligente.api.entities;
+
+import br.com.modelo.pontointeligente.api.enums.TipoEnum;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+
+@Entity
+@Table(name = "tb_lancamento", schema = "ponto")
+public class Lancamento implements Serializable{
+
+    private static final long serialVersionUID = -6537171332109604772L;
+
+    private Long id;
+    private Date data;
+    private String descricao;
+    private String localizacao;
+    private Date dataCriacao;
+    private Date dataAtualizacao;
+    private TipoEnum tipo;
+    private Funcionario funcionario;
+
+    public Lancamento() {
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "lancamento_seq_gen")
+    @SequenceGenerator(name = "lancamento_seq_gen", sequenceName = "ponto.tb_lancamento_ci_lancamento_seq")
+    @Column(name = "ci_lancamento", nullable = false)
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "dt_ponto", nullable = false)
+    public Date getData() {
+        return data;
+    }
+
+    public void setData(Date data) {
+        this.data = data;
+    }
+
+    @Column(name = "ds_informacao")
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    @Column(name = "ds_localizacao")
+    public String getLocalizacao() {
+        return localizacao;
+    }
+
+    public void setLocalizacao(String localizacao) {
+        this.localizacao = localizacao;
+    }
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "dt_criacao", nullable = false)
+    public Date getDataCriacao() {
+        return dataCriacao;
+    }
+
+    public void setDataCriacao(Date dataCriacao) {
+        this.dataCriacao = dataCriacao;
+    }
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "dt_atualizacao", nullable = false)
+    public Date getDataAtualizacao() {
+        return dataAtualizacao;
+    }
+
+    public void setDataAtualizacao(Date dataAtualizacao) {
+        this.dataAtualizacao = dataAtualizacao;
+    }
+
+    @Enumerated(EnumType.ORDINAL)
+    @Column(name = "cd_tipo")
+    public TipoEnum getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(TipoEnum tipo) {
+        this.tipo = tipo;
+    }
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    public Funcionario getFuncionario() {
+        return funcionario;
+    }
+
+    public void setFuncionario(Funcionario funcionario) {
+        this.funcionario = funcionario;
+    }
+
+
+    @PreUpdate
+    public void preUpdate(){
+        dataAtualizacao = new Date();
+    }
+
+    @PrePersist
+    public void prePersist(){
+        final Date atual = new Date();
+        dataCriacao = atual;
+        dataAtualizacao = atual;
+    }
+}
