796615d83f80967fd505a0513e5b0a6372af7bff
Luc Esape <luc.esape@gmail.com>
2018-05-30 02:57:15 +0200

    Bug commit from the following repository RipMeApp/ripme This bug commit is a reflect of source code from: http://github.com/RipMeApp/ripme/commit/629d2a21363708104129e4a5bca2603bdee3ae84.

---
+++ src/test/java/com/rarchives/ripme/tst/ripper/rippers/DeviantartRipperTest.java
@@ -0,0 +1,25 @@
+package com.rarchives.ripme.tst.ripper.rippers;
+
+import java.io.IOException;
+import java.net.URL;
+
+import com.rarchives.ripme.ripper.rippers.DeviantartRipper;
+
+public class DeviantartRipperTest extends RippersTest {
+    public void testDeviantartAlbum() throws IOException {
+        DeviantartRipper ripper = new DeviantartRipper(new URL("http://airgee.deviantart.com/gallery/"));
+        testRipper(ripper);
+    }
+
+    public void testDeviantartNSFWAlbum() throws IOException {
+        // NSFW gallery
+        DeviantartRipper ripper = new DeviantartRipper(new URL("http://faterkcx.deviantart.com/gallery/"));
+        testRipper(ripper);
+    }
+
+    public void testGetGID() throws IOException {
+        URL url = new URL("http://airgee.deviantart.com/gallery/");
+        DeviantartRipper ripper = new DeviantartRipper(url);
+        assertEquals("airgee", ripper.getGID(url));
+    }
+}
