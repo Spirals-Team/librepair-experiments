be87533ec78fe3bd2a6a7013c4164f5509ad07b4
Luc Esape <luc.esape@gmail.com>
2018-06-01 15:54:37 +0200

    Bug commit from the following repository danglotb/dspot This bug commit is a reflect of source code from: http://github.com/danglotb/dspot/commit/0dc7d68ba526c6532785a125fc65243c355a7b5f.

---
+++ dspot/src/main/java/eu/stamp_project/utils/Initializer.java
@@ -0,0 +1,25 @@
+package eu.stamp_project.utils;
+
+import eu.stamp_project.automaticbuilder.AutomaticBuilder;
+import eu.stamp_project.automaticbuilder.AutomaticBuilderFactory;
+import eu.stamp_project.utils.sosiefier.InputConfiguration;
+
+import java.io.IOException;
+
+/**
+ * Created by Benjamin DANGLOT
+ * benjamin.danglot@inria.fr
+ * on 15/09/17
+ */
+public class Initializer {
+
+	public static void initialize(InputConfiguration configuration)
+			throws IOException, InterruptedException {
+		configuration.setAbsolutePathToProjectRoot(InputConfiguration.computeProgramDirectory.apply(configuration));
+		AutomaticBuilderFactory.reset();
+		AutomaticBuilder builder = AutomaticBuilderFactory.getAutomaticBuilder(configuration);
+		builder.compile();
+		DSpotUtils.copyPackageFromResources();
+	}
+
+}
