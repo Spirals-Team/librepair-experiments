d9b3e6d4213396768acda76fcefb2b29ca0d615d
Luc Esape <luc.esape@gmail.com>
2018-06-04 03:52:12 +0200

    Bug commit from the following repository JMRI/JMRI This bug commit is a reflect of source code from: http://github.com/JMRI/JMRI/commit/20ae80570858a76e85ca3964d9f7c1c30e04f22c.

---
+++ java/test/jmri/jmrix/loconet/LnTrafficRouterTest.java
@@ -0,0 +1,102 @@
+package jmri.jmrix.loconet;
+
+import jmri.util.JUnitUtil;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ *
+ * @author Bob Jacobsen Copyright (c) 2002
+ */
+public class LnTrafficRouterTest {
+
+    @Test
+    public void testConnectAndSend() {
+        // scaffold for upstream
+        LocoNetInterfaceScaffold upstream = new LocoNetInterfaceScaffold();
+
+        // create object
+        LnTrafficRouter router = new LnTrafficRouter();
+        router.setSystemConnectionMemo(memo);
+        memo.setLnTrafficController(router);
+
+        Assert.assertEquals("router is tc", memo.getLnTrafficController(), router);
+
+        // connect
+        router.connect(upstream);
+        Assert.assertTrue("connected", router.status());
+
+        // send a message
+        LocoNetMessage m = new LocoNetMessage(3);
+        router.sendLocoNetMessage(m);
+
+        // check receipt
+        Assert.assertEquals("one message sent", 1, upstream.outbound.size());
+        Assert.assertTrue(upstream.outbound.elementAt(0) == m);
+    }
+
+    private LocoNetSystemConnectionMemo memo;
+    static int count = 0;
+
+    @Test
+    public void testReceiveAndForward() {
+        // create object
+        LnTrafficRouter router = new LnTrafficRouter();
+        router.setSystemConnectionMemo(memo);
+        memo.setLnTrafficController(router);
+        Assert.assertEquals("router is tc", memo.getLnTrafficController(), router);
+
+        count = 0;
+        // register a listener
+        LocoNetListener l = new LocoNetListener() {
+            @Override
+            public void message(LocoNetMessage m) {
+                count++;
+            }
+        };
+        router.addLocoNetListener(~0, l);
+        // send a message
+        LocoNetMessage m = new LocoNetMessage(3);
+        router.message(m);
+
+        // check receipt
+        Assert.assertEquals("one message sent", 1, count);
+    }
+
+    @Test
+    public void testConnectAndDisconnect() {
+        // scaffold for upstream
+        LocoNetInterfaceScaffold upstream = new LocoNetInterfaceScaffold();
+        upstream.setSystemConnectionMemo(memo);
+
+        // create object
+        LnTrafficRouter router = new LnTrafficRouter();
+        router.setSystemConnectionMemo(memo);
+        memo.setLnTrafficController(router);
+        Assert.assertEquals("router is tc", memo.getLnTrafficController(), router);
+
+        // connect
+        router.connect(upstream);
+        Assert.assertTrue("connected", router.status());
+
+        // disconnect
+        router.disconnectPort(upstream);
+        Assert.assertTrue("not connected", !router.status());
+    }
+
+    // The minimal setup for log4J
+    @Before
+    public void setUp() {
+        JUnitUtil.setUp();
+        memo = new LocoNetSystemConnectionMemo();
+    }
+
+    @After
+    public void tearDown() {
+        memo.dispose();
+        JUnitUtil.tearDown();
+    }
+
+}
