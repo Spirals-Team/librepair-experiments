6ecaa6766f3e09271fe836a44fb6b0c6c885f20c
Luc Esape <luc.esape@gmail.com>
2018-06-08 10:24:05 +0200

    Bug commit from the following repository opentimestamps/java-opentimestamps This bug commit is a reflect of source code from: http://github.com/opentimestamps/java-opentimestamps/commit/e8e94d02b2614228ec15acd0de8828d380c78d7f.

---
+++ src/main/java/com/eternitywall/ots/Calendar.java
@@ -0,0 +1,138 @@
+package com.eternitywall.ots;
+/**
+ * com.eternitywall.ots.Calendar module.
+ *
+ * @module com.eternitywall.ots.Calendar
+ * @author EternityWall
+ * @license LPGL3
+ */
+
+
+import com.eternitywall.http.Request;
+import com.eternitywall.http.Response;
+import com.eternitywall.ots.exceptions.*;
+import org.bitcoinj.core.ECKey;
+
+import java.net.*;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Logger;
+
+/** Class representing Remote com.eternitywall.ots.Calendar server interface */
+public class Calendar{
+
+    private String url;
+    private ECKey key;
+
+    private static Logger log = Utils.getLogger(Calendar.class.getName());
+
+    /**
+     * Create a RemoteCalendar.
+     * @param url The server url.
+     */
+    public Calendar(String url) {
+        this.url = url;
+    }
+
+    /**
+     * Set private key.
+     * @param key The private key.
+     */
+    public void setKey(ECKey key) {
+        this.key = key;
+    }
+
+    /**
+     * Get private key.
+     * @return The private key.
+     */
+    public ECKey getKey() {
+        return this.key;
+    }
+
+    /**
+     * Get calendar url.
+     * @return The calendar url.
+     */
+    public String getUrl() {
+        return this.url;
+    }
+
+    /**
+     * Submitting a digest to remote calendar. Returns a com.eternitywall.ots.Timestamp committing to that digest.
+     * @param digest The digest hash to send.
+     * @return the Timestamp received from the calendar.
+     * @throws ExceededSizeException if response is too big.
+     * @throws UrlException if url is not reachable.
+     */
+    public Timestamp submit(byte[] digest) throws ExceededSizeException, UrlException {
+        try {
+
+            Map<String, String> headers = new HashMap<>();
+            headers.put("Accept","application/vnd.opentimestamps.v1");
+            headers.put("User-Agent","java-opentimestamps");
+            headers.put("Content-Type","application/x-www-form-urlencoded");
+
+            if (key != null ) {
+                String signature = key.signMessage(Utils.bytesToHex(digest).toLowerCase());
+                headers.put("x-signature", signature);
+            }
+
+            URL obj = new URL(url + "/digest");
+            Request task = new Request(obj);
+            task.setData(digest);
+            task.setHeaders(headers);
+            Response response = task.call();
+            byte[] body = response.getBytes();
+
+            if (body.length > 10000) {
+                throw new ExceededSizeException("Calendar response exceeded size limit");
+            }
+
+            StreamDeserializationContext ctx = new StreamDeserializationContext(body);
+            return Timestamp.deserialize(ctx, digest);
+
+        } catch (Exception e) {
+            throw new UrlException(e.getMessage());
+        }
+    }
+
+
+    /**
+     * Get a timestamp for a given commitment.
+     * @param commitment The digest hash to send.
+     * @return the Timestamp from the calendar server (with blockchain information if already written).
+     * @throws ExceededSizeException if response is too big.
+     * @throws UrlException if url is not reachable.
+     * @throws CommitmentNotFoundException if commit is not found.
+     */
+    public Timestamp getTimestamp(byte[] commitment) throws ExceededSizeException, CommitmentNotFoundException, UrlException {
+        try {
+
+            Map<String, String> headers = new HashMap<>();
+            headers.put("Accept","application/vnd.opentimestamps.v1");
+            headers.put("User-Agent","java-opentimestamps");
+            headers.put("Content-Type","application/x-www-form-urlencoded");
+
+            URL obj = new URL(url + "/timestamp/" + Utils.bytesToHex(commitment).toLowerCase());
+            Request task = new Request(obj);
+            task.setHeaders(headers);
+            Response response = task.call();
+            byte[] body = response.getBytes();
+
+            if (body.length > 10000) {
+                throw new ExceededSizeException("Calendar response exceeded size limit");
+            }
+            if(!response.isOk()) {
+                throw new CommitmentNotFoundException("com.eternitywall.ots.Calendar response a status code != 200 which is: " + response.getStatus());
+            }
+
+            StreamDeserializationContext ctx = new StreamDeserializationContext(body);
+            return Timestamp.deserialize(ctx, commitment);
+
+        } catch (Exception e) {
+            throw new UrlException(e.getMessage());
+        }
+    }
+}
---
+++ src/main/java/com/eternitywall/ots/op/OpUnary.java
@@ -0,0 +1,50 @@
+package com.eternitywall.ots.op;
+
+import com.eternitywall.ots.StreamDeserializationContext;
+
+import java.util.Arrays;
+import java.util.logging.Logger;
+import com.eternitywall.ots.Utils;
+
+/**
+ * Operations that act on a single message.
+ *
+ * @see com.eternitywall.ots.op.Op
+ */
+public abstract class OpUnary extends Op {
+
+
+    private static Logger log = Utils.getLogger(OpUnary.class.getName());
+
+
+    @Override
+    public String _TAG_NAME() {
+        return "";
+    }
+
+    public OpUnary() {
+        super();
+    }
+
+
+    public static Op deserializeFromTag(StreamDeserializationContext ctx, byte tag) {
+        if (tag == OpSHA1._TAG) {
+            return new OpSHA1();
+        } else if (tag == OpSHA256._TAG) {
+            return new OpSHA256();
+        } else if (tag == OpRIPEMD160._TAG) {
+            return new OpRIPEMD160();
+        } else if (tag == OpKECCAK256._TAG) {
+            return new OpKECCAK256();
+        } else {
+            log.severe("Unknown operation tag: " + tag);
+            return null;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return this._TAG_NAME();
+    }
+
+}
