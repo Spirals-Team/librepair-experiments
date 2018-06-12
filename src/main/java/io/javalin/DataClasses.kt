d4aac306d14f5dff3950cc3415a14129a4436720
Luc Esape <luc.esape@gmail.com>
2018-06-12 22:43:42 +0200

    Bug commit from the following repository tipsy/javalin This bug commit is a reflect of source code from: http://github.com/tipsy/javalin/commit/af726a1c2107afeef2f38795828be3ae52dc8d0b.

---
+++ src/main/java/io/javalin/DataClasses.kt
@@ -0,0 +1,46 @@
+/*
+ * Javalin - https://javalin.io
+ * Copyright 2017 David Åse
+ * Licensed under Apache 2.0: https://github.com/tipsy/javalin/blob/master/LICENSE
+ */
+
+package io.javalin
+
+import java.io.InputStream
+
+/**
+ * Data class containing the content and meta-info of an uploaded file.
+ * [contentType]: the content-type passed by the client
+ * [inputStream]: the file-content as an [InputStream]
+ * [name]: the file-name reported by the client
+ * [extension]: the file-extension, extracted from the [name]
+ * @see Context.uploadedFile
+ * @see <a href="https://javalin.io/documentation#faq">Uploads in FAQ</a>
+ */
+data class UploadedFile(val contentType: String, val content: InputStream, val name: String, val extension: String)
+
+/**
+ * Internal request logging level. Default is [LogLevel.OFF]
+ * @see Javalin.enableStandardRequestLogging
+ * @see Javalin.requestLogLevel
+ */
+enum class LogLevel { EXTENSIVE, STANDARD, MINIMAL, OFF; }
+
+/**
+ * Auth credentials for basic HTTP authorization.
+ * Contains the Base64 decoded [username] and [password] from the Authorization header.
+ * @see Context.basicAuthCredentials
+ */
+data class BasicAuthCredentials(val username: String, val password: String)
+
+/**
+ * Server lifecycle events
+ * @see Javalin.event
+ */
+enum class JavalinEvent {
+    SERVER_STARTING,
+    SERVER_STARTED,
+    SERVER_START_FAILED,
+    SERVER_STOPPING,
+    SERVER_STOPPED
+}
