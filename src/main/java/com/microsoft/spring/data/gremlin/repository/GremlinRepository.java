dc6613343766b0da0169f439700602d35096fb3c
Luc Esape <luc.esape@gmail.com>
2018-06-13 07:47:32 +0200

    Bug commit from the following repository Microsoft/spring-data-gremlin This bug commit is a reflect of source code from: http://github.com/Microsoft/spring-data-gremlin/commit/1fcc554f488822417394aae00e5399fcffb0b985.

---
+++ src/main/java/com/microsoft/spring/data/gremlin/repository/GremlinRepository.java
@@ -0,0 +1,24 @@
+/**
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See LICENSE in the project root for
+ * license information.
+ */
+package com.microsoft.spring.data.gremlin.repository;
+
+import com.microsoft.spring.data.gremlin.common.GremlinEntityType;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.NoRepositoryBean;
+
+import java.io.Serializable;
+
+@NoRepositoryBean
+public interface GremlinRepository<T, ID extends Serializable> extends CrudRepository<T, ID> {
+
+    Iterable<T> findAll(Class<T> domainClass);
+
+    void deleteAll(GremlinEntityType type);
+
+    long vertexCount();
+
+    long edgeCount();
+}
