92252f495b399e1e653c9ca76bba4083e9e37f1e
Luc Esape <luc.esape@gmail.com>
2018-06-15 06:44:42 +0200

    Bug commit from the following repository zalando/riptide This bug commit is a reflect of source code from: http://github.com/zalando/riptide/commit/479f00965638113d406aeae453249ce8a42859e8.

---
+++ riptide-metrics/src/test/java/org/zalando/riptide/metrics/MetricsPluginTest.java
@@ -0,0 +1,132 @@
+package org.zalando.riptide.metrics;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.restdriver.clientdriver.ClientDriverRule;
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Timer;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.springframework.core.task.AsyncListenableTaskExecutor;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.scheduling.concurrent.ConcurrentTaskExecutor;
+import org.zalando.riptide.Http;
+import org.zalando.riptide.httpclient.RestAsyncClientHttpRequestFactory;
+
+import java.io.IOException;
+import java.net.SocketTimeoutException;
+import java.util.concurrent.CompletionException;
+
+import static com.github.restdriver.clientdriver.RestClientDriver.giveEmptyResponse;
+import static com.github.restdriver.clientdriver.RestClientDriver.onRequestTo;
+import static java.util.concurrent.TimeUnit.MILLISECONDS;
+import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.fail;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyZeroInteractions;
+import static org.mockito.Mockito.when;
+import static org.mockito.hamcrest.MockitoHamcrest.longThat;
+import static org.springframework.http.HttpStatus.Series.SUCCESSFUL;
+import static org.zalando.riptide.Bindings.on;
+import static org.zalando.riptide.Navigators.series;
+import static org.zalando.riptide.PassRoute.pass;
+
+public class MetricsPluginTest {
+
+    @Rule
+    public final ClientDriverRule driver = new ClientDriverRule();
+
+    private final CloseableHttpClient client = HttpClientBuilder.create()
+            .setDefaultRequestConfig(RequestConfig.custom()
+                    .setSocketTimeout(500)
+                    .build())
+            .build();
+    private final AsyncListenableTaskExecutor executor = new ConcurrentTaskExecutor();
+    private final RestAsyncClientHttpRequestFactory factory = new RestAsyncClientHttpRequestFactory(client, executor);
+
+    private final MeterRegistry registry = mock(MeterRegistry.class);
+    private final Timer timer = mock(Timer.class);
+
+    private final Http unit = Http.builder()
+            .requestFactory(factory)
+            .baseUrl(driver.getBaseUrl())
+            .converter(createJsonConverter())
+            .plugin(new MetricsPlugin(registry, (arguments, response) -> arguments.getMethod().name()))
+            .build();
+
+    private static MappingJackson2HttpMessageConverter createJsonConverter() {
+        final MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+        converter.setObjectMapper(createObjectMapper());
+        return converter;
+    }
+
+    private static ObjectMapper createObjectMapper() {
+        return new ObjectMapper().findAndRegisterModules()
+                .disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+    }
+
+    @Before
+    public void setUp() {
+        when(registry.timer(any())).thenReturn(timer);
+    }
+
+    @After
+    public void tearDown() throws IOException {
+        client.close();
+    }
+
+    @Test
+    public void shouldRecordSuccessMetric() {
+        driver.addExpectation(onRequestTo("/foo"), giveEmptyResponse());
+
+        unit.get("/foo")
+                .call(pass())
+                .join();
+
+        verify(registry).timer("GET");
+        verify(timer).record(longThat(is(greaterThan(0L))), eq(MILLISECONDS));
+    }
+
+    @Test
+    public void shouldRecordFailureMetric() {
+        driver.addExpectation(onRequestTo("/foo"),
+                giveEmptyResponse().withStatus(503));
+
+        unit.get("/foo")
+                .dispatch(series(),
+                        on(SUCCESSFUL).call(pass()))
+                .exceptionally(e -> null)
+                .join();
+
+        verify(registry).timer("GET");
+        verify(timer).record(longThat(is(greaterThan(0L))), eq(MILLISECONDS));
+    }
+
+    @Test(expected = SocketTimeoutException.class)
+    public void shouldNotRecordMetric() throws Throwable {
+        driver.addExpectation(onRequestTo("/foo"),
+                giveEmptyResponse().after(750, MILLISECONDS));
+
+        try {
+            unit.get("/foo")
+                    .call(pass())
+                    .join();
+
+            fail("Expected exception");
+        } catch (final CompletionException e) {
+            throw e.getCause();
+        } finally {
+            verifyZeroInteractions(registry);
+        }
+    }
+
+}
