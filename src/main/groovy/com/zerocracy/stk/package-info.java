c075ec013f4621be631cc2f5ed3cfb382cce1bf9
Luc Esape <luc.esape@gmail.com>
2018-06-15 10:07:41 +0200

    Bug commit from the following repository zerocracy/farm This bug commit is a reflect of source code from: http://github.com/zerocracy/farm/commit/81548ad9c797d614457da845e40f7ebb727c7007.

---
+++ src/main/groovy/com/zerocracy/stk/package-info.java
@@ -0,0 +1,31 @@
+/**
+ * Copyright (c) 2016-2018 Zerocracy
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to read
+ * the Software only. Permissions is hereby NOT GRANTED to use, copy, modify,
+ * merge, publish, distribute, sublicense, and/or sell copies of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+/**
+ * Groovy stakeholder scripts.
+ *
+ * @author Kirill (g4s8.public@gmail.com)
+ * @version $Id$
+ * @since 0.22
+ * @todo #1105:30min Title element, of an order in agenda, has been declared in
+ *  agenda.xsd. The stakeholder set_agenda_title_from_github.groovy must be
+ *  implemented correctly: it just runs an script that returns the
+ *  same value. It must get the title value from github issue and set agenda
+ *  title with this value. The tests were already implemented in #1105
+ *  resolution.
+ */
+package com.zerocracy.stk;
