4aab654e25b402fd9555eb3bf8cf0ac1a77dcd19
Luc Esape <luc.esape@gmail.com>
2018-06-17 20:38:37 +0200

    Bug commit from the following repository openschoolmanagement/calendar-task-project-service This bug commit is a reflect of source code from: http://github.com/openschoolmanagement/calendar-task-project-service/commit/417bc69a9ef663a81dd7b3c8efa74f53af69f2d7.

---
+++ src/test/java/open/schoolmanagement/time/calendartaskprojectservice/CalendarTaskProjectServiceApplicationTests.java
@@ -0,0 +1,30 @@
+package open.schoolmanagement.time.calendartaskprojectservice;
+
+import static org.mockito.MockitoAnnotations.initMocks;
+
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@ActiveProfiles("test")
+@SpringBootTest
+public class CalendarTaskProjectServiceApplicationTests {
+
+
+  @Before
+  public void beforeTestCase() {
+    initMocks(this);
+  }
+
+  @Ignore
+  @Test
+  public void contextLoads() {
+
+  }
+
+}
