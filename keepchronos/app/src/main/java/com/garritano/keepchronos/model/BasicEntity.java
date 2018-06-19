099d26ff13d34f2c63c4c683e76b45e871dc8489
Luc Esape <luc.esape@gmail.com>
2018-06-19 16:41:44 +0200

    Bug commit from the following repository andryxxx/KeepChronos This bug commit is a reflect of source code from: http://github.com/andryxxx/KeepChronos/commit/ee14b4a22b9d1430e181e81cf1ad3555553ae8f7.

---
+++ keepchronos/app/src/main/java/com/garritano/keepchronos/model/BasicEntity.java
@@ -0,0 +1,78 @@
+package com.garritano.keepchronos.model;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
+public abstract class BasicEntity {
+
+	private Long id;
+	private String title;
+	private String description;
+
+	@Id
+	@GeneratedValue
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((description == null) ? 0 : description.hashCode());
+		result = prime * result + ((title == null) ? 0 : title.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		BasicEntity other = (BasicEntity) obj;
+		if (description == null) {
+			if (other.description != null) {
+				return false;
+			}
+		} else if (!description.equals(other.description)) {
+			return false;
+		}
+		if (title == null) {
+			if (other.title != null) {
+				return false;
+			}
+		} else if (!title.equals(other.title)) {
+			return false;
+		}
+		return true;
+	}
+
+}
---
+++ keepchronos/database/src/main/java/com/garritano/keepchronos/dao/TaskDao.java
@@ -0,0 +1,45 @@
+package com.garritano.keepchronos.dao;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+
+import com.garritano.keepchronos.model.Project;
+import com.garritano.keepchronos.model.Task;
+
+public class TaskDao implements TaskDaoInterface {
+
+	private EntityManager entityManager;
+
+	public TaskDao(EntityManager entityManager) {
+		this.entityManager = entityManager;
+	}
+
+	public void save(Task task) {
+		entityManager.getTransaction().begin();
+		entityManager.persist(task);
+		entityManager.getTransaction().commit();
+	}
+
+	public List<Task> getAll() {
+		return entityManager.createQuery("select p from Task p", Task.class).getResultList();
+	}
+
+	public Task findById(Long id) {
+		return entityManager.find(Task.class, id);
+	}
+
+	public void update(Task task) {
+		entityManager.getTransaction().begin();
+		entityManager.merge(task);
+		entityManager.getTransaction().commit();
+	}
+
+	public Project getProjectByTaskId(Long id) {
+		Task tempTask = entityManager.find(Task.class, id);
+		if (tempTask != null) {
+			return tempTask.getProject();
+		}
+		return null;
+	}
+}
