fc48ad901b1a58e212d427f0f4ce1cec9458af50
Luc Esape <luc.esape@gmail.com>
2018-06-19 12:12:24 +0200

    Bug commit from the following repository resteasy/Resteasy This bug commit is a reflect of source code from: http://github.com/resteasy/Resteasy/commit/ca2daaab3c59372c65459bec7ce9f4978d6c1a6c.

---
+++ resteasy-rxjava2/src/main/java/org/jboss/resteasy/rxjava2/ObservableProvider.java
@@ -0,0 +1,26 @@
+package org.jboss.resteasy.rxjava2;
+
+import javax.ws.rs.ext.Provider;
+
+import org.jboss.resteasy.spi.AsyncStreamProvider;
+import org.reactivestreams.Publisher;
+
+import io.reactiverse.reactivecontexts.core.Context;
+import io.reactivex.BackpressureStrategy;
+import io.reactivex.Observable;
+
+@Provider
+public class ObservableProvider implements AsyncStreamProvider<Observable<?>>
+{
+   static
+   {
+      Context.load();
+   }
+
+   @Override
+   public Publisher<?> toAsyncStream(Observable<?> asyncResponse)
+   {
+      return asyncResponse.toFlowable(BackpressureStrategy.BUFFER);
+   }
+
+}
