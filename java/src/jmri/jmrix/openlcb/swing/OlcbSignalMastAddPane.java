ff2abc13d64b8e31e3f93f3c4668d403cee90fad
Luc Esape <luc.esape@gmail.com>
2018-06-20 21:56:07 +0200

    Bug commit from the following repository bobjacobsen/JMRI This bug commit is a reflect of source code from: http://github.com/bobjacobsen/JMRI/commit/bb95d47e7b13adf2861529b9134e6cd5407d7934.

---
+++ java/src/jmri/jmrix/openlcb/swing/OlcbSignalMastAddPane.java
@@ -0,0 +1,55 @@
+package jmri.jmrix.openlcb.swing;
+
+import jmri.InstanceManager;
+import jmri.jmrit.beantable.signalmast.SignalMastAddPane;
+import jmri.jmrix.SystemConnectionMemo;
+
+import javax.annotation.Nonnull;
+import org.openide.util.lookup.ServiceProvider;
+
+/**
+ * A pane for configuring OlcbSignalMast objects
+ * <P>
+ * @see jmri.jmrit.beantable.signalmast.SignalMastAddPane
+ * @author Bob Jacobsen Copyright (C) 2018
+ * @since 4.11.2
+ */
+public class OlcbSignalMastAddPane extends SignalMastAddPane {
+
+    /** {@inheritDoc} */
+    @Override
+    @Nonnull public String getPaneName() {
+        return "OlcbSignalMast";
+    }
+
+    @ServiceProvider(service = SignalMastAddPane.SignalMastAddPaneProvider.class)
+    static public class SignalMastAddPaneProvider extends SignalMastAddPane.SignalMastAddPaneProvider {
+
+        /**
+         * {@inheritDoc}
+         * Requires a valid OpenLCB connection
+         */
+        @Override
+        public boolean isAvailable() {
+            for (SystemConnectionMemo memo : InstanceManager.getList(SystemConnectionMemo.class)) {
+                if (memo instanceof jmri.jmrix.can.CanSystemConnectionMemo) {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        /** {@inheritDoc} */
+        @Override
+        @Nonnull public String getPaneName() {
+            return "OlcbSignalMast";
+        }
+
+        /** {@inheritDoc} */
+        @Override
+        @Nonnull public SignalMastAddPane getNewPane() {
+            return new OlcbSignalMastAddPane();
+        }
+    }
+
+}
