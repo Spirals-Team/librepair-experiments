d5661306921a8b8116ed28d254c5c0b187da1b57
Luc Esape <luc.esape@gmail.com>
2018-06-20 16:16:09 +0200

    Bug commit from the following repository zerocracy/farm This bug commit is a reflect of source code from: http://github.com/zerocracy/farm/commit/d1e8ed423b8090a644d8c3b8d5b6730d638d4e04.

---
+++ src/main/java/com/zerocracy/tk/project/TkBadge.java
@@ -0,0 +1,46 @@
+/**
+ * Copyright (c) 2016-2018 Zerocracy
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to read
+ * the Software only. Permissions is hereby NOT GRANTED to use, copy, modify,
+ * merge, publish, distribute, sublicense, and/or sell copies of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package com.zerocracy.tk.project;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import org.takes.Response;
+import org.takes.facets.fork.RqRegex;
+import org.takes.facets.fork.TkRegex;
+import org.takes.rs.RsText;
+import org.takes.rs.RsWithStatus;
+
+/**
+ * Project badge.
+ *
+ * @author Yegor Bugayenko (yegor256@gmail.com)
+ * @version $Id$
+ * @since 0.18
+ * @checkstyle ClassDataAbstractionCouplingCheck (500 lines)
+ * @todo #1168:30min TkBadge is temporary disabled because of high load.
+ *  There are a lot of requests to project badges, each request trigger
+ *  SyncFarm lock acquire, which is broken right now.
+ */
+public final class TkBadge implements TkRegex {
+    @Override
+    public Response act(final RqRegex req) throws IOException {
+        return new RsWithStatus(
+            new RsText("badges are temporary disabled"),
+            HttpURLConnection.HTTP_UNAVAILABLE
+        );
+    }
+}
---
+++ src/test/java/com/zerocracy/tk/profile/TkAgendaTest.java
@@ -0,0 +1,119 @@
+/**
+ * Copyright (c) 2016-2018 Zerocracy
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to read
+ * the Software only. Permissions is hereby NOT GRANTED to use, copy, modify,
+ * merge, publish, distribute, sublicense, and/or sell copies of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package com.zerocracy.tk.profile;
+
+import com.jcabi.matchers.XhtmlMatchers;
+import com.zerocracy.Farm;
+import com.zerocracy.farm.fake.FkFarm;
+import com.zerocracy.farm.props.PropsFarm;
+import com.zerocracy.pmo.People;
+import com.zerocracy.tk.RqWithUser;
+import com.zerocracy.tk.TkApp;
+import java.net.HttpURLConnection;
+import org.cactoos.list.ListOf;
+import org.hamcrest.MatcherAssert;
+import org.junit.Test;
+import org.takes.facets.hamcrest.HmRsStatus;
+import org.takes.rq.RqFake;
+import org.takes.rs.RsPrint;
+
+/**
+ * Test case for {@link TkAgenda}.
+ * @author Yegor Bugayenko (yegor256@gmail.com)
+ * @version $Id$
+ * @since 0.13
+ * @checkstyle JavadocMethodCheck (500 lines)
+ * @checkstyle ClassDataAbstractionCouplingCheck (500 lines)
+ * @todo #402:30min Let's write a test for RsPage that will check a general
+ *  case that any page sent to Firefox will be rendered as html and not xml.
+ * @todo #402:30min Write a test confirming that awards page when opened in a
+ *  Firefox browser will always render html and not xml.
+ * @todo #402:30min Write a test confirming that profile page when opened in a
+ *  Firefox browser will always render html and not xml.
+ */
+@SuppressWarnings("PMD.AvoidDuplicateLiterals")
+public final class TkAgendaTest {
+
+    @Test
+    public void rendersXmlAgendaPage() throws Exception {
+        final Farm farm = new PropsFarm(new FkFarm());
+        MatcherAssert.assertThat(
+            XhtmlMatchers.xhtml(
+                new RsPrint(
+                    new TkApp(farm).act(
+                        new RqWithUser(
+                            farm,
+                            new RqFake(
+                                new ListOf<>(
+                                    "GET /u/Yegor256/agenda",
+                                    "Host: www.example.com",
+                                    "Accept: application/xml"
+                                ),
+                                ""
+                            )
+                        )
+                    )
+                ).printBody()
+            ),
+            XhtmlMatchers.hasXPaths("/page")
+        );
+    }
+
+    @Test
+    public void rendersHtmlAgendaPageForFirefox() throws Exception {
+        final Farm farm = new PropsFarm(new FkFarm());
+        MatcherAssert.assertThat(
+            XhtmlMatchers.xhtml(
+                new RsPrint(
+                    new TkApp(farm).act(
+                        new RqWithUser(
+                            farm,
+                            new RqFake(
+                                new ListOf<>(
+                                    "GET /u/yegor256/agenda",
+                                    "Host: www.example.com",
+                                    // @checkstyle LineLength (1 line)
+                                    "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:62.0) Gecko/20100101 Firefox/62.0"
+                                ),
+                                ""
+                            )
+                        )
+                    )
+                ).printBody()
+            ),
+            XhtmlMatchers.hasXPaths("//xhtml:body")
+        );
+    }
+
+    @Test
+    public void redirectsWhenAccessingNonexistentUsersAgenda()
+        throws Exception {
+        final Farm farm = new PropsFarm(new FkFarm());
+        new People(farm).bootstrap().touch("yegor256");
+        MatcherAssert.assertThat(
+            new RsPrint(
+                new TkApp(farm).act(
+                    new RqWithUser(
+                        farm,
+                        new RqFake("GET", "/u/foo-user/agenda")
+                    )
+                )
+            ),
+            new HmRsStatus(HttpURLConnection.HTTP_SEE_OTHER)
+        );
+    }
+}
