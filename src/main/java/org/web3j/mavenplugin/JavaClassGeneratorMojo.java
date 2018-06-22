de058253fff022350a50cb30a1defc11531eebd7
Luc Esape <luc.esape@gmail.com>
2018-06-22 13:24:20 +0200

    Bug commit from the following repository web3j/web3j-maven-plugin This bug commit is a reflect of source code from: http://github.com/web3j/web3j-maven-plugin/commit/a78b814d3dc6e52a353e36ca1bbf080a7315335f.

---
+++ src/main/java/org/web3j/mavenplugin/JavaClassGeneratorMojo.java
@@ -0,0 +1,197 @@
+package org.web3j.mavenplugin;
+
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.shared.model.fileset.FileSet;
+import org.apache.maven.shared.model.fileset.util.FileSetManager;
+import org.web3j.codegen.SolidityFunctionWrapper;
+import org.web3j.mavenplugin.solidity.CompilerResult;
+import org.web3j.mavenplugin.solidity.SolidityCompiler;
+
+import java.io.IOException;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+
+/**
+ * Maven Plugin to generate the java classes out of the solidity contract files.
+ */
+@Mojo(name = "generate-sources",
+        defaultPhase = LifecyclePhase.PROCESS_RESOURCES)
+public class JavaClassGeneratorMojo extends AbstractMojo {
+
+    private static final String DEFAULT_INCLUDE = "**/*.sol";
+    private static final String DEFAULT_PACKAGE = "org.web3j.model";
+    private static final String DEFAULT_SOURCE_DESTINATION = "src/main/java";
+    private static final String DEFAULT_SOLIDITY_SOURCES = "src/main/resources";
+
+    @Parameter(property = "packageName", defaultValue = DEFAULT_PACKAGE)
+    protected String packageName;
+
+    @Parameter(property = "sourceDestination", defaultValue = DEFAULT_SOURCE_DESTINATION)
+    protected String sourceDestination;
+
+    @Parameter(property = "soliditySourceFiles")
+    protected FileSet soliditySourceFiles = new FileSet();
+
+    @Parameter(property = "contract")
+    protected Contract contract;
+
+    @Parameter(property = "nativeJavaType", defaultValue = "true")
+    protected boolean nativeJavaType;
+
+    public void execute() throws MojoExecutionException {
+
+        if (soliditySourceFiles.getDirectory() == null) {
+            getLog().info("No solidity directory specified, using default directory [" + DEFAULT_SOLIDITY_SOURCES + "]");
+            soliditySourceFiles.setDirectory(DEFAULT_SOLIDITY_SOURCES);
+        }
+        if (soliditySourceFiles.getIncludes().size() == 0) {
+            getLog().info("No solidity contracts specified, using the default [" + DEFAULT_INCLUDE + "]");
+            soliditySourceFiles.setIncludes(Collections.singletonList(DEFAULT_INCLUDE));
+        }
+
+        String[] files = new FileSetManager().getIncludedFiles(soliditySourceFiles);
+        if (files != null) {
+            processContractFile(Stream.of(files)
+                    .filter(f -> {
+                        getLog().info("Adding to process '" + f + "'");
+                        return true;
+                    })
+                    .collect(Collectors.toList()));
+        }
+		/*
+        for (String includedFile : new FileSetManager().getIncludedFiles(soliditySourceFiles)) {
+            getLog().info("process '" + includedFile + "'");
+            processContractFile(includedFile);
+            getLog().debug("processed '" + includedFile + "'");
+        }
+		*/
+    }
+
+    private Map<String, Map<String, String>> extractContracts(String result) throws MojoExecutionException {
+        try {
+            ScriptEngine engine = new ScriptEngineManager().getEngineByName("nashorn");
+            String script = "JSON.parse(JSON.stringify(" + result + "))";
+            Map<String, Object> json = (Map<String, Object>) engine.eval(script);
+            Map<String, Map<String, String>> contracts = (Map<String, Map<String, String>>) json.get("contracts");
+            if (contracts == null) {
+                getLog().warn("no contracts found");
+                return null;
+            }
+            Map<String, String> contractRemap = new HashMap<>();
+            for (String contractFilename : contracts.keySet()) {
+                Map<String, String> contractMetadata = contracts.get(contractFilename);
+                String metadata = contractMetadata.get("metadata");
+                if (metadata == null || metadata.length() == 0) {
+                  contracts.remove(contractFilename);
+                  continue;
+                } 
+                getLog().debug("metadata:" + metadata);
+                String metadataScript = "JSON.parse(JSON.stringify(" + metadata + "))";
+                Map<String, Object> metadataJson = (Map<String, Object>) engine.eval(metadataScript);
+                Object settingsMap = metadataJson.get("settings");
+                if (settingsMap != null) {
+                    Map<String, String> compilationTarget = ((Map<String, Map<String, String>>) settingsMap).get("compilationTarget");
+                    if (compilationTarget != null) {
+                        for (Map.Entry<String, String> entry : compilationTarget.entrySet()) {
+                            String value = entry.getValue();
+                            contractRemap.put(contractFilename, value);
+                        }
+                    }
+                }
+                Map<String, String> compiledContract = contracts.remove(contractFilename);
+                String contractName = contractRemap.get(contractFilename);
+                contracts.put(contractName, compiledContract);
+            }
+            return contracts;
+        } catch (ScriptException e) {
+            throw new MojoExecutionException("Could not parse SolC result", e);
+        }
+    }
+
+    private String parseSoliditySources(Collection<String> includedFiles) throws MojoExecutionException {
+        if (includedFiles == null || includedFiles.isEmpty())
+            return "{}";
+        CompilerResult result = SolidityCompiler.getInstance(getLog()).compileSrc(
+                soliditySourceFiles.getDirectory(),
+                includedFiles,
+                SolidityCompiler.Options.ABI,
+                SolidityCompiler.Options.BIN,
+                SolidityCompiler.Options.INTERFACE,
+                SolidityCompiler.Options.METADATA
+        );
+        if (result.isFailed()) {
+            throw new MojoExecutionException("Could not compile solidity files\n" + result.errors);
+        }
+
+        getLog().debug("\t\tResult:\t" + result.output);
+        if (result.errors.contains("Warning:")) {
+            getLog().info("\tCompile Warning:\n" + result.errors);
+        } else {
+            getLog().debug("\t\tError: \t" + result.errors);
+        }
+        return result.output;
+    }
+
+    private void processContractFile(Collection<String> files) throws MojoExecutionException {
+        String result = parseSoliditySources(files);
+        processResult(result, "\tNo Contract found in files '" + files + "'");
+    }
+
+    private void processResult(String result, String warnMsg) throws MojoExecutionException {
+        Map<String, Map<String, String>> contracts = extractContracts(result);
+        if (contracts == null) {
+            getLog().warn(warnMsg);
+            return;
+        }
+        for (String contractName : contracts.keySet()) {
+            if (isFiltered(contractName)) {
+                getLog().debug("\tContract '" + contractName + "' is filtered");
+                continue;
+            }
+            try {
+                generatedJavaClass(contracts, contractName);
+                getLog().info("\tBuilt Class for contract '" + contractName + "'");
+            } catch (ClassNotFoundException | IOException ioException) {
+                getLog().error("Could not build java class for contract '" + contractName + "'", ioException);
+            }
+        }
+    }
+
+    private void generatedJavaClass(Map<String, Map<String, String>> result, String contractName) throws IOException, ClassNotFoundException {
+        new SolidityFunctionWrapper(nativeJavaType).generateJavaFiles(
+                contractName,
+                result.get(contractName).get(SolidityCompiler.Options.BIN.getName()),
+                result.get(contractName).get(SolidityCompiler.Options.ABI.getName()),
+                sourceDestination,
+                packageName);
+    }
+
+    private boolean isFiltered(String contractName) {
+        if (contract == null) {
+            return false;
+        }
+
+        if (contract.getExcludes() != null && !contract.getExcludes().isEmpty()) {
+            return contract.getExcludes().contains(contractName);
+        }
+
+        if (contract.getIncludes() == null || contract.getIncludes().isEmpty()) {
+            return false;
+        } else {
+            return !contract.getIncludes().contains(contractName);
+        }
+    }
+}
---
+++ src/test/java/org/web3j/mavenplugin/JavaClassGeneratorITest.java
@@ -0,0 +1,118 @@
+package org.web3j.mavenplugin;
+
+import org.apache.maven.plugin.testing.ConfigurationException;
+import org.apache.maven.plugin.testing.MojoRule;
+import org.apache.maven.plugin.testing.resources.TestResources;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+public class JavaClassGeneratorITest {
+
+    @Rule
+    public MojoRule mojoRule = new MojoRule();
+
+    @Rule
+    public TestResources resources = new TestResources();
+
+    @Rule
+    public TemporaryFolder testFolder = new TemporaryFolder();
+
+    @Test
+    public void pomStandard() throws Exception {
+        File pom = new File(resources.getBasedir("valid"), "pom.xml");
+        assertNotNull(pom);
+        assertTrue(pom.exists());
+
+        JavaClassGeneratorMojo mojo = (JavaClassGeneratorMojo) mojoRule.lookupMojo("generate-sources", pom);
+        assertNotNull(mojo);
+
+        mojo.sourceDestination = testFolder.getRoot().getPath();
+        mojo.execute();
+
+        Path path = Paths.get(mojo.sourceDestination);
+
+        List<Path> files = Files.find(path, 99, (p, bfa) -> bfa.isRegularFile()).collect(Collectors.toList());
+        assertEquals("Greeter and Mortal Class", 2l, files.size());
+    }
+
+    @Test
+    public void pomWithImport() throws Exception {
+        File pom = new File(resources.getBasedir("import"), "pom.xml");
+        assertNotNull(pom);
+        assertTrue(pom.exists());
+
+        JavaClassGeneratorMojo mojo = (JavaClassGeneratorMojo) mojoRule.lookupMojo("generate-sources", pom);
+        assertNotNull(mojo);
+
+        mojo.sourceDestination = testFolder.getRoot().getPath();
+        mojo.execute();
+
+        Path path = Paths.get(mojo.sourceDestination);
+
+        List<Path> files = Files.find(path, 99, (p, bfa) -> bfa.isRegularFile()).collect(Collectors.toList());
+        assertEquals("Main, Upper and Util Class", 3, files.size());
+    }
+
+
+    @Test
+    public void pomWithEmptyContract() throws Exception {
+        File pom = new File(resources.getBasedir("valid"), "empty.pom.xml");
+        assertNotNull(pom);
+        assertTrue(pom.exists());
+
+        JavaClassGeneratorMojo mojo = (JavaClassGeneratorMojo) mojoRule.lookupMojo("generate-sources", pom);
+        assertNotNull(mojo);
+
+        mojo.sourceDestination = testFolder.getRoot().getPath();
+        mojo.execute();
+
+        Path path = Paths.get(mojo.sourceDestination);
+
+        List<Path> files = Files.find(path, 99, (p, bfa) -> bfa.isRegularFile()).collect(Collectors.toList());
+        assertEquals("no files in default value", 0l, files.size());
+    }
+
+    @Test
+    public void pomWithoutConfiguration() throws Exception {
+        File pom = new File(resources.getBasedir("valid"), "default.pom.xml");
+        assertNotNull(pom);
+        assertTrue(pom.exists());
+
+        JavaClassGeneratorMojo mojo = (JavaClassGeneratorMojo) mojoRule.lookupMojo("generate-sources", pom);
+        assertNotNull(mojo);
+
+        mojo.sourceDestination = testFolder.getRoot().getPath();
+        mojo.execute();
+
+        Path path = Paths.get(mojo.sourceDestination);
+
+        List<Path> files = Files.find(path, 99, (p, bfa) -> bfa.isRegularFile()).collect(Collectors.toList());
+        assertEquals("no files in default value", 0l, files.size());
+    }
+
+    @Test(expected = ConfigurationException.class)
+    public void invalidPom() throws Exception {
+        File pom = new File(resources.getBasedir("invalid"), "pom.xml");
+
+        assertNotNull(pom);
+        assertTrue(pom.exists());
+
+        JavaClassGeneratorMojo mojo = (JavaClassGeneratorMojo) mojoRule.lookupMojo("generate-sources", pom);
+        assertNotNull(mojo);
+
+        mojo.execute();
+        // soliditySourceFiles is missing
+    }
+}
