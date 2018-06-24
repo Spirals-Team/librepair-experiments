1902c30b89e4ed21850834c36bb1217c91da6323
Luc Esape <luc.esape@gmail.com>
2018-06-24 07:14:20 +0200

    Bug commit from the following repository alibaba/jetcache This bug commit is a reflect of source code from: http://github.com/alibaba/jetcache/commit/8056e5e846464f71c5d1319b4f2bfbd736b5fc81.

---
+++ jetcache-support/jetcache-redis/src/main/java/com/alicp/jetcache/redis/RedisCacheBuilder.java
@@ -0,0 +1,42 @@
+package com.alicp.jetcache.redis;
+
+import com.alicp.jetcache.external.ExternalCacheBuilder;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.util.Pool;
+
+/**
+ * Created on 2016/10/7.
+ *
+ * @author <a href="mailto:areyouok@gmail.com">huangli</a>
+ */
+public class RedisCacheBuilder<T extends ExternalCacheBuilder<T>> extends ExternalCacheBuilder<T> {
+    public static class RedisCacheBuilderImpl extends RedisCacheBuilder<RedisCacheBuilderImpl> {
+    }
+
+    public static RedisCacheBuilderImpl createRedisCacheBuilder() {
+        return new RedisCacheBuilderImpl();
+    }
+
+    protected RedisCacheBuilder() {
+        buildFunc(config -> new RedisCache((RedisCacheConfig) config));
+    }
+
+    @Override
+    public RedisCacheConfig getConfig() {
+        if (config == null) {
+            config = new RedisCacheConfig();
+        }
+        return (RedisCacheConfig) config;
+    }
+
+    public T jedisPool(Pool<Jedis> pool){
+        getConfig().setJedisPool(pool);
+        return self();
+    }
+
+    public void setJedisPool(JedisPool jedisPool) {
+        getConfig().setJedisPool(jedisPool);
+    }
+
+}
---
+++ jetcache-test/src/main/java/com/alicp/jetcache/test/beans/FactoryBeanTargetImpl.java
@@ -0,0 +1,16 @@
+/**
+ * Created on  13-10-28 23:44
+ */
+package com.alicp.jetcache.test.beans;
+
+/**
+ * @author <a href="mailto:areyouok@gmail.com">huangli</a>
+ */
+public class FactoryBeanTargetImpl implements FactoryBeanTarget {
+
+    int count;
+
+    public int count() {
+        return count++;
+    }
+}
