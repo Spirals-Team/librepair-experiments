348349f56c23292f8fb9714ee8c483b295a0086b
Luc Esape <luc.esape@gmail.com>
2018-06-25 18:15:21 +0200

    Bug commit from the following repository nosan/embedded-cassandra This bug commit is a reflect of source code from: http://github.com/nosan/embedded-cassandra/commit/10fa7277d0102c02d4caf6f529ba44163a87a3bf.

---
+++ embedded-cassandra/src/main/java/com/github/nosan/embedded/cassandra/customizer/JmxPortCustomizer.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2012-2018 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.github.nosan.embedded.cassandra.customizer;
+
+import java.io.File;
+
+import de.flapdoodle.embed.process.distribution.Distribution;
+import de.flapdoodle.embed.process.distribution.Platform;
+
+/**
+ * {@link FileCustomizer} to set JMX_PORT.
+ *
+ * @author Dmytro Nosan
+ */
+public class JmxPortCustomizer extends AbstractSourceLineFileCustomizer {
+
+	private static final String[] CANDIDATES = new String[] { "$JMX_PORT=", "JMX_PORT=" };
+
+	private final int port;
+
+	public JmxPortCustomizer(int port) {
+		this.port = port;
+	}
+
+	@Override
+	protected boolean isMatch(File file, Distribution distribution) {
+		String fileName = file.getName();
+		return (distribution.getPlatform() != Platform.Windows
+				? fileName.equals("cassandra-env.sh")
+				: fileName.equals("cassandra-env.ps1"));
+	}
+
+	@Override
+	protected String process(String line, File file, Distribution distribution) {
+		String trimmed = line.trim();
+		for (String candidate : CANDIDATES) {
+			if (trimmed.startsWith(candidate)) {
+				return candidate + this.port;
+			}
+		}
+		return line;
+	}
+
+}
