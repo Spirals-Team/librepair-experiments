1a425908296e8b76c7815c3378821acbc185d12f
Luc Esape <luc.esape@gmail.com>
2018-06-25 08:53:52 +0200

    Bug commit from the following repository molgenis/molgenis This bug commit is a reflect of source code from: http://github.com/molgenis/molgenis/commit/6f837cfdf2e513a7b3c9bcfb2589696ba57d5ed3.

---
+++ molgenis-data-elasticsearch/src/main/java/org/molgenis/data/elasticsearch/generator/model/SortOrder.java
@@ -0,0 +1,32 @@
+package org.molgenis.data.elasticsearch.generator.model;
+
+import com.google.auto.value.AutoValue;
+
+@AutoValue
+@SuppressWarnings("squid:S1610") // Abstract classes without fields should be converted to interfaces
+public abstract class SortOrder
+{
+	public abstract String getField();
+
+	public abstract SortDirection getDirection();
+
+	public static SortOrder create(String newField, SortDirection newDirection)
+	{
+		return builder().setField(newField).setDirection(newDirection).build();
+	}
+
+	public static Builder builder()
+	{
+		return new AutoValue_SortOrder.Builder().setDirection(SortDirection.ASC);
+	}
+
+	@AutoValue.Builder
+	public abstract static class Builder
+	{
+		public abstract Builder setField(String newField);
+
+		public abstract Builder setDirection(SortDirection newDirection);
+
+		public abstract SortOrder build();
+	}
+}
---
+++ molgenis-semantic-mapper/src/main/java/org/molgenis/semanticmapper/algorithmgenerator/rules/quality/impl/NumericQuality.java
@@ -0,0 +1,24 @@
+package org.molgenis.semanticmapper.algorithmgenerator.rules.quality.impl;
+
+import com.google.auto.value.AutoValue;
+import org.molgenis.semanticmapper.algorithmgenerator.rules.quality.Quality;
+import org.molgenis.util.AutoGson;
+
+@AutoValue
+@AutoGson(autoValueClass = AutoValue_NumericQuality.class)
+@SuppressWarnings("squid:S1610") // Abstract classes without fields should be converted to interfaces
+public abstract class NumericQuality extends Quality<Double>
+{
+	public abstract Double getQualityIndicator();
+
+	public static NumericQuality create(double qualityIndicator)
+	{
+		return new AutoValue_NumericQuality(qualityIndicator);
+	}
+
+	@Override
+	public int compareTo(Quality<Double> quality)
+	{
+		return Double.compare(quality.getQualityIndicator(), getQualityIndicator());
+	}
+}
