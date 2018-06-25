b36bd4294cdc688ae16315fc2ea94f26371cd850
Luc Esape <luc.esape@gmail.com>
2018-06-25 16:13:07 +0200

    Bug commit from the following repository nosan/embedded-cassandra This bug commit is a reflect of source code from: http://github.com/nosan/embedded-cassandra/commit/f536c000c0e547046b7ae0372ea7f62ef98e3b94.

---
+++ embedded-cassandra/src/main/java/com/github/nosan/embedded/cassandra/customizer/Java8FileCustomizer.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2012-2018 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.github.nosan.embedded.cassandra.customizer;
+
+import java.io.File;
+
+import de.flapdoodle.embed.process.distribution.Distribution;
+
+/**
+ * todo leave javadoc here.
+ *
+ * @author Dmytro Nosan
+ */
+public class Java8FileCustomizer extends AbstractSourceLineFileCustomizer {
+
+	@Override
+	protected boolean isMatch(File file, Distribution distribution) {
+		return file.getName().equals("jvm.options");
+	}
+
+	@Override
+	protected String process(String line, File file, Distribution distribution) {
+		if (line.trim().startsWith("-XX:+UseNUMA")) {
+			return "#" + line;
+		}
+		return line;
+	}
+
+}
---
+++ embedded-cassandra/src/main/java/com/github/nosan/embedded/cassandra/customizer/JmxDisableFileCustomizer.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2012-2018 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.github.nosan.embedded.cassandra.customizer;
+
+import java.io.File;
+
+import de.flapdoodle.embed.process.distribution.Distribution;
+import de.flapdoodle.embed.process.distribution.Platform;
+
+/**
+ * Environment {@link FileCustomizer} to comment `jmx.local.port` and `jmx.remote.port`.
+ *
+ * @author Dmytro Nosan
+ */
+public class JmxDisableFileCustomizer extends AbstractSourceLineFileCustomizer {
+
+	private static final String[] COMMENT_CANDIDATES = new String[] {
+			"-Dcom.sun.management.jmxremote.rmi.port", "-Dcassandra.jmx.local.port",
+			"-Dcassandra.jmx.remote.port" };
+
+	@Override
+	protected boolean isMatch(File file, Distribution distribution) {
+		String fileName = file.getName();
+		return (distribution.getPlatform() != Platform.Windows
+				? fileName.equals("cassandra-env.sh")
+				: fileName.equals("cassandra-env.ps1"));
+	}
+
+	@Override
+	protected String process(String line, File file, Distribution distribution) {
+		String trimmed = line.trim();
+		for (String candidate : COMMENT_CANDIDATES) {
+			if (trimmed.contains(candidate)) {
+				return "#" + line;
+			}
+		}
+		return line;
+	}
+
+}
