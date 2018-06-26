806766026e4f218ec20b4f5ba7bed79f8dc7c81f
Luc Esape <luc.esape@gmail.com>
2018-06-26 22:16:38 +0200

    Bug commit from the following repository jdbi/jdbi This bug commit is a reflect of source code from: http://github.com/jdbi/jdbi/commit/acad98eb810d19d736cb2ad4a3ff7393f0b976f5.

---
+++ core/src/test/java/org/jdbi/v3/core/statement/TestStatements.java
@@ -0,0 +1,133 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jdbi.v3.core.statement;
+
+import java.util.stream.Collectors;
+import org.jdbi.v3.core.Handle;
+import org.jdbi.v3.core.Something;
+import org.jdbi.v3.core.result.NoResultsException;
+import org.jdbi.v3.core.result.ResultProducers;
+import org.jdbi.v3.core.rule.H2DatabaseRule;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class TestStatements {
+    @Rule
+    public H2DatabaseRule dbRule = new H2DatabaseRule();
+
+    private Handle h;
+
+    @Before
+    public void setUp() throws Exception {
+        h = dbRule.openHandle();
+    }
+
+    @After
+    public void doTearDown() throws Exception {
+        if (h != null) {
+            h.close();
+        }
+    }
+
+    @Test
+    public void testStatement() {
+        int rows = h.createUpdate("insert into something (id, name) values (1, 'eric')").execute();
+        assertThat(rows).isEqualTo(1);
+    }
+
+    @Test
+    public void testSimpleInsert() {
+        int c = h.execute("insert into something (id, name) values (1, 'eric')");
+        assertThat(c).isEqualTo(1);
+    }
+
+    @Test
+    public void testUpdate() {
+        h.execute("insert into something (id, name) values (1, 'eric')");
+        h.createUpdate("update something set name = 'ERIC' where id = 1").execute();
+        Something eric = h.createQuery("select * from something where id = 1").mapToBean(Something.class).list().get(0);
+        assertThat(eric.getName()).isEqualTo("ERIC");
+    }
+
+    @Test
+    public void testSimpleUpdate() {
+        h.execute("insert into something (id, name) values (1, 'eric')");
+        h.execute("update something set name = 'cire' where id = 1");
+        Something eric = h.createQuery("select * from something where id = 1").mapToBean(Something.class).list().get(0);
+        assertThat(eric.getName()).isEqualTo("cire");
+    }
+
+    @Test
+    public void testStatementWithRequiredResults() {
+        assertThatThrownBy(() -> h.createQuery("commit").mapTo(Integer.class).findFirst()).isInstanceOf(NoResultsException.class);
+    }
+
+    @Test
+    public void testStatementWithOptionalResults() {
+        h.getConfig(ResultProducers.class).allowNoResults(true);
+        assertThat(h.createQuery("commit").mapTo(Integer.class).findFirst()).isEmpty();
+    }
+
+    @Test
+    public void testStatementWithOptionalBeanResults() {
+        h.getConfig(ResultProducers.class).allowNoResults(true);
+        assertThat(h.createQuery("commit").mapToBean(Object.class).findFirst()).isEmpty();
+    }
+
+    @Test
+    public void testStatementWithOptionalMapResults() {
+        h.getConfig(ResultProducers.class).allowNoResults(true);
+        assertThat(h.createQuery("commit").mapToMap().findFirst()).isEmpty();
+    }
+
+    @Test
+    public void testUnusedBinding() {
+        assertThatThrownBy(() -> h.createQuery("select * from something")
+            .bind("id", 1)
+            .collectRows(Collectors.counting())
+        ).isInstanceOf(UnableToCreateStatementException.class);
+    }
+
+    @Test
+    // only works for empty/non-empty for now
+    public void testUsedAndUnusedBinding() {
+        assertThatThrownBy(() -> h.createQuery("select * from something")
+            .bind("id", 1)
+            .collectRows(Collectors.counting())
+        ).isInstanceOf(UnableToCreateStatementException.class);
+    }
+
+    @Test
+    public void testPermittedUnusedBinding() {
+       assertThatCode(() -> h.configure(SqlStatements.class, s -> s.setUnusedBindingAllowed(true))
+           .createQuery("select * from something")
+           .bind("id", 1)
+           .collectRows(Collectors.counting())).doesNotThrowAnyException();
+    }
+
+    @Test
+    public void testPermittedUsedAndUnusedBinding() {
+        assertThatCode(() -> h.configure(SqlStatements.class, s -> s.setUnusedBindingAllowed(true))
+            .createQuery("select * from something where id = :id")
+            .bind("id", 1)
+            .bind("name", "jack")
+            .collectRows(Collectors.counting())).doesNotThrowAnyException();
+    }
+}
---
+++ sqlobject/src/main/java/org/jdbi/v3/sqlobject/statement/internal/ResultReturner.java
@@ -0,0 +1,317 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jdbi.v3.sqlobject.statement.internal;
+
+import java.lang.reflect.Method;
+import java.lang.reflect.Type;
+import java.util.Iterator;
+import java.util.Optional;
+import java.util.function.Consumer;
+import java.util.stream.Collector;
+import java.util.stream.Stream;
+import org.jdbi.v3.core.generic.GenericTypes;
+import org.jdbi.v3.core.result.ResultIterable;
+import org.jdbi.v3.core.result.ResultIterator;
+import org.jdbi.v3.core.statement.StatementContext;
+import org.jdbi.v3.sqlobject.SingleValue;
+
+import static org.jdbi.v3.core.generic.GenericTypes.getErasedType;
+
+/**
+ * Helper class used by the {@link CustomizingStatementHandler}s to assemble
+ * the result Collection, Iterable, etc.
+ */
+abstract class ResultReturner {
+    /**
+     * If the return type is {@code void}, swallow results.
+     * @param extensionType
+     * @param method
+     * @see ResultReturner#forMethod(Class, Method) if the return type is not void
+     * @return
+     */
+    static ResultReturner forOptionalReturn(Class<?> extensionType, Method method) {
+        if (method.getReturnType() == void.class) {
+            return new VoidReturner();
+        }
+        return forMethod(extensionType, method);
+    }
+
+    /**
+     * Inspect a Method for its return type, and choose a ResultReturner subclass
+     * that handles any container that might wrap the results.
+     * @param extensionType the type that owns the Method
+     * @param method the method whose return type chooses the ResultReturner
+     * @return an instance that takes a ResultIterable and constructs the return value
+     */
+    static ResultReturner forMethod(Class<?> extensionType, Method method) {
+        Type returnType = GenericTypes.resolveType(method.getGenericReturnType(), extensionType);
+        Class<?> returnClass = getErasedType(returnType);
+        if (Void.TYPE.equals(returnClass)) {
+            return findConsumer(extensionType, method)
+                .orElseThrow(() -> new IllegalStateException(String.format(
+                    "Method %s#%s is annotated as if it should return a value, but the method is void.",
+                    method.getDeclaringClass().getName(),
+                    method.getName())));
+        } else if (ResultIterable.class.equals(returnClass)) {
+            return new ResultIterableReturner(returnType);
+        } else if (Stream.class.equals(returnClass)) {
+            return new StreamReturner(returnType);
+        } else if (ResultIterator.class.equals(returnClass)) {
+            return new ResultIteratorReturner(returnType);
+        } else if (Iterator.class.equals(returnClass)) {
+            return new IteratorReturner(returnType);
+        } else if (method.isAnnotationPresent(SingleValue.class)) {
+            return new SingleValueReturner(returnType);
+        } else {
+            return new CollectedResultReturner(returnType);
+        }
+    }
+
+    /**
+     * Inspect a Method for a {@link Consumer} to execute for each produced row.
+     * @param extensionType the extension that owns the method
+     * @param method the method called
+     * @return a ResultReturner that invokes the consumer and does not return a value
+     */
+    static Optional<ResultReturner> findConsumer(Class<?> extensionType, Method method) {
+        final Class<?>[] paramTypes = method.getParameterTypes();
+        for (int i = 0; i < paramTypes.length; i++) {
+            if (paramTypes[i] == Consumer.class) {
+                return Optional.of(new ConsumerResultReturner(method, i));
+            }
+        }
+        return Optional.empty();
+    }
+
+    protected abstract Object mappedResult(ResultIterable<?> iterable, StatementContext ctx);
+    protected abstract Object reducedResult(Stream<?> stream, StatementContext ctx);
+
+    protected abstract Type elementType(StatementContext ctx);
+
+    private static Object checkResult(Object result, Type type) {
+        if (result == null && getErasedType(type).isPrimitive()) {
+            throw new IllegalStateException("SQL method returns primitive " + type + ", but statement returned no results");
+        }
+        return result;
+    }
+
+    static class VoidReturner extends ResultReturner {
+        @Override
+        protected Void mappedResult(ResultIterable<?> iterable, StatementContext ctx) {
+            iterable.stream().forEach(i -> {}); // Make sure to consume the result
+            return null;
+        }
+
+        @Override
+        protected Void reducedResult(Stream<?> stream, StatementContext ctx) {
+            throw new UnsupportedOperationException("Cannot return void from a @UseRowReducer method");
+        }
+
+        @Override
+        protected Type elementType(StatementContext ctx) {
+            return null;
+        }
+    }
+
+    static class ResultIterableReturner extends ResultReturner {
+
+        private final Type elementType;
+
+        ResultIterableReturner(Type returnType) {
+            // extract T from Query<T>
+            elementType = GenericTypes.findGenericParameter(returnType, ResultIterable.class)
+                .orElseThrow(() -> new IllegalStateException(
+                    "Cannot reflect ResultIterable<T> element type T in method return type " + returnType));
+        }
+
+        @Override
+        protected ResultIterable<?> mappedResult(ResultIterable<?> iterable, StatementContext ctx) {
+            return iterable;
+        }
+
+        @Override
+        protected ResultIterator<?> reducedResult(Stream<?> stream, StatementContext ctx) {
+            throw new UnsupportedOperationException("Cannot return ResultIterable from a @UseRowReducer method");
+        }
+
+        @Override
+        protected Type elementType(StatementContext ctx) {
+            return elementType;
+        }
+    }
+
+    static class StreamReturner extends ResultReturner {
+        private final Type elementType;
+
+        StreamReturner(Type returnType) {
+            elementType = GenericTypes.findGenericParameter(returnType, Stream.class)
+                .orElseThrow(() -> new IllegalStateException(
+                    "Cannot reflect Stream<T> element type T in method return type " + returnType));
+        }
+
+        @Override
+        protected Stream<?> mappedResult(ResultIterable<?> iterable, StatementContext ctx) {
+            return iterable.stream();
+        }
+
+        @Override
+        protected Stream<?> reducedResult(Stream<?> stream, StatementContext ctx) {
+            return stream;
+        }
+
+        @Override
+        protected Type elementType(StatementContext ctx) {
+            return elementType;
+        }
+    }
+
+    static class ResultIteratorReturner extends ResultReturner {
+        private final Type elementType;
+
+        ResultIteratorReturner(Type returnType) {
+            this.elementType = GenericTypes.findGenericParameter(returnType, Iterator.class)
+                .orElseThrow(() -> new IllegalStateException(
+                    "Cannot reflect ResultIterator<T> element type T in method return type " + returnType));
+        }
+
+        @Override
+        protected ResultIterator<?> mappedResult(ResultIterable<?> iterable, StatementContext ctx) {
+            return iterable.iterator();
+        }
+
+        @Override
+        protected ResultIterator<?> reducedResult(Stream<?> stream, StatementContext ctx) {
+            throw new UnsupportedOperationException("Cannot return ResultIterator from a @UseRowReducer method");
+        }
+
+        @Override
+        protected Type elementType(StatementContext ctx) {
+            return elementType;
+        }
+    }
+
+    static class IteratorReturner extends ResultReturner {
+        private final Type elementType;
+
+        IteratorReturner(Type returnType) {
+            this.elementType = GenericTypes.findGenericParameter(returnType, Iterator.class)
+                .orElseThrow(() -> new IllegalStateException(
+                    "Cannot reflect Iterator<T> element type T in method return type " + returnType));
+        }
+
+        @Override
+        protected Iterator<?> mappedResult(ResultIterable<?> iterable, StatementContext ctx) {
+            return iterable.iterator();
+        }
+
+        @Override
+        protected Iterator<?> reducedResult(Stream<?> stream, StatementContext ctx) {
+            return stream.iterator();
+        }
+
+        @Override
+        protected Type elementType(StatementContext ctx) {
+            return elementType;
+        }
+    }
+
+    static class SingleValueReturner extends ResultReturner {
+        private final Type returnType;
+
+        SingleValueReturner(Type returnType) {
+            this.returnType = returnType;
+        }
+
+        @Override
+        protected Object mappedResult(ResultIterable<?> iterable, StatementContext ctx) {
+            return checkResult(iterable.findFirst().orElse(null), returnType);
+        }
+
+        @Override
+        protected Object reducedResult(Stream<?> stream, StatementContext ctx) {
+            return checkResult(stream.findFirst().orElse(null), returnType);
+        }
+
+        @Override
+        protected Type elementType(StatementContext ctx) {
+            return returnType;
+        }
+    }
+
+    static class CollectedResultReturner extends ResultReturner {
+        private final Type returnType;
+
+        CollectedResultReturner(Type returnType) {
+            this.returnType = returnType;
+        }
+
+        @Override
+        @SuppressWarnings({ "unchecked", "rawtypes" })
+        protected Object mappedResult(ResultIterable<?> iterable, StatementContext ctx) {
+            Collector collector = ctx.findCollectorFor(returnType).orElse(null);
+            if (collector != null) {
+                return iterable.collect(collector);
+            }
+            return checkResult(iterable.findFirst().orElse(null), returnType);
+        }
+
+        @Override
+        protected Object reducedResult(Stream<?> stream, StatementContext ctx) {
+            Collector collector = ctx.findCollectorFor(returnType).orElse(null);
+            if (collector != null) {
+                return stream.collect(collector);
+            }
+            return checkResult(stream.findFirst().orElse(null), returnType);
+        }
+
+        @Override
+        protected Type elementType(StatementContext ctx) {
+            // if returnType is not supported by a collector factory, assume it to be a single-value return type.
+            return ctx.findElementTypeFor(returnType).orElse(returnType);
+        }
+    }
+
+    static class ConsumerResultReturner extends ResultReturner {
+        private final int consumerIndex;
+        private final Type elementType;
+
+        ConsumerResultReturner(Method method, int consumerIndex) {
+            this.consumerIndex = consumerIndex;
+            elementType = method.getGenericParameterTypes()[consumerIndex];
+        }
+
+        @Override
+        protected Void mappedResult(ResultIterable<?> iterable, StatementContext ctx) {
+            @SuppressWarnings("unchecked")
+            Consumer<Object> consumer = (Consumer<Object>)
+                ctx.getConfig(SqlObjectStatementConfiguration.class).getArgs()[consumerIndex];
+            iterable.forEach(consumer);
+            return null;
+        }
+
+        @Override
+        protected Void reducedResult(Stream<?> stream, StatementContext ctx) {
+            @SuppressWarnings("unchecked")
+            Consumer<Object> consumer = (Consumer<Object>)
+                ctx.getConfig(SqlObjectStatementConfiguration.class).getArgs()[consumerIndex];
+            stream.forEach(consumer);
+            return null;
+        }
+
+        @Override
+        protected Type elementType(StatementContext ctx) {
+            return elementType;
+        }
+    }
+}
