3b4ec2d965a410d1bf1b1716e7bd6c843e073176
Luc Esape <luc.esape@gmail.com>
2018-06-27 22:04:00 +0200

    Bug commit from the following repository FluentLenium/FluentLenium This bug commit is a reflect of source code from: http://github.com/FluentLenium/FluentLenium/commit/9420d434e05d79e885eeb16bae1c735267dfd253.

---
+++ fluentlenium-core/src/main/java/org/fluentlenium/core/FluentPageControl.java
@@ -0,0 +1,72 @@
+package org.fluentlenium.core;
+
+import org.fluentlenium.core.url.ParsedUrlTemplate;
+
+/**
+ * Control a Page Object.
+ *
+ * @see FluentPage
+ */
+public interface FluentPageControl extends FluentControl {
+
+    /**
+     * URL of the page
+     * It can contains mandatory parameters <code>{param}</code> and optional parameters <code>{param1}</code>
+     *
+     * @return page URL
+     */
+    String getUrl();
+
+    /**
+     * URL of the page, after replacing parameters with given values.
+     *
+     * @param parameters parameter values
+     * @return Effective url generated for given parameter values
+     * @throws IllegalArgumentException if some required parameters are missing
+     */
+    String getUrl(Object... parameters);
+
+    /**
+     * Check if the browser is on this page.
+     */
+    void isAt();
+
+    /**
+     * Check if the browser is on this page, after replacing parameters with given values.
+     *
+     * @param parameters list of parameters
+     */
+    void isAt(Object... parameters);
+
+    /**
+     * Go to the url defined in the page
+     *
+     * @return <P> FluentPage object
+     */
+    <P extends FluentPage> P go(); // NOPMD ShortMethodName
+
+    /**
+     * Got to the url defined in the page, using given parameters.
+     *
+     * @param params page url parameter values
+     * @throws IllegalArgumentException if some required parameters are missing
+     *
+     * @return <P> FluentPage object
+     */
+    <P extends FluentPage> P go(Object... params);
+
+    /**
+     * Get the parameter values of page URL extracted from current URL.
+     *
+     * @return parameter values
+     */
+    ParsedUrlTemplate parseUrl();
+
+    /**
+     * Get the parameter values of page URL extracted from given URL.
+     *
+     * @param url url to parse
+     * @return parameter values
+     */
+    ParsedUrlTemplate parseUrl(String url);
+}
