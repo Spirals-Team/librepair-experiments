db35262eccc127e7f78f0d6deddf195f51502326
Luc Esape <luc.esape@gmail.com>
2018-07-01 09:26:31 +0200

    Bug commit from the following repository scottleedavis/mattermost-remind This bug commit is a reflect of source code from: http://github.com/scottleedavis/mattermost-remind/commit/608e99e179b0e28c9c661860222f675e177a8abf.

---
+++ src/test/java/scottleedavis/mattermost/remind/db/ReminderServiceTests.java
@@ -0,0 +1,85 @@
+package scottleedavis.mattermost.remind.db;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.annotation.Resource;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.assertTrue;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ReminderServiceTests {
+
+    @Autowired
+    ReminderService reminderService;
+
+    @Resource
+    ReminderRepository reminderRepository;
+
+    @Resource
+    ReminderOccurrenceRepository reminderOccurrenceRepository;
+
+    @Before
+    public void setUp() {
+        Reminder reminder = new Reminder();
+        reminder.setMessage("foo to the bar");
+        reminder.setTarget("@foo");
+        reminder.setUserName("@foo");
+        ReminderOccurrence reminderOccurrence = new ReminderOccurrence();
+        reminderOccurrence.setOccurrence(LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS));
+        reminderOccurrence.setReminder(reminder);
+        ReminderOccurrence reminderOccurrence2 = new ReminderOccurrence();
+        reminderOccurrence2.setOccurrence(LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS));
+        reminderOccurrence2.setReminder(reminder);
+        reminder.setOccurrences(Arrays.asList(reminderOccurrence, reminderOccurrence2));
+        reminderRepository.save(reminder);
+    }
+
+    @After
+    public void tearDown() {
+        reminderRepository.deleteAll();
+    }
+
+    @Test
+    public void findByOccurrence() {
+
+        List<ReminderOccurrence> reminderOccurrences = reminderService.findByOccurrence(LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS));
+        assertTrue(reminderOccurrences.size() == 2);
+    }
+
+    @Test
+    public void findByUsername() {
+        List<Reminder> reminders = reminderService.findByUsername("@foo");
+        assertTrue(reminders.size() == 1);
+    }
+
+    @Test
+    public void schedule() {
+        assertTrue(false);
+    }
+
+    @Test
+    public void delete() {
+        assertTrue(false);
+    }
+
+    @Test
+    public void complete() {
+        assertTrue(false);
+    }
+
+    @Test
+    public void snooze() {
+        assertTrue(false);
+    }
+}
