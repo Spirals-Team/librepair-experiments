033c2669f6e0254d9ecedf8bbe34389bf578cfc2
Luc Esape <luc.esape@gmail.com>
2018-07-04 20:41:46 +0200

    Bug commit from the following repository zalando/riptide This bug commit is a reflect of source code from: http://github.com/zalando/riptide/commit/6a7e4b18595c85f34d41aeca52d28a998d579b71.

---
+++ riptide-failsafe/src/main/java/org/zalando/riptide/failsafe/FailsafePlugin.java
@@ -0,0 +1,73 @@
+package org.zalando.riptide.failsafe;
+
+import net.jodah.failsafe.CircuitBreaker;
+import net.jodah.failsafe.Failsafe;
+import net.jodah.failsafe.Listeners;
+import net.jodah.failsafe.RetryPolicy;
+import org.apiguardian.api.API;
+import org.springframework.http.client.ClientHttpResponse;
+import org.zalando.riptide.Plugin;
+import org.zalando.riptide.RequestArguments;
+import org.zalando.riptide.RequestExecution;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ScheduledExecutorService;
+
+import static org.apiguardian.api.API.Status.INTERNAL;
+import static org.apiguardian.api.API.Status.STABLE;
+import static org.zalando.riptide.CancelableCompletableFuture.forwardTo;
+import static org.zalando.riptide.CancelableCompletableFuture.preserveCancelability;
+
+@API(status = STABLE)
+public final class FailsafePlugin implements Plugin {
+
+    private static final RetryPolicy NEVER = new RetryPolicy().withMaxRetries(0);
+
+    private final ScheduledExecutorService scheduler;
+    private final RetryPolicy retryPolicy;
+    private final CircuitBreaker circuitBreaker;
+    private final Listeners<ClientHttpResponse> listeners;
+
+    public FailsafePlugin(final ScheduledExecutorService scheduler) {
+        this(scheduler, NEVER, new CircuitBreaker(), new Listeners<>());
+    }
+
+    // used by riptide-spring-boot-starter
+    @API(status = INTERNAL)
+    FailsafePlugin(final ScheduledExecutorService scheduler, final RetryPolicy retryPolicy,
+            final CircuitBreaker circuitBreaker, final Listeners<ClientHttpResponse> listeners) {
+        this.scheduler = scheduler;
+        this.retryPolicy = retryPolicy;
+        this.circuitBreaker = circuitBreaker;
+        this.listeners = listeners;
+    }
+
+    public FailsafePlugin withRetryPolicy(final RetryPolicy retryPolicy) {
+        return new FailsafePlugin(scheduler, retryPolicy, circuitBreaker, listeners);
+    }
+
+    public FailsafePlugin withCircuitBreaker(final CircuitBreaker circuitBreaker) {
+        return new FailsafePlugin(scheduler, retryPolicy, circuitBreaker, listeners);
+    }
+
+    public FailsafePlugin withListeners(final Listeners<ClientHttpResponse> listeners) {
+        return new FailsafePlugin(scheduler, retryPolicy, circuitBreaker, listeners);
+    }
+
+    @Override
+    public RequestExecution prepare(final RequestArguments arguments, final RequestExecution execution) {
+        return () -> {
+            final CompletableFuture<ClientHttpResponse> original = Failsafe
+                    .with(retryPolicy)
+                    .with(circuitBreaker)
+                    .with(scheduler)
+                    .with(listeners)
+                    .future(execution::execute);
+
+            final CompletableFuture<ClientHttpResponse> cancelable = preserveCancelability(original);
+            original.whenComplete(forwardTo(cancelable));
+            return cancelable;
+        };
+    }
+
+}
