6b4394309849c0505b825dad20f466e518d415da
Luc Esape <luc.esape@gmail.com>
2018-07-05 13:26:40 +0200

    Bug commit from the following repository FluentLenium/FluentLenium This bug commit is a reflect of source code from: http://github.com/FluentLenium/FluentLenium/commit/e0f1d5940faa7e87496a8c350057528a931f1abd.

---
+++ fluentlenium-core/src/test/java/org/fluentlenium/integration/ActionOnSelectorAdvancedUserIntegrationTest.java
@@ -0,0 +1,15 @@
+package org.fluentlenium.integration;
+
+import org.junit.Test;
+
+public class ActionOnSelectorAdvancedUserIntegrationTest extends AbstractActionOnSelectorAdvancedUserIntegrationTest {
+    @Test
+    public void checkDoubleClickAction() {
+        checkDoubleClick();
+    }
+
+    @Test
+    public void checkMouseOverAction() {
+        checkMouseOver();
+    }
+}
