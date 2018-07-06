0a6516812a7950550092a4e064a585c1c36f5357
Luc Esape <luc.esape@gmail.com>
2018-07-06 12:34:23 +0200

    Bug commit from the following repository molgenis/molgenis This bug commit is a reflect of source code from: http://github.com/molgenis/molgenis/commit/fb794324c10f1a16baab3ab46096467cf03f3eb4.

---
+++ molgenis-core-ui/src/main/java/org/molgenis/core/ui/admin/usermanager/UserViewData.java
@@ -0,0 +1,89 @@
+package org.molgenis.core.ui.admin.usermanager;
+
+import org.molgenis.data.security.auth.User;
+
+import java.util.Objects;
+
+public class UserViewData
+{
+	private final String id;
+	private final String username;
+	private String firstName;
+	private String middleName;
+	private String lastName;
+	private String fullName; // first, middle, last name
+	private Boolean active;
+	private Boolean superuser;
+
+	UserViewData(User mu)
+	{
+		this(mu.getId(), mu.getUsername());
+		firstName = (null == mu.getFirstName() ? "" : mu.getFirstName());
+		middleName = (null == mu.getMiddleNames() ? "" : mu.getMiddleNames());
+		lastName = (null == mu.getLastName() ? "" : mu.getLastName());
+
+		fullName = firstName + ' ' + middleName + ' ' + lastName;
+
+		this.active = mu.isActive();
+		this.superuser = mu.isSuperuser();
+
+	}
+
+	UserViewData(String id, final String username)
+	{
+		if (null == id)
+		{
+			throw new IllegalArgumentException("id is null");
+		}
+		if (null == username)
+		{
+			throw new IllegalArgumentException("username is null");
+		}
+		this.id = id;
+		this.username = username;
+	}
+
+	public String getId()
+	{
+		return id;
+	}
+
+	public String getUsername()
+	{
+		return username;
+	}
+
+	public String getFullName()
+	{
+		return fullName;
+	}
+
+	public Boolean isActive()
+	{
+		return this.active;
+	}
+
+	public Boolean isSuperuser()
+	{
+		return this.superuser;
+	}
+
+	@Override
+	public boolean equals(Object o)
+	{
+		if (this == o) return true;
+		if (!(o instanceof UserViewData)) return false;
+		UserViewData that = (UserViewData) o;
+		return Objects.equals(getId(), that.getId()) && Objects.equals(getUsername(), that.getUsername())
+				&& Objects.equals(firstName, that.firstName) && Objects.equals(middleName, that.middleName)
+				&& Objects.equals(lastName, that.lastName) && Objects.equals(getFullName(), that.getFullName())
+				&& Objects.equals(active, that.active) && Objects.equals(superuser, that.superuser);
+	}
+
+	@Override
+	public int hashCode()
+	{
+
+		return Objects.hash(getId(), getUsername(), firstName, middleName, lastName, getFullName(), active, superuser);
+	}
+}
---
+++ molgenis-ontology-core/src/test/java/org/molgenis/ontology/core/repository/OntologyRepositoryTest.java
@@ -0,0 +1,91 @@
+package org.molgenis.ontology.core.repository;
+
+import org.mockito.ArgumentCaptor;
+import org.molgenis.data.DataService;
+import org.molgenis.data.support.QueryImpl;
+import org.molgenis.ontology.core.model.Ontology;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+import static java.util.Arrays.asList;
+import static java.util.Collections.singletonList;
+import static java.util.stream.Collectors.toList;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.molgenis.ontology.core.meta.OntologyMetaData.*;
+import static org.testng.Assert.assertEquals;
+
+@ContextConfiguration(classes = OntologyRepositoryTest.Config.class)
+public class OntologyRepositoryTest extends AbstractTestNGSpringContextTests
+{
+	@Autowired
+	private DataService dataService;
+
+	@Autowired
+	private OntologyRepository ontologyRepository;
+
+	private org.molgenis.ontology.core.meta.Ontology ontologyEntity;
+
+	@BeforeTest
+	public void beforeTest()
+	{
+		ontologyEntity = mock(org.molgenis.ontology.core.meta.Ontology.class);
+		when(ontologyEntity.getString(ID)).thenReturn("1");
+		when(ontologyEntity.getString(ONTOLOGY_IRI)).thenReturn("http://www.ontology.com/test");
+		when(ontologyEntity.getString(ONTOLOGY_NAME)).thenReturn("testOntology");
+		when(ontologyEntity.getString(SIMPLE_NAME)).thenReturn("test");
+	}
+
+	@Test
+	public void testGetOntologies()
+	{
+		when(dataService.findAll(eq(ONTOLOGY))).thenReturn(Stream.of(ontologyEntity));
+		List<Ontology> ontologies = ontologyRepository.getOntologies().collect(toList());
+		assertEquals(ontologies, asList(Ontology.create("1", "http://www.ontology.com/test", "testOntology")));
+	}
+
+	@Test
+	public void testGetOntologiesList()
+	{
+		ArgumentCaptor<Stream<Object>> idCaptor = ArgumentCaptor.forClass(Stream.class);
+		when(dataService.findAll(eq(ONTOLOGY), idCaptor.capture(),
+				eq(org.molgenis.ontology.core.meta.Ontology.class))).thenReturn(Stream.of(ontologyEntity));
+		List<Ontology> ontologies = ontologyRepository.getOntologies(singletonList("1")).collect(toList());
+		assertEquals(ontologies, asList(Ontology.create("1", "http://www.ontology.com/test", "testOntology")));
+		assertEquals(idCaptor.getValue().collect(toList()), singletonList("1"));
+	}
+
+	@Test
+	public void testGetOntology()
+	{
+		when(dataService.findOne(ONTOLOGY, QueryImpl.EQ(ONTOLOGY_IRI, "http://www.ontology.com/test"))).thenReturn(
+				ontologyEntity);
+		assertEquals(ontologyRepository.getOntology("http://www.ontology.com/test"),
+				Ontology.create("1", "http://www.ontology.com/test", "testOntology"));
+	}
+
+	@Configuration
+	public static class Config
+	{
+		@Bean
+		public DataService dataService()
+		{
+			return mock(DataService.class);
+		}
+
+		@Bean
+		public OntologyRepository ontologyRepository()
+		{
+			return new OntologyRepository();
+		}
+	}
+}
