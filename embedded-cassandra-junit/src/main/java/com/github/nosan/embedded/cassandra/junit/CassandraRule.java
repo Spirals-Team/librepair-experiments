b111053acd9e077d2df7a2f8d24f43d3140025c7
Luc Esape <luc.esape@gmail.com>
2018-07-08 12:27:38 +0200

    Bug commit from the following repository nosan/embedded-cassandra This bug commit is a reflect of source code from: http://github.com/nosan/embedded-cassandra/commit/162faa24882b7d6921b5097c799ddf3430da6b4e.

---
+++ embedded-cassandra-junit/src/main/java/com/github/nosan/embedded/cassandra/junit/CassandraRule.java
@@ -0,0 +1,90 @@
+/*
+ * Copyright 2012-2018 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.github.nosan.embedded.cassandra.junit;
+
+import de.flapdoodle.embed.process.config.IRuntimeConfig;
+import org.junit.rules.TestRule;
+import org.junit.runner.Description;
+import org.junit.runners.model.Statement;
+
+import com.github.nosan.embedded.cassandra.Cassandra;
+import com.github.nosan.embedded.cassandra.ExecutableConfig;
+import com.github.nosan.embedded.cassandra.support.ExecutableConfigBuilder;
+import com.github.nosan.embedded.cassandra.support.RuntimeConfigBuilder;
+
+/**
+ * JUnit {@link TestRule TestRule} for running an Embedded Cassandra. Cassandra will be
+ * started on the random ports. <pre>
+ *     public class CassandraTests {
+ * 	 &#64;ClassRule
+ * 	public static CassandraRule cassandra = new CassandraRule();
+ * 	 &#64;Before
+ * 	public void setUp() throws Exception {
+ * 	 //before actions
+ *    }
+ * 	 &#64;After
+ * 	public void tearDown() throws Exception {
+ * 	 //after actions
+ *    }
+ * 	 &#64;Test
+ * 	public void test() {
+ * 	//test me
+ *    }
+ *    }
+ * }</pre>
+ *
+ * @author Dmytro Nosan
+ * @see RuntimeConfigBuilder
+ * @see ExecutableConfigBuilder
+ * @see Cassandra
+ */
+public class CassandraRule extends Cassandra implements TestRule {
+
+	public CassandraRule(IRuntimeConfig runtimeConfig,
+			ExecutableConfig executableConfig) {
+		super(runtimeConfig, executableConfig);
+	}
+
+	public CassandraRule(IRuntimeConfig runtimeConfig) {
+		super(runtimeConfig, new ExecutableConfigBuilder().build());
+	}
+
+	public CassandraRule(ExecutableConfig executableConfig) {
+		super(executableConfig);
+	}
+
+	public CassandraRule() {
+		this(new ExecutableConfigBuilder().build());
+	}
+
+	@Override
+	public Statement apply(Statement base, Description description) {
+		return new Statement() {
+			@Override
+			public void evaluate() throws Throwable {
+				start();
+				try {
+					base.evaluate();
+				}
+				finally {
+					stop();
+				}
+			}
+		};
+	}
+
+}
---
+++ embedded-cassandra/src/main/java/com/github/nosan/embedded/cassandra/process/CassandraProcess.java
@@ -0,0 +1,206 @@
+/*
+ * Copyright 2012-2018 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.github.nosan.embedded.cassandra.process;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Set;
+
+import de.flapdoodle.embed.process.config.IRuntimeConfig;
+import de.flapdoodle.embed.process.config.io.ProcessOutput;
+import de.flapdoodle.embed.process.distribution.Distribution;
+import de.flapdoodle.embed.process.distribution.Platform;
+import de.flapdoodle.embed.process.extract.IExtractedFileSet;
+import de.flapdoodle.embed.process.io.IStreamProcessor;
+import de.flapdoodle.embed.process.io.LogWatchStreamProcessor;
+import de.flapdoodle.embed.process.io.Processors;
+import de.flapdoodle.embed.process.io.StreamToLineProcessor;
+import de.flapdoodle.embed.process.runtime.AbstractProcess;
+import de.flapdoodle.embed.process.runtime.ProcessControl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.github.nosan.embedded.cassandra.Config;
+import com.github.nosan.embedded.cassandra.ExecutableConfig;
+
+/**
+ * Basic implementation of {@link AbstractProcess} for Cassandra.
+ *
+ * @author Dmytro Nosan
+ */
+public final class CassandraProcess
+		extends AbstractProcess<ExecutableConfig, CassandraExecutable, CassandraProcess> {
+
+	private static final Logger log = LoggerFactory.getLogger(CassandraProcess.class);
+
+	private IRuntimeConfig runtimeConfig;
+
+	private Context context;
+
+	CassandraProcess(Distribution distribution, ExecutableConfig config,
+			IRuntimeConfig runtimeConfig, CassandraExecutable executable)
+			throws IOException {
+		super(distribution, config, runtimeConfig, executable);
+	}
+
+	@Override
+	protected List<String> getCommandLine(Distribution distribution,
+			ExecutableConfig config, IExtractedFileSet fileSet) throws IOException {
+		this.context = new Context(distribution, this.runtimeConfig, config, fileSet);
+		List<String> args = new Arguments().get(this.context);
+		log.info("Starting the new cassandra server using directory '" + fileSet.baseDir()
+				+ "' with arguments " + args);
+		return args;
+	}
+
+	@Override
+	protected void onBeforeProcess(IRuntimeConfig runtimeConfig) throws IOException {
+		this.runtimeConfig = runtimeConfig;
+	}
+
+	@Override
+	protected void onBeforeProcessStart(ProcessBuilder processBuilder,
+			ExecutableConfig config, IRuntimeConfig runtimeConfig) {
+		for (ContextCustomizer customizer : getCustomizers()) {
+			customizer.customize(this.context);
+		}
+	}
+
+	@Override
+	protected void onAfterProcessStart(ProcessControl process,
+			IRuntimeConfig runtimeConfig) throws IOException {
+		setProcessId(getProcessId());
+		ProcessOutput processOutput = runtimeConfig.getProcessOutput();
+		ExecutableConfig config = getConfig();
+		LogWatch logWatch = new LogWatch(config.getConfig(), processOutput.getOutput());
+
+		Processors.connect(process.getReader(), StreamToLineProcessor.wrap(logWatch));
+		Processors.connect(process.getError(),
+				StreamToLineProcessor.wrap(processOutput.getError()));
+
+		logWatch.waitForResult(config.getTimeout().toMillis());
+
+		if (!logWatch.isInitWithSuccess()) {
+			String failureFound = logWatch.getFailureFound();
+			if (failureFound == null) {
+				throw new IOException(
+						"Could not start a process. Please increase your startup timeout.");
+			}
+			throw new IOException("Could not start a process: " + failureFound);
+		}
+
+		log.info("Cassandra server has been started.");
+
+	}
+
+	@Override
+	protected void stopInternal() {
+		if (isProcessRunning()) {
+			if (!sendKillToProcess() || !sendTermToProcess()) {
+				tryKillToProcess();
+			}
+			try {
+				stopProcess();
+			}
+			catch (RuntimeException ex) {
+				if (isProcessRunning()) {
+					throw ex;
+				}
+			}
+		}
+		log.info("Cassandra server has been stopped.");
+
+	}
+
+	@Override
+	protected void cleanupInternal() {
+
+	}
+
+	private List<ContextCustomizer> getCustomizers() {
+		List<ContextCustomizer> customizers = new ArrayList<>();
+		FileCustomizers fileCustomizers = new FileCustomizers();
+		fileCustomizers.addCustomizer(new JavaCompatibilityFileCustomizer());
+		fileCustomizers.addCustomizer(new JmxPortFileCustomizer());
+		fileCustomizers.addCustomizer(new JVMOptionsFileCustomizer());
+		fileCustomizers.addCustomizer(new ConfigFileCustomizer());
+		customizers.add(new RandomPortCustomizer());
+		customizers.add(fileCustomizers);
+		return customizers;
+	}
+
+	/**
+	 * Utility class for watching cassandra log's.
+	 */
+	private static final class LogWatch extends LogWatchStreamProcessor {
+
+		LogWatch(Config config, IStreamProcessor output) {
+			super(getSuccess(config), getFailures(), output);
+		}
+
+		private static String getSuccess(Config config) {
+
+			if (config.isStartNativeTransport()) {
+				return "Starting listening for CQL";
+			}
+
+			if (config.isStartRpc()) {
+				return "Listening for thrift clients";
+			}
+
+			if (config.getListenInterface() != null || config.getRpcInterface() != null) {
+				return "Starting Messaging Service";
+			}
+
+			return "Not starting";
+		}
+
+		private static Set<String> getFailures() {
+			return new LinkedHashSet<>(Arrays.asList("encountered during startup",
+					"Missing required", "Address already in use", "Port already in use",
+					"ConfigurationException", "syntax error near unexpected",
+					"Error occurred during initialization",
+					"Cassandra 3.0 and later require Java"));
+		}
+
+	}
+
+	/**
+	 * Utility class for creating command line.
+	 */
+	static final class Arguments {
+
+		List<String> get(Context context) {
+			IExtractedFileSet fileSet = context.getExtractedFileSet();
+			Distribution distribution = context.getDistribution();
+			List<String> args = new ArrayList<>();
+			if (distribution.getPlatform() == Platform.Windows) {
+				args.add("powershell");
+				args.add("-ExecutionPolicy");
+				args.add("Bypass");
+			}
+			args.add(fileSet.executable().getAbsolutePath());
+			args.add("-f");
+			return args;
+		}
+
+	}
+
+}
