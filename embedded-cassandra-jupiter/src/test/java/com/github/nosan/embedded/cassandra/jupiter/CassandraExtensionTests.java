e104047de56d881cb0724852ac3b9fc2e5f97468
Luc Esape <luc.esape@gmail.com>
2018-07-08 12:29:56 +0200

    Bug commit from the following repository nosan/embedded-cassandra This bug commit is a reflect of source code from: http://github.com/nosan/embedded-cassandra/commit/7df32f4741196bcddf32f2bffbcf01c769159b10.

---
+++ embedded-cassandra-jupiter/src/test/java/com/github/nosan/embedded/cassandra/jupiter/CassandraExtensionTests.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2012-2018 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.github.nosan.embedded.cassandra.jupiter;
+
+import com.datastax.driver.core.Cluster;
+import com.datastax.driver.core.Session;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+
+import com.github.nosan.embedded.cassandra.Config;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Tests for {@link CassandraExtension}.
+ *
+ * @author Dmytro Nosan
+ */
+public class CassandraExtensionTests {
+
+	@RegisterExtension
+	public static CassandraExtension cassandra = new CassandraExtension();
+
+	private Cluster cluster;
+
+	@BeforeEach
+	public void setUp() throws Exception {
+		Config config = cassandra.getExecutableConfig().getConfig();
+		this.cluster = Cluster.builder().withPort(config.getNativeTransportPort())
+				.addContactPoint(config.getListenAddress()).build();
+		try (Session session = this.cluster.connect()) {
+			session.execute("CREATE KEYSPACE  test  WITH REPLICATION = { 'class' : "
+					+ "'SimpleStrategy', 'replication_factor' : 1 }; ");
+			session.execute("CREATE TABLE  test.roles (   id text PRIMARY KEY );");
+		}
+	}
+
+	@AfterEach
+	public void tearDown() throws Exception {
+		if (this.cluster != null) {
+			this.cluster.close();
+		}
+	}
+
+	@Test
+	public void select() {
+		try (Session session = this.cluster.connect()) {
+			assertThat(session.execute("SELECT * FROM  test.roles").wasApplied())
+					.isTrue();
+		}
+	}
+
+}
---
+++ embedded-cassandra/src/test/java/com/github/nosan/embedded/cassandra/process/CassandraStarterTests.java
@@ -0,0 +1,142 @@
+/*
+ * Copyright 2012-2018 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.github.nosan.embedded.cassandra.process;
+
+import java.io.IOException;
+import java.time.Duration;
+
+import com.datastax.driver.core.Cluster;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.github.nosan.embedded.cassandra.Config;
+import com.github.nosan.embedded.cassandra.ExecutableConfig;
+import com.github.nosan.embedded.cassandra.support.ExecutableConfigBuilder;
+import com.github.nosan.embedded.cassandra.support.RuntimeConfigBuilder;
+
+/**
+ * Tests for {@link CassandraStarter}.
+ *
+ * @author Dmytro Nosan
+ */
+public class CassandraStarterTests {
+
+	@Rule
+	public ExpectedException expectedException = ExpectedException.none();
+
+	private static final Logger log = LoggerFactory
+			.getLogger(CassandraStarterTests.class);
+
+	private static Cluster cluster(Config config) {
+		return Cluster.builder().addContactPoint(config.getListenAddress())
+				.withPort(config.getNativeTransportPort()).build();
+	}
+
+	@Test
+	public void useNativeTransport() throws Exception {
+		ExecutableConfig executableConfig = new ExecutableConfigBuilder().build();
+		start(executableConfig, () -> {
+			try (Cluster cluster = cluster(executableConfig.getConfig())) {
+				cluster.connect().execute("CREATE KEYSPACE IF NOT EXISTS test  "
+						+ "WITH REPLICATION = { 'class' : 'SimpleStrategy', 'replication_factor' : 1 }");
+			}
+		});
+	}
+
+	@Test
+	public void restart() throws Exception {
+		ExecutableConfig executableConfig = new ExecutableConfigBuilder().build();
+		Config config = new Config();
+		config.setNativeTransportPort(9042);
+		executableConfig.setConfig(config);
+		start(executableConfig);
+		start(executableConfig);
+	}
+
+	@Test
+	public void multiplyInstances() throws Exception {
+		start(new ExecutableConfigBuilder().build(),
+				() -> start(new ExecutableConfigBuilder().build()));
+	}
+
+	@Test
+	public void invalidConfig() throws Exception {
+		this.expectedException.expect(IOException.class);
+		this.expectedException.expectMessage("Missing required directive CommitLogSync");
+		ExecutableConfig executableConfig = new ExecutableConfigBuilder().build();
+		executableConfig.getConfig().setCommitlogSync(null);
+		start(executableConfig);
+	}
+
+	@Test
+	public void timeout() throws Exception {
+		this.expectedException.expect(IOException.class);
+		this.expectedException.expectMessage(
+				"Could not start a process. Please increase your startup timeout.");
+		ExecutableConfig executableConfig = new ExecutableConfigBuilder()
+				.timeout(Duration.ofSeconds(1)).build();
+		start(executableConfig);
+	}
+
+	@Test
+	public void shouldUseRpcTransport() throws Exception {
+		ExecutableConfig executableConfig = new ExecutableConfigBuilder().build();
+		Config config = executableConfig.getConfig();
+		config.setStartNativeTransport(false);
+		config.setStartRpc(true);
+		start(executableConfig);
+	}
+
+	@Test
+	public void shouldWorkWithDisableTransport() throws Exception {
+		ExecutableConfig executableConfig = new ExecutableConfigBuilder().build();
+		Config config = executableConfig.getConfig();
+		config.setStartNativeTransport(false);
+		config.setStartRpc(false);
+
+		start(executableConfig);
+
+	}
+
+	private static void start(ExecutableConfig executableConfig) throws Exception {
+		start(executableConfig, () -> {
+		});
+	}
+
+	private static void start(ExecutableConfig executableConfig, Callback callback)
+			throws Exception {
+		CassandraExecutable executable = new CassandraStarter(
+				new RuntimeConfigBuilder(log).build()).prepare(executableConfig);
+		try {
+			executable.start();
+			callback.run();
+		}
+		finally {
+			executable.stop();
+		}
+	}
+
+	interface Callback {
+
+		void run() throws Exception;
+
+	}
+
+}
