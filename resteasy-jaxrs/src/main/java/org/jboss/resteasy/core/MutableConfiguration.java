056333a24bb507d472598b166ad0e007de4f8afe
Luc Esape <luc.esape@gmail.com>
2018-07-08 10:52:29 +0200

    Bug commit from the following repository resteasy/Resteasy This bug commit is a reflect of source code from: http://github.com/resteasy/Resteasy/commit/7a34466233cc12a4c4feabda7e58179929004d14.

---
+++ resteasy-jaxrs/src/main/java/org/jboss/resteasy/core/MutableConfiguration.java
@@ -0,0 +1,13 @@
+package org.jboss.resteasy.core;
+
+import java.util.Map;
+
+/**
+ * RESTEasy Configuration that contains mutable properties.
+ * This is used for changing properties in configurable classes
+ * such as ResteasyProviderFactory or ClientConfiguration.
+ *
+ */
+public interface MutableConfiguration {
+    public Map<String, Object> getMutableProperties();
+}
