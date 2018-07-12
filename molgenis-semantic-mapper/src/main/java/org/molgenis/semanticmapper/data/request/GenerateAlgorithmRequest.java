b84bc3d43b1a0a8518131bb6c6955ae4e6c10590
Luc Esape <luc.esape@gmail.com>
2018-07-12 10:09:22 +0200

    Bug commit from the following repository molgenis/molgenis This bug commit is a reflect of source code from: http://github.com/molgenis/molgenis/commit/47a2a9efe31fc69b547b7f1f3147197957d1009f.

---
+++ molgenis-semantic-mapper/src/main/java/org/molgenis/semanticmapper/data/request/GenerateAlgorithmRequest.java
@@ -0,0 +1,26 @@
+package org.molgenis.semanticmapper.data.request;
+
+import com.google.auto.value.AutoValue;
+import org.molgenis.util.AutoGson;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import java.util.List;
+
+@AutoValue
+@AutoGson(autoValueClass = AutoValue_GenerateAlgorithmRequest.class)
+@SuppressWarnings("squid:S1610") // Abstract classes without fields should be converted to interfaces
+public abstract class GenerateAlgorithmRequest
+{
+	@NotNull
+	public abstract String getTargetEntityTypeId();
+
+	@NotNull
+	public abstract String getTargetAttributeName();
+
+	@NotNull
+	public abstract String getSourceEntityTypeId();
+
+	@NotEmpty
+	public abstract List<String> getSourceAttributes();
+}
