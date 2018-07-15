852d307f52efa2f1eb7420757a19ee086323f82c
Luc Esape <luc.esape@gmail.com>
2018-07-15 20:53:20 +0200

    Bug commit from the following repository joel-costigliola/assertj-core This bug commit is a reflect of source code from: http://github.com/joel-costigliola/assertj-core/commit/dc6e6f258ae68dce07d6e17ba7b9c4d9d3ae313d.

---
+++ src/test/java/org/assertj/core/api/Assertions_assertThat_inUnicode_Test.java
@@ -0,0 +1,45 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ * Copyright 2012-2018 the original author or authors.
+ */
+package org.assertj.core.api;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+
+import org.junit.Test;
+
+/**
+ * Tests for {@link org.assertj.core.presentation.UnicodeRepresentation#toStringOf(Object)}.
+ *
+ * @author Mariusz Smykula
+ */
+public class Assertions_assertThat_inUnicode_Test {
+
+  @Test
+  public void should_assert_String_in_unicode() {
+    assertThatExceptionOfType(AssertionError.class).isThrownBy(() -> assertThat("a6c").inUnicode().isEqualTo("abó"))
+                                                   .withMessage("expected:<a[b\\u00f3]> but was:<a[6c]>");
+  }
+
+  @Test
+  public void should_assert_Character_in_unicode() {
+    assertThatExceptionOfType(AssertionError.class).isThrownBy(() -> assertThat('o').inUnicode().isEqualTo('ó'))
+                                                   .withMessage("expected:<[\\u00f3]> but was:<[o]>");
+  }
+
+  @Test
+  public void should_assert_char_array_in_unicode_representation() {
+    assertThatExceptionOfType(AssertionError.class).isThrownBy(() -> assertThat("a6c".toCharArray()).inUnicode()
+                                                                                                    .isEqualTo("abó".toCharArray()))
+                                                   .withMessage("expected:<[a, [b, \\u00f3]]> but was:<[a, [6, c]]>");
+  }
+}
