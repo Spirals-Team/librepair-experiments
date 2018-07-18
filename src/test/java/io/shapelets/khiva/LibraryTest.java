294ae5152bbf2e0466ec0dd14a32df11497c1761
Luc Esape <luc.esape@gmail.com>
2018-07-18 16:26:53 +0200

    Bug commit from the following repository shapelets/khiva-java This bug commit is a reflect of source code from: http://github.com/shapelets/khiva-java/commit/9d0471226777136f0d6e7d4b12fcfc2298d1f3df.

---
+++ src/test/java/io/shapelets/khiva/LibraryTest.java
@@ -0,0 +1,127 @@
+/*
+ * Copyright (c) 2018 Shapelets.io
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ */
+
+package io.shapelets.khiva;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class LibraryTest {
+
+    @Test
+    public void testPrintBackendInfo(){
+
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        PrintStream ps = new PrintStream(baos);
+        PrintStream old = System.out;
+        System.setOut(ps);
+
+        Library.printBackendInfo();
+
+        // Put things back
+        System.out.flush();
+        System.setOut(old);
+
+        String info = baos.toString();
+        String words[] = info.split(" ");
+        Assert.assertEquals(words[0], "ArrayFire");
+    }
+
+    @Test
+    public void testBackendInfo(){
+        String info = Library.getBackendInfo();
+        String words[] = info.split(" ");
+        Assert.assertEquals(words[0], "ArrayFire");
+    }
+
+    @Test
+    public void testSetBackend() {
+        int backends = Library.getKhivaBackends();
+        int cuda = backends & Library.Backend.KHIVA_BACKEND_CUDA.getKhivaOrdinal();
+        int opencl = backends & Library.Backend.KHIVA_BACKEND_OPENCL.getKhivaOrdinal();
+        int cpu = backends & Library.Backend.KHIVA_BACKEND_CPU.getKhivaOrdinal();
+
+        if (cuda != 0) {
+            Library.setKhivaBackend(Library.Backend.KHIVA_BACKEND_CUDA);
+            Assert.assertEquals(Library.getKhivaBackend(), Library.Backend.KHIVA_BACKEND_CUDA);
+        }
+        if (opencl != 0) {
+            Library.setKhivaBackend(Library.Backend.KHIVA_BACKEND_OPENCL);
+            Assert.assertEquals(Library.getKhivaBackend(), Library.Backend.KHIVA_BACKEND_OPENCL);
+        }
+        if (cpu != 0) {
+            Library.setKhivaBackend(Library.Backend.KHIVA_BACKEND_CPU);
+            Assert.assertEquals(Library.getKhivaBackend(), Library.Backend.KHIVA_BACKEND_CPU);
+        }
+    }
+
+    @Test
+    public void testGetDevice() {
+        int backends = Library.getKhivaBackends();
+        int cuda = backends & Library.Backend.KHIVA_BACKEND_CUDA.getKhivaOrdinal();
+        int opencl = backends & Library.Backend.KHIVA_BACKEND_OPENCL.getKhivaOrdinal();
+        int cpu = backends & Library.Backend.KHIVA_BACKEND_CPU.getKhivaOrdinal();
+
+        if (cuda != 0) {
+            Library.setKhivaBackend(Library.Backend.KHIVA_BACKEND_CUDA);
+            for (int i = 0; i < Library.getKhivaDeviceCount(); i++) {
+                Library.setKhivaDevice(i);
+                Assert.assertEquals(Library.getKhivaDeviceID(), i);
+            }
+        }
+        if (opencl != 0) {
+            Library.setKhivaBackend(Library.Backend.KHIVA_BACKEND_OPENCL);
+            for (int i = 0; i < Library.getKhivaDeviceCount(); i++) {
+                Library.setKhivaDevice(i);
+                Assert.assertEquals(Library.getKhivaDeviceID(), i);
+            }
+        }
+        if (cpu != 0) {
+            Library.setKhivaBackend(Library.Backend.KHIVA_BACKEND_CPU);
+            for (int i = 0; i < Library.getKhivaDeviceCount(); i++) {
+                Library.setKhivaDevice(i);
+                Assert.assertEquals(Library.getKhivaDeviceID(), i);
+            }
+        }
+    }
+
+    @Test
+    public void testGetKhivaVersion() { Assert.assertEquals(Library.getKhivaVersion(), getKhivaVersionFromFile()); }
+
+    private String getKhivaVersionFromFile() {
+        String version = "";
+        String filePath;
+
+        if(System.getProperty("os.name").startsWith("Windows")){
+            filePath = "C:/Program Files/Khiva/v0/include/khiva/version.h";
+        }else{
+            filePath = "/usr/local/include/khiva/version.h";
+        }
+
+        String data = "";
+        try {
+            data = new String(Files.readAllBytes(Paths.get(filePath)));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Matcher m = Pattern.compile("([0-9]+\\.[0-9]+\\.[0-9]+)").matcher(data);
+        if (m.find()){
+            version = m.group(1);
+        }
+
+        return version;
+    }
+}
---
+++ src/test/java/io/shapelets/khiva/MatrixTest.java
@@ -0,0 +1,207 @@
+/*
+ * Copyright (c) 2018 Shapelets.io
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ */
+
+package io.shapelets.khiva;
+
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class MatrixTest {
+    private static final double DELTA = 1e-6;
+
+    @BeforeClass
+    public static void setUp() {
+        Library.setKhivaBackend(Library.Backend.KHIVA_BACKEND_CPU);
+    }
+
+
+    @Test
+    public void testStompSelfJoin() throws Exception {
+        double[] ta = {10, 10, 10, 11, 12, 11, 10, 10, 11, 12, 11, 10, 10, 10};
+        long[] dims = {14, 1, 1, 1};
+        Array a = new Array(ta, dims);
+        Array[] stompSelfJoinResult = Matrix.stompSelfJoin(a, 3);
+        double[] expectedIndex = {11, 6, 7, 8, 9, 10, 1, 2, 3, 4, 5, 0};
+        double[] matrix = stompSelfJoinResult[0].getData();
+        int[] index = stompSelfJoinResult[1].getData();
+
+        for (int i = 0; i < expectedIndex.length; i++) {
+            Assert.assertEquals(matrix[i], 0, DELTA);
+            Assert.assertEquals(index[i], expectedIndex[i], DELTA);
+        }
+    }
+
+    @Test
+    public void testStomp() throws Exception {
+        double[] tss = {10, 10, 11, 11, 10, 11, 10, 10};
+
+        long[] dims = {8, 1, 1, 1};
+
+        Array a = new Array(tss, dims);
+        Array b = new Array(tss, dims);
+
+        double[] expectedIndex = {0, 1, 2, 3, 4, 5};
+        Array[] stompResult = Matrix.stomp(a, b, 3);
+        double[] matrix = stompResult[0].getData();
+        int[] index = stompResult[1].getData();
+        for (int i = 0; i < expectedIndex.length; i++) {
+            Assert.assertEquals(matrix[i], 0, DELTA);
+            Assert.assertEquals(index[i], expectedIndex[i], DELTA);
+        }
+    }
+
+    @Test
+    public void testFindBestNMotifs() throws Exception {
+        float[][] tss = {{10, 10, 10, 10, 10, 10, 9, 10, 10, 10, 10, 10, 11, 10, 9},
+                {10, 11, 10, 9}};
+        long[] dims = {15, 1, 1, 1};
+        long[] dimsB = {4, 1, 1, 1};
+
+        Array a = new Array(tss[0], dims);
+        Array b = new Array(tss[1], dimsB);
+
+        Array[] stompResult = Matrix.stomp(a, b, 3);
+        Array[] findMotifs = Matrix.findBestNMotifs(stompResult[0], stompResult[1], 3, 1);
+
+        int[] index = findMotifs[1].getData();
+        int[] subsequenceIndex = findMotifs[2].getData();
+
+        Assert.assertEquals(index[0], 12, DELTA);
+        Assert.assertEquals(subsequenceIndex[0], 1, DELTA);
+    }
+
+    @Test
+    public void testFindBestNMotifsMultipleProfiles() throws Exception {
+        float[] dataA = {10, 10, 10, 10, 10, 10, 9, 10, 10, 10, 10, 10, 11, 10, 9, 10, 10, 10, 10, 10, 10, 9, 10, 10, 10, 10, 10, 11, 10, 9};
+        float[] dataB = {10, 11, 10, 9, 10, 11, 10, 9};
+        long[] dims = {15, 2, 1, 1};
+        long[] dimsB = {4, 2, 1, 1};
+
+        Array a = new Array(dataA, dims);
+        Array b = new Array(dataB, dimsB);
+
+        Array[] stompResult = Matrix.stomp(a, b, 3);
+        Array[] findMotifs = Matrix.findBestNMotifs(stompResult[0], stompResult[1], 3, 1);
+
+        int[] index = findMotifs[1].getData();
+        int[] subsequenceIndex = findMotifs[2].getData();
+
+        int[] expectedIndex = {12, 12, 12, 12};
+        int[] expectedSubsequenceIndex = {1, 1, 1, 1};
+
+        Assert.assertArrayEquals(index, expectedIndex);
+        Assert.assertArrayEquals(subsequenceIndex, expectedSubsequenceIndex);
+    }
+
+    @Test
+    public void testFindBestNMotifsMirror() throws Exception {
+        float[] tss = {10.1f, 11, 10.2f, 10.15f, 10.775f, 10.1f, 11, 10.2f};
+        long[] dims = {8, 1, 1, 1};
+
+        Array a = new Array(tss, dims);
+
+        Array[] stompResult = Matrix.stompSelfJoin(a, 3);
+        Array[] findMotifs = Matrix.findBestNMotifs(stompResult[0], stompResult[1], 3, 2, true);
+
+        int[] index = findMotifs[1].getData();
+        int[] subsequenceIndex = findMotifs[2].getData();
+
+        int[] expectedIndex = {0, 0};
+        int[] expectedSubsequenceIndex = {5, 3};
+
+        Assert.assertArrayEquals(index, expectedIndex);
+        Assert.assertArrayEquals(subsequenceIndex, expectedSubsequenceIndex);
+    }
+
+    @Test
+    public void testFindBestNMotifsConsecutive() throws Exception {
+        float[] tss = {10.1f, 11, 10.1f, 10.15f, 10.075f, 10.1f, 11, 10.1f, 10.15f};
+        long[] dims = {9, 1, 1, 1};
+
+        Array a = new Array(tss, dims);
+
+        Array[] stompResult = Matrix.stompSelfJoin(a, 3);
+        Array[] findMotifs = Matrix.findBestNMotifs(stompResult[0], stompResult[1], 3, 2, true);
+
+        int[] index = findMotifs[1].getData();
+        int[] subsequenceIndex = findMotifs[2].getData();
+
+        Assert.assertEquals(index[1], 6, DELTA);
+        Assert.assertEquals(subsequenceIndex[1], 3, DELTA);
+    }
+
+    @Test
+    public void testFindBestNDiscords() throws Exception {
+        float[] dataA = {11, 10, 11, 10, 11, 10, 11, 10, 11, 10, 11, 10, 1};
+        float[] dataB = {9, 10.1f, 10.2f, 10.1f, 10.2f, 10.1f, 10.2f, 10.1f, 10.2f, 10.1f, 10.2f, 10.1f, 9};
+
+        long[] dims = {13, 1, 1, 1};
+        Array a = new Array(dataA, dims);
+        Array b = new Array(dataB, dims);
+
+        Array[] stompResult = Matrix.stomp(a, b, 3);
+        Array[] findDiscords = Matrix.findBestNDiscords(stompResult[0], stompResult[1], 3, 2);
+        int[] subsequenceIndex = findDiscords[2].getData();
+
+        Assert.assertEquals(subsequenceIndex[0], 0, DELTA);
+        Assert.assertEquals(subsequenceIndex[1], 10, DELTA);
+    }
+
+    @Test
+    public void testFindBestNDiscordsMultipleProfiles() throws Exception {
+        float[] dataA = {11, 10, 11, 10, 11, 10, 11, 10, 11, 10, 11, 10, 1, 11, 10, 11, 10, 11, 10, 11, 10, 11, 10, 11,
+                10, 1};
+        float[] dataB = {9, 10.1f, 10.2f, 10.1f, 10.2f, 10.1f, 10.2f, 10.1f, 10.2f, 10.1f, 10.2f, 10.1f, 9, 9, 10.1f,
+                10.2f, 10.1f, 10.2f, 10.1f, 10.2f, 10.1f, 10.2f, 10.1f, 10.2f, 10.1f, 9};
+
+        long[] dims = {13, 2, 1, 1};
+        Array a = new Array(dataA, dims);
+        Array b = new Array(dataB, dims);
+
+        Array[] stompResult = Matrix.stomp(a, b, 3);
+        Array[] findDiscords = Matrix.findBestNDiscords(stompResult[0], stompResult[1], 3, 2);
+        int[] subsequenceIndex = findDiscords[2].getData();
+
+        int[] expectedSubsequenceIndex = {0, 10, 0, 10, 0, 10, 0, 10};
+
+        Assert.assertArrayEquals(subsequenceIndex, expectedSubsequenceIndex);
+    }
+
+    @Test
+    public void testFindBestNDiscordsMirror() throws Exception {
+        float[] dataA = {10, 11, 10, 10, 11, 10};
+
+        long[] dims = {6, 1, 1, 1};
+        Array a = new Array(dataA, dims);
+
+        Array[] stompResult = Matrix.stompSelfJoin(a, 3);
+        Array[] findDiscords = Matrix.findBestNDiscords(stompResult[0], stompResult[1], 3, 1, true);
+        int[] index = findDiscords[1].getData();
+        int[] subsequenceIndex = findDiscords[2].getData();
+
+        Assert.assertEquals(index[0], 3, DELTA);
+        Assert.assertEquals(subsequenceIndex[0], 1, DELTA);
+    }
+
+    @Test
+    public void testFindBestNDiscordsConsecutive() throws Exception {
+        float[] dataA = {10, 11, 10, 11, 10, 11, 10, 11, 10, 11, 10, 11, 10, 9.999f, 9.998f};
+
+        long[] dims = {15, 1, 1, 1};
+        Array a = new Array(dataA, dims);
+
+        Array[] stompResult = Matrix.stompSelfJoin(a, 3);
+        Array[] findDiscords = Matrix.findBestNDiscords(stompResult[0], stompResult[1], 3, 2, true);
+        int[] subsequenceIndex = findDiscords[2].getData();
+
+        Assert.assertEquals(subsequenceIndex[0], 12, DELTA);
+        Assert.assertNotEquals(subsequenceIndex[1], 11, DELTA);
+    }
+}
