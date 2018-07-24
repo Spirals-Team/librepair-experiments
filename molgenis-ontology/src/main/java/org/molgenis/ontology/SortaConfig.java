911d76865b6c3a396aa757fc5238af86a42d694c
Luc Esape <luc.esape@gmail.com>
2018-07-24 11:09:45 +0200

    Bug commit from the following repository molgenis/molgenis This bug commit is a reflect of source code from: http://github.com/molgenis/molgenis/commit/a1a1ad24848066129b9aaa1f648f3db1e808842f.

---
+++ molgenis-ontology/src/main/java/org/molgenis/ontology/SortaConfig.java
@@ -0,0 +1,50 @@
+package org.molgenis.ontology;
+
+import org.molgenis.data.DataService;
+import org.molgenis.ontology.core.ic.OntologyTermFrequencyServiceImpl;
+import org.molgenis.ontology.core.ic.TermFrequencyService;
+import org.molgenis.ontology.core.meta.OntologyTermSynonymFactory;
+import org.molgenis.ontology.roc.InformationContentService;
+import org.molgenis.ontology.sorta.meta.OntologyTermHitMetaData;
+import org.molgenis.ontology.sorta.service.SortaService;
+import org.molgenis.ontology.sorta.service.impl.SortaServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import static java.util.Objects.requireNonNull;
+
+@Configuration
+public class SortaConfig
+{
+	private final DataService dataService;
+	private final OntologyTermHitMetaData ontologyTermHitMetaData;
+	private final OntologyTermSynonymFactory ontologyTermSynonymFactory;
+
+	public SortaConfig(DataService dataService, OntologyTermHitMetaData ontologyTermHitMetaData,
+			OntologyTermSynonymFactory ontologyTermSynonymFactory)
+	{
+		System.setProperty("jdk.xml.entityExpansionLimit", "1280000");
+		this.dataService = requireNonNull(dataService);
+		this.ontologyTermHitMetaData = requireNonNull(ontologyTermHitMetaData);
+		this.ontologyTermSynonymFactory = requireNonNull(ontologyTermSynonymFactory);
+	}
+
+	@Bean
+	public TermFrequencyService termFrequencyService()
+	{
+		return new OntologyTermFrequencyServiceImpl(dataService);
+	}
+
+	@Bean
+	public SortaService sortaService()
+	{
+		return new SortaServiceImpl(dataService, informationContentService(), ontologyTermHitMetaData,
+				ontologyTermSynonymFactory);
+	}
+
+	@Bean
+	public InformationContentService informationContentService()
+	{
+		return new InformationContentService(dataService);
+	}
+}
