84bbc3a50f2f0fff5f764d405bb29bc30816720a
Luc Esape <luc.esape@gmail.com>
2018-07-26 17:47:41 +0200

    Bug commit from the following repository xlui/seckill This bug commit is a reflect of source code from: http://github.com/xlui/seckill/commit/64ca7a19a71059cb6837ac03c45dce31dac3b611.

---
+++ src/main/java/app/xlui/seckill/web/DistributedSeckillController.java
@@ -0,0 +1,59 @@
+package app.xlui.seckill.web;
+
+import app.xlui.seckill.config.SeckillProperties;
+import app.xlui.seckill.entity.resp.Response;
+import app.xlui.seckill.service.DistributedSeckillService;
+import app.xlui.seckill.service.SeckillService;
+import app.xlui.seckill.web.util.ControllerUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+@RestController
+@RequestMapping("/d")
+public class DistributedSeckillController {
+	private static final Logger log = LoggerFactory.getLogger(DistributedSeckillController.class);
+	private static ExecutorService executor = Executors.newCachedThreadPool();
+	private final DistributedSeckillService distributedSeckillService;
+	private final SeckillService seckillService;
+	private final SeckillProperties properties;
+
+	@Autowired
+	public DistributedSeckillController(SeckillService seckillService, SeckillProperties properties, DistributedSeckillService distributedSeckillService) {
+		this.seckillService = seckillService;
+		this.properties = properties;
+		this.distributedSeckillService = distributedSeckillService;
+	}
+
+	@RequestMapping(value = "/v1", method = RequestMethod.GET)
+	public Response v1(long itemId) {
+		return ControllerUtils.mock(
+				"Redis Distributed Lock",
+				itemId,
+				log,
+				executor,
+				seckillService,
+				properties,
+				(i) -> distributedSeckillService.redisLockStart(itemId, i)
+		);
+	}
+
+	@RequestMapping(value = "/v2", method = RequestMethod.GET)
+	public Response v2(long itemId) {
+		return ControllerUtils.mock(
+				"Zookeeper Distributed Lock",
+				itemId,
+				log,
+				executor,
+				seckillService,
+				properties,
+				(i) -> distributedSeckillService.zkLockStart(itemId, i)
+		);
+	}
+}
---
+++ src/main/java/app/xlui/seckill/web/SeckillController.java
@@ -0,0 +1,144 @@
+package app.xlui.seckill.web;
+
+import app.xlui.seckill.config.SeckillProperties;
+import app.xlui.seckill.entity.resp.Response;
+import app.xlui.seckill.service.SeckillService;
+import app.xlui.seckill.web.util.ControllerUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.*;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+@RestController
+public class SeckillController {
+	private static final Logger LOGGER = LoggerFactory.getLogger(SeckillController.class);
+	private static final Lock lock = new ReentrantLock(true);
+	//	private static ExecutorService executor = Executors.newCachedThreadPool();
+	private static ThreadPoolExecutor executor = new ThreadPoolExecutor(
+			0,
+			Integer.MAX_VALUE,
+			60L,
+			TimeUnit.SECONDS,
+			new SynchronousQueue<>(),
+			Executors.defaultThreadFactory(),
+			new ThreadPoolExecutor.AbortPolicy()
+	);
+	private final SeckillService seckillService;
+	private final SeckillProperties properties;
+
+	@Autowired
+	public SeckillController(SeckillService seckillService, SeckillProperties properties) {
+		this.seckillService = seckillService;
+		this.properties = properties;
+	}
+
+	private String waitForResult(String type, long itemId, long start, CountDownLatch wait) {
+		try {
+			wait.await();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+		String ret;
+		String cost = "cost about: " + (System.currentTimeMillis() - start) / 1000 + "s.";
+		long count = seckillService.successCount(itemId);
+		ret = "total seckill " + count + " items.";
+		LOGGER.info("Seckill Type: [" + type + "].");
+		LOGGER.info(ret);
+		LOGGER.info(cost);
+		return ret;
+	}
+
+	/**
+	 * Version 1: no lock and no synchronization
+	 */
+	@RequestMapping(value = "/v1", method = RequestMethod.GET)
+	public Response v1(long itemId) {
+		return ControllerUtils.mock(
+				"No Lock And No Synchronization",
+				itemId,
+				LOGGER,
+				executor,
+				seckillService,
+				properties,
+				(i) -> seckillService.normalStart(itemId, i)
+		);
+	}
+
+	/**
+	 * Version 2: Reentrant Lock
+	 */
+	@RequestMapping(value = "/v2", method = RequestMethod.GET)
+	public Response v2(long itemId) {
+		return ControllerUtils.mock(
+				"Reentrant Lock",
+				itemId,
+				LOGGER,
+				executor,
+				seckillService,
+				properties,
+				(i) -> seckillService.lockStart(itemId, i)
+		);
+	}
+
+	/**
+	 * Version 3: AOP Reentrant Lock
+	 */
+	@RequestMapping(value = "/v3", method = RequestMethod.GET)
+	public Response v3(long itemId) {
+		return ControllerUtils.mock(
+				"AOP Reentrant Lock",
+				itemId,
+				LOGGER,
+				executor,
+				seckillService,
+				properties,
+				(i) -> seckillService.aopLockStart(itemId, i)
+		);
+	}
+
+	@RequestMapping(value = "/v4", method = RequestMethod.GET)
+	public Response v4(long itemId) {
+		return ControllerUtils.mock(
+				"Database Pessimistic Lock(SELECT ... FOR UPDATE)",
+				itemId,
+				LOGGER,
+				executor,
+				seckillService,
+				properties,
+				(i) -> seckillService.dbPessimisticLockStart(itemId, i)
+		);
+	}
+
+	@RequestMapping(value = "/v5", method = RequestMethod.GET)
+	public Response v5(long itemId) {
+		return ControllerUtils.mock(
+				"Database Pessimistic Lock Version 2(Do seckill and check count at the same time)",
+				itemId,
+				LOGGER,
+				executor,
+				seckillService,
+				properties,
+				(i) -> seckillService.dbPessimisticLock2Start(itemId, i)
+		);
+	}
+
+	@RequestMapping(value = "/v6", method = RequestMethod.GET)
+	public Response v6(long itemId) {
+		return ControllerUtils.mock(
+				"Database Optimistic Lock, using @Version",
+				itemId,
+				LOGGER,
+				executor,
+				seckillService,
+				properties,
+				(i) -> seckillService.dbPessimisticLock2Start(itemId, i)
+		);
+	}
+}
