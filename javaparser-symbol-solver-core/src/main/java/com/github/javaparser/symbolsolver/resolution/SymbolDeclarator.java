e358aeaeb14dad8dd5061be07348a5a7b90045f3
Luc Esape <luc.esape@gmail.com>
2018-07-29 11:25:28 +0200

    Bug commit from the following repository ftomassetti/javaparser This bug commit is a reflect of source code from: http://github.com/ftomassetti/javaparser/commit/1b47288751aae6a20466bb405b5331c719afb0e0.

---
+++ javaparser-symbol-solver-core/src/main/java/com/github/javaparser/symbolsolver/resolution/SymbolDeclarator.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2016 Federico Tomassetti
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.github.javaparser.symbolsolver.resolution;
+
+import com.github.javaparser.resolution.declarations.ResolvedValueDeclaration;
+
+import javax.swing.text.html.Option;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * @author Federico Tomassetti
+ */
+public interface SymbolDeclarator {
+
+//    Optional<SymbolDeclarator> parent();
+    List<ResolvedValueDeclaration> getSymbolDeclarations();
+
+//    default List<ResolvedValueDeclaration> getSymbolDeclarationsRecursively() {
+//        if (parent().isPresent()) {
+//            List<ResolvedValueDeclaration> all = new LinkedList<>();
+//            all.addAll(parent().get().getSymbolDeclarationsRecursively());
+//            all.addAll(getSymbolDeclarations());
+//            return all;
+//        } else {
+//            return getSymbolDeclarations();
+//        }
+//    }
+}
