88191d31794550a09bb89b3f0851140e39946ae5
Luc Esape <luc.esape@gmail.com>
2018-07-30 09:54:45 +0200

    Bug commit from the following repository pgjdbc/pgjdbc This bug commit is a reflect of source code from: http://github.com/pgjdbc/pgjdbc/commit/448b981ff1676bd8f204c612d4e98106c5850196.

---
+++ pgjdbc/src/main/java/org/postgresql/jdbc/AutoSave.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2005, PostgreSQL Global Development Group
+ * See the LICENSE file in the project root for more information.
+ */
+
+package org.postgresql.jdbc;
+
+public enum AutoSave {
+  NEVER,
+  ALWAYS,
+  SERVER,
+  CONSERVATIVE;
+
+  private final String value;
+
+  AutoSave() {
+    value = this.name().toLowerCase();
+  }
+
+  public String value() {
+    return value;
+  }
+
+  public static AutoSave of(String value) {
+    return valueOf(value.toUpperCase());
+  }
+}
