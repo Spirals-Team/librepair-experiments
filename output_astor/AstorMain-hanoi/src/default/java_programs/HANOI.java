b1de38f9838a72c3325480b5a8d204e2324ad966
Luc Esape <luc.esape@gmail.com>
2018-08-01 13:58:29 +0200

    Bug commit from the following repository SpoonLabs/astor This bug commit is a reflect of source code from: http://github.com/SpoonLabs/astor/commit/0e5f90844749fc22675a7673c2621f33a8bef953.

---
+++ output_astor/AstorMain-hanoi/src/default/java_programs/HANOI.java
@@ -0,0 +1,64 @@
+package java_programs;
+
+
+public class HANOI {
+
+
+
+
+
+
+
+
+
+
+	public static java.util.List<java_programs.HANOI.Pair<java.lang.Integer, java.lang.Integer>> hanoi(int height, int start, int end) {
+		java.util.ArrayList<java_programs.HANOI.Pair<java.lang.Integer, java.lang.Integer>> steps = new java.util.ArrayList<java_programs.HANOI.Pair<java.lang.Integer, java.lang.Integer>>();
+
+		if (height > 0) {
+			java.util.PriorityQueue<java.lang.Integer> crap_set = new java.util.PriorityQueue<java.lang.Integer>();
+			crap_set.add(1);
+			crap_set.add(2);
+			crap_set.add(3);
+			crap_set.remove(start);
+			crap_set.remove(end);
+			int helper = crap_set.poll();
+			steps.addAll(java_programs.HANOI.hanoi((height - 1), start, helper));
+			steps.add(new java_programs.HANOI.Pair<java.lang.Integer, java.lang.Integer>(start, helper));
+			steps.addAll(java_programs.HANOI.hanoi((height - 1), helper, end));
+		}
+
+		return steps;
+	}
+
+
+	public static class Pair<F, S> {
+		private F first;
+		private S second;
+
+		public Pair(F first, S second) {
+			this.first = first;
+			this.second = second;
+		}
+
+		public void setFirst(F first) {
+			this.first = first;
+		}
+
+		public void setSecond(S second) {
+			this.second = second;
+		}
+
+		public F getFirst() {
+			return first;
+		}
+
+		public S getSecond() {
+			return second;
+		}
+
+		@java.lang.Override
+		public java.lang.String toString() {
+			return ((("(" + (java.lang.String.valueOf(first))) + ", ") + (java.lang.String.valueOf(second))) + ")";
+		}
+	}}
---
+++ output_astor/AstorMain-quicksort/src/variant-7/java_programs/QUICKSORT.java
@@ -0,0 +1,38 @@
+package java_programs;
+
+
+public class QUICKSORT {
+
+
+
+
+
+
+
+
+
+	public static java.util.ArrayList<java.lang.Integer> quicksort(java.util.ArrayList<java.lang.Integer> arr) {
+		if (arr.isEmpty()) {
+			return new java.util.ArrayList<java.lang.Integer>();
+		}
+
+		java.lang.Integer pivot = arr.get(0);
+		java.util.ArrayList<java.lang.Integer> lesser = new java.util.ArrayList<java.lang.Integer>();
+		java.util.ArrayList<java.lang.Integer> greater = new java.util.ArrayList<java.lang.Integer>();
+
+		for (java.lang.Integer x : arr.subList(1, arr.size())) {
+			if (x <= pivot) {
+				lesser.add(x);
+			}else 				if (x > pivot) {
+					greater.add(x);
+				}
+		}
+		java.util.ArrayList<java.lang.Integer> middle = new java.util.ArrayList<java.lang.Integer>();
+		middle.add(pivot);
+		lesser = java_programs.QUICKSORT.quicksort(lesser);
+		greater = java_programs.QUICKSORT.quicksort(greater);
+		middle.addAll(greater);
+		lesser.addAll(middle);
+		return lesser;
+
+	}}
