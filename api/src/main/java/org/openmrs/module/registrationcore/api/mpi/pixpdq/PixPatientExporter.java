b059def22f64dd353aa1bdac11d0f18b49a264b6
Luc Esape <luc.esape@gmail.com>
2018-08-03 17:09:18 +0200

    Bug commit from the following repository openmrs/openmrs-module-registrationcore This bug commit is a reflect of source code from: http://github.com/openmrs/openmrs-module-registrationcore/commit/719c880f39019dc609972bfa9a8c464c046dc87c.

---
+++ api/src/main/java/org/openmrs/module/registrationcore/api/mpi/pixpdq/PixPatientExporter.java
@@ -0,0 +1,76 @@
+package org.openmrs.module.registrationcore.api.mpi.pixpdq;
+
+import ca.uhn.hl7v2.model.Message;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.openmrs.Patient;
+import org.openmrs.api.PatientService;
+import org.openmrs.PatientIdentifierType;
+import org.openmrs.module.registrationcore.api.mpi.common.MpiProperties;
+import org.openmrs.module.registrationcore.api.mpi.common.MpiException;
+import org.openmrs.module.registrationcore.api.mpi.common.MpiPatientExporter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+
+public class PixPatientExporter implements MpiPatientExporter {
+
+    private final Log log = LogFactory.getLog(this.getClass());
+
+    @Autowired
+    @Qualifier("registrationcore.mpiPixPdqMessageUtil")
+    private PixPdqMessageUtil pixPdqMessageUtil;
+
+    @Autowired
+    @Qualifier("registrationcore.Hl7SenderHolder")
+    private Hl7SenderHolder hl7SenderHolder;
+
+    @Autowired
+    @Qualifier("registrationcore.mpiPatientFetcherPdq")
+    private PdqPatientFetcher pdqPatientFetcher;
+
+    @Autowired
+    @Qualifier("registrationcore.mpiProperties")
+    private MpiProperties mpiProperties;
+
+    @Autowired
+    private PatientService patientService;
+
+    @Override
+    public String exportPatient(Patient patient) {
+        try {
+        	// Ensure that patient with that identifier does not already exist on the MPI
+	        Patient existingMpiPatient = pdqPatientFetcher.fetchMpiPatient(patient.getPatientIdentifier());
+	        if (existingMpiPatient != null) {
+		        throw new MpiException("Patient with that identifier is already present on MPI. "
+				        + "Unable to create patient");
+	        }
+	        
+            Message admitMessage = pixPdqMessageUtil.createAdmit(patient);
+            Message response = hl7SenderHolder.getHl7v2Sender().sendPixMessage(admitMessage);
+
+            if (pixPdqMessageUtil.isQueryError(response)) {
+                throw new MpiException("Error querying patient data during export");
+            }
+
+            Patient mpiPatient = pdqPatientFetcher.fetchMpiPatient(patient.getPatientIdentifier());
+            if (mpiPatient == null) {
+                throw new MpiException("Patient has not been created on MPI. "
+                        + "Probably patient with the same IDs already exists");
+            }
+            return getMpiIdentifier(mpiPatient);
+        } catch (Exception e) {
+            log.error(e);
+            if (e instanceof  MpiException) {
+                throw (MpiException) e;
+            } else {
+                throw new MpiException("Error while exporting patient", e);
+            }
+        }
+    }
+
+    private String getMpiIdentifier(Patient mpiPatient) {
+        PatientIdentifierType patientIdentifierType = patientService.getPatientIdentifierTypeByUuid(
+                mpiProperties.getMpiPersonIdentifierTypeUuid());
+        return mpiPatient.getPatientIdentifier(patientIdentifierType).getIdentifier();
+    }
+}
