e8e874e2fb1ec2e86fd46ee4b4d12de2df75f3a3
Luc Esape <luc.esape@gmail.com>
2018-08-03 15:17:13 +0200

    Bug commit from the following repository nbaars/paseto4j This bug commit is a reflect of source code from: http://github.com/nbaars/paseto4j/commit/2c38d299f6406af0406eb84ce598b2404df8182b.

---
+++ src/main/java/org/paseto4j/Util.java
@@ -0,0 +1,35 @@
+package org.paseto4j;
+
+import okio.Buffer;
+
+public class Util {
+
+    /**
+     * Authentication Padding
+     * <p>
+     * https://github.com/paragonie/paseto/blob/master/docs/01-Protocol-Versions/Common.md#pae-definition
+     *
+     * @param pieces string[] of the pieces
+     */
+    public static String pae(String... pieces) {
+        Buffer accumulator = new Buffer();
+        accumulator.writeLongLe(pieces.length);
+
+        for (String piece : pieces) {
+            accumulator.writeLongLe(piece.length());
+            accumulator.writeUtf8(piece);
+        }
+        return accumulator.snapshot().hex();
+    }
+
+    public static byte[] pae(byte[]... pieces) {
+        Buffer accumulator = new Buffer();
+        accumulator.writeLongLe(pieces.length);
+
+        for (byte[] piece : pieces) {
+            accumulator.writeLongLe(piece.length);
+            accumulator.write(piece);
+        }
+        return accumulator.snapshot().toByteArray();
+    }
+}
