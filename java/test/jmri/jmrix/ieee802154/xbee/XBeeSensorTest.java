c24e16fbcedb506158817795fa0d656bd11cf3cb
Luc Esape <luc.esape@gmail.com>
2018-08-05 18:59:23 +0200

    Bug commit from the following repository JMRI/JMRI This bug commit is a reflect of source code from: http://github.com/JMRI/JMRI/commit/681a03d7d60d839ede9f0d0d5859752ec0f82620.

---
+++ java/test/jmri/jmrix/ieee802154/xbee/XBeeSensorTest.java
@@ -0,0 +1,99 @@
+package jmri.jmrix.ieee802154.xbee;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * XBeeSensorTest.java
+ *
+ * Description:	tests for the jmri.jmrix.ieee802154.xbee.XBeeSensor class
+ *
+ * @author	Paul Bender Copyright (C) 2012,2016
+ */
+public class XBeeSensorTest extends jmri.implementation.AbstractSensorTestBase {
+
+    @Override
+    public int numListeners() {return 0;}
+
+    @Override
+    public void checkOnMsgSent() {}
+
+    @Override
+    public void checkOffMsgSent() {}
+
+    @Override
+    public void checkStatusRequestMsgSent() {}
+
+    XBeeTrafficController tc;
+    XBeeConnectionMemo memo;
+
+    @Test
+    public void testCtorAddressPinName() {
+        XBeeSensor s = new XBeeSensor("ABCS123:4", "XBee Sensor Test", tc) {
+            @Override
+            public void requestUpdateFromLayout() {
+            }
+        };
+        Assert.assertNotNull("exists", s);
+    }
+
+    @Test
+    public void testCtor16BitHexNodeAddress() {
+        XBeeSensor s = new XBeeSensor("ABCSABCD:4", "XBee Sensor Test", tc) {
+            @Override
+            public void requestUpdateFromLayout() {
+            }
+        };
+        Assert.assertNotNull("exists", s);
+    }
+
+    @Test
+    public void testCtor16BitHexStringNodeAddress() {
+        XBeeSensor s = new XBeeSensor("ABCSAB CD:4", "XBee Sensor Test", tc) {
+            @Override
+            public void requestUpdateFromLayout() {
+            }
+        };
+        Assert.assertNotNull("exists", s);
+    }
+
+    @Test
+    public void testCtor64BitHexStringNodeAddress() {
+        XBeeSensor s = new XBeeSensor("ABCS00 13 A2 00 40 A0 4D 2D:4", "XBee Sensor Test", tc) {
+            @Override
+            public void requestUpdateFromLayout() {
+            }
+        };
+        Assert.assertNotNull("exists", s);
+    }
+
+    // The minimal setup for log4J
+    @Before
+    public void setUp() {
+        jmri.util.JUnitUtil.setUp();
+        tc = new XBeeInterfaceScaffold();
+        memo = new XBeeConnectionMemo();
+        memo.setSystemPrefix("ABC");
+        memo.setSensorManager(new XBeeSensorManager(tc, "ABC"));
+        tc.setAdapterMemo(memo);
+        t = new XBeeSensor("ABCS1234", "XBee Sensor Test", tc) {
+            @Override
+            public void requestUpdateFromLayout() {
+            }
+	    @Override
+	    public PullResistance getPullResistance(){
+		    return PullResistance.PULL_OFF;
+            }
+        };
+    }
+
+    @After
+    public void tearDown() {
+	t.dispose();
+        tc.terminate();
+        jmri.util.JUnitUtil.tearDown();
+    }
+
+}
