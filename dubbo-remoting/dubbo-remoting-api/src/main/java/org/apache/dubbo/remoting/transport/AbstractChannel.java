207dc384506bb079615215f219d3b87295365d1d
Luc Esape <luc.esape@gmail.com>
2018-08-07 04:25:47 +0200

    Bug commit from the following repository apache/incubator-dubbo This bug commit is a reflect of source code from: http://github.com/apache/incubator-dubbo/commit/828bd738d8b80413b4916d9acfa2ed9109c173c5.

---
+++ dubbo-remoting/dubbo-remoting-api/src/main/java/org/apache/dubbo/remoting/transport/AbstractChannel.java
@@ -0,0 +1,83 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.dubbo.remoting.transport;
+
+import org.apache.dubbo.common.URL;
+import org.apache.dubbo.remoting.Channel;
+import org.apache.dubbo.remoting.ChannelHandler;
+import org.apache.dubbo.remoting.RemotingException;
+import org.apache.dubbo.remoting.exchange.Request;
+import org.apache.dubbo.remoting.exchange.Response;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * AbstractChannel
+ */
+public abstract class AbstractChannel extends AbstractPeer implements Channel {
+
+    private static final Map<Long, Request> UN_FINISH_REQUESTS_MAP = new ConcurrentHashMap<Long, Request>();
+
+    public AbstractChannel(URL url, ChannelHandler handler) {
+        super(url, handler);
+    }
+
+    @Override
+    public void send(Object message, boolean sent) throws RemotingException {
+        if (isClosed()) {
+            throw new RemotingException(this, "Failed to send message "
+                    + (message == null ? "" : message.getClass().getName()) + ":" + message
+                    + ", cause: Channel closed. channel: " + getLocalAddress() + " -> " + getRemoteAddress());
+        }
+        if (message instanceof Request) {
+            Request r = (Request) message;
+            UN_FINISH_REQUESTS_MAP.put(r.getId(), r);
+        }
+    }
+
+    @Override
+    public List<Request> unFinishRequests() {
+        Collection<Request> c = UN_FINISH_REQUESTS_MAP.values();
+        return new ArrayList<>(c);
+    }
+
+    @Override
+    public void finishRequest(Response response) {
+        UN_FINISH_REQUESTS_MAP.remove(response.getId());
+    }
+
+    @Override
+    public void clearUnFinishedRequests() {
+        // clear unfinish requests when close the channel.
+        UN_FINISH_REQUESTS_MAP.clear();
+    }
+
+    @Override
+    public String toString() {
+        return getLocalAddress() + " -> " + getRemoteAddress();
+    }
+
+    @Override
+    public void close() {
+        super.close();
+        clearUnFinishedRequests();
+    }
+}
