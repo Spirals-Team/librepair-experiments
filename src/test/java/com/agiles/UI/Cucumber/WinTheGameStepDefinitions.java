0992daf3b9e565cf28766f950fcc1e51de862c99
Luc Esape <luc.esape@gmail.com>
2018-08-10 07:13:55 +0200

    Bug commit from the following repository andreslopapa/AhorcadoWeb2 This bug commit is a reflect of source code from: http://github.com/andreslopapa/AhorcadoWeb2/commit/9b34282b8df00c78d5aec212d864ead9a02c7bef.

---
+++ src/test/java/com/agiles/UI/Cucumber/WinTheGameStepDefinitions.java
@@ -0,0 +1,74 @@
+package com.agiles.UI.Cucumber;
+
+import com.codeborne.selenide.Configuration;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import static com.codeborne.selenide.Selenide.*;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebDriver.Options;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import static com.codeborne.selenide.Condition.*;
+
+public class WinTheGameStepDefinitions {
+	@Given("^an open browser with AhorcadoWeb/index\\.jsp$")
+	public void an_open_browser_with_AhorcadoWeb_index_jsp() throws Throwable {
+//		ChromeOptions options = new ChromeOptions();
+//        options.addArguments("--no-sandbox");
+//        System.setProperty("webdriver.chrome.driver","/usr/bin/google-chrome");
+//        WebDriver browser = new ChromeDriver(options);
+        
+//		Configuration.browser = "chrome";
+//	    Configuration.startMaximized = false;
+//		System.setProperty("webdriver.chrome.driver", "/usr/bin/google-chrome");
+//		ChromeOptions options = new ChromeOptions();
+//		options.addArguments("start-maximized"); // open Browser in maximized mode
+//		options.addArguments("disable-infobars"); // disabling infobars
+//		options.addArguments("--disable-extensions"); // disabling extensions
+//		options.addArguments("--disable-gpu"); // applicable to windows os only
+//		options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
+//		options.addArguments("--no-sandbox"); // Bypass OS security model
+//		WebDriver driver = new ChromeDriver(options);
+//		driver.get("/AhorcadoWeb/index.jsp");
+	   
+        
+	    open("/AhorcadoWeb/index.jsp");
+	}
+	
+	@When("^I click the letter \"([^\"]*)\"$")
+	public void i_click_the_letter(String letter) throws Throwable {
+		if(letter.equals("ñ")){
+			letter="enie";
+		}
+		$("#letter"+letter).click();
+	}
+	
+
+	@Then("^a message saying \"([^\"]*)\" should appear$")
+	public void a_message_saying_should_appear(String winTit) throws Throwable {
+		$(".ganador-tit").shouldHave(text(winTit));
+	}
+
+	@Then("^the keyboard should disappear$")
+	public void the_keyboard_should_disappear() throws Throwable {
+		$(".alphabets").shouldNot(exist);
+	}
+
+	@Then("^the remaining errors should disappear$")
+	public void the_remaining_errors_should_disappear() throws Throwable {
+		$(".errorestit").shouldNot(exist);
+		$(".errores").shouldNot(exist);
+	}
+
+	@Then("^the entered letters should disappear$")
+	public void the_entered_letters_should_disappear() throws Throwable {
+		$(".letrasUsadasTit").shouldNot(exist);
+		$(".letrasUsadas").shouldNot(exist);
+	}
+
+
+}
---
+++ src/test/java/com/agiles/UI/selenide/UITestAhorcado.java
@@ -0,0 +1,90 @@
+package com.agiles.UI.selenide;
+
+import org.junit.Test;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Condition.*;
+
+
+public class UITestAhorcado {
+	
+	
+	@Test
+	public void ingresarUnaSolaLetra() {
+		
+		 preparePage();
+		 $("#letterh").click();
+		 $("#letrasUsadas").shouldHave(text("h"));
+		 $(".letras-palabra").shouldHave(text("h _ _ _ "));
+		 $(".errores").shouldHave(text("4"));
+		 $(".ganador-section").shouldNot(exist);
+		 $(".perdedor-section").shouldNot(exist);
+		 $(".hangman").isImage();
+		 $(".hangman").shouldHave(attribute("src","http://localhost:8080/AhorcadoWeb/images/playhangman1.png"));
+	}
+	
+	@Test
+	public void juegoGanado() {
+		
+		 preparePage();
+		 $("#letterh").click();
+		 $("#lettero").click();
+		 $("#letterl").click();
+		 $("#lettera").click();
+		 $("#letrasUsadas").shouldNot(exist);
+		 $(".letras-palabra").shouldHave(text("h o l a "));
+		 $(".errores").shouldNot(exist);
+		 $(".alphabets").shouldNot(exist);
+		 $(".ganador-tit").shouldHave(text("Te Salvaste Perro!"));
+		 $(".perdedor-section").shouldNot(exist);
+		 $(".hangman").isImage();
+		 $(".hangman").shouldHave(attribute("src","http://localhost:8080/AhorcadoWeb/images/playhangman1.png"));
+	}
+	
+	@Test
+	public void juegoPerdido() {
+		
+		 preparePage();
+		 $("#letterg").click();
+		 $("#letterr").click();
+		 $("#lettere").click();
+		 $("#lettert").click();
+		 $("#letterz").click();
+		 $("#letrasUsadas").shouldNot(exist);
+		 $(".letras-palabra").shouldHave(text("_ _ _ _ "));
+		 $(".errores").shouldNot(exist);
+		 $(".alphabets").shouldNot(exist);
+		 $(".ganador-section").shouldNot(exist);
+		 $(".perdedor-tit").shouldHave(text("Perdiste Perro!"));
+		 $(".palabraera").shouldHave(text("La palabra era: hola"));
+		 $(".hangman").isImage();
+		 $(".hangman").shouldHave(attribute("src","http://localhost:8080/AhorcadoWeb/images/playhangman6.png"));
+	}
+	
+	@Test
+	public void juegoGanadoConCincoLetras(){
+		preparePage();
+		 $("#letterh").click();
+		 $("#letterr").click();
+		 $("#letterl").click();
+		 $("#lettera").click();
+		 $("#lettero").click();
+		 $("#letrasUsadas").shouldNot(exist);
+		 $(".letras-palabra").shouldHave(text("h o l a "));
+		 $(".errores").shouldNot(exist);
+		 $(".alphabets").shouldNot(exist);
+		 $(".ganador-tit").shouldHave(text("Te Salvaste Perro!"));
+		 $(".perdedor-section").shouldNot(exist);
+		 $(".hangman").isImage();
+		 $(".hangman").shouldHave(attribute("src","http://localhost:8080/AhorcadoWeb/images/playhangman2.png"));
+	
+	}
+	
+	
+	
+	public void preparePage(){
+		System.setProperty("selenide.browser", "chrome");
+		open("/AhorcadoWeb");
+	}
+}
+
+
