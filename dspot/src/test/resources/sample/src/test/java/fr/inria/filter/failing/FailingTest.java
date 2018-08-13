f67859a678bd849a7d19d1e510c93fd52fb4533e
Luc Esape <luc.esape@gmail.com>
2018-08-13 23:42:17 +0200

    Bug commit from the following repository STAMP-project/dspot This bug commit is a reflect of source code from: http://github.com/STAMP-project/dspot/commit/88cc6e04e575447b66beef61457ad4adab857d52.

---
+++ dspot/src/test/resources/sample/src/test/java/fr/inria/filter/failing/FailingTest.java
@@ -0,0 +1,28 @@
+package fr.inria.filter.failing;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+
+/**
+ * Created by Benjamin DANGLOT
+ * benjamin.danglot@inria.fr
+ * on 18/10/17
+ */
+public class FailingTest {
+
+    @Test
+    public void testAssertionError() throws Exception {
+        assertFalse(true);
+    }
+
+    @Test
+    public void testFailingWithException() throws Exception {
+        String nullString = null;
+        assertEquals(-1, nullString.length());
+        new ArrayList<>().get(-100);
+    }
+}
