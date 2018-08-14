0df6f9a140a999a55393326d89b2becbe8a0846b
Luc Esape <luc.esape@gmail.com>
2018-08-14 15:15:42 +0200

    Bug commit from the following repository project-sunbird/open-saber This bug commit is a reflect of source code from: http://github.com/project-sunbird/open-saber/commit/316241f98cf1d4141316130cf1f0ae8b6be793b5.

---
+++ java/middleware/registry-middleware/authorization/src/main/java/io/opensaber/registry/authorization/pojos/AuthInfo.java
@@ -0,0 +1,41 @@
+package io.opensaber.registry.authorization.pojos;
+
+import java.security.Key;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwsHeader;
+import io.jsonwebtoken.SigningKeyResolverAdapter;
+
+public class AuthInfo extends SigningKeyResolverAdapter{
+		
+	private String aud;
+	
+	private String sub;
+	
+	private String name;
+
+	public String getAud() {
+		return aud;
+	}
+
+	public void setAud(String aud) {
+		this.aud = aud;
+	}
+
+	public String getSub() {
+		return sub;
+	}
+
+	public void setSub(String sub) {
+		this.sub = sub;
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
---
+++ java/registry/src/main/java/io/opensaber/registry/service/RegistryService.java
@@ -0,0 +1,38 @@
+package io.opensaber.registry.service;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+import io.opensaber.pojos.HealthCheckResponse;
+import io.opensaber.registry.exception.*;
+import org.apache.jena.rdf.model.Model;
+
+public interface RegistryService {
+	
+	public List getEntityList();
+	
+	//public String addEntity(Model rdfModel) throws DuplicateRecordException, EntityCreationException, EncryptionException, AuditFailedException, MultipleEntityException, RecordNotFoundException;
+	
+	public String addEntity(Model rdfModel, String subject, String property) throws DuplicateRecordException, EntityCreationException, 
+	EncryptionException, AuditFailedException, MultipleEntityException, RecordNotFoundException;
+	
+	public boolean updateEntity(Model entity) throws RecordNotFoundException, EntityCreationException, EncryptionException, AuditFailedException, MultipleEntityException;
+	
+	public org.eclipse.rdf4j.model.Model getEntityById(String id) throws RecordNotFoundException, EncryptionException, AuditFailedException;
+	
+	//public boolean deleteEntity(Model rdfModel) throws AuditFailedException, RecordNotFoundException;
+
+	public HealthCheckResponse health() throws Exception;
+
+	public String frameEntity(org.eclipse.rdf4j.model.Model entityModel) throws IOException, MultipleEntityException, EntityCreationException;
+	
+	public String frameSearchEntity(org.eclipse.rdf4j.model.Model entityModel) throws IOException, MultipleEntityException, EntityCreationException;
+	
+	public String frameAuditEntity(org.eclipse.rdf4j.model.Model entityModel) throws IOException;
+
+	public org.eclipse.rdf4j.model.Model getAuditNode(String id) throws IOException, NoSuchElementException, RecordNotFoundException,
+	EncryptionException, AuditFailedException;
+
+	public boolean deleteEntityById(String id) throws AuditFailedException, RecordNotFoundException;
+}
