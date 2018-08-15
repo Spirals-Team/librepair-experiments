a80e20bc273b915dfe7c4c639d5b858adf777e08
Luc Esape <luc.esape@gmail.com>
2018-08-15 16:23:53 +0200

    Bug commit from the following repository OpenSRP/opensrp-server-openlmis This bug commit is a reflect of source code from: http://github.com/OpenSRP/opensrp-server-openlmis/commit/0e77edb1b171e16e89abe9dfdd826fc19ea8fe4c.

---
+++ openlmis-stock/src/main/java/org/opensrp/stock/openlmis/domain/metadata/BaseMetaData.java
@@ -0,0 +1,46 @@
+package org.opensrp.stock.openlmis.domain.metadata;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY, property = "type", visible = true)
+
+@JsonSubTypes({ @JsonSubTypes.Type(value = ProgramMetaData.class, name = "program"),
+        @JsonSubTypes.Type(value = CommodityTypeMetaData.class, name = "commodity_type"),
+        @JsonSubTypes.Type(value = TradeItemMetaData.class, name = "trade_item"),
+        @JsonSubTypes.Type(value = DispensableMetaData.class, name = "dispensable"),
+        @JsonSubTypes.Type(value = TradeItemClassificationMetaData.class, name = "trade_item_classification"),
+        @JsonSubTypes.Type(value = ReasonMetaData.class, name = "reason")})
+@JsonIgnoreProperties({"type"})
+public class BaseMetaData {
+
+    @JsonProperty
+    private String id;
+
+    @JsonProperty
+    private long serverVersion;
+
+    protected BaseMetaData() {}
+
+    public BaseMetaData(String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public long getServerVersion() {
+        return serverVersion;
+    }
+
+    public void setServerVersion(long serverVersion) {
+        this.serverVersion = serverVersion;
+    }
+}
---
+++ openlmis-stock/src/main/java/org/opensrp/stock/openlmis/util/Utils.java
@@ -0,0 +1,74 @@
+package org.opensrp.stock.openlmis.util;
+
+import java.lang.reflect.Type;
+import java.util.List;
+
+import org.joda.time.DateTime;
+
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParseException;
+import com.google.gson.JsonPrimitive;
+import com.google.gson.JsonSerializationContext;
+import com.google.gson.JsonSerializer;
+import com.mysql.jdbc.StringUtils;
+import org.opensrp.stock.openlmis.domain.MasterTableEntry;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class Utils {
+
+    public static final int DEFAULT_FETCH_LIMIT = 1000;
+
+    public static final String SYNC_SERVER_VERSION = "sync_server_version";
+
+    public static final String COMMODITY_TYPE = "CommodityType";
+
+    public static final String COMMODITY_TYPES = "commodity_types";
+
+    public static final String LOTS = "lots";
+
+    public static final String ORDERABLES = "orderables";
+
+    public static final String PROGRAM_ORDERABLES = "program_orderables";
+
+    public static final String PROGRAM = "Program";
+
+    public static final String PROGRAMS = "programs";
+
+    public static final String REASON = "Reason";
+
+    public static final String REASONS = "reasons";
+
+    public static  final String TRADE_ITEM = "TradeItem";
+
+    public final static String TRADE_ITEMS = "trade_items";
+
+    public static Long getCurrentTime() {
+        return System.nanoTime();
+    }
+
+    public static class DateTimeTypeConverter implements JsonSerializer<DateTime>, JsonDeserializer<DateTime> {
+
+        @Override
+        public DateTime deserialize(JsonElement json, Type typeOfT,
+                                    JsonDeserializationContext context) throws JsonParseException {
+            return new DateTime(json.getAsString());
+        }
+
+        @Override
+        public JsonElement serialize(DateTime src, Type typeOfSrc, JsonSerializationContext context) {
+            return new JsonPrimitive(src.toString());
+        }
+    }
+
+    public static Long getLongFilter(String filter, HttpServletRequest req) {
+        String strval = getStringFilter(filter, req);
+        return strval == null ? null : Long.parseLong(strval);
+    }
+
+    public static String getStringFilter(String filter, HttpServletRequest req) {
+        return StringUtils.isEmptyOrWhitespaceOnly(req.getParameter(filter)) ? null : req.getParameter(filter);
+    }
+}
