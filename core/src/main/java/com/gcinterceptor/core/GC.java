0ddc63b8663f1c03f34de0ed9de827564a2ad8af
Luc Esape <luc.esape@gmail.com>
2018-08-16 22:03:29 +0200

    Bug commit from the following repository gcinterceptor/gci-java This bug commit is a reflect of source code from: http://github.com/gcinterceptor/gci-java/commit/1325a07a26017616d93275beae93a03b0297b9a9.

---
+++ core/src/main/java/com/gcinterceptor/core/GC.java
@@ -0,0 +1,10 @@
+package com.gcinterceptor.core;
+
+// TODO: Move this functionality to the Runtime class.
+public class GC {
+	public static native void force();
+
+	static {
+		System.load(System.getProperty("jvmtilib"));
+	}
+}
