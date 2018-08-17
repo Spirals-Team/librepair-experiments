9d27bb18b8ef17cffdbb502f6b2b270a1b288618
Luc Esape <luc.esape@gmail.com>
2018-08-17 10:19:48 +0200

    Bug commit from the following repository fr3anthe/ifedorenko This bug commit is a reflect of source code from: http://github.com/fr3anthe/ifedorenko/commit/8240ff7ff63ebd51d11265b4f8c692a5eb32575a.

---
+++ 2.4_Servlet_JSP/src/main/java/ru/job4j/servlets/http/Store.java
@@ -0,0 +1,52 @@
+package ru.job4j.servlets.http;
+
+import java.util.List;
+
+/**
+ * Interface Store.
+ * @author ifedorenko
+ * @since 27.07.2018
+ */
+public interface Store {
+    /**
+     * add.
+     * @param user for adding
+     */
+    void add(User user);
+
+    /**
+     * update.
+     * @param login use for find user
+     * @param name name for update
+     * @param email email for update
+     * @param role role for update
+     * @param password password for update
+     */
+    void update(String login, String name, String email, String role, String password);
+
+    /**
+     * delete.
+     * @param id use for find user.
+     */
+    void delete(int id);
+
+    /**
+     * findAll.
+     * @return all user.
+     */
+    List<User> findAll();
+
+    /**
+     * findById.
+     * @param id use for find user
+     * @return user
+     */
+    User findById(int id);
+
+    /**
+     * findByLogin
+     * @param login use for find user
+     * @return n user
+     */
+    User findByLogin(String login);
+}
