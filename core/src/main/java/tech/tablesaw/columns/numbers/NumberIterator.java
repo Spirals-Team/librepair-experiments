72745c1b5c942e654540b8b454fb1963d62f7b04
Luc Esape <luc.esape@gmail.com>
2018-08-18 20:14:24 +0200

    Bug commit from the following repository jtablesaw/tablesaw This bug commit is a reflect of source code from: http://github.com/jtablesaw/tablesaw/commit/985c32e4733d2a7443cf0eafabed229cfc86c2b7.

---
+++ core/src/main/java/tech/tablesaw/columns/numbers/NumberIterator.java
@@ -0,0 +1,109 @@
+package tech.tablesaw.columns.numbers;
+
+import java.util.Iterator;
+import java.util.ListIterator;
+
+import it.unimi.dsi.fastutil.doubles.DoubleArrayList;
+import it.unimi.dsi.fastutil.doubles.DoubleIterator;
+import it.unimi.dsi.fastutil.floats.FloatArrayList;
+import it.unimi.dsi.fastutil.floats.FloatIterator;
+import it.unimi.dsi.fastutil.ints.IntArrayList;
+import it.unimi.dsi.fastutil.ints.IntIterator;
+
+public class NumberIterator {
+
+    private DoubleArrayList dList;
+    private IntArrayList iList;
+    private FloatArrayList fList;
+
+    private final ListIterator<?> iterator;
+
+    public NumberIterator(DoubleArrayList list) {
+        this.dList = list;
+        this.iterator = list.iterator();
+    }
+
+    public NumberIterator(FloatArrayList list) {
+        this.fList = list;
+        this.iterator = list.iterator();
+    }
+
+    public NumberIterator(IntArrayList list) {
+        this.iList = list;
+        this.iterator = list.iterator();
+    }
+
+    public int nextIndex() {
+        return iterator.nextIndex();
+    }
+
+    public int previousIndex() {
+        return iterator.previousIndex();
+    }
+
+    public double next() {
+        if (dList != null) {
+            return ((DoubleIterator) iterator).nextDouble();
+        } else if (iList != null){
+            return ((IntIterator) iterator).nextInt();
+        } else {
+            return ((FloatIterator) iterator).nextFloat();
+        }
+    }
+
+    public int nextInt() {
+        if (dList != null) {
+            return (int) ((DoubleIterator) iterator).nextDouble();
+        } else if (iList != null){
+            return ((IntIterator) iterator).nextInt();
+        } else {
+            return (int) ((FloatIterator) iterator).nextFloat();
+        }
+    }
+
+    public float nextFloat() {
+        if (dList != null) {
+            return (float) ((DoubleIterator) iterator).nextDouble();
+        } else if (iList != null){
+            return ((IntIterator) iterator).nextInt();
+        } else {
+            return ((FloatIterator) iterator).nextFloat();
+        }
+    }
+
+    public boolean hasNext() {
+        return iterator.hasNext();
+    }
+
+    public Iterator<Double> iterator() {
+        if (dList != null) {
+            return dList.iterator();
+        } else if (iList != null) {
+            return new Iterator<Double>() {
+                IntIterator intIterator = iList.iterator();
+                @Override
+                public boolean hasNext() {
+                    return intIterator.hasNext();
+                }
+
+                @Override
+                public Double next() {
+                    return (double) intIterator.nextInt();
+                }
+            };
+        } else {
+            return new Iterator<Double>() {
+                FloatIterator floatIterator = fList.iterator();
+                @Override
+                public boolean hasNext() {
+                    return floatIterator.hasNext();
+                }
+
+                @Override
+                public Double next() {
+                    return (double) floatIterator.nextFloat();
+                }
+            };
+        }
+    }
+}
