113fb19070c1402bef3344db0c0286041660770a
Luc Esape <luc.esape@gmail.com>
2018-08-20 09:26:37 +0200

    Bug commit from the following repository Microsoft/spring-data-cosmosdb This bug commit is a reflect of source code from: http://github.com/Microsoft/spring-data-cosmosdb/commit/6d5866769afbbc93fc2e9e4bc8d90427d04d2eb6.

---
+++ src/test/java/com/microsoft/azure/spring/data/cosmosdb/repository/repository/ProjectRepository.java
@@ -0,0 +1,44 @@
+/**
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See LICENSE in the project root for
+ * license information.
+ */
+package com.microsoft.azure.spring.data.cosmosdb.repository.repository;
+
+import com.microsoft.azure.spring.data.cosmosdb.domain.Project;
+import com.microsoft.azure.spring.data.cosmosdb.repository.DocumentDbRepository;
+import org.springframework.data.domain.Sort;
+
+import java.util.List;
+
+public interface ProjectRepository extends DocumentDbRepository<Project, String> {
+
+    List<Project> findByNameAndStarCount(String name, Long startCount);
+
+    List<Project> findByNameOrForkCount(String name, Long forkCount);
+
+    List<Project> findByNameAndCreator(String name, String creator);
+
+    List<Project> findByNameOrCreator(String name, String creator);
+
+    List<Project> findByNameAndCreatorOrForkCount(String name, String creator, Long forkCount);
+
+    List<Project> findByNameOrCreatorAndForkCount(String name, String creator, Long forkCount);
+
+    List<Project> findByNameOrCreatorOrForkCount(String name, String creator, Long forkCount);
+
+    List<Project> findByNameOrCreatorAndForkCountOrStarCount(String name, String creator,
+                                                             Long forkCount, Long starCount);
+
+    List<Project> findByForkCountGreaterThan(Long forkCount);
+
+    List<Project> findByCreatorAndForkCountGreaterThan(String creator, Long forkCount);
+
+    List<Project> findByCreatorOrForkCountGreaterThan(String creator, Long forkCount);
+
+    List<Project> findByNameOrCreator(String name, String creator, Sort sort);
+
+    List<Project> findByNameAndCreator(String name, String creator, Sort sort);
+
+    List<Project> findByForkCount(Long forkCount, Sort sort);
+}
