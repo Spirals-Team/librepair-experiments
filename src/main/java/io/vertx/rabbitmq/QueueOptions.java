efb4028954714f61020138eb69dc099f807ec4b7
Luc Esape <luc.esape@gmail.com>
2018-08-21 09:22:48 +0200

    Bug commit from the following repository Sammers21/vertx-rabbitmq-client This bug commit is a reflect of source code from: http://github.com/Sammers21/vertx-rabbitmq-client/commit/8770b20ad6af7b45bb46f97177a3781f020053a1.

---
+++ src/main/java/io/vertx/rabbitmq/QueueOptions.java
@@ -0,0 +1,81 @@
+package io.vertx.rabbitmq;
+
+import io.vertx.codegen.annotations.DataObject;
+import io.vertx.core.Handler;
+import io.vertx.core.json.JsonObject;
+
+/**
+ * Aimed to specify queue consumer settings when calling {@link RabbitMQClient#basicConsumer(String, QueueOptions, Handler)}
+ */
+@DataObject(generateConverter = true)
+public class QueueOptions {
+
+  private static final int DEFAULT_QUEUE_SIZE = Integer.MAX_VALUE;
+  private static final boolean DEFAULT_AUTO_ACK = true;
+  private static final boolean DEFAULT_KEEP_MOST_RECENT = false;
+
+  private boolean autoAck = DEFAULT_AUTO_ACK;
+  private boolean keepMostRecent = DEFAULT_KEEP_MOST_RECENT;
+  private int maxInternalQueueSize = DEFAULT_QUEUE_SIZE;
+
+
+  public QueueOptions() {
+  }
+
+  public QueueOptions(JsonObject json) {
+    this();
+    QueueOptionsConverter.fromJson(json, this);
+  }
+
+  /**
+   * @param autoAck true if the server should consider messages
+   *                acknowledged once delivered; false if the server should expect
+   *                explicit acknowledgements
+   */
+  public QueueOptions setAutoAck(boolean autoAck) {
+    this.autoAck = autoAck;
+    return this;
+  }
+
+  /**
+   * @param keepMostRecent {@code true} for discarding old messages instead of recent ones,
+   *                       otherwise use {@code false}
+   */
+  public QueueOptions setKeepMostRecent(boolean keepMostRecent) {
+    this.keepMostRecent = keepMostRecent;
+    return this;
+  }
+
+
+  /**
+   * @param maxInternalQueueSize the size of internal queue
+   */
+  public QueueOptions setMaxInternalQueueSize(int maxInternalQueueSize) {
+    this.maxInternalQueueSize = maxInternalQueueSize;
+    return this;
+  }
+
+  /**
+   * @return true if the server should consider messages
+   * acknowledged once delivered; false if the server should expect
+   * explicit acknowledgements
+   */
+  public boolean isAutoAck() {
+    return autoAck;
+  }
+
+  /**
+   * @return the size of internal queue
+   */
+  public int maxInternalQueueSize() {
+    return maxInternalQueueSize;
+  }
+
+  /**
+   * @return {@code true} if old messages will be discarded instead of recent ones,
+   * otherwise use {@code false}
+   */
+  public boolean isKeepMostRecent() {
+    return keepMostRecent;
+  }
+}
---
+++ src/main/java/io/vertx/rabbitmq/impl/RabbitMQConsumerImpl.java
@@ -0,0 +1,153 @@
+package io.vertx.rabbitmq.impl;
+
+import io.vertx.core.AsyncResult;
+import io.vertx.core.Future;
+import io.vertx.core.Handler;
+import io.vertx.core.Vertx;
+import io.vertx.core.logging.Logger;
+import io.vertx.core.logging.LoggerFactory;
+import io.vertx.core.queue.Queue;
+import io.vertx.core.streams.ReadStream;
+import io.vertx.rabbitmq.QueueOptions;
+import io.vertx.rabbitmq.RabbitMQConsumer;
+import io.vertx.rabbitmq.RabbitMQMessage;
+
+import java.io.IOException;
+
+/**
+ * A implementation of {@link RabbitMQConsumer}
+ */
+public class RabbitMQConsumerImpl implements RabbitMQConsumer {
+
+  private static final Logger log = LoggerFactory.getLogger(RabbitMQConsumerImpl.class);
+
+  private Handler<Throwable> exceptionHandler;
+  private Handler<Void> endHandler;
+  private final QueueConsumerHandler consumerHandler;
+  private final boolean keepMostRecent;
+  private final Queue<RabbitMQMessage> pending;
+  private final int maxQueueSize;
+
+  RabbitMQConsumerImpl(Vertx vertx, QueueConsumerHandler consumerHandler, QueueOptions options) {
+    this.consumerHandler = consumerHandler;
+    this.keepMostRecent = options.isKeepMostRecent();
+    this.maxQueueSize = options.maxInternalQueueSize();
+    this.pending = Queue.queue(vertx.getOrCreateContext(), maxQueueSize);
+
+    pending.resume();
+  }
+
+  @Override
+  public RabbitMQConsumer exceptionHandler(Handler<Throwable> exceptionHandler) {
+    this.exceptionHandler = exceptionHandler;
+    return this;
+  }
+
+  @Override
+  public RabbitMQConsumer handler(Handler<RabbitMQMessage> handler) {
+    if (handler != null) {
+      pending.handler(msg -> {
+        try {
+          handler.handle(msg);
+        } catch (Exception e) {
+          handleException(e);
+        }
+      });
+    } else {
+      pending.handler(null);
+    }
+    return this;
+  }
+
+  @Override
+  public RabbitMQConsumer pause() {
+    pending.pause();
+    return this;
+  }
+
+  @Override
+  public RabbitMQConsumer resume() {
+    pending.resume();
+    return this;
+  }
+
+  @Override
+  public ReadStream<RabbitMQMessage> fetch(long amount) {
+    pending.take(amount);
+    return this;
+  }
+
+  @Override
+  public RabbitMQConsumer endHandler(Handler<Void> endHandler) {
+    this.endHandler = endHandler;
+    return this;
+  }
+
+  @Override
+  public String consumerTag() {
+    return consumerHandler.getConsumerTag();
+  }
+
+  @Override
+  public void cancel() {
+    cancel(null);
+  }
+
+  @Override
+  public void cancel(Handler<AsyncResult<Void>> cancelResult) {
+    AsyncResult<Void> operationResult;
+    try {
+      consumerHandler.getChannel().basicCancel(consumerTag());
+      operationResult = Future.succeededFuture();
+    } catch (IOException e) {
+      operationResult = Future.failedFuture(e);
+    }
+    if (cancelResult != null) {
+      cancelResult.handle(operationResult);
+    }
+    handleEnd();
+  }
+
+  @Override
+  public boolean isPaused() {
+    return false;
+  }
+
+  /**
+   * Push message to stream.
+   * <p>
+   * Should be called from a vertx thread.
+   *
+   * @param message received message to deliver
+   */
+  void handleMessage(RabbitMQMessage message) {
+
+    if (pending.size() >= maxQueueSize) {
+      if (keepMostRecent) {
+        pending.poll();
+      } else {
+        log.debug("Discard a received message since stream is paused and buffer flag is false");
+        return;
+      }
+    }
+    pending.add(message);
+  }
+
+  /**
+   * Trigger exception handler with given exception
+   */
+  private void handleException(Throwable exception) {
+    if (exceptionHandler != null) {
+      exceptionHandler.handle(exception);
+    }
+  }
+
+  /**
+   * Trigger end of stream handler
+   */
+  void handleEnd() {
+    if (endHandler != null) {
+      endHandler.handle(null);
+    }
+  }
+}
