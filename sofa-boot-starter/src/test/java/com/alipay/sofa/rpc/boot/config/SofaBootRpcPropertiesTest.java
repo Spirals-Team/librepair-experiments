0d522eb7ee6c43fafa51596987e846c9c40f9c25
Luc Esape <luc.esape@gmail.com>
2018-08-21 15:26:35 +0200

    Bug commit from the following repository alipay/sofa-rpc-boot-projects This bug commit is a reflect of source code from: http://github.com/alipay/sofa-rpc-boot-projects/commit/1fb8d706c8fca7c3a897a63e72612df2a3879e1f.

---
+++ sofa-boot-starter/src/test/java/com/alipay/sofa/rpc/boot/config/SofaBootRpcPropertiesTest.java
@@ -0,0 +1,64 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.alipay.sofa.rpc.boot.config;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Map;
+
+@RunWith(SpringRunner.class)
+@SpringBootApplication
+@SpringBootTest(properties = {
+                              SofaBootRpcProperties.PREFIX + ".bolt.port=5000",
+                              "com_alipay_sofa_rpc_bolt_thread_pool_max_size=600",
+                              SofaBootRpcProperties.PREFIX + ".registries.zk1=zookeeper://xxxx"
+})
+public class SofaBootRpcPropertiesTest {
+    @Autowired
+    private SofaBootRpcProperties sofaBootRpcProperties;
+
+    @Test
+    public void testCamelCaseToDot() {
+        Assert.assertEquals("com.alipay.sofa", sofaBootRpcProperties.camelToDot("comAlipaySofa"));
+        Assert.assertEquals("com.alipay.sofa", sofaBootRpcProperties.camelToDot("ComAlipaySofa"));
+    }
+
+    @Test
+    public void testDotConfig() {
+        Assert.assertEquals("5000", sofaBootRpcProperties.getBoltPort());
+    }
+
+    @Test
+    public void testUnderscoreConfig() {
+        Assert.assertEquals("600", sofaBootRpcProperties.getBoltThreadPoolMaxSize());
+    }
+
+    @Test
+    public void testCustoMapConfig() {
+        Map<String, String> map = sofaBootRpcProperties.getRegistries();
+
+        Assert.assertTrue(map != null);
+        Assert.assertEquals("zookeeper://xxxx", map.get("zk1"));
+    }
+
+}
