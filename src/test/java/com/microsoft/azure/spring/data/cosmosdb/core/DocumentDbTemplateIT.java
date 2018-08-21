b1ef5032163e1a362739d9867f8376db833630a3
Luc Esape <luc.esape@gmail.com>
2018-08-21 08:16:42 +0200

    Bug commit from the following repository Microsoft/spring-data-cosmosdb This bug commit is a reflect of source code from: http://github.com/Microsoft/spring-data-cosmosdb/commit/ea056134301576922655682810273958fbf3d47b.

---
+++ src/test/java/com/microsoft/azure/spring/data/cosmosdb/core/DocumentDbTemplateIT.java
@@ -0,0 +1,226 @@
+/**
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See LICENSE in the project root for
+ * license information.
+ */
+
+package com.microsoft.azure.spring.data.cosmosdb.core;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.microsoft.azure.documentdb.*;
+import com.microsoft.azure.spring.data.cosmosdb.common.TestConstants;
+import com.microsoft.azure.spring.data.cosmosdb.common.TestUtils;
+import com.microsoft.azure.spring.data.cosmosdb.core.convert.MappingDocumentDbConverter;
+import com.microsoft.azure.spring.data.cosmosdb.core.mapping.DocumentDbMappingContext;
+import com.microsoft.azure.spring.data.cosmosdb.core.query.Criteria;
+import com.microsoft.azure.spring.data.cosmosdb.core.query.CriteriaType;
+import com.microsoft.azure.spring.data.cosmosdb.core.query.DocumentDbPageRequest;
+import com.microsoft.azure.spring.data.cosmosdb.core.query.DocumentQuery;
+import com.microsoft.azure.spring.data.cosmosdb.domain.Person;
+import com.microsoft.azure.spring.data.cosmosdb.exception.DocumentDBAccessException;
+import com.microsoft.azure.spring.data.cosmosdb.repository.support.DocumentDbEntityInformation;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.domain.EntityScanner;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.data.annotation.Persistent;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.util.Assert;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
+
+import static com.microsoft.azure.spring.data.cosmosdb.common.PageTestUtils.validateLastPage;
+import static com.microsoft.azure.spring.data.cosmosdb.common.PageTestUtils.validateNonLastPage;
+import static com.microsoft.azure.spring.data.cosmosdb.common.TestConstants.PAGE_SIZE_1;
+import static com.microsoft.azure.spring.data.cosmosdb.common.TestConstants.PAGE_SIZE_2;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertTrue;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@PropertySource(value = {"classpath:application.properties"})
+public class DocumentDbTemplateIT {
+    private static final Person TEST_PERSON = new Person(TestConstants.ID_1, TestConstants.FIRST_NAME,
+            TestConstants.LAST_NAME, TestConstants.HOBBIES, TestConstants.ADDRESSES);
+
+    private static final Person TEST_PERSON_2 = new Person(TestConstants.ID_2, TestConstants.NEW_FIRST_NAME,
+            TestConstants.NEW_LAST_NAME, TestConstants.HOBBIES, TestConstants.ADDRESSES);
+
+    @Value("${cosmosdb.uri}")
+    private String documentDbUri;
+    @Value("${cosmosdb.key}")
+    private String documentDbKey;
+
+    private DocumentClient documentClient;
+    private DocumentDbTemplate dbTemplate;
+
+    private MappingDocumentDbConverter dbConverter;
+    private DocumentDbMappingContext mappingContext;
+    private ObjectMapper objectMapper;
+    private DocumentCollection collectionPerson;
+    private DocumentDbEntityInformation<Person, String> personInfo;
+    private String collectionName;
+
+    @Autowired
+    private ApplicationContext applicationContext;
+
+    @Before
+    public void setup() throws ClassNotFoundException {
+        mappingContext = new DocumentDbMappingContext();
+        objectMapper = new ObjectMapper();
+        personInfo = new DocumentDbEntityInformation<>(Person.class);
+        collectionName = personInfo.getCollectionName();
+
+        mappingContext.setInitialEntitySet(new EntityScanner(this.applicationContext).scan(Persistent.class));
+
+        dbConverter = new MappingDocumentDbConverter(mappingContext, objectMapper);
+        documentClient = new DocumentClient(documentDbUri, documentDbKey,
+                ConnectionPolicy.GetDefault(), ConsistencyLevel.Session);
+
+        dbTemplate = new DocumentDbTemplate(documentClient, dbConverter, TestConstants.DB_NAME);
+
+        collectionPerson = dbTemplate.createCollectionIfNotExists(this.personInfo, null);
+        dbTemplate.insert(Person.class.getSimpleName(), TEST_PERSON, null);
+    }
+
+    @After
+    public void cleanup() {
+        dbTemplate.deleteAll(Person.class.getSimpleName());
+    }
+
+    @Test(expected = DocumentDBAccessException.class)
+    public void testInsertDuplicateId() {
+        dbTemplate.insert(Person.class.getSimpleName(), TEST_PERSON, null);
+    }
+
+    @Test
+    public void testFindAll() {
+        final List<Person> result = dbTemplate.findAll(Person.class.getSimpleName(), Person.class);
+        assertThat(result.size()).isEqualTo(1);
+        assertThat(result.get(0)).isEqualTo(TEST_PERSON);
+    }
+
+    @Test
+    public void testFindById() {
+        final Person result = dbTemplate.findById(Person.class.getSimpleName(),
+                TEST_PERSON.getId(), Person.class);
+        assertTrue(result.equals(TEST_PERSON));
+
+        final Person nullResult = dbTemplate.findById(Person.class.getSimpleName(),
+                TestConstants.NOT_EXIST_ID, Person.class);
+        assertThat(nullResult).isNull();
+    }
+
+    @Test
+    public void testUpsertNewDocument() {
+        // Delete first as was inserted in setup
+        dbTemplate.deleteById(Person.class.getSimpleName(), TEST_PERSON.getId(), null);
+
+        final String firstName = TestConstants.NEW_FIRST_NAME + "_" + UUID.randomUUID().toString();
+        final Person newPerson = new Person(null, firstName, TestConstants.NEW_FIRST_NAME, null, null);
+
+        dbTemplate.upsert(Person.class.getSimpleName(), newPerson, null);
+
+        final List<Person> result = dbTemplate.findAll(Person.class);
+
+        assertThat(result.size()).isEqualTo(1);
+        assertTrue(result.get(0).getFirstName().equals(firstName));
+    }
+
+    @Test
+    public void testUpdate() {
+        final Person updated = new Person(TEST_PERSON.getId(), TestConstants.UPDATED_FIRST_NAME,
+                TEST_PERSON.getLastName(), TEST_PERSON.getHobbies(), TEST_PERSON.getShippingAddresses());
+        dbTemplate.upsert(Person.class.getSimpleName(), updated, null);
+
+        final Person result = dbTemplate.findById(Person.class.getSimpleName(),
+                updated.getId(), Person.class);
+
+        assertTrue(result.equals(updated));
+    }
+
+    @Test
+    public void testDeleteById() {
+        dbTemplate.insert(TEST_PERSON_2, null);
+        assertThat(dbTemplate.findAll(Person.class).size()).isEqualTo(2);
+
+        dbTemplate.deleteById(Person.class.getSimpleName(), TEST_PERSON.getId(), null);
+
+        final List<Person> result = dbTemplate.findAll(Person.class);
+        assertThat(result.size()).isEqualTo(1);
+        assertTrue(result.get(0).equals(TEST_PERSON_2));
+    }
+
+    @Test
+    public void testDocumentDBAnnotation() {
+        final IndexingPolicy policy = collectionPerson.getIndexingPolicy();
+
+        Assert.isTrue(policy.getAutomatic() == TestConstants.DEFAULT_INDEXINGPOLICY_AUTOMATIC,
+                "class Person collection policy should be default automatic");
+        Assert.isTrue(policy.getIndexingMode() == TestConstants.DEFAULT_INDEXINGPOLICY_MODE,
+                "class Person collection policy should be default indexing mode");
+
+        TestUtils.testIndexingPolicyPathsEquals(policy.getIncludedPaths(), TestConstants.DEFAULT_INCLUDEDPATHS);
+        TestUtils.testIndexingPolicyPathsEquals(policy.getExcludedPaths(), TestConstants.DEFAULT_EXCLUDEDPATHS);
+    }
+
+    @Test
+    public void testCountByCollection() {
+        final long prevCount = dbTemplate.count(collectionName);
+        assertThat(prevCount).isEqualTo(1);
+
+        dbTemplate.insert(TEST_PERSON_2, null);
+
+        final long newCount = dbTemplate.count(collectionName);
+        assertThat(newCount).isEqualTo(2);
+    }
+
+    @Test
+    public void testCountByQuery() {
+        dbTemplate.insert(TEST_PERSON_2, null);
+
+        final Criteria criteria = Criteria.getUnaryInstance(CriteriaType.IS_EQUAL, "firstName",
+                Arrays.asList(TEST_PERSON_2.getFirstName()));
+        final DocumentQuery query = new DocumentQuery(criteria);
+
+        final long count = dbTemplate.count(query, Person.class, collectionName);
+        assertThat(count).isEqualTo(1);
+    }
+
+    @Test
+    public void testFindAllPageableMultiPages() {
+        dbTemplate.insert(TEST_PERSON_2, null);
+
+        final DocumentDbPageRequest pageRequest = new DocumentDbPageRequest(0, PAGE_SIZE_1, null);
+        final Page<Person> page1 = dbTemplate.findAll(pageRequest, Person.class, collectionName);
+
+        assertThat(page1.getContent().size()).isEqualTo(PAGE_SIZE_1);
+        validateNonLastPage(page1, PAGE_SIZE_1);
+
+        final Page<Person> page2 = dbTemplate.findAll(page1.getPageable(), Person.class, collectionName);
+        assertThat(page2.getContent().size()).isEqualTo(1);
+        validateLastPage(page2, PAGE_SIZE_1);
+    }
+
+    @Test
+    public void testPaginationQuery() {
+        dbTemplate.insert(TEST_PERSON_2, null);
+
+        final Criteria criteria = Criteria.getUnaryInstance(CriteriaType.IS_EQUAL, "firstName",
+                Arrays.asList(TestConstants.FIRST_NAME));
+        final PageRequest pageRequest = new DocumentDbPageRequest(0, PAGE_SIZE_2, null);
+        final DocumentQuery query = new DocumentQuery(criteria).with(pageRequest);
+
+        final Page<Person> page = dbTemplate.paginationQuery(query, Person.class, collectionName);
+        assertThat(page.getContent().size()).isEqualTo(1);
+        validateLastPage(page, PAGE_SIZE_2);
+    }
+}
