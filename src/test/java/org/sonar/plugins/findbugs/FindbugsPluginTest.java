95e384fcc731b720e77282328c51dbef933231ed
Luc Esape <luc.esape@gmail.com>
2018-08-22 00:53:54 +0200

    Bug commit from the following repository spotbugs/sonar-findbugs This bug commit is a reflect of source code from: http://github.com/spotbugs/sonar-findbugs/commit/bb91280f64c01b254875433dd7bb6a9d24081e7f.

---
+++ src/test/java/org/sonar/plugins/findbugs/FindbugsPluginTest.java
@@ -0,0 +1,44 @@
+/*
+ * SonarQube Findbugs Plugin
+ * Copyright (C) 2012 SonarSource
+ * sonarqube@googlegroups.com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02
+ */
+package org.sonar.plugins.findbugs;
+
+import org.junit.Test;
+import org.sonar.api.Plugin;
+import org.sonar.api.SonarRuntime;
+import org.sonar.api.utils.Version;
+
+import static junit.framework.TestCase.assertEquals;
+import static org.mockito.Mockito.mock;
+
+public class FindbugsPluginTest {
+
+  @Test
+  public void testGetExtensions() {
+
+    //Plugin.Context ctx = new FindbugsPlugin.Context(Version.parse("1.33.7"));
+    Plugin.Context ctx = new Plugin.Context(mock(SonarRuntime.class));
+
+    FindbugsPlugin plugin = new FindbugsPlugin();
+    plugin.define(ctx);
+
+    assertEquals("extension count", 23, ctx.getExtensions().size());
+  }
+
+}
