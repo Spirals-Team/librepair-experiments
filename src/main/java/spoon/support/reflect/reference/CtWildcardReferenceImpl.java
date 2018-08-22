8c2ddd9ed0f7578d370dbc54c97bccda85a3fe51
Luc Esape <luc.esape@gmail.com>
2018-08-22 19:56:54 +0200

    Bug commit from the following repository INRIA/spoon This bug commit is a reflect of source code from: http://github.com/INRIA/spoon/commit/891ec3f4104dde60043215bd53916af280f9c5d4.

---
+++ src/main/java/spoon/support/reflect/reference/CtWildcardReferenceImpl.java
@@ -0,0 +1,124 @@
+/**
+ * Copyright (C) 2006-2018 INRIA and contributors
+ * Spoon - http://spoon.gforge.inria.fr/
+ *
+ * This software is governed by the CeCILL-C License under French law and
+ * abiding by the rules of distribution of free software. You can use, modify
+ * and/or redistribute the software under the terms of the CeCILL-C license as
+ * circulated by CEA, CNRS and INRIA at http://www.cecill.info.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the CeCILL-C License for more details.
+ *
+ * The fact that you are presently reading this means that you have had
+ * knowledge of the CeCILL-C license and that you accept its terms.
+ */
+package spoon.support.reflect.reference;
+
+import static spoon.reflect.path.CtRole.BOUNDING_TYPE;
+import static spoon.reflect.path.CtRole.IS_UPPER;
+
+import java.util.List;
+
+import spoon.SpoonException;
+import spoon.reflect.annotations.MetamodelPropertyField;
+import spoon.reflect.declaration.CtType;
+import spoon.reflect.reference.CtIntersectionTypeReference;
+import spoon.reflect.reference.CtReference;
+import spoon.reflect.reference.CtTypeParameterReference;
+import spoon.reflect.reference.CtTypeReference;
+import spoon.reflect.reference.CtWildcardReference;
+import spoon.reflect.visitor.CtVisitor;
+import spoon.support.UnsettableProperty;
+
+public class CtWildcardReferenceImpl extends CtTypeParameterReferenceImpl implements CtWildcardReference {
+
+	@MetamodelPropertyField(role = BOUNDING_TYPE)
+	private CtTypeReference<?> superType;
+
+	@MetamodelPropertyField(role = IS_UPPER)
+	boolean upper = true;
+
+	@Override
+	public void accept(CtVisitor visitor) {
+		visitor.visitCtWildcardReference(this);
+	}
+
+	public CtWildcardReferenceImpl() {
+		simplename = "?";
+		setBoundingType(null);
+	}
+
+	@Override
+	public boolean isUpper() {
+		return upper;
+	}
+
+	@Override
+	public <T extends CtTypeParameterReference> T setUpper(boolean upper) {
+		getFactory().getEnvironment().getModelChangeListener().onObjectUpdate(this, IS_UPPER, upper, this.upper);
+		this.upper = upper;
+		return (T) this;
+	}
+
+	@Override
+	public <T extends CtTypeParameterReference> T setBoundingType(CtTypeReference<?> superType) {
+		if (superType != null) {
+			superType.setParent(this);
+		}
+
+		// ugly but else make testSetterInNodes failed
+		if (superType == null) { // if null, set bounding type to object
+			superType = getFactory().Type().objectType();
+			superType.setImplicit(true);
+			superType.setParent(this);
+		}
+
+		getFactory().getEnvironment().getModelChangeListener().onObjectUpdate(this, BOUNDING_TYPE, superType, this.superType);
+		this.superType = superType;
+		return (T) this;
+	}
+
+	@Override
+	public <T extends CtTypeParameterReference> T setBounds(List<CtTypeReference<?>> bounds) {
+		if (bounds == null || bounds.isEmpty()) {
+			setBoundingType(null);
+			return (T) this;
+		}
+		if (getBoundingType() instanceof CtIntersectionTypeReference<?>) {
+			throw new SpoonException("Cannot be CtIntersectionTypeReference");
+		} else if (bounds.size() > 1) {
+			throw new SpoonException("Type parameter reference accepts only one bound");
+		} else {
+			setBoundingType(bounds.get(0));
+		}
+		return (T) this;
+	}
+
+	@Override
+	public CtTypeReference<?> getBoundingType() {
+		return superType;
+	}
+
+	@Override
+	@UnsettableProperty
+	public <T extends CtReference> T setSimpleName(String simplename) {
+		return (T) this;
+	}
+
+	@Override
+	public CtWildcardReference clone() {
+		return (CtWildcardReference) super.clone();
+	}
+
+	@Override
+	public CtType<Object> getTypeDeclaration() {
+		return getFactory().Type().get(Object.class);
+	}
+
+	@Override
+	protected boolean isWildcard() {
+		return true;
+	}
+}
