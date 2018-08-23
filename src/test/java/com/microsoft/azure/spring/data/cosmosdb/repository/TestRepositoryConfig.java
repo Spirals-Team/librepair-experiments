615e65be64c49287373376c79b8314877826a072
Luc Esape <luc.esape@gmail.com>
2018-08-23 05:10:18 +0200

    Bug commit from the following repository Microsoft/spring-data-cosmosdb This bug commit is a reflect of source code from: http://github.com/Microsoft/spring-data-cosmosdb/commit/197366e5f27b5ea40cf8d8273c6623e66f0ca1c3.

---
+++ src/test/java/com/microsoft/azure/spring/data/cosmosdb/repository/TestRepositoryConfig.java
@@ -0,0 +1,32 @@
+/**
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See LICENSE in the project root for
+ * license information.
+ */
+package com.microsoft.azure.spring.data.cosmosdb.repository;
+
+import com.microsoft.azure.spring.data.cosmosdb.config.AbstractDocumentDbConfiguration;
+import com.microsoft.azure.spring.data.cosmosdb.config.DocumentDBConfig;
+import com.microsoft.azure.spring.data.cosmosdb.repository.config.EnableDocumentDbRepositories;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@PropertySource(value = {"classpath:application.properties"})
+@EnableDocumentDbRepositories
+public class TestRepositoryConfig extends AbstractDocumentDbConfiguration {
+    @Value("${cosmosdb.uri}")
+    private String documentDbUri;
+
+    @Value("${cosmosdb.key}")
+    private String documentDbKey;
+
+    @Value("${cosmosdb.database}")
+    private String database;
+
+    @Override
+    public DocumentDBConfig getConfig() {
+        return DocumentDBConfig.builder(documentDbUri, documentDbKey, database).build();
+    }
+}
