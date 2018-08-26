cea23eb594720658246a12762684c3cd7b3fd8aa
Luc Esape <luc.esape@gmail.com>
2018-08-26 20:39:02 +0200

    Bug commit from the following repository bobjacobsen/JMRI This bug commit is a reflect of source code from: http://github.com/bobjacobsen/JMRI/commit/6d87aec562ee3ef4db61347ba7e1ccd8333b9318.

---
+++ java/test/jmri/jmrix/loconet/LocoNetThrottledTransmitterTest.java
@@ -0,0 +1,157 @@
+package jmri.jmrix.loconet;
+
+import java.util.concurrent.TimeUnit;
+import jmri.util.JUnitUtil;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+
+/**
+ * Tests for the jmri.jmrix.loconet.LocoNetThrottledTransmitter class.
+ *
+ * @author Bob Jacobsen Copyright 2001, 2002, 2009, 2015
+ */
+public class LocoNetThrottledTransmitterTest {
+
+    @Test
+    public void testCtorAndDispose() {
+        LocoNetThrottledTransmitter q = new LocoNetThrottledTransmitter(null, false);
+        q.dispose();
+        JUnitUtil.waitFor(()->{return !q.running;}, "stopped");
+    }
+
+    @Test
+    public void testMemoCtor() {
+        LocoNetThrottledTransmitter q = new LocoNetThrottledTransmitter(null, false);
+        new LocoNetThrottledTransmitter.Memo(null, 100, TimeUnit.MILLISECONDS);
+
+        q.dispose();
+        JUnitUtil.waitFor(()->{return !q.running;}, "stopped");
+    }
+
+    @Test
+    public void testMemoComparable() {
+        LocoNetThrottledTransmitter.Memo m50   = new LocoNetThrottledTransmitter.Memo(null, 50, TimeUnit.MILLISECONDS);
+        LocoNetThrottledTransmitter.Memo m100a = new LocoNetThrottledTransmitter.Memo(null, 100, TimeUnit.MILLISECONDS);
+        LocoNetThrottledTransmitter.Memo m100b = new LocoNetThrottledTransmitter.Memo(null, 100, TimeUnit.MILLISECONDS);
+        LocoNetThrottledTransmitter.Memo m200a = new LocoNetThrottledTransmitter.Memo(null, 200, TimeUnit.MILLISECONDS);
+        LocoNetThrottledTransmitter.Memo m200b = new LocoNetThrottledTransmitter.Memo(null, 200, TimeUnit.MILLISECONDS);
+
+        Assert.assertNotNull("exists", m100b);
+        Assert.assertNotNull("exists", m200b);
+        Assert.assertEquals("same object", 0, m100a.compareTo(m100a));
+        Assert.assertEquals("same object", 0, m100a.compareTo(m100a));
+
+        Assert.assertEquals("less than 1", -1, m100a.compareTo(m200a));
+        Assert.assertEquals("less than 2", -1, m50.compareTo(m100a));
+        Assert.assertEquals("less than 3", -1, m50.compareTo(m200a));
+
+        Assert.assertEquals("greater than 1", 1, m200a.compareTo(m100a));
+        Assert.assertEquals("greater than 2", 1, m100a.compareTo(m50));
+        Assert.assertEquals("greater than 3", 1, m200a.compareTo(m50));
+    }
+
+    @Test
+    public void testThreadStartStop() {
+        LocoNetThrottledTransmitter q = new LocoNetThrottledTransmitter(null, false);
+        JUnitUtil.waitFor(()->{return q.running;}, "started");
+
+        Assert.assertTrue("started", q.running);
+
+        q.dispose();
+        JUnitUtil.waitFor(()->{return !q.running;}, "stopped");
+    }
+
+    @Test
+    public void testSendOneImmediate() {
+        LocoNetInterfaceScaffold s = new LocoNetInterfaceScaffold();
+        LocoNetThrottledTransmitter q = new LocoNetThrottledTransmitter(s, false);
+
+        LocoNetMessage m1;
+
+        m1 = new LocoNetMessage(1);
+        m1.setElement(0, 0x01);  // dummy value
+
+        q.minInterval = 0;
+        q.sendLocoNetMessage(m1);
+
+        JUnitUtil.waitFor(()->{return s.outbound.size() == 1;}, "one sent");
+
+        Assert.assertEquals("one sent", 1, s.outbound.size());
+        Assert.assertEquals("right one", m1, s.outbound.elementAt(s.outbound.size() - 1));
+
+        q.dispose();
+        JUnitUtil.waitFor(()->{return !q.running;}, "stopped");
+    }
+
+    @Test
+    public void testSendOneNowOneLater() {
+        LocoNetInterfaceScaffold s = new LocoNetInterfaceScaffold();
+        LocoNetThrottledTransmitter q = new LocoNetThrottledTransmitter(s, false);
+
+        LocoNetMessage m1 = new LocoNetMessage(1);
+        m1.setElement(0, 0x01);  // dummy value
+        LocoNetMessage m2 = new LocoNetMessage(1);
+        m2.setElement(0, 0x02);  // dummy value
+
+        q.minInterval = 1;
+        q.sendLocoNetMessage(m1);
+        q.minInterval = 100;
+        q.sendLocoNetMessage(m2);
+
+        JUnitUtil.waitFor(()->{return s.outbound.size() == 1;}, "only one sent failed with s.outbound.size() "+s.outbound.size());
+
+        Assert.assertEquals("only one sent", 1, s.outbound.size());
+        Assert.assertEquals("right one", m1, s.outbound.elementAt(0));
+
+        JUnitUtil.waitFor(()->{return s.outbound.size() == 2;}, "only two sent failed with s.outbound.size() "+s.outbound.size());
+
+        Assert.assertEquals("two sent", 2, s.outbound.size());
+        Assert.assertEquals("right 2nd", m2, s.outbound.elementAt(1));
+
+        q.dispose();
+        JUnitUtil.waitFor(()->{return !q.running;}, "stopped");
+    }
+
+    @Test
+    public void testAfterTimeNewMessageSentImmediately() {
+        LocoNetInterfaceScaffold s = new LocoNetInterfaceScaffold();
+        LocoNetThrottledTransmitter q = new LocoNetThrottledTransmitter(s, false);
+
+        LocoNetMessage m1 = new LocoNetMessage(1);
+        m1.setElement(0, 0x01);  // dummy value
+        LocoNetMessage m2 = new LocoNetMessage(1);
+        m2.setElement(0, 0x02);  // dummy value
+
+        q.minInterval = 100;
+        q.sendLocoNetMessage(m1);
+
+        JUnitUtil.waitFor(()->{return s.outbound.size() == 1;}, "only one sent");
+
+        Assert.assertEquals("only one sent", 1, s.outbound.size());
+        Assert.assertEquals("right one", m1, s.outbound.elementAt(0));
+
+        q.sendLocoNetMessage(m2);
+        JUnitUtil.waitFor(()->{return s.outbound.size() == 2;}, "two sent");
+
+        Assert.assertEquals("two sent", 2, s.outbound.size());
+        Assert.assertEquals("right 2nd", m2, s.outbound.elementAt(1));
+
+        q.dispose();
+        JUnitUtil.waitFor(()->{return !q.running;}, "stopped");
+    }
+
+    // The minimal setup for log4J
+    @Before
+    public void setUp() {
+        JUnitUtil.setUp();
+    }
+
+    @After
+    public void tearDown() {
+        JUnitUtil.tearDown();
+    }
+
+}
---
+++ java/test/jmri/jmrix/qsi/qsimon/QsiMonFrameTest.java
@@ -0,0 +1,129 @@
+package jmri.jmrix.qsi.qsimon;
+
+import java.awt.GraphicsEnvironment;
+import java.util.ArrayList;
+import jmri.jmrix.qsi.QsiListener;
+import jmri.jmrix.qsi.QsiMessage;
+import jmri.jmrix.qsi.QsiReply;
+import jmri.jmrix.qsi.QsiTrafficController;
+
+import org.junit.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * JUnit tests for the QsiProgrammer class
+ *
+ * @author	Bob Jacobsen
+ */
+public class QsiMonFrameTest {
+
+    @Test
+    public void testCreate() {
+        Assume.assumeFalse(GraphicsEnvironment.isHeadless());
+        QsiMonFrame f = new QsiMonFrame(new jmri.jmrix.qsi.QsiSystemConnectionMemo());
+        Assert.assertNotNull("exists", f);
+    }
+
+    // Following is not reliable, apparently time-sensitive, so commented out
+    @Ignore
+    @Test
+    public void testMsg() {
+        QsiMessage m = new QsiMessage(3);
+        m.setOpCode('L');
+        m.setElement(1, '0');
+        m.setElement(2, 'A');
+
+        QsiMonFrame f = new QsiMonFrame(new jmri.jmrix.qsi.QsiSystemConnectionMemo());
+
+        f.message(m);
+
+        Assert.assertEquals("length ", "cmd: \"L0A\"\n".length(), f.getFrameText().length());
+        Assert.assertEquals("display", "cmd: \"L0A\"\n", f.getFrameText());
+    }
+
+    // Following is not reliable, apparently time-sensitive, so commented out
+    @Ignore
+    @Test
+    public void testReply() {
+        QsiReply m = new QsiReply();
+        m.setOpCode('C');
+        m.setElement(1, 'o');
+        m.setElement(2, ':');
+
+        QsiMonFrame f = new QsiMonFrame(new jmri.jmrix.qsi.QsiSystemConnectionMemo());
+
+        f.reply(m);
+
+        Assert.assertEquals("display", "rep: \"Co:\"\n", f.getFrameText());
+        Assert.assertEquals("length ", "rep: \"Co:\"\n".length(), f.getFrameText().length());
+    }
+
+    @Test
+    public void testWrite() {
+        new QsiInterfaceScaffold();
+    }
+
+    // service internal class to handle transmit/receive for tests
+    class QsiInterfaceScaffold extends QsiTrafficController {
+
+        public QsiInterfaceScaffold() {
+        }
+
+        // override some QsiInterfaceController methods for test purposes
+        @Override
+        public boolean status() {
+            return true;
+        }
+
+        /**
+         * record messages sent, provide access for making sure they are OK
+         */
+        public ArrayList<QsiMessage> outbound = new ArrayList<>();  // public OK here, so long as this is a test class
+
+        @Override
+        public void sendQsiMessage(QsiMessage m, QsiListener l) {
+            if (log.isDebugEnabled()) {
+                log.debug("sendQsiMessage [" + m + "]");
+            }
+            // save a copy
+            outbound.add(m);
+        }
+
+        // test control member functions
+        /**
+         * forward a message to the listeners, e.g. test receipt
+         */
+        protected void sendTestMessage(QsiMessage m) {
+            // forward a test message to Listeners
+            if (log.isDebugEnabled()) {
+                log.debug("sendTestMessage    [" + m + "]");
+            }
+            notifyMessage(m, null);
+        }
+
+        protected void sendTestReply(QsiReply m) {
+            // forward a test message to Listeners
+            if (log.isDebugEnabled()) {
+                log.debug("sendTestReply    [" + m + "]");
+            }
+            notifyReply(m);
+        }
+
+        /*
+         * Check number of listeners, used for testing dispose()
+         */
+        public int numListeners() {
+            return cmdListeners.size();
+        }
+
+    }
+
+    @Before
+    public void setUp() {
+        jmri.util.JUnitUtil.setUp();
+    }
+
+    private final static Logger log = LoggerFactory.getLogger(QsiMonFrameTest.class);
+
+}
