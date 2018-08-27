fad12612bc7ceb19babc9c66aad1639e665e6d3c
Luc Esape <luc.esape@gmail.com>
2018-08-28 00:44:41 +0200

    Bug commit from the following repository powerflows/powerflows-dmn This bug commit is a reflect of source code from: http://github.com/powerflows/powerflows-dmn/commit/244fd3b0bf88823cf1cafa1fb1e67958eb45e4cf.

---
+++ src/main/java/org/powerflows/dmn/engine/evaluator/entry/expression/provider/ExpressionEvaluable.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2018-present PowerFlows.org - all rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.powerflows.dmn.engine.evaluator.entry.expression.provider;
+
+import org.powerflows.dmn.engine.model.decision.field.Input;
+import org.powerflows.dmn.engine.model.decision.field.Output;
+import org.powerflows.dmn.engine.model.decision.rule.entry.InputEntry;
+import org.powerflows.dmn.engine.model.decision.rule.entry.OutputEntry;
+import org.powerflows.dmn.engine.model.evaluation.context.ContextVariables;
+import org.powerflows.dmn.engine.model.evaluation.result.EntryResult;
+
+public interface ExpressionEvaluable {
+
+    boolean evaluateInput(InputEntry inputEntry, Input input, ContextVariables contextVariables);
+
+    EntryResult evaluateOutput(OutputEntry outputEntry, Output output, ContextVariables contextVariables);
+}
---
+++ src/main/java/org/powerflows/dmn/engine/model/decision/field/Output.java
@@ -0,0 +1,123 @@
+/*
+ * Copyright (c) 2018-present PowerFlows.org - all rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.powerflows.dmn.engine.model.decision.field;
+
+import org.powerflows.dmn.engine.model.builder.AbstractBuilder;
+import org.powerflows.dmn.engine.model.decision.expression.Expression;
+
+import java.io.Serializable;
+import java.util.function.Consumer;
+import java.util.function.Function;
+
+public class Output implements Serializable {
+
+    private static final long serialVersionUID = 1;
+
+    private String name;
+    private String description;
+    private ValueType type;
+    private Expression expression;
+
+    private Output() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public ValueType getType() {
+        return type;
+    }
+
+    public Expression getExpression() {
+        return expression;
+    }
+
+    public static <P extends AbstractBuilder> FluentBuilder<P> fluentBuilder(P parentBuilder, Consumer<Output> outputConsumer) {
+        return new FluentBuilder<>(parentBuilder, outputConsumer);
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    private static abstract class OutputBuilder<B extends Output.OutputBuilder<B>> extends AbstractBuilder<Output> {
+        @Override
+        protected void initProduct() {
+            this.product = new Output();
+        }
+
+        public B name(String name) {
+            this.product.name = name;
+
+            return (B) this;
+        }
+
+        public B description(String description) {
+            this.product.description = description;
+
+            return (B) this;
+        }
+
+        public B type(ValueType type) {
+            this.product.type = type;
+
+            return (B) this;
+        }
+    }
+
+    public static final class Builder extends OutputBuilder<Builder> {
+
+        public Builder withExpression(final Function<Expression.Builder, Expression> expressionBuilderConsumer) {
+            this.product.expression = expressionBuilderConsumer.apply(Expression.builder());
+
+            return this;
+        }
+    }
+
+    public static final class FluentBuilder<P extends AbstractBuilder> extends Output.OutputBuilder<Output.FluentBuilder<P>> {
+        private final P parentBuilder;
+        private final Consumer<Output> callback;
+
+        private FluentBuilder(final P parentBuilder, final Consumer<Output> callback) {
+            this.parentBuilder = parentBuilder;
+            this.callback = callback;
+        }
+
+        public Expression.FluentBuilder<FluentBuilder<P>> withExpression() {
+            final Consumer<Expression> expressionConsumer = expression -> this.product.expression = expression;
+
+            return Expression.fluentBuilder(this, expressionConsumer);
+        }
+
+        public FluentBuilder<P> next() {
+            callback.accept(build());
+
+            return fluentBuilder(parentBuilder, callback);
+        }
+
+        public P end() {
+            callback.accept(build());
+
+            return parentBuilder;
+        }
+    }
+}
