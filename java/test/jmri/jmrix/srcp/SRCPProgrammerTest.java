d64f4af8fe5e3e89fad837f9bf65de42db9f3ce5
Luc Esape <luc.esape@gmail.com>
2018-08-28 06:09:42 +0200

    Bug commit from the following repository JMRI/JMRI This bug commit is a reflect of source code from: http://github.com/JMRI/JMRI/commit/90690d95dd00ca59fe7f6d73b9daf99d59a34e24.

---
+++ java/test/jmri/jmrix/srcp/SRCPProgrammerTest.java
@@ -0,0 +1,49 @@
+package jmri.jmrix.srcp;
+
+import jmri.util.JUnitUtil;
+import jmri.ProgrammingMode;
+import org.junit.*;
+
+/**
+ * SRCPProgrammerTest.java
+ *
+ * Description:	tests for the jmri.jmrix.srcp.SRCPProgrammer class
+ *
+ * @author	Bob Jacobsen
+ */
+public class SRCPProgrammerTest extends jmri.jmrix.AbstractProgrammerTest {
+
+    @Test
+    @Override
+    public void testDefault() {
+        Assert.assertEquals("Check Default", ProgrammingMode.DIRECTBYTEMODE,
+                programmer.getMode());        
+    }
+
+    @Override
+    @Test
+    public void testDefaultViaBestMode() {
+        Assert.assertEquals("Check Default", ProgrammingMode.DIRECTBYTEMODE,
+                ((SRCPProgrammer)programmer).getBestMode());        
+    }
+
+    // The minimal setup for log4J
+    @Override
+    @Before
+    public void setUp() {
+        JUnitUtil.setUp();
+        SRCPBusConnectionMemo sm = new SRCPBusConnectionMemo(new SRCPTrafficController() {
+            @Override
+            public void sendSRCPMessage(SRCPMessage m, SRCPListener reply) {
+            }
+        }, "A", 1);
+        programmer = new SRCPProgrammer(sm);
+    }
+
+    @Override
+    @After
+    public void tearDown() {
+        programmer = null;
+        JUnitUtil.tearDown();
+    }
+}
