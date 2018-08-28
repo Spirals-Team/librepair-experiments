b3555994887e923b1f73e4c8878da9ad8066d47c
Luc Esape <luc.esape@gmail.com>
2018-08-28 15:53:37 +0200

    Bug commit from the following repository SpoonLabs/astor This bug commit is a reflect of source code from: http://github.com/SpoonLabs/astor/commit/43eb641b07c89853b8bdf5208161cdef75ede675.

---
+++ output_astor/AstorMain-lis/src/default_f/java_programs/LIS.java
@@ -0,0 +1,26 @@
+package java_programs;
+
+
+public class LIS {
+	public static int lis(int[] arr) {
+		java.util.Map<java.lang.Integer, java.lang.Integer> ends = new java.util.HashMap<java.lang.Integer, java.lang.Integer>(100);
+		int longest = 0;
+		int i = 0;
+		for (int val : arr) {
+			java.util.ArrayList<java.lang.Integer> prefix_lengths = new java.util.ArrayList<java.lang.Integer>(100);
+			for (int j = 1; j < (longest + 1); j++) {
+				if ((arr[ends.get(j)]) < val) {
+					prefix_lengths.add(j);
+				}
+			}
+			int length = (!(prefix_lengths.isEmpty())) ? java.util.Collections.max(prefix_lengths) : 0;
+			if ((length == longest) || (val < (arr[ends.get((length + 1))]))) {
+				ends.put((length + 1), i);
+				longest = length + 1;
+			}
+			i++;
+		}
+		return longest;
+	}
+}
+
