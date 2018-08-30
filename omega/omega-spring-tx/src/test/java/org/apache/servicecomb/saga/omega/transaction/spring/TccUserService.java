7838f97b003105e12887c15e2614040dd2207411
Luc Esape <luc.esape@gmail.com>
2018-08-30 19:45:13 +0200

    Bug commit from the following repository apache/incubator-servicecomb-saga This bug commit is a reflect of source code from: http://github.com/apache/incubator-servicecomb-saga/commit/0d851ddc845206f89cbe30cef7ad294fdc4bdbed.

---
+++ omega/omega-spring-tx/src/test/java/org/apache/servicecomb/saga/omega/transaction/spring/TccUserService.java
@@ -0,0 +1,60 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package org.apache.servicecomb.saga.omega.transaction.spring;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+import org.apache.servicecomb.saga.omega.transaction.annotations.Participate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+class TccUserService {
+  static final String ILLEGAL_USER = "Illegal User";
+  private final UserRepository userRepository;
+
+  private int count = 0;
+
+  @Autowired
+  TccUserService(UserRepository userRepository) {
+    this.userRepository = userRepository;
+  }
+
+  void resetCount() {
+    this.count = 0;
+  }
+
+  @Participate(confirmMethod = "confirm", cancelMethod = "cancel")
+  User add(User user) {
+    if (ILLEGAL_USER.equals(user.username())) {
+      throw new IllegalArgumentException("User is illegal");
+    }
+    return userRepository.save(user);
+  }
+
+  void confirm(User user) {
+    userRepository.findByUsername(user.username());
+  }
+
+  void cancel(User user) {
+    userRepository.delete(user);
+  }
+
+
+}
