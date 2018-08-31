45b7a54f772b56b976438d948c9cf594bedb65d2
Luc Esape <luc.esape@gmail.com>
2018-09-01 00:04:31 +0200

    Bug commit from the following repository raphw/byte-buddy This bug commit is a reflect of source code from: http://github.com/raphw/byte-buddy/commit/b6852c4d3ef09ff4c3028247303ed9e90f1340d2.

---
+++ byte-buddy-maven-plugin/src/test/java/net/bytebuddy/test/SimplePlugin.java
@@ -0,0 +1,22 @@
+package net.bytebuddy.test;
+
+import net.bytebuddy.build.Plugin;
+import net.bytebuddy.description.type.TypeDescription;
+import net.bytebuddy.dynamic.ClassFileLocator;
+import net.bytebuddy.dynamic.DynamicType;
+import net.bytebuddy.implementation.FixedValue;
+
+import static net.bytebuddy.matcher.ElementMatchers.named;
+
+public class SimplePlugin implements Plugin {
+
+    @Override
+    public boolean matches(TypeDescription target) {
+        return target.getName().equals("foo.Bar");
+    }
+
+    @Override
+    public DynamicType.Builder<?> apply(DynamicType.Builder<?> builder, TypeDescription typeDescription, ClassFileLocator classFileLocator) {
+        return builder.method(named("foo")).intercept(FixedValue.value("qux"));
+    }
+}
